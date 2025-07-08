339f64fe837f683eb8adbdc5769afba909b2d4d0
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 23:12:39 +0600

    in3050

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -160,4 +160,8 @@ public class Warmup1 {
         int correctB = Math.abs(b - 10);
         return (correctA == correctB)? 0 :  (correctA < correctB) ? a : b;
     }
+
+    public boolean in3050(int a, int b) {
+        return (a >= 30 && a <= 40 && b >= 30 && b <=40 || a >= 40 && a <= 50 && b >= 40 && b <=50);
+    }
 }
