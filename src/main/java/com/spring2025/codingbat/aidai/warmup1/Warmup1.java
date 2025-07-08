dfe56253a966b841b587659e19f05c73bc853303
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 00:40:33 +0600

    aidai/codingbat/warmup1-nearHundred

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -63,5 +63,16 @@ public boolean parrotTrouble(boolean talking, int hour) {
 public boolean makes10(int a, int b) {
     return (a == 10 || b == 10 || a+b ==10);
 }
+/**
+ Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+
+ nearHundred(93) → true
+ nearHundred(90) → true
+ nearHundred(89) → false
+ */
+public boolean nearHundred(int n) {
+    return (n >= 90 && n <= 110) || (n >= 190 && n <= 210);
+}
 }
 
