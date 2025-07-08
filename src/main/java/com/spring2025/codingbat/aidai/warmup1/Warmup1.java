8958aa168affe1c9a6179f4728fd47ccff72b634
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 00:17:46 +0600

    aidai/codingbat/warmup1-monkeyTrouble

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -5,12 +5,20 @@ public class Warmup1 {
      * @author Aidai
      */
 
-        /**
-         The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-         */
-        public boolean sleepIn(boolean weekday, boolean vacation) {
+    /**
+     * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+     */
+    public boolean sleepIn(boolean weekday, boolean vacation) {
 
-            return !weekday||vacation;
-        }
+        return !weekday || vacation;
     }
+
+    /**
+     * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+     */
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return aSmile == bSmile;
+
+    }
+}
 
