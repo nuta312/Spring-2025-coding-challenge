b96e466a5444f54f28d7e1e1dd4db21a0813d352
Talgat <talaronin26@gmail.com>
2025-07-09 00:49:15 +0600

    Talgat/warmup2/1.stringTimes

---
+++ src/main/java/com/spring2025/codingbat/talgat/warmup2/Warmap.java
@@ -0,0 +1,16 @@
+package com.spring2025.codingbat.talgat.warmup2;
+
+public class Warmap {
+    public static void main(String[] args) {
+
+    }
+    // 1
+    public String stringTimes(String str, int n) {
+        String strN = "";
+        for(int i = 1;i<=n;i++){
+            strN = strN+str;
+        }
+        return strN;
+    }
+
+}
