52908de6185bba32ccabfcdcd805749a32426607
Nursultan <nursultan.musakunov@geniusto.com>
2025-07-08 11:24:48 +0600

    nuta/codingbat/string1-helloName

---
+++ src/main/java/com/spring2025/codingbat/nuta/string1/String1.java
@@ -0,0 +1,19 @@
+package com.spring2025.codingbat.nuta.string1;
+
+/**
+ * @author Nuta
+ */
+public class String1 {
+
+    /**
+     Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+     helloName("Bob") → "Hello Bob!"
+     helloName("Alice") → "Hello Alice!"
+     helloName("X") → "Hello X!"
+     */
+
+    public static String helloName(String name) {
+        return "Hello " + name+"!";
+    }
+}
