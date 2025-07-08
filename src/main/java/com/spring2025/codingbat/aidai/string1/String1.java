5aa61a512909dae2e94f48f11814d07a35c5b693
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:13:36 +0600

    aidai/codingbat/string1-nonStart

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -113,4 +113,15 @@ public String comboString(String a, String b) {
         return b + a + b;
     }
 }
+/**
+ Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+
+ nonStart("Hello", "There") → "ellohere"
+ nonStart("java", "code") → "avaode"
+ nonStart("shotl", "java") → "hotlava"
+ */
+public String nonStart(String a, String b) {
+    return a.substring(1) + b.substring(1);
+}
 }
