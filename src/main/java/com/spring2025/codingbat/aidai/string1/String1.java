e5c0178b84c8ea1a9de772cde5dd82aadf5b8617
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:27:13 +0600

    aidai/codingbat/string1-lastTwo

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -303,4 +303,21 @@ public String conCat(String a, String b) {
     }
     return a + b;
 }
+/**
+ Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+
+
+ lastTwo("coding") → "codign"
+ lastTwo("cat") → "cta"
+ lastTwo("ab") → "ba"
+ */
+public String lastTwo(String str) {
+    if (str.length() < 2) {
+        return str;
+    }
+    String start = str.substring(0, str.length() - 2);
+    char secondLast = str.charAt(str.length() - 2);
+    char last = str.charAt(str.length() - 1);
+    return start + last + secondLast;
+}
 }
