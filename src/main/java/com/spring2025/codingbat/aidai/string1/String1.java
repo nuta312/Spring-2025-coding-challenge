8b87ddfc89c80d7557ee1c780b0ce4d0320c0c79
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:10:22 +0600

    aidai/codingbat/string1-firstHalf

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -76,4 +76,15 @@ public String firstTwo(String str) {
     }
     return str.substring(0,2);
 }
+/**
+ Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+
+ firstHalf("WooHoo") → "Woo"
+ firstHalf("HelloThere") → "Hello"
+ firstHalf("abcdef") → "abc"
+ */
+public String firstHalf(String str) {
+    return str.substring(0, str.length() / 2);
+}
 }
