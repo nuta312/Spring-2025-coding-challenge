e609cb931d7a4c61dbd3d47b1e2a6012ce2a3944
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 01:42:10 +0600

    aidai/codingbat/warmup1-intMax

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -294,5 +294,23 @@ public class Warmup1 {
         }
         return result;
     }
+    /**
+     Given three int values, a b c, return the largest.
+
+
+     intMax(1, 2, 3) → 3
+     intMax(1, 3, 2) → 3
+     intMax(3, 2, 1) → 3
+     */
+    public int intMax(int a, int b, int c) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
 }
 
