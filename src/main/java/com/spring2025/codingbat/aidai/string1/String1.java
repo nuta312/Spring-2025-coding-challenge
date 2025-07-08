d3dc8efc984f065fee793e46745ad869aa255870
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:17:21 +0600

    aidai/codingbat/string1-withouEnd2

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -161,4 +161,18 @@ public String theEnd(String str, boolean front) {
         return String.valueOf(str.charAt(str.length() - 1));
     }
 }
+/**
+ Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
+
+
+ withouEnd2("Hello") → "ell"
+ withouEnd2("abc") → "b"
+ withouEnd2("ab") → ""
+ */
+public String withouEnd2(String str) {
+    if (str.length() < 2) {
+        return "";
+    }
+    return str.substring(1, str.length() - 1);
+}
 }
