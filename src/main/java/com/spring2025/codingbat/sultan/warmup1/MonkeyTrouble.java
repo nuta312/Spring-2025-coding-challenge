7ae6b182d1d7ccfe4fa61cb5d4fa13d3e4c299ee
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-08 20:54:51 +0600

    CoddingBat Warmup1 ->monkeyTrouble

---
+++ src/main/java/com/spring2025/codingbat/sultan/warmup1/MonkeyTrouble.java
@@ -0,0 +1,28 @@
+package com.spring2025.codingbat.sultan.warmup1;
+
+public class MonkeyTrouble {
+    /**
+     We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+     We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+     monkeyTrouble(true, true) → true
+     monkeyTrouble(false, false) → true
+     monkeyTrouble(true, false) → false
+     */
+    public static void main(String[] args) {
+        monkeyTrouble(true, true);
+        monkeyTrouble(false, false);
+        monkeyTrouble(true, false);
+    }
+
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if(aSmile && bSmile){
+            return true;
+        } else if(!aSmile && !bSmile){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
+
