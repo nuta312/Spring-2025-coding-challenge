6a21bcaff31409e82e275419e34f1c5d8513b6a7
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 22:58:59 +0600

    front3

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -87,4 +87,8 @@ public class Warmup1 {
     public String frontBack(String str) {
         return (str.length() > 1)? str.substring(str.length() - 1) + str.substring(1, str.length() - 1 ) + str.substring(0,1) : str ;
     }
+
+    public String front3(String str) {
+        return (str.length() >= 3) ? str.substring(0,3) + str.substring(0,3) + str.substring(0,3) : str + str + str;
+    }
 }
