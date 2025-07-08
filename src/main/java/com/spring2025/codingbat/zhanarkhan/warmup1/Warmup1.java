da84030c923afadc9b2cd39aa2e25a5c92608aa3
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 23:10:31 +0600

    mixStart

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -142,4 +142,9 @@ public class Warmup1 {
             return str;
         }
     }
+
+    public boolean mixStart(String str) {
+        return (str.length() < 3) ? false : (str.substring(1,3).equals("ix"));
+    }
+
 }
