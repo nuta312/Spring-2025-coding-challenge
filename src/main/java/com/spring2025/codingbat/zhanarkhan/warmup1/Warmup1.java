30b0f3bda6b4d22858b4af54f80a7fd8dd980982
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 22:58:21 +0600

    frontBack

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -84,4 +84,7 @@ public class Warmup1 {
         return (str.length() > n && n >= 0)? sb1.deleteCharAt(n).toString() : str;
     }
 
+    public String frontBack(String str) {
+        return (str.length() > 1)? str.substring(str.length() - 1) + str.substring(1, str.length() - 1 ) + str.substring(0,1) : str ;
+    }
 }
