470914fbb01a3a7da71322546681fd625465395c
mahabat <mahabatka@gmail.com>
2025-07-08 20:27:42 +0600

    master/mahabat/codingbat/warmup1/notString/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -85,4 +85,12 @@ public class Warmup1 {
     public boolean posNeg(int a, int b, boolean negative) {
         return ((a > 0 && b < 0 && !negative) || (b > 0 && a < 0 && !negative) || (a < 0 && b < 0 && negative));
     }
+
+    //Given a string, return a new string where "not " has been added to the front.
+    // However, if the string already begins with "not", return the string unchanged.
+    // Note: use .equals() to compare 2 strings.
+
+    public String notString(String str) {
+        return (str.startsWith("not") ? str : "not ".concat(str));
+    }
 }
