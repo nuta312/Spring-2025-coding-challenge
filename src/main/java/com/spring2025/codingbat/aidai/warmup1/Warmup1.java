87b11bc928745c29fb3fdfea117ba44b5021da99
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 01:46:05 +0600

    aidai/codingbat/warmup1-max1020

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -344,5 +344,27 @@ public class Warmup1 {
         boolean bothIn40to50 = (a >= 40 && a <= 50) && (b >= 40 && b <= 50);
         return bothIn30to40 || bothIn40to50;
     }
+    /**
+     Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
+
+
+     max1020(11, 19) → 19
+     max1020(19, 11) → 19
+     max1020(11, 9) → 11
+     */
+    public int max1020(int a, int b) {
+        boolean aInRange = (a >= 10 && a <= 20);
+        boolean bInRange = (b >= 10 && b <= 20);
+
+        if (aInRange && bInRange) {
+            return Math.max(a, b);
+        } else if (aInRange) {
+            return a;
+        } else if (bInRange) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
 }
 
