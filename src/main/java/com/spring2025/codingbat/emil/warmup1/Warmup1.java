88621a649ec31ac76aa049f8aa4ebe5667642c12
Emil <emkerimov@mail.ru>
2025-07-09 04:14:01 +0600

    Warmup1

--- src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
@@ -424,4 +424,16 @@ in3050(40, 50) → true
         return count >= 1 && count <= 3;
     }
 
+    /*
+    Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+
+
+    lastDigit(7, 17) → true
+    lastDigit(6, 17) → false
+    lastDigit(3, 113) → true
+     */
+    public boolean lastDigit(int a, int b) {
+        return (a % 10 == b % 10);
+    }
+
 }
