1833dd57b9795ea34e9cb3a98eca9b28fc3ccf58
mahabat <mahabatka@gmail.com>
2025-07-08 12:40:37 +0600

    master/mahabat/codingbat/warmup1/monkeyTrouble/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -13,4 +13,16 @@ public class Warmup1 {
         }
         return false;
     }
+    /*We have two monkeys, a and b,
+    and the parameters aSmile and bSmile indicate if each is smiling.
+    We are in trouble if they are both smiling or if neither of them is smiling.
+    Return true if we are in trouble.
+ */
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if((aSmile && bSmile) || (!aSmile && !bSmile)){
+            return true;
+        }
+        return false;
+    }
+
 }
