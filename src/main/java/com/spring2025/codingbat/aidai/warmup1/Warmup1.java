e3b57aef8136dde6cefad47b55a828996287b1c9
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 01:38:56 +0600

    aidai/codingbat/warmup1-mixStart

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -263,5 +263,18 @@ public class Warmup1 {
         }
         return str;
     }
+    /**
+     Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+
+     mixStart("mix snacks") → true
+     mixStart("pix snacks") → true
+     mixStart("piz snacks") → false
+     */
+    public boolean mixStart(String str) {
+        if (str.contains("ix")){
+            return true;
+        } return false;
+    }
 }
 
