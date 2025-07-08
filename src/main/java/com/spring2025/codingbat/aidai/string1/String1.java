f3dc641babaa0e2cc3fc5b50b9fb2712ccaf3c81
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:01:37 +0600

    aidai/codingbat/string1-helloName

---
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -0,0 +1,20 @@
+package com.spring2025.codingbat.aidai.string1;
+
+public class String1 {
+/**
+ * @author Aidai
+ */
+
+/**
+ Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+ helloName("Bob") → "Hello Bob!"
+ helloName("Alice") → "Hello Alice!"
+ helloName("X") → "Hello X!"
+ */
+public String helloName(String name) {
+    String hello = "Hello " + name + "!";
+    return hello;
+}
+}
