a4ddeda3706763d5739eb39ef299f63468860fce
= <=>
2025-07-09 01:26:50 +0600

    amina/codingbat/string1-firstHalf

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -15,4 +15,20 @@ public class String1 {
     public String helloName(String name) {
         return "Hello " + name + "!";
     }
+
+    /**
+     Задача 2 - firstHalf
+
+     Given a string of even length, return the first half.
+     So the string "WooHoo" yields "Woo".
+
+     firstHalf("WooHoo") → "Woo"
+     firstHalf("HelloThere") → "Hello"
+     firstHalf("abcdef") → "abc"
+     */
+    public String firstHalf(String str) {
+        return str.substring(0, str.length() / 2);
+    }
+
+
 }
