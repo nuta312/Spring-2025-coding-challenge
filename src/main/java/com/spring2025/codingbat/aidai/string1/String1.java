d594bca83a0db40df0d7a98c9363e0e15ccc2fe1
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:22:49 +0600

    aidai/codingbat/string1-middleThree

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -225,4 +225,16 @@ public String twoChar(String str, int index) {
     }
     return str.substring(index, index + 2);
 }
+/**
+ Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+
+
+ middleThree("Candy") → "and"
+ middleThree("and") → "and"
+ middleThree("solving") → "lvi"
+ */
+public String middleThree(String str) {
+    int mid = str.length() / 2;
+    return str.substring(mid - 1, mid + 2);
+}
 }
