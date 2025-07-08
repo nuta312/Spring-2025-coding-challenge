77dd5f81b94e9b1ea483fff7495866a5441302f1
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:16:20 +0600

    aidai/codingbat/string1-theEnd

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -146,4 +146,19 @@ public String left2(String str) {
 public String right2(String str) {
     return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
 }
+/**
+ Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+
+ theEnd("Hello", true) → "H"
+ theEnd("Hello", false) → "o"
+ theEnd("oh", true) → "o"
+ */
+public String theEnd(String str, boolean front) {
+    if (front) {
+        return String.valueOf(str.charAt(0));
+    } else {
+        return String.valueOf(str.charAt(str.length() - 1));
+    }
+}
 }
