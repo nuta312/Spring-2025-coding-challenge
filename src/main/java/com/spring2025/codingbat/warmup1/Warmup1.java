c6fa5f58ed8bbc3916e081e7119ff7a49752aad9
admin <admin@MacBook-Air-admin.local>
2025-07-08 14:11:15 +0600

    diff21

--- src/main/java/com/spring2025/codingbat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/warmup1/Warmup1.java
@@ -1,5 +1,7 @@
 package com.spring2025.codingbat.warmup1;
 
+    /// Author Aibekov Daniyar
+
 public class Warmup1 {
 
     /// sleepIn(false, false) → true
@@ -30,5 +32,26 @@ public class Warmup1 {
             return (a + b);
         }
     }
+    /**
+     Given an int n, return the absolute
+     difference between n and 21, except return double the absolute difference if n is over 21.
 
-}
+     /// diff21(19) → 2
+     /// diff21(10) → 11
+     /// diff21(21) → 0
+
+     Warmup-1 > diff21
+     */
+    public int diff21 (int n){
+        if(n <= 21){
+            return 21 - n;
+        }else{
+            return (n -21) * 2;
+        }
+    }
+
+
+
+
+
+    }
