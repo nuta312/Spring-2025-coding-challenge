e355a1accfb810ad5b61b9379c6577f3e3591cc2
mahabat <mahabatka@gmail.com>
2025-07-09 05:18:31 +0600

    master/mahabat/codingbat/warmup1/endUp/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -333,6 +333,22 @@ public class Warmup1 {
         return false;
     }
 
+    // Given a string, return a new string where the last 3 chars are now in upper case.
+    // If the string has less than 3 chars, uppercase whatever is there.
+    // Note that str.toUpperCase() returns the uppercase version of a string.
+
+    public String endUp(String str) {
+
+        if (str.length() <=3 )
+            return str.toUpperCase();
+        int cut = str.length()-3;
+        String front = str.substring (0,cut);
+        String back = str.substring (cut);
+        return front + back.toUpperCase();
+    }
+
+
+
 
 
 
