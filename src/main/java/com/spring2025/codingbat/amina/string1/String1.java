33d238d1b62d879ddba16d0e533dee96b4e39cf1
= <=>
2025-07-09 01:46:57 +0600

    amina/codingbat/string1-withoutEnd

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -193,4 +193,19 @@ public class String1 {
 
     }
 
+    /**
+     Задача 14 - withoutEnd
+
+     Given a string, return a version without the first and last char, so "Hello" yields "ell".
+     The string length will be at least 2.
+
+     withoutEnd("Hello") → "ell"
+     withoutEnd("java") → "av"
+     withoutEnd("coding") → "odin"
+     */
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
+
 }
