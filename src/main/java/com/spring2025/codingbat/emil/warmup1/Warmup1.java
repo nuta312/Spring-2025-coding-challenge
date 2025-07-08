491ccc3c968f01abd2f50c41daa12b410cc14876
Emil <emkerimov@mail.ru>
2025-07-09 04:08:46 +0600

    Warmup1

--- src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
@@ -270,5 +270,18 @@ startHi("hello hi") → false
         }
 
     }
+/*
+We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+
+
+loneTeen(13, 99) → true
+loneTeen(21, 19) → true
+loneTeen(13, 13) → false
+ */
+public boolean loneTeen(int a, int b) {
+    boolean ateen = (a >= 13 && a <= 19);
+    boolean bteen = (b >= 13 && b <= 19);
+    return ((ateen && !bteen) || (!ateen && bteen));
+}
 
 }
