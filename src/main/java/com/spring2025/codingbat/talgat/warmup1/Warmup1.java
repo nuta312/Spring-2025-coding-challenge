75e180d8beb76b0780d8d2f9bb678e3bacd8a01c
Talgat <talaronin26@gmail.com>
2025-07-08 23:48:38 +0600

    Talgat/warmup1/8.posNeg

--- src/main/java/com/spring2025/codingbat/talgat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/talgat/warmup1/Warmup1.java
@@ -47,4 +47,8 @@ public class Warmup1 {
     public boolean nearHundred(int n) {
         return (Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10);
     }
+
+    public boolean posNeg(int a, int b, boolean negative) {
+        return negative ? (a < 0 && b < 0) : (a < 0 && b > 0) || (a > 0 && b < 0);
+    }
 }
