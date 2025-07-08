abe35b33eec47852a4bd56710aa4191a2bccdd2c
= <=>
2025-07-09 01:38:35 +0600

    amina/codingbat/string1-extraFront

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -133,4 +133,19 @@ public class String1 {
         return "";
     }
 
+    /**
+     Задача 10 - extraFront
+
+     Given a string, return a new string made of 3 copies of the first 2 chars of the original string.
+     The string may be any length. If there are fewer than 2 chars, use whatever is there.
+
+     extraFront("Hello") → "HeHeHe"
+     extraFront("ab") → "ababab"
+     extraFront("H") → "HHH"
+     */
+    public String extraFront(String str) {
+        String front = str.length() < 2 ? str : str.substring(0, 2);
+        return front + front + front;
+    }
+
 }
