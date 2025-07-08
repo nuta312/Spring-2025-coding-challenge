00a13cd2b20e134203d4b53de26cc10e9da57bbd
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 01:31:48 +0600

    aidai/codingbat/warmup1-front22

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -178,5 +178,18 @@ public class Warmup1 {
     public boolean or35(int n) {
         return (n % 3 == 0 || n % 5 == 0);
     }
+    /**
+     Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+
+     front22("kitten") → "kikittenki"
+     front22("Ha") → "HaHaHa"
+     front22("abc") → "ababcab"
+     */
+    public String front22(String str) {
+        String front = str.length() < 2 ? str : str.substring(0, 2);
+        return front + str + front;
+    }
+
 }
 
