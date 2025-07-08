d29b8022c33f611b2325a67185634039e2adf980
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 01:25:41 +0600

    aidai/codingbat/warmup1-frontBack

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -115,5 +115,21 @@ public String notString(String str) {
 public String missingChar(String str, int n) {
     return str.substring(0, n) + str.substring(n + 1);
 }
+/**
+ Given a string, return a new string where the first and last chars have been exchanged.
+
+
+ frontBack("code") → "eodc"
+ frontBack("a") → "a"
+ frontBack("ab") → "ba"
+ */
+public String frontBack(String str) {
+    if (str.length() <= 1) {
+        return str;
+    }
+    return str.charAt(str.length() - 1)
+            + str.substring(1, str.length() - 1)
+            + str.charAt(0);
+}
 }
 
