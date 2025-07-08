f80a33acf2729ec3bce44534ffb2efb40f6a7453
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 01:19:49 +0600

    aidai/codingbat/warmup1-posNeg

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -74,5 +74,20 @@ public boolean makes10(int a, int b) {
 public boolean nearHundred(int n) {
     return (n >= 90 && n <= 110) || (n >= 190 && n <= 210);
 }
+/**
+ Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+
+ posNeg(1, -1, false) → true
+ posNeg(-1, 1, false) → true
+ posNeg(-4, -5, true) → true
+ */
+public boolean posNeg(int a, int b, boolean negative) {
+    if (negative) {
+        return (a < 0 && b < 0);
+    } else {
+        return (a < 0 && b > 0) || (a > 0 && b < 0);
+    }
+}
 }
 
