f163cde0f2a73ec612ea384d9e94178ee31a6e4f
elviraim <elya89china@gmail.com>
2025-07-08 20:34:52 +0600

    diff21

--- src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
@@ -28,6 +28,13 @@ public class Warmup1 {
             return a + b;
         } else return (a + b) * 2;
     }
+    public static int diff21(int n) {
+        if (n <= 21) {
+            return 21 - n;
+        }
+        return (n - 21) * 2;
+    }
+
 
 
 }
