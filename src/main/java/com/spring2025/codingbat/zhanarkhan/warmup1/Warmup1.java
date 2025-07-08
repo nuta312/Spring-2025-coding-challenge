ef7380316b3b556ceb8fffa275024ddb85d56642
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 22:54:03 +0600

    makes10

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -53,4 +53,12 @@ public class Warmup1 {
             return false;
         }
     }
+
+    public boolean makes10(int a, int b) {
+        if ((a == 10 || b == 10) || (a + b == 10)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
