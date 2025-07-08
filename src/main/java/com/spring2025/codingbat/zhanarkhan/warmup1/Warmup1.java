1fa0fa48bef98ef78f6ddce10e1a246211b06f6d
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 23:09:59 +0600

    delDel

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -133,4 +133,13 @@ public class Warmup1 {
             return false;
         }
     }
+
+    public String delDel(String str) {
+
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4, str.length());
+        } else {
+            return str;
+        }
+    }
 }
