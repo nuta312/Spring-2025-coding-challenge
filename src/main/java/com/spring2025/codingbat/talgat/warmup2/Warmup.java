4c59d8b90d3d75f848eb8b20eacbbef29629fee5
Talgat <talaronin26@gmail.com>
2025-07-09 00:55:28 +0600

    Talgat/warmup2/5.stringSplosion

--- src/main/java/com/spring2025/codingbat/talgat/warmup2/Warmup.java
+++ src/main/java/com/spring2025/codingbat/talgat/warmup2/Warmup.java
@@ -51,5 +51,13 @@ public class Warmup {
         return result;
     }
 
+    public String stringSplosion(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result += str.substring(0,i+1);
+        }
+        return result;
+    }
+
 
 }
