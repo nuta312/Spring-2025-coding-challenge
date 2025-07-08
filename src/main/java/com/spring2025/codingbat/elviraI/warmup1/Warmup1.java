a388dc4cf503feee1f535c219e73d076b7384cb4
elviraim <elya89china@gmail.com>
2025-07-08 20:36:22 +0600

    parrot Trouble

--- src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
@@ -34,6 +34,16 @@ public class Warmup1 {
         }
         return (n - 21) * 2;
     }
+    public static boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && (hour < 7 || hour > 20)) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+
 
 
 
