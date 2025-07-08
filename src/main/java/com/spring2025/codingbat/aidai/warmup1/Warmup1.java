d61c8fea8455befe43ed226ee1cbf952de982743
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 00:25:21 +0600

    Stop tracking .idea

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -31,5 +31,15 @@ public class Warmup1 {
             return (a + b);
         }
     }
+/**
+ Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+ */
+public int diff21(int n) {
+    if (n > 21){
+        return 2 * (n - 21);
+    } else {
+        return 21 - n;
+    }
+}
 }
 
