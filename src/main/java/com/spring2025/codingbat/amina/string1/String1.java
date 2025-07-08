838207aee7f2be4ab9262e6a3fd3f9414f823b27
= <=>
2025-07-09 01:48:38 +0600

    amina/codingbat/string1-withouEnd2

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -221,5 +221,19 @@ public class String1 {
         return str.substring(2) + str.substring(0, 2);
     }
 
+    /**
+     Задача 16 - withouEnd2
+
+     Given a string, return a version without both the first and last char of the string.
+     The string may be any length, including 0.
+
+     withouEnd2("Hello") → "ell"
+     withouEnd2("abc") → "b"
+     withouEnd2("ab") → ""
+     */
+    public String withouEnd2(String str) {
+        return str.length() <= 2 ? "" : str.substring(1, str.length() - 1);
+    }
+
 
 }
