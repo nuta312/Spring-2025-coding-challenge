89745a1daaeace8c83839dc7241f957a183d09d4
mahabat <mahabatka@gmail.com>
2025-07-09 05:47:58 +0600

    master/mahabat/codingbat/warmup2/arrayFront9/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
@@ -105,4 +105,17 @@ public class WarmUp2 {
         return count;
     }
 
+    /* Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+    The array length may be less than 4.*/
+
+    public boolean arrayFront9(int[] nums) {
+
+        int end = nums.length;
+        if (end > 4) end = 4;
+        for (int i = 0; i < end; i++) {
+            if (nums[i] == 9)return true;
+        }
+        return false;
+    }
+
 }
