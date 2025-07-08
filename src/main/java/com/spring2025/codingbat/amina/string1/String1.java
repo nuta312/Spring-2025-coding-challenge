cf776f3876567f65676d18ece88c3a9b2b5ca31e
= <=>
2025-07-09 01:51:04 +0600

    amina/codingbat/string1-conCat

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -265,5 +265,22 @@ public class String1 {
         return false;
     }
 
+    /**
+     Задача 19 - conCat
+
+     Concatenate two strings. If the concatenation creates a double-char,
+     then omit one of the chars.
+
+     conCat("abc", "cat") → "abcat"
+     conCat("dog", "cat") → "dogcat"
+     conCat("abc", "") → "abc"
+     */
+    public String conCat(String a, String b) {
+        if (a.length() > 0 && b.length() > 0 && a.charAt(a.length() - 1) == b.charAt(0)) {
+            return a + b.substring(1);
+        }
+        return a + b;
+    }
+
 
 }
