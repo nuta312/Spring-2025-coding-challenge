963b0d7784b4d419d7825c9e2a6c41d957a0dc18
Emil <emkerimov@mail.ru>
2025-07-09 04:09:52 +0600

    Warmup1

--- src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
@@ -299,4 +299,16 @@ startHi("hello hi") → false
                 : str;
     }
 
+    /*
+    Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+
+    mixStart("mix snacks") → true
+    mixStart("pix snacks") → true
+    mixStart("piz snacks") → false
+     */
+    public boolean mixStart(String str) {
+        return str.length() >= 3 && str.charAt(1) == 'i' && str.charAt(2) == 'x';
+    }
+
 }
