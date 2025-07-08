49781338ce93b9d29a8d0af8f1ce94586bee09ad
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 22:52:24 +0600

    diff21

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -37,4 +37,12 @@ public class Warmup1 {
         }
     }
 
+
+    public int diff21(int n) {
+        if (n >= 21) {
+            return (n-21)*2;
+        } else {
+            return 21 - n;
+        }
+    }
 }
