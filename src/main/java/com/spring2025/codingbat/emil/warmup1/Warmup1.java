17486f696cab398d4d544933e181ed5926b6e081
Emil <emkerimov@mail.ru>
2025-07-09 04:13:32 +0600

    Warmup1

--- src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
@@ -404,4 +404,24 @@ in3050(40, 50) → true
         return Math.max(a, b);
     }
 
+    /*
+        Return true if the given string contains between 1 and 3 'e' chars.
+
+
+    stringE("Hello") → true
+    stringE("Heelle") → true
+    stringE("Heelele") → false
+     */
+    public boolean stringE(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'e' || ch == 'E') {
+                count++;
+            }
+        }
+        return count >= 1 && count <= 3;
+    }
+
 }
