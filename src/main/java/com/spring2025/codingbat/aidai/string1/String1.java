1bd09561f58ca598a2c606fcc2b17f3a12a4814c
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:20:51 +0600

    aidai/codingbat/string1-nTwice

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -198,4 +198,17 @@ public String middleTwo(String str) {
 public boolean endsLy(String str) {
     return str.endsWith("ly");
 }
+/**
+ Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+
+
+ nTwice("Hello", 2) → "Helo"
+ nTwice("Chocolate", 3) → "Choate"
+ nTwice("Chocolate", 1) → "Ce"
+ */
+public String nTwice(String str, int n) {
+    String start = str.substring(0, n);
+    String end = str.substring(str.length() - n);
+    return start + end;
+}
 }
