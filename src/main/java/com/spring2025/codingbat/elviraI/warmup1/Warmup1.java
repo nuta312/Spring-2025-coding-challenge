06e2cafb320dd5351a76aa871db6d7edc4c20cf8
elviraim <elya89china@gmail.com>
2025-07-08 20:44:11 +0600

    startHi

--- src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
@@ -94,6 +94,9 @@ public class Warmup1 {
     public String front22(String str) {
         return (str.length() >= 2) ? str.substring(0, 2) + str + str.substring(0, 2) : str + str + str;
     }
+    public boolean startHi(String str) {
+        return (str.startsWith("hi")) ? true : false;
+    }
 
 
 
