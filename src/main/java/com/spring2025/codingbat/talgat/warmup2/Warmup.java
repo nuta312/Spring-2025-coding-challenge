2e766a96a748b81a6f50f71f27903541529e22f0
Talgat <talaronin26@gmail.com>
2025-07-09 01:09:13 +0600

    Talgat/warmup2/15.has271

--- src/main/java/com/spring2025/codingbat/talgat/warmup2/Warmup.java
+++ src/main/java/com/spring2025/codingbat/talgat/warmup2/Warmup.java
@@ -194,4 +194,15 @@ public class Warmup {
     }
 
 
+    public boolean has271(int[] nums) {
+        boolean result = false;
+        for(int i = 0;i<nums.length-2;i++){
+            if(Math.abs(nums[i] - nums[i+1])==5 && Math.abs(nums[i]-1-nums[i+2])<=2){
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
