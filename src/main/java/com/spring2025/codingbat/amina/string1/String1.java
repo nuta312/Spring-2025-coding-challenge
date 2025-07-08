c0ce3415ab70e7706fe8bbaf41d5577fee4deace
= <=>
2025-07-09 01:41:10 +0600

    amina/codingbat/string1-makeAbba

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -169,4 +169,16 @@ public class String1 {
         return "";
     }
 
+    /**
+     Задача 12 - startWord
+     Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+     makeAbba("Hi", "Bye") → "HiByeByeHi"
+     makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+     makeAbba("What", "Up") → "WhatUpUpWhat"
+     */
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+
 }
