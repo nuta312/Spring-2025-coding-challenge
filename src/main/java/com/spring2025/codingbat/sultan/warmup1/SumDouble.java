ab7dd673d9693baf3ab18ccb83807b6025a4b584
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-08 20:58:54 +0600

    CoddingBat Warmup1 -> sumDouble

---
+++ src/main/java/com/spring2025/codingbat/sultan/warmup1/SumDouble.java
@@ -0,0 +1,23 @@
+package com.spring2025.codingbat.sultan.warmup1;
+
+public class SumDouble {
+    /**
+     Given two int values, return their sum.
+     Unless the two values are the same, then return double their sum.
+     sumDouble(1, 2) → 3
+     sumDouble(3, 2) → 5
+     sumDouble(2, 2) → 8
+     */
+    public static void main(String[] args) {
+        sumDouble(1, 2);
+        sumDouble(3, 2);
+        sumDouble(2, 2);
+    }
+    public static int sumDouble(int a, int b) {
+        if(a!=b){
+            return a+b;
+        }else{
+            return (a+b)*2;
+        }
+    }
+}
