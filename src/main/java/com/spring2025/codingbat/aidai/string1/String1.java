75d6063e34783eea68c9975b66146c4c0df71a21
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:11:20 +0600

    aidai/codingbat/string1-withoutEnd

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -87,4 +87,15 @@ public String firstTwo(String str) {
 public String firstHalf(String str) {
     return str.substring(0, str.length() / 2);
 }
+/**
+ Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+
+ withoutEnd("Hello") → "ell"
+ withoutEnd("java") → "av"
+ withoutEnd("coding") → "odin"
+ */
+public String withoutEnd(String str) {
+    return str.substring(1, str.length() - 1);
+}
 }
