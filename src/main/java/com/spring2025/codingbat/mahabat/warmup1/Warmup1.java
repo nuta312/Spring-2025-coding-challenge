68401ded239bdf5b021cd1138fc6865e36c513c1
mahabat <mahabatka@gmail.com>
2025-07-09 05:12:13 +0600

    master/mahabat/codingbat/warmup1/close10/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -264,6 +264,24 @@ public class Warmup1 {
         }
     }
 
+    // Given 2 int values, return whichever value is nearest to the value 10,
+    // or return 0 in the event of a tie.
+    // Note that Math.abs(n) returns the absolute value of a number.
+
+    public int close10(int a, int b) {
+
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+        if (aDiff < bDiff){
+            return a;
+        }if (aDiff > bDiff){
+            return b;
+        } else {
+            return 0;
+        }
+    }
+
+
 
 
 
