47f995f7313b86d46da6be7edf9631c5a28e51e8
staldybaev <staldybaev@beeline.kz>
2025-07-08 11:50:23 +0600

    added main method

--- src/main/java/com/spring2025/codingbat/nuta/string1/cholpon/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/nuta/string1/cholpon/warmup1/Warmup1.java
@@ -1,4 +1,7 @@
 package com.spring2025.codingbat.nuta.string1.cholpon.warmup1;
 
 public class Warmup1 {
+    public static void main(String[] args) {
+        System.out.println("First mr");
+    }
 }
