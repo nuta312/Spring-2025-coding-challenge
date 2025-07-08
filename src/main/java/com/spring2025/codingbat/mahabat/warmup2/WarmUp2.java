ba40f967af9936abcb4d435bff3f6a72b34c4d18
mahabat <mahabatka@gmail.com>
2025-07-09 05:40:21 +0600

    master/mahabat/codingbat/warmup2/stringSplosion/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
@@ -64,4 +64,15 @@ public class WarmUp2 {
         }
         return newStr;
     }
+
+    // Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+    public String stringSplosion(String str) {
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result = result + str.substring(0,i+1);
+        }
+        return result;
+    }
 }
