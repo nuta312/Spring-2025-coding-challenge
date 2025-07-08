c1cdac75fbd6ea7c19830cf079a96ced0d906227
= <=>
2025-07-09 01:49:40 +0600

    amina/codingbat/string1-nTwice

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -235,5 +235,19 @@ public class String1 {
         return str.length() <= 2 ? "" : str.substring(1, str.length() - 1);
     }
 
+    /**
+     Задача 17 - nTwice
+
+     Given a string and an int n, return a string made of the first and last n chars from the string.
+     The string length will be at least n.
+
+     nTwice("Hello", 2) → "Helo"
+     nTwice("Chocolate", 3) → "Choate"
+     nTwice("Chocolate", 1) → "Ce"
+     */
+    public String nTwice(String str, int n) {
+        return str.substring(0, n) + str.substring(str.length() - n);
+    }
+
 
 }
