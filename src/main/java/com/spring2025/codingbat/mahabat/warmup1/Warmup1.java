beab82a33997956b9292ff393dfa16d694ea3e67
mahabat <mahabatka@gmail.com>
2025-07-08 12:48:56 +0600

    master/mahabat/codingbat/warmup1/sumDouble/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -25,4 +25,16 @@ public class Warmup1 {
         return false;
     }
 
+//    Given two int values, return their sum.
+//    Unless the two values are the same, then return double their sum.
+
+    public int sumDouble(int a, int b) {
+        int sum = a+b;
+        if (a != b){
+            return sum;
+        }else{
+            return sum*2;
+        }
+    }
+
 }
