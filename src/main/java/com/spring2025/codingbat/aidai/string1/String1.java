50ba3313b9cad13a0f009aeed97bb73d13ce3437
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:32:27 +0600

    aidai/codingbat/string1-deFront

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -410,4 +410,25 @@ public String without2(String str) {
     }
     return str;
 }
+/**
+ Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+
+
+ deFront("Hello") → "llo"
+ deFront("java") → "va"
+ deFront("away") → "aay"
+ */
+public String deFront(String str) {
+    String result = "";
+    if (str.length() >= 1 && str.substring(0, 1).equals("a")) {
+        result += "a";
+    }
+    if (str.length() >= 2 && str.substring(1, 2).equals("b")) {
+        result += "b";
+    }
+    if (str.length() > 2) {
+        result += str.substring(2);
+    }
+    return result;
+}
 }
