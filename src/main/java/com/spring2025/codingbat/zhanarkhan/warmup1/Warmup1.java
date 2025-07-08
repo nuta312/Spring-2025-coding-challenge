93465e471fc7cc514725ad49ad4b24a05fc7ad89
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 23:07:14 +0600

    startHi

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -103,4 +103,8 @@ public class Warmup1 {
     public String front22(String str) {
         return (str.length() >= 2)? str.substring(0,2) + str + str.substring(0,2) : str+str+str;
     }
+
+    public boolean startHi(String str) {
+        return (str.length() >=2 && str.substring(0,2).equals("hi"))? true : false;
+    }
 }
