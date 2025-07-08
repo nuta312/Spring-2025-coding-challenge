ce0270453e898b1504e2d5ffda4d1a98716a5c77
mahabat <mahabatka@gmail.com>
2025-07-09 05:48:56 +0600

    master/mahabat/codingbat/warmup2/array123/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
@@ -118,4 +118,15 @@ public class WarmUp2 {
         return false;
     }
 
+    // Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+    public boolean array123(int[] nums) {
+
+        for(int i = 0; i < (nums.length -2); i++){
+            if(nums[i] == 1 && nums[i+1] == 2 && nums[i +2] == 3) return true;
+        }
+        return false;
+    }
+
+
 }
