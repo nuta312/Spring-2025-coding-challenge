9b012bf5cddf87e0cdec1615d101aac4bd5eca49
mahabat <mahabatka@gmail.com>
2025-07-08 20:24:43 +0600

    master/mahabat/codingbat/warmup1/posNeg/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -79,4 +79,10 @@ public class Warmup1 {
         return false;
     }
 
+// Given 2 int values, return true if one is negative and one is positive.
+// Except if the parameter "negative" is true, then return true only if both are negative.
+
+    public boolean posNeg(int a, int b, boolean negative) {
+        return ((a > 0 && b < 0 && !negative) || (b > 0 && a < 0 && !negative) || (a < 0 && b < 0 && negative));
+    }
 }
