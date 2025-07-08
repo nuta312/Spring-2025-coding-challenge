794edbe4b0f18b187fda765f017525e308c5df17
elviraim <elya89china@gmail.com>
2025-07-08 20:43:17 +0600

    front22

--- src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
@@ -91,6 +91,9 @@ public class Warmup1 {
             return false;
         }
     }
+    public String front22(String str) {
+        return (str.length() >= 2) ? str.substring(0, 2) + str + str.substring(0, 2) : str + str + str;
+    }
 
 
 
