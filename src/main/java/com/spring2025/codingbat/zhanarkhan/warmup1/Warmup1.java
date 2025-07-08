d61275c237422a97ad6a450ba8194bdcfe4d9ae1
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-08 22:54:44 +0600

    nearHundred

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup1/Warmup1.java
@@ -61,4 +61,13 @@ public class Warmup1 {
             return false;
         }
     }
+
+
+    public boolean nearHundred(int n) {
+        if ((n >= 90 && n <= 110) || (n >= 190 && n <= 210)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
