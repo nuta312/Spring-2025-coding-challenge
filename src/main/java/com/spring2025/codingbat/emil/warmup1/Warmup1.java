340490169a434509152b154b310bf0463557f749
Emil <emkerimov@mail.ru>
2025-07-09 04:07:07 +0600

    Warmup1

--- src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
@@ -228,5 +228,16 @@ startHi("hello hi") → false
             return false;
         }
     }
+/*
+Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+
+icyHot(120, -1) → true
+icyHot(-1, 120) → true
+icyHot(2, 120) → false
+ */
+public boolean icyHot(int temp1, int temp2) {
+    return (temp1 < 0 && temp2 > 100 || temp2 < 0 && temp1 > 100);
+}
 
 }
