50fdaef3448f81ecd4f604c8cf3cc08996db3295
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 02:14:55 +0600

    aidai/codingbat/warmup2-stringYak

--- src/main/java/com/spring2025/codingbat/aidai/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup2/Warmup2.java
@@ -237,4 +237,25 @@ public String altPairs(String str) {
     }
     return res.toString();
 }
+/**
+ Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+
+ stringYak("yakpak") → "pak"
+ stringYak("pakyak") → "pak"
+ stringYak("yak123ya") → "123ya"
+ */
+public String stringYak(String str) {
+    StringBuilder res = new StringBuilder();
+    int i = 0;
+    while (i < str.length()){
+        if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+            i += 3;
+        } else {
+            res.append(str.charAt(i));
+            i++;
+        }
+    }
+    return res.toString();
+}
 }
