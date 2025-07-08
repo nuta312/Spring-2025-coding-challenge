6a0088a0d6caf5309d0734cbeab80e65281344e6
= <=>
2025-07-09 00:52:14 +0600

    amina/codingbat/string1/helloName

---
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -0,0 +1,18 @@
+package com.spring2025.codingbat.amina.string1;
+/**
+ * @author Amina
+ */
+public class String1 {
+    /**
+     Задача 1 - helloName
+
+     Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+     helloName("Bob") → "Hello Bob!"
+     helloName("Alice") → "Hello Alice!"
+     helloName("X") → "Hello X!"
+     */
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
