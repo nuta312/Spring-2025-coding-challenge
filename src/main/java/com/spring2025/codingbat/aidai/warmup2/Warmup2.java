0d7a04fe6eff1c2d905e6d60953ea1f8428c9c97
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 02:05:14 +0600

    aidai/codingbat/warmup2-doubleX

--- src/main/java/com/spring2025/codingbat/aidai/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup2/Warmup2.java
@@ -53,4 +53,24 @@ int countXX(String str) {
     }
     return count;
 }
+/**
+ Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+
+ doubleX("axxbb") → true
+ doubleX("axaxax") → false
+ doubleX("xxxxx") → true
+ */
+boolean doubleX(String str) {
+    for (int i = 0; i < str.length() - 1; i++) {
+        if (str.charAt(i) == 'x') {
+            if (str.charAt(i + 1) == 'x') {
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+    return false;
+}
 }
