5970cdbcd5eaf287e36b322bfbab62db9d599d79
mahabat <mahabatka@gmail.com>
2025-07-09 05:20:16 +0600

    master/mahabat/codingbat/warmup1/everyNth/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -347,6 +347,19 @@ public class Warmup1 {
         return front + back.toUpperCase();
     }
 
+    // Given a non-empty string and an int N, return the string made starting with char 0,
+    // and then every Nth char of the string.
+    // So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+
+    public String everyNth(String str, int n) {
+
+        String str1 = "";
+        for (int i = 0; i < str.length(); i =i +n){
+            str1 = str1 + str.charAt(i);
+        }
+        return str1;
+    }
+
 
 
 
