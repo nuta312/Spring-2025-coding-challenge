7f7393d0c532812dd3338302cf3d783ffe78ea3c
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 01:26:47 +0600

    aidai/codingbat/warmup1-front3

--- src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aidai/warmup1/Warmup1.java
@@ -31,105 +31,126 @@ public class Warmup1 {
             return (a + b);
         }
     }
-/**
- Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
- */
-public int diff21(int n) {
-    if (n > 21){
-        return 2 * (n - 21);
-    } else {
-        return 21 - n;
+
+    /**
+     * Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+     */
+    public int diff21(int n) {
+        if (n > 21) {
+            return 2 * (n - 21);
+        } else {
+            return 21 - n;
+        }
     }
-}
-/**
- We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
 
-
- parrotTrouble(true, 6) → true
- parrotTrouble(true, 7) → false
- parrotTrouble(false, 6) → false
- */
-public boolean parrotTrouble(boolean talking, int hour) {
-    return talking && (hour < 7 || hour > 20 );
-}
-/**
- Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
-
-
- makes10(9, 10) → true
- makes10(9, 9) → false
- makes10(1, 9) → true
- */
-public boolean makes10(int a, int b) {
-    return (a == 10 || b == 10 || a+b ==10);
-}
-/**
- Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
-
-
- nearHundred(93) → true
- nearHundred(90) → true
- nearHundred(89) → false
- */
-public boolean nearHundred(int n) {
-    return (n >= 90 && n <= 110) || (n >= 190 && n <= 210);
-}
-/**
- Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
-
-
- posNeg(1, -1, false) → true
- posNeg(-1, 1, false) → true
- posNeg(-4, -5, true) → true
- */
-public boolean posNeg(int a, int b, boolean negative) {
-    if (negative) {
-        return (a < 0 && b < 0);
-    } else {
-        return (a < 0 && b > 0) || (a > 0 && b < 0);
+    /**
+     * We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+     * <p>
+     * <p>
+     * parrotTrouble(true, 6) → true
+     * parrotTrouble(true, 7) → false
+     * parrotTrouble(false, 6) → false
+     */
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return talking && (hour < 7 || hour > 20);
     }
-}
-/**
- Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
 
-
- notString("candy") → "not candy"
- notString("x") → "not x"
- notString("not bad") → "not bad"
- */
-public String notString(String str) {
-    if (str.startsWith("not")) {
-        return str;
-    } else {
-        return "not " + str;
+    /**
+     * Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+     * <p>
+     * <p>
+     * makes10(9, 10) → true
+     * makes10(9, 9) → false
+     * makes10(1, 9) → true
+     */
+    public boolean makes10(int a, int b) {
+        return (a == 10 || b == 10 || a + b == 10);
     }
-}
-/**
- Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
 
-
- missingChar("kitten", 1) → "ktten"
- missingChar("kitten", 0) → "itten"
- missingChar("kitten", 4) → "kittn"
- */
-public String missingChar(String str, int n) {
-    return str.substring(0, n) + str.substring(n + 1);
-}
-/**
- Given a string, return a new string where the first and last chars have been exchanged.
-
-
- frontBack("code") → "eodc"
- frontBack("a") → "a"
- frontBack("ab") → "ba"
- */
-public String frontBack(String str) {
-    if (str.length() <= 1) {
-        return str;
+    /**
+     * Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+     * <p>
+     * <p>
+     * nearHundred(93) → true
+     * nearHundred(90) → true
+     * nearHundred(89) → false
+     */
+    public boolean nearHundred(int n) {
+        return (n >= 90 && n <= 110) || (n >= 190 && n <= 210);
     }
-    return str.charAt(str.length() - 1)
-            + str.substring(1, str.length() - 1)
-            + str.charAt(0);
-}
+
+    /**
+     * Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+     * <p>
+     * <p>
+     * posNeg(1, -1, false) → true
+     * posNeg(-1, 1, false) → true
+     * posNeg(-4, -5, true) → true
+     */
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        } else {
+            return (a < 0 && b > 0) || (a > 0 && b < 0);
+        }
+    }
+
+    /**
+     * Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+     * <p>
+     * <p>
+     * notString("candy") → "not candy"
+     * notString("x") → "not x"
+     * notString("not bad") → "not bad"
+     */
+    public String notString(String str) {
+        if (str.startsWith("not")) {
+            return str;
+        } else {
+            return "not " + str;
+        }
+    }
+
+    /**
+     * Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+     * <p>
+     * <p>
+     * missingChar("kitten", 1) → "ktten"
+     * missingChar("kitten", 0) → "itten"
+     * missingChar("kitten", 4) → "kittn"
+     */
+    public String missingChar(String str, int n) {
+        return str.substring(0, n) + str.substring(n + 1);
+    }
+
+    /**
+     * Given a string, return a new string where the first and last chars have been exchanged.
+     * <p>
+     * <p>
+     * frontBack("code") → "eodc"
+     * frontBack("a") → "a"
+     * frontBack("ab") → "ba"
+     */
+    public String frontBack(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+        return str.charAt(str.length() - 1)
+                + str.substring(1, str.length() - 1)
+                + str.charAt(0);
+    }
+
+    /**
+     * Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+     * <p>
+     * <p>
+     * front3("Java") → "JavJavJav"
+     * front3("Chocolate") → "ChoChoCho"
+     * front3("abc") → "abcabcabc"
+     */
+    public String front3(String str) {
+        String front = str.length() < 3 ? str : str.substring(0, 3);
+        return front + front + front;
+    }
 }
 
