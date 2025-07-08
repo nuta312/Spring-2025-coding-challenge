a6c99145ede6d34f47914924c118befdb573581c
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:23:33 +0600

    aidai/codingbat/string1-hasBad

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -237,4 +237,16 @@ public String middleThree(String str) {
     int mid = str.length() / 2;
     return str.substring(mid - 1, mid + 2);
 }
+/**
+ Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+
+
+ hasBad("badxx") → true
+ hasBad("xbadxx") → true
+ hasBad("xxbadxx") → false
+ */
+public boolean hasBad(String str) {
+    return (str.length() >= 3 && str.substring(0, 3).equals("bad")) ||
+            (str.length() >= 4 && str.substring(1, 4).equals("bad"));
+}
 }
