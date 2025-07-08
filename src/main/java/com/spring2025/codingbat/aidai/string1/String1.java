8e01597a1bb3ed4bb18e392c74fd12b8b9d3dba3
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:19:33 +0600

    aidai/codingbat/string1-endsLy

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -187,4 +187,15 @@ public String middleTwo(String str) {
     int mid = str.length() / 2;
     return str.substring(mid - 1, mid + 1);
 }
+/**
+ Given a string, return true if it ends in "ly".
+
+
+ endsLy("oddly") → true
+ endsLy("y") → false
+ endsLy("oddy") → false
+ */
+public boolean endsLy(String str) {
+    return str.endsWith("ly");
+}
 }
