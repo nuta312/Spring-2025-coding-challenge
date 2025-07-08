8095ef2690aac31c7d44648314f39c104e26a4cb
admin <admin@MacBook-Air-admin.local>
2025-07-08 13:57:48 +0600

    sleepIn

--- src/main/java/com/spring2025/codingbat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/warmup1/Warmup1.java
@@ -1,4 +1,14 @@
 package com.spring2025.codingbat.warmup1;
 
 public class Warmup1 {
+
+    /// sleepIn(false, false) → true
+    /// sleepIn(true, false) → false
+    /// sleepIn(false, true) → true
+
+    ///Warmup-1 > sleepIn
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
 }
