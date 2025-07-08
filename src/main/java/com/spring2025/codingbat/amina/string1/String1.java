8c805f29595effa0dd9a413351feabe79d7a5745
= <=>
2025-07-09 01:35:42 +0600

    amina/codingbat/string1-middleThree

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -86,4 +86,19 @@ public class String1 {
         return str.endsWith("ly");
     }
 
+    /**
+     Задача 7 - middleThree
+
+     Given a string of odd length, return the string length 3 from its middle,
+     so "Candy" yields "and". The string length will be at least 3.
+
+     middleThree("Candy") → "and"
+     middleThree("and") → "and"
+     middleThree("solving") → "lvi"
+     */
+    public String middleThree(String str) {
+        int mid = str.length() / 2;
+        return str.substring(mid - 1, mid + 2);
+    }
+
 }
