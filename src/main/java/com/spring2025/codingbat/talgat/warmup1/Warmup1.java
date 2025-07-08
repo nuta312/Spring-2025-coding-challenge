66bfe0476c73b71915eaf9079694e2ea30a00fb3
Talgat <talaronin26@gmail.com>
2025-07-08 23:52:18 +0600

    Talgat/warmup1/12.front3

--- src/main/java/com/spring2025/codingbat/talgat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/talgat/warmup1/Warmup1.java
@@ -85,4 +85,9 @@ public class Warmup1 {
 
     }
 
+    public String front3(String str) {
+        return (str.length()>3) ? str.substring(0,3)+str.substring(0,3)+str.substring(0,3):str + str + str;
+    }
+
+
 }
