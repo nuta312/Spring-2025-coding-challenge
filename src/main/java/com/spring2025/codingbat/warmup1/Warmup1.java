303245f94b3f48a89cebd77cf0c9c5fe950bd568
= <=>
2025-07-08 13:22:02 +0600

    Решение первой задачи

---
+++ src/main/java/com/spring2025/codingbat/warmup1/Warmup1.java
@@ -0,0 +1,11 @@
+package com.spring2025.codingbat.warmup1;
+
+public class Warmup1 {
+    public static void main(String[] args) {
+
+
+    }
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+}
