4cc45b47bf688453d89a2491bb23d6ca425ae9e7
= <=>
2025-07-09 01:54:02 +0600

    amina/codingbat/string1-withouX

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -312,5 +312,23 @@ public class String1 {
         return str;
     }
 
+    /**
+     Задача 22 - withoutX
+
+     If the first or last chars are 'x', return the string without those 'x' chars.
+
+     withoutX("xHix") → "Hi"
+     withoutX("xHi") → "Hi"
+     withoutX("Hxix") → "Hxi"
+     */
+    public String withoutX(String str) {
+        if (str.length() >= 1 && str.charAt(0) == 'x') {
+            str = str.substring(1);
+        }
+        if (str.length() >= 1 && str.charAt(str.length() - 1) == 'x') {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
 
 }
