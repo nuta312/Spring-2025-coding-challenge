46f4610a478e3b9a618ecf7c03d280c649219d60
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:12:41 +0600

    aidai/codingbat/string1-comboString

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -98,4 +98,19 @@ public String firstHalf(String str) {
 public String withoutEnd(String str) {
     return str.substring(1, str.length() - 1);
 }
+/**
+ Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+
+
+ comboString("Hello", "hi") → "hiHellohi"
+ comboString("hi", "Hello") → "hiHellohi"
+ comboString("aaa", "b") → "baaab"
+ */
+public String comboString(String a, String b) {
+    if (a.length() < b.length()) {
+        return a + b + a;
+    } else {
+        return b + a + b;
+    }
+}
 }
