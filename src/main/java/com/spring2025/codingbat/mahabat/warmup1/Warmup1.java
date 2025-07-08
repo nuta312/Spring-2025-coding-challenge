20e347d7ab0a37fa8f2ac94f27dbaba944a42ab7
mahabat <mahabatka@gmail.com>
2025-07-08 21:41:04 +0600

    master/mahabat/codingbat/warmup1/backAround/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -127,4 +127,15 @@ public class Warmup1 {
         return str1 +str1 + str1;
     }
 
+    // Given a string, take the last char and return a new string with the last char
+    // added at the front and back, so "cat" yields "tcatt".
+    // The original string will be length 1 or more.
+
+    public String backAround(String str) {
+
+        String newStr = str.substring(str.length()-1);
+        return newStr + str + newStr;
+    }
+
+
 }
