83ce535e3441a47fbc0f17e2433adf8f10897d11
Emil <emkerimov@mail.ru>
2025-07-09 04:06:24 +0600

    Warmup1

--- src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
@@ -212,5 +212,21 @@ missingChar("kitten", 4) → "kittn"
     public String front22(String str) {
         return (str.length() >= 2) ? str.substring(0, 2) + str + str.substring(0, 2) : str + str + str;
     }
+/*
+Given a string, return true if the string starts with "hi" and false otherwise.
+
+
+startHi("hi there") → true
+startHi("hi") → true
+startHi("hello hi") → false
+ */
+
+    public boolean startHi(String str) {
+        if (str.startsWith("hi")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
