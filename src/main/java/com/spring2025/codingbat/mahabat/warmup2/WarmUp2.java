e83e054d1248e83d7013f1c2afb25a824789d941
mahabat <mahabatka@gmail.com>
2025-07-09 05:51:23 +0600

    master/mahabat/codingbat/warmup2/stringX/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup2/WarmUp2.java
@@ -146,5 +146,20 @@ public class WarmUp2 {
         return count;
     }
 
+    /* Given a string, return a version where all the "x" have been removed.
+    Except an "x" at the very start or end should not be removed.*/
+
+    public String stringX(String str) {
+
+        String result = "";
+        for(int i = 0; i < str.length(); i++){
+            if(!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))){
+                result = result + str.substring(i, i+1);
+            }
+        }
+        return result;
+    }
+
+
 
 }
