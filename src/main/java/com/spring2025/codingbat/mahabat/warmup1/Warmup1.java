3a3e7670a302ed05d63e158e785820e159e0d100
mahabat <mahabatka@gmail.com>
2025-07-08 20:03:54 +0600

    master/mahabat/codingbat/warmup1/diff21/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -37,4 +37,16 @@ public class Warmup1 {
         }
     }
 
+//    Given an int n, return the absolute difference between n and 21,
+//    except return double the absolute difference if n is over 21
+
+    public int diff21(int n) {
+        int zero = (n - 21) * 2;
+        int result = 21 - n;
+        if(n >= 21 ){
+            return zero;
+        }
+        return result;
+    }
+
 }
