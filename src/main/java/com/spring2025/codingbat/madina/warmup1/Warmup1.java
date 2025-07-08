c443739e00c5b284a57713f55d6032f8b279a7e9
Madina Raiymbekova <1madilyao@MacBook-Pro-Madina.local>
2025-07-08 19:05:02 +0600

    Sleepin

--- src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
@@ -1,4 +1,22 @@
 package com.spring2025.codingbat.madina.warmup1;
 
 public class Warmup1 {
+    public static void main(String[] args) {
+        Warmup1 obj =new Warmup1();
+        System.out.println(obj.sleepIn(false,false));
+    }
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!(weekday) && (vacation)) {
+            return true;
+        } else if (weekday && !(vacation)) {
+            return false;
+        } else if (!(weekday) && vacation) {
+            return true;
+        } else {
+            return true;
+        }
+
+    }
+
 }
