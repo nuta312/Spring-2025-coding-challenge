d404591191a243e97de09b9bba987570c354286b
mahabat <mahabatka@gmail.com>
2025-07-09 05:15:40 +0600

    master/mahabat/codingbat/warmup1/stringE/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -309,6 +309,19 @@ public class Warmup1 {
         return 0;
     }
 
+    // Return true if the given string contains between 1 and 3 'e' chars.
+
+    public boolean stringE(String str) {
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e')
+                count++;
+
+        }
+        return count >= 1 && count <= 3;
+    }
+
 
 
 
