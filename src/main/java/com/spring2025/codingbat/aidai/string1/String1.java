c413720618fa543bb6dc525b1db8b8a51ff376e1
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:18:29 +0600

    aidai/codingbat/string1-middleTwo

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -175,4 +175,16 @@ public String withouEnd2(String str) {
     }
     return str.substring(1, str.length() - 1);
 }
+/**
+ Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+
+
+ middleTwo("string") → "ri"
+ middleTwo("code") → "od"
+ middleTwo("Practice") → "ct"
+ */
+public String middleTwo(String str) {
+    int mid = str.length() / 2;
+    return str.substring(mid - 1, mid + 1);
+}
 }
