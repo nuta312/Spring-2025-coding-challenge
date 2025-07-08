a152c429337dd5b2d2e6c8b649864195ad3cf5af
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:36:32 +0600

    aidai/codingbat/string1-withoutX2

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -471,4 +471,26 @@ public String withoutX(String str) {
     }
     return str;
 }
+/**
+ Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged. This is a little harder than it looks.
+
+
+ withoutX2("xHi") → "Hi"
+ withoutX2("Hxi") → "Hi"
+ withoutX2("Hi") → "Hi"
+ */
+public String withoutX2(String str) {
+    String result = "";
+    if (str.length() >= 1 && str.charAt(0) != 'x') {
+        result += str.charAt(0);
+    }
+    if (str.length() >= 2 && str.charAt(1) != 'x') {
+        result += str.charAt(1);
+    }
+    if (str.length() > 2) {
+        result += str.substring(2);
+    }
+
+    return result;
+}
 }
