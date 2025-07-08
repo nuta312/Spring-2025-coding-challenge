1486918977f03ea4882fb5cdaa147634c8b913b3
mahabat <mahabatka@gmail.com>
2025-07-08 21:46:28 +0600

    master/mahabat/codingbat/warmup1/icyHot/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -164,5 +164,18 @@ public class Warmup1 {
         return false;
     }
 
+    // Given two temperatures,
+    // return true if one is less than 0 and the other is greater than 100.
+
+    public boolean icyHot(int temp1, int temp2) {
+
+        if((temp1 < 0 && temp2 > 100) || ( temp2 < 0 && temp1 > 100)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 
 }
