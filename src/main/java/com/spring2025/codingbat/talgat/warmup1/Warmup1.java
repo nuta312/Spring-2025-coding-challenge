6e8a0eced6400c2d8dde251283535862b65f7145
Talgat <talaronin26@gmail.com>
2025-07-08 23:22:20 +0600

    Talgat/warmup1/1.sleepIn

---
+++ src/main/java/com/spring2025/codingbat/talgat/warmup1/Warmup1.java
@@ -0,0 +1,12 @@
+package com.spring2025.codingbat.talgat.warmup1;
+
+public class Warmup1 {
+    public static void main(String[] args) {
+
+    }
+
+    // W1 sleepIn
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+}
