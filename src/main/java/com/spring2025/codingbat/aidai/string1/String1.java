610d460bef5b5026e312c9530fb86a0dfe8ec52b
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:05:16 +0600

    aidai/codingbat/string1-makeAbba

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -17,4 +17,16 @@ public String helloName(String name) {
     String hello = "Hello " + name + "!";
     return hello;
 }
+/**
+ Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+
+ makeAbba("Hi", "Bye") → "HiByeByeHi"
+ makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+ makeAbba("What", "Up") → "WhatUpUpWhat"
+ */
+public String makeAbba(String a, String b) {
+    return a + b + b + a;
+}
+
 }
