0126e4e17b5013383db4e4331ee07f50e928df87
Emil <emkerimov@mail.ru>
2025-07-09 03:45:06 +0600

    Warmup1

--- src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/emil/warmup1/Warmup1.java
@@ -1,4 +1,19 @@
 package com.spring2025.codingbat.emil.warmup1;
 
+/*
+ * @author Emil
+ */
 public class Warmup1 {
+    /*The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+        sleepIn(false, false) → true
+        sleepIn(true, false) → false
+        sleepIn(false, true) → true
+
+     */
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+
 }
