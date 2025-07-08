5d8db8f5e658aa0966182f421784cff2a635a8d0
mahabat <mahabatka@gmail.com>
2025-07-08 21:47:21 +0600

    master/mahabat/codingbat/warmup1/in1020/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -176,6 +176,17 @@ public class Warmup1 {
         }
     }
 
+    // Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+
+    public boolean in1020(int a, int b) {
+
+        if ((a <= 20 && a >= 10) || (b >= 10 && b <= 20)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 
 
 }
