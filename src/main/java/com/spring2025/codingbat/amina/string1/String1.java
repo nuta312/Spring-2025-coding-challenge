1a5b80d2563d014a6d011bf6127e5142106e42fe
= <=>
2025-07-09 01:30:02 +0600

    amina/codingbat/string1-nonStart

--- src/main/java/com/spring2025/codingbat/amina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/amina/string1/String1.java
@@ -44,4 +44,18 @@ public class String1 {
         return out.substring(0, 2) + word + out.substring(2);
     }
 
+    /**
+     Задача 4 - nonStart
+
+     Given 2 strings, return their concatenation, except omit the first char of each.
+     The strings will be at least length 1.
+
+     nonStart("Hello", "There") → "ellohere"
+     nonStart("java", "code") → "avaode"
+     nonStart("shotl", "java") → "hotlava"
+     */
+    public String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+
 }
