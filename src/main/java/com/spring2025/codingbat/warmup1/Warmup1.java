102e6ea0d368e7125b7225abc26252e4443f4af0
admin <admin@MacBook-Air-admin.local>
2025-07-08 14:35:31 +0600

    backAround

--- src/main/java/com/spring2025/codingbat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/warmup1/Warmup1.java
@@ -143,5 +143,15 @@ public class Warmup1 {
             }
             return str1 + str1 + str1;
         }
+        /// Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt".
+        /// The original string will be length 1 or more.
+        /// backAround("cat") → "tcatt"
+        /// backAround("Hello") → "oHelloo"
+        /// backAround("a") → "aaa"
+        /// Warmup-1 > backAround
+        public String backAround(String str) {
+            String last = str.substring(str.length()-1);
+            return last + str + last;
+        }
 
     }
