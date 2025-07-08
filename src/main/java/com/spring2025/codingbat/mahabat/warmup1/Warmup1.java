dd5de305f8d319b2b9ef9b8eeafa7db24a9637b4
mahabat <mahabatka@gmail.com>
2025-07-08 21:50:22 +0600

    master/mahabat/codingbat/warmup1/loneTeen/

--- src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/warmup1/Warmup1.java
@@ -194,6 +194,20 @@ public class Warmup1 {
         return(a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
     }
 
+    // We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+    // Given 2 int values, return true if one or the other is teen, but not both.
+
+    public boolean loneTeen(int a, int b) {
+
+        boolean aTrue = (a >= 13 && a <= 19);
+        boolean bTrue = (b >= 13 && b <= 19);
+        if((aTrue && !bTrue) || (bTrue && !aTrue)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 
 
