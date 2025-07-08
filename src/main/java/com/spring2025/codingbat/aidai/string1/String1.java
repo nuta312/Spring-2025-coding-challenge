e821c35e3ac764f6f1cf56751b078b939a3058b5
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:08:35 +0600

    aidai/codingbat/string1-extraEnd

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -50,4 +50,16 @@ public String makeTags(String tag, String word) {
 public String makeOutWord(String out, String word) {
     return out.substring(0, 2) + word + out.substring(2, 4);
 }
+/**
+ Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+
+
+ extraEnd("Hello") → "lololo"
+ extraEnd("ab") → "ababab"
+ extraEnd("Hi") → "HiHiHi"
+ */
+public String extraEnd(String str) {
+    String lastTwo = str.substring(str.length() - 2);
+    return lastTwo + lastTwo + lastTwo;
+}
 }
