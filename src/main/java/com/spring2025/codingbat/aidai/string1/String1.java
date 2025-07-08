148205fda3ce4a36bba265f7b1a4f85ce9dddf1d
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:30:52 +0600

    aidai/codingbat/string1-extraFront

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -375,4 +375,21 @@ public String minCat(String a, String b) {
         return  a + re2;
     }
 }
+/**
+ Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+
+
+ extraFront("Hello") → "HeHeHe"
+ extraFront("ab") → "ababab"
+ extraFront("H") → "HHH"
+ */
+public String extraFront(String str) {
+    String front;
+    if (str.length() >= 2) {
+        front = str.substring(0, 2);
+    } else {
+        front = str;
+    }
+    return front + front + front;
+}
 }
