026432d2ff0fda347c05a8bd1761f2bc3e57e184
elviraim <elya89china@gmail.com>
2025-07-08 20:44:58 +0600

    rest of warmup

--- src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elviraI/warmup1/Warmup1.java
@@ -97,6 +97,147 @@ public class Warmup1 {
     public boolean startHi(String str) {
         return (str.startsWith("hi")) ? true : false;
     }
+    public boolean hasTeen(int a, int b, int c) {
+        return ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19));
+    }
+
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
+
+    public String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4);
+        }
+        return str;
+    }
+
+    public boolean mixStart(String str) {
+        return (str.length() >= 3 && str.substring(1, 3).equals("ix"));
+    }
+
+    public String startOz(String str) {
+        String result = "";
+        if (str.length() >= 1 && str.charAt(0) == 'o') {
+            result = result + str.charAt(0);
+        }
+        if (str.length() >= 2 && str.charAt(1) == 'z') {
+            result = result + str.charAt(1);
+        }
+        return result;
+    }
+
+    public int intMax(int a, int b, int c) {
+        int max = a;
+        int[] nums = {a, b, c};
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+
+    public int close10(int a, int b) {
+        int c;
+        int d;
+        c = abs(10 - a);
+        d = abs(10 - b);
+        if (c < d) {
+            return a;
+        } else if (c == d) {
+            return 0;
+        } else {
+            return b;
+        }
+    }
+
+    public boolean in3050(int a, int b) {
+        if (a >= 30 && a <= 40 && b >= 30 && b <= 40) {
+            return true;
+        }
+        if (a >= 40 && a <= 50 && b >= 40 && b <= 50) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean in1020(int a, int b) {
+        if ((a >= 10 && a <= 20) || (b >= 10 && b <= 20)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean icyHot(int temp1, int temp2) {
+        if ((temp1 < 0 && temp2 > 100) || (temp2 < 0 && temp1 > 100)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int max1020(int a, int b) {
+
+        if ((a >= 10 && a <= 20) && (b >= 10 && b <= 20)) {
+            if (a > b) {
+                return a;
+            } else {
+                return b;
+            }
+        } else if ((a >= 10 && a <= 20) && !(b >= 10 && b <= 20)) {
+            return a;
+        } else if (!(a >= 10 && a <= 20) && (b >= 10 && b <= 20)) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
+
+    public int max1020_(int a, int b) {
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // Knowing a is bigger, just check a first
+        if (a >= 10 && a <= 20) return a;
+        if (b >= 10 && b <= 20) return b;
+        return 0;
+    }
+
+    public boolean lastDigit(int a, int b) {
+        if (a % 10 == b % 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String everyNth(String str, int n) {
+        String result = "";
+        for (int i = 0; i < str.length(); i = i + n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
+    public String endUp(String str) {
+        if (str.length() < 4) {
+            return str.toUpperCase();
+        } else {
+            String str2 = str.substring(str.length() - 3, str.length()).toUpperCase();
+            String str1 = str.substring(0, str.length() - 3);
+            return str1 + str2;
+        }
+    }
+
+}
+
 
 
 
