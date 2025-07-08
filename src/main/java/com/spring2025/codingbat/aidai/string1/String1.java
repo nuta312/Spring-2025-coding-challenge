117eb712333d88dc9a2e0fa36812530f11cad0df
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 03:07:46 +0600

    aidai/codingbat/string1-makeOutWord

--- src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aidai/string1/String1.java
@@ -39,4 +39,15 @@ public String makeAbba(String a, String b) {
 public String makeTags(String tag, String word) {
     return "<" + tag + ">" + word + "</" + tag + ">";
 }
+/**
+ Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+
+
+ makeOutWord("<<>>", "Yay") → "<<Yay>>"
+ makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+ makeOutWord("[[]]", "word") → "[[word]]"
+ */
+public String makeOutWord(String out, String word) {
+    return out.substring(0, 2) + word + out.substring(2, 4);
+}
 }
