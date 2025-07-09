5f66da09a518f594d824ebebb9f47ce2d7ac4a68
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:26:28 +0600

    aidai/codingbat/string2-getSandwich

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -173,4 +173,22 @@ public class String2 {
         }
         return false;
     }
+    /**
+     * A sandwich is two pieces of bread with something in between. Return the string that is between the first and last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of bread.
+     *
+     *
+     * getSandwich("breadjambread") → "jam"
+     * getSandwich("xxbreadjambreadyy") → "jam"
+     * getSandwich("xxbreadyy") → ""
+     */
+    public String getSandwich(String str) {
+        String bread = "bread";
+        int firstIndex = str.indexOf(bread);
+        int lastIndex = str.lastIndexOf(bread);
+
+        if (firstIndex == -1 || lastIndex == -1 || firstIndex == lastIndex) {
+            return "";
+        }
+        return str.substring(firstIndex + bread.length(), lastIndex);
+    }
 }
