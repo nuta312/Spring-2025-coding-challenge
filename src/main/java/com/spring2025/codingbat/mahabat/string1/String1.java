d7431e821216bb7409e0b71c4de899891c2f8ac5
mahabat <mahabatka@gmail.com>
2025-07-09 07:19:00 +0600

    master/mahabat/codingbat/string/lastChars/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -218,6 +218,18 @@ public class String1 {
         }
     }
 
+    /* Given 2 strings, a and b, return a new string made of the first char of a and the l
+    ast char of b, so "yo" and "java" yields "ya".
+    If either string is length 0, use '@' for its missing char.*/
+
+    public String lastChars(String a, String b) {
+
+        String first = a.isEmpty() ? "@" : a.substring(0, 1);
+        String last = b.isEmpty() ? "@" : b.substring(b.length() - 1);
+        return first + last;
+    }
+
+
 
 
 
