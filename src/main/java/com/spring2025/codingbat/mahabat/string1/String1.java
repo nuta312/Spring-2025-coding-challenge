66f61e9831234a3410385f3998b10de74cae236a
mahabat <mahabatka@gmail.com>
2025-07-09 06:50:15 +0600

    master/mahabat/codingbat/string/makeAbba/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -9,4 +9,12 @@ public class String1 {
         return hello.concat(name).concat("!");
     }
 
+    /*Given two strings, a and b, return the result of putting them
+    together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".*/
+
+    public String makeAbba(String a, String b) {
+        return a + b + b +a;
+    }
+
+
 }
