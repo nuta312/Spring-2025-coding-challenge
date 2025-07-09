d7065b232463ac3f91f265a73e2760b2112b4db7
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-10 02:17:53 +0600

    CoddingBat string1 - middleTwo

--- src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
@@ -185,5 +185,16 @@ public class String1 {
         return str.substring(1, str.length()-1);
     }
 
+    /**
+     Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+     middleTwo("string") → "ri"
+     middleTwo("code") → "od"
+     middleTwo("Practice") → "ct"
+     */
+
+    public String middleTwo(String str) {
+        int mid = str.length()/2;
+        return str.substring(mid-1, mid+1);
+    }
 
 }
