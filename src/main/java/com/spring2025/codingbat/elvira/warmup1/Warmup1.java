abc3fba704fb98412a9d2c5789cf1251d1151e0e
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 08:39:27 +0600

    or35

--- src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
@@ -207,5 +207,18 @@ public class Warmup1 {
         return (str.length() >= 2) ? r+str+r : str+str+str;
     }
 
+    /**
+     * Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+     *
+     *
+     * or35(3) → true
+     * or35(10) → true
+     * or35(8) → false
+     */
+
+    public boolean or35(int n) {
+        return (n % 3 == 0 || n % 5 ==0) ? true : false;
+    }
+
 
 }
