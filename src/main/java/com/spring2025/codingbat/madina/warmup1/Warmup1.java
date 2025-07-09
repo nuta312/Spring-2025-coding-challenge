da3c9622a84def3a3b58ec55cc3a9e7f5835ee41
Madina <1madilyao@gmail.com>
2025-07-09 16:49:19 +0600

    posneg

--- src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
@@ -61,4 +61,11 @@ public class Warmup1 {
         boolean near200 = (n >= 190 && n <= 210);
         return near100 || near200;
     }
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative){
+            return a <0 && b < 0;
+        } else {
+            return (a<0 && b > 0 || b<0 && a>0 );
+        }
+    }
 }
