17a34561b81e86dfd0e55bce2cca9bb6c9a8276a
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-10 02:11:19 +0600

    CoddingBat string1 - nonStart

--- src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
@@ -120,4 +120,18 @@ public class String1 {
         return a+b+a;
     }
 
+    /**
+     Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+     nonStart("Hello", "There") → "ellohere"
+     nonStart("java", "code") → "avaode"
+     nonStart("shotl", "java") → "hotlava"
+     */
+
+    public String nonStart(String a, String b) {
+        String word = "";
+        word = a.substring(1) + b.substring(1);
+        return word;
+    }
+
+
 }
