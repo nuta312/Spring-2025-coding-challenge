27fde33bdefa3a49ef521ff6f1faa2168f7389fa
= <dzhetybaev19emir@gmail.com>
2025-07-09 21:03:24 +0600

    commonEnd fixed

--- src/main/java/com/spring2025/codingbat/tim/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/tim/array1/Array1.java
@@ -26,5 +26,11 @@ public class Array1 {
 
 
     }
+    public boolean commonEnd(int[] a, int[] b) {
+        if(a.length>=1 && b.length>=1){
+            return a[0] == b[0]||a[a.length-1] == b[b.length-1];
+        }
+        return false;
+    }
 
 }
