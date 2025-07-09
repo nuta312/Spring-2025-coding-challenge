d82e4ecf24b0371e83d81a46b3fae3dbcb931203
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 18:14:50 +0300

    stringTimes

---
+++ src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
@@ -0,0 +1,22 @@
+package com.spring2025.codingbat.beslan.warmup2;
+
+/*
+@author Beslan
+ */
+public class Warmup2 {
+    /*
+    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+    stringTimes("Hi", 2) → "HiHi"
+    stringTimes("Hi", 3) → "HiHiHi"
+    stringTimes("Hi", 1) → "Hi"
+    */
+
+    public String stringTimes(String str, int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result = result + str;
+        }
+        return result;
+    }
+}
