b1ea293862b2cd17dbb36ce12f5326f3311b776d
Altynai R <madakeevaaltynai@gmail.com>
2025-07-08 20:48:10 -0700

    altynai/codingbat/functional1/rigthDigit

--- src/main/java/com/spring2025/codingbat/altynai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/altynai/functional1/Functional1.java
@@ -40,4 +40,19 @@ public class Functional1 {
         }
        return result;
    }
+
+   //Given a list of non-negative integers, return an integer list of the rightmost digits. (Note: use %)
+   //
+   //
+   //rightDigit([1, 22, 93]) → [1, 2, 3]
+   //rightDigit([16, 8, 886, 8, 1]) → [6, 8, 6, 8, 1]
+   //rightDigit([10, 0]) → [0, 0]
+   public static List<Integer>rightDigit(List<Integer>nums){
+       List<Integer> result=new ArrayList<>();
+       for(Integer num:nums){
+           int lastDigit=num % 10;
+           result.add(lastDigit);
+       }
+       return result;
+   }
 }
