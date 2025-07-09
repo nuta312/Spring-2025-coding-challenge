02b4a6003a49b8a1f695a1a9576152ffb6903ecd
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 20:12:16 +0300

    arrayFront9

--- src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
@@ -1,5 +1,7 @@
 package com.spring2025.codingbat.beslan.warmup2;
 
+import java.util.Arrays;
+
 /*
 @author Beslan
  */
@@ -126,4 +128,33 @@ public class Warmup2 {
         }
         return count;
     }
+
+    /*
+    Given an array of ints, return the number of 9's in the array.
+
+    arrayCount9([1, 2, 9]) → 1
+    arrayCount9([1, 9, 9]) → 2
+    arrayCount9([1, 9, 9, 3, 9]) → 3
+     */
+    public int arrayCount9(int[] nums) {
+        return (int) Arrays.stream(nums).filter(a -> a == 9).count();
+    }
+
+    /*
+    Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+    The array length may be less than 4.
+
+    arrayFront9([1, 2, 9, 3, 4]) → true
+    arrayFront9([1, 2, 3, 4, 9]) → false
+    arrayFront9([1, 2, 3, 4, 5]) → false
+     */
+    public boolean arrayFront9(int[] nums) {
+        int end = nums.length;
+        if (end > 4) end = 4;
+        for (int i = 0; i < end; i++) {
+            if (nums[i] == 9) return true;
+        }
+        return false;
+    }
+
 }
