601df7aa25a5102996ce3a844309ac4aa1cb0297
= <=>
2025-07-10 00:35:56 +0600

    icyHot

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -117,4 +117,10 @@ public class WarmUp1 {
         }
         return false;
     }
+    public boolean icyHot(int temp1, int temp2) {
+        if (temp1 < 0 && temp2 > 100 || temp1 > 100 && temp2 < 0){
+            return true;
+        }
+        return false;
+    }
 }
