f2358767f5ab1414a015202dcab63cae8a5c785c
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 15:26:35 +0600

    or35

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -138,4 +138,10 @@ public class Warmup1 {
         String lastChar = str.substring(str.length() - 1);
         return lastChar + str + lastChar;
     }
+
+    // Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+
+    public boolean or35(int n) {
+        return (n % 3 == 0) || (n % 5 == 0);
+    }
 }
