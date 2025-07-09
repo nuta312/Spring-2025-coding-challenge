59abc8804d4e586681329170dbd6c6b32d082c5a
= <=>
2025-07-09 15:37:08 +0600

    amina/codingbat/warmup2/stringX

--- src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
@@ -270,4 +270,20 @@ public class Warmup2 {
         return false;
     }
 
+    /**
+     Task 15 – stringX
+
+     Given a string, return a version where all the "x" have been removed,
+     except an "x" at the very start or end should not be removed.
+
+     stringX("xxHxix") → "xHix"
+     stringX("abxxxcd") → "abcd"
+     stringX("xabxxxcdx") → "xabcdx"
+     */
+    public String stringX(String str) {
+        if (str.length() <= 2) return str;
+
+        String middle = str.substring(1, str.length() - 1).replace("x", "");
+        return str.charAt(0) + middle + str.charAt(str.length() - 1);
+    }
 }
