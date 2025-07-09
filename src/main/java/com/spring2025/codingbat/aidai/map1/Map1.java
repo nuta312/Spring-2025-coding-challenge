57567bfe4aa65474eac501657a098c541f808dc2
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:51:44 +0600

    aidai/codingbat/map1-mapShare

--- src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
+++ src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
@@ -22,4 +22,20 @@ public class Map1 {
         }
         return map;
     }
+    /**
+     * Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
+     *
+     *
+     * mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+     * mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+     * mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
+     */
+    public Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            String value = map.get("a");
+            map.put("b", value);
+        }
+        map.remove("c");
+        return map;
+    }
 }
