511958b84b976fd2a746ee39b13ff5e2b7bcb42a
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:59:31 +0600

    aidai/codingbat/map1-mapAB4

--- src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
+++ src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
@@ -146,4 +146,26 @@ public class Map1 {
         }
         return map;
     }
+    /**
+     * Modify and return the given map as follows: if the keys "a" and "b" have values that have different lengths, then set "c" to have the longer value. If the values exist and have the same length, change them both to the empty string in the map.
+     *
+     *
+     * mapAB4({"a": "aaa", "b": "bb", "c": "cake"}) → {"a": "aaa", "b": "bb", "c": "aaa"}
+     * mapAB4({"a": "aa", "b": "bbb", "c": "cake"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+     * mapAB4({"a": "aa", "b": "bbb"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+     */
+    public Map<String, String> mapAB4(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")){
+            String value1 = map.get("a");
+            String value2 = map.get("b");
+            if (value1.length() != value2.length()) {
+                String max = value1.length() > value2.length() ? value1 : value2;
+                map.put("c", max);
+            }else {
+                map.put("a","");
+                map.put("b","");
+            }
+        }
+        return map;
+    }
 }
