9285ab2299693c449c252a50bd5ded3e99c1260f
Madina <1madilyao@gmail.com>
2025-07-09 19:52:13 +0600

    extra

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -44,4 +44,15 @@ public class String1 {
        String right =out.substring(out.length()/2);
        return left+word+right;
    }
+   /* Given a string, return a new string made
+   of 3 copies of the last 2 chars of the original string.
+   The string length will be at least 2.
+    extraEnd("Hello") → "lololo"
+    extraEnd("ab") → "ababab"
+    extraEnd("Hi") → "HiHiHi"
+    */
+   public String extraEnd(String str) {
+       String last2 =str.substring(str.length()-2);
+       return last2+last2+last2;
+   }
 }
