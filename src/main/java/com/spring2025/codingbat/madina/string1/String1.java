7dc443fcfd2fdfdb652ae8c7ca8539039b0e6f58
Madina <1madilyao@gmail.com>
2025-07-09 20:32:48 +0600

    seecolor

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -249,6 +249,21 @@ public class String1 {
       char last = str.charAt(str.length() - 1);
       return start + last +""+ secondLast;
   }
+    /*Given a string, if the string begins with
+     "red" or "blue" return that color string,
+      otherwise return the empty string.
+    seeColor("redxx") → "red"
+    seeColor("xxred") → ""
+    seeColor("blueTimes") → "blue"
+     */
+    public String seeColor(String str) {
+        if (str.startsWith("red") ){
+            return "red";
+        }else if(str.startsWith("blue" )){
+            return "blue";
+        } return "";
+    }
+
 
 
 
