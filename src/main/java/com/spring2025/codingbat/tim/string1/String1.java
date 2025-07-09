142f00faf16b980946a5fd0c0f19fc64723e8e6f
= <dzhetybaev19emir@gmail.com>
2025-07-09 17:03:55 +0600

    fix for the strign functions

---
+++ src/main/java/com/spring2025/codingbat/tim/string1/String1.java
@@ -0,0 +1,12 @@
+package com.spring2025.codingbat.tim.string1;
+
+public class String1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static class String12{
+        public static void main(String[] args) {
+            System.out.println("Hello Bob!");
+        }
+    }
+}
