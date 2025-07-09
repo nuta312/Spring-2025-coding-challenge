424be846e1792c040ca3ccecfb3e06cb5617d220
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 15:42:33 +0600

    makeAbba

--- src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -17,4 +17,17 @@ public class String1 {
     public String helloName(String name) {
         return "Hello " + name + "!";
     }
+
+    /**
+     * Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+     *
+     *
+     * makeAbba("Hi", "Bye") → "HiByeByeHi"
+     * makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+     * makeAbba("What", "Up") → "WhatUpUpWhat"
+     */
+
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
 }
