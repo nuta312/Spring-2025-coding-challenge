21197f51e19d02291cc31d775933bf989dec330b
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 18:24:57 +0600

    middleTwo

--- src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -205,6 +205,22 @@ public class String1 {
         return "";
     }
 
+    /**
+     * Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+     *
+     *
+     * middleTwo("string") → "ri"
+     * middleTwo("code") → "od"
+     * middleTwo("Practice") → "ct"
+     */
+
+    public String middleTwo(String str) {
+        if(str.length() % 2 == 0){
+            return str.substring(str.length() / 2 - 1, str.length() / 2 + 1 );
+        }
+        return str;
+    }
+
 
 
 
