9d950eefac7712ea05919a97fb2952c8e2ed4e1b
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:17:08 +0600

    aidai/codingbat/string2-repeatEnd

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -85,4 +85,21 @@ public class String2 {
         }
         return res;
     }
+    /**
+     * Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
+     *
+     *
+     * repeatEnd("Hello", 3) → "llollollo"
+     * repeatEnd("Hello", 2) → "lolo"
+     * repeatEnd("Hello", 1) → "o"
+     */
+    public String repeatEnd(String str, int n) {
+        String end = str.substring(str.length() - n);
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            result += end;
+        }
+        return result;
+    }
 }
