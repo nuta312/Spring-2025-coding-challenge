49e253bdd712f4a7a5ab1ad4cbe2f3bfe6471baa
Madina <1madilyao@gmail.com>
2025-07-09 19:53:47 +0600

    firstHalf

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -55,4 +55,13 @@ public class String1 {
        String last2 =str.substring(str.length()-2);
        return last2+last2+last2;
    }
+    /**Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+     firstHalf("WooHoo") → "Woo"
+    firstHalf("HelloThere") → "Hello"
+    firstHalf("abcdef") → "abc"
+     */
+    public  String firstHalf(String str) {
+        int half =str.length()/2;
+        return str.substring(0,half);
+    }
 }
