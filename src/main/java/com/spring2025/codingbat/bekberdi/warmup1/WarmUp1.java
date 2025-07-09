a193a07e21d7561a52cf1baec8a06b0ea68f7c3d
= <=>
2025-07-10 00:40:20 +0600

    intMax

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -164,4 +164,13 @@ public class WarmUp1 {
             return "o";
         }return "";
     }
+    public int intMax(int a, int b, int c) {
+        if (a > b && a > c){
+            return a;
+        } else if (b >a && b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
 }
