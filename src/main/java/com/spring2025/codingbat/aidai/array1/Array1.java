eda0aa55cbe0fc2d5cc2f823d4c6977be8adc193
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:17:58 +0600

    aidai/codingbat/array1-reverse3

--- src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
@@ -94,4 +94,18 @@ public class Array1 {
         int last = nums[nums.length -1];
         return new int[] {  middle, last,first };
     }
+    /**
+     * Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+     *
+     *
+     * reverse3([1, 2, 3]) → [3, 2, 1]
+     * reverse3([5, 11, 9]) → [9, 11, 5]
+     * reverse3([7, 0, 0]) → [0, 0, 7]
+     */
+    public int[] reverse3(int[] nums) {
+        int first = nums[0];
+        int middle = nums[nums.length / 2];
+        int last = nums[nums.length - 1];
+        return new int[]{last, middle, first};
+    }
 }
