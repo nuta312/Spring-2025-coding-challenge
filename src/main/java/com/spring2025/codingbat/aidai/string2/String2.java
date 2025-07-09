ac345d2894c99386e3fd5b3c79d04230aa4e249d
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:22:13 +0600

    aidai/codingbat/string2-repeatSeparator

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -117,4 +117,22 @@ public class String2 {
         }
         return res;
     }
+    /**
+     * Given two strings, word and a separator sep, return a big string made of count occurrences of the word, separated by the separator string.
+     *
+     *
+     * repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+     * repeatSeparator("This", "And", 2) → "ThisAndThis"
+     * repeatSeparator("This", "And", 1) → "This"
+     */
+    public String repeatSeparator(String word, String sep, int count) {
+        String res = "";
+        for(int i = 0; i < count ; i ++){
+            res += word;
+            if(i < count -1){
+                res += sep;
+            }
+        }
+        return res;
+    }
 }
