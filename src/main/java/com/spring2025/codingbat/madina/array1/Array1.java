5c1bac63af9cada8ede04962bb3335e0f37cdb03
Madina <1madilyao@gmail.com>
2025-07-09 23:50:33 +0600

    midlii

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -252,4 +252,16 @@ public class Array1 {
         nums[nums.length - 1] = temp;
         return nums;
     }
+   /* Given an array of ints of odd length, return a new array length 3 containing the elements from the middle of the array. The array length will be at least 3.
+
+
+    midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
+    midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
+    midThree([1, 2, 3]) → [1, 2, 3]
+
+    */
+   public int[] midThree(int[] nums) {
+       int half= nums.length /2 ;
+       return new int[] { nums[half - 1], nums[half], nums[half + 1] };
+   }
 }
