408d971786c112499894690fec70db50eece1b94
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 16:03:14 +0600

    mixStart

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -197,4 +197,16 @@ public class Warmup1 {
             return str;
         }
     }
+
+    // Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+    public boolean mixStart(String str) {
+        if (str.length() < 3) return false;
+        String two = str.substring(1, 3);
+        if (two.equals("ix")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
