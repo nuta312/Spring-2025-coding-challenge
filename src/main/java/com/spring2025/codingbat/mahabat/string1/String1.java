c145656c0f153497c47d82095659f18e79240c7e
mahabat <mahabatka@gmail.com>
2025-07-09 07:07:14 +0600

    master/mahabat/codingbat/string/right2/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -114,5 +114,15 @@ public class String1 {
         return str.substring(2).concat(two);
     }
 
+    /* Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
+    The string length will be at least 2.*/
+
+    public String right2(String str) {
+
+        String last = str.substring(str.length()-2);
+        String rest = str.substring(0,str.length()-2);
+        return last + rest;
+    }
+
 
 }
