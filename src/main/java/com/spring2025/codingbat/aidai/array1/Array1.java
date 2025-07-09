712211527323cff3b2200b5a989a126df6ca8e48
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:19:32 +0600

    aidai/codingbat/array1-sum2

--- src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
@@ -120,4 +120,21 @@ public class Array1 {
         int max = Math.max(nums[0], nums[nums.length - 1]);
         return new int[] {max,max,max};
     }
+    /**
+     * Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+     *
+     *
+     * sum2([1, 2, 3]) → 3
+     * sum2([1, 1]) → 2
+     * sum2([1, 1, 1, 1]) → 2
+     */
+    public int sum2(int[] nums) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        if (nums.length < 2) {
+            return nums[0];
+        }
+        return nums[0] + nums[1];
+    }
 }
