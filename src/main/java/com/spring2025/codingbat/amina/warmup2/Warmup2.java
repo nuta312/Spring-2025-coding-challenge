5b2c5b548675bced137c698b9506aaf7437a0225
= <=>
2025-07-09 15:35:06 +0600

    amina/codingbat/warmup2/stringYak

--- src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
@@ -189,6 +189,31 @@ public class Warmup2 {
         return count;
     }
 
+    /**
+     Task 11 – stringYak
+
+     Given a string, return a version where all the "yak" are removed,
+     but the 'a' can be any char. The "yak" strings will not overlap.
+
+     stringYak("yakpak") → "pak"
+     stringYak("pakyak") → "pak"
+     stringYak("yak123ya") → "123ya"
+     */
+    public String stringYak(String str) {
+        String result = "";
+        int i = 0;
+
+        while (i < str.length()) {
+            if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+                i += 3;
+            } else {
+                result += str.charAt(i);
+                i++;
+            }
+        }
+
+        return result;
+    }
 
 
 }
