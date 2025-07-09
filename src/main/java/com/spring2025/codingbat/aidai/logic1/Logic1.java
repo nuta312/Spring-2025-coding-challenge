79308f78e4e41aebfbd35713d7673ba6c03d78fc
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 20:13:23 +0600

    aidai/codingbat/logic1-nearTen

--- src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
@@ -204,4 +204,19 @@ public boolean less20(int n) {
         return false;
     }
 }
+/**
+ * Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+ *
+ *
+ * nearTen(12) → true
+ * nearTen(17) → false
+ * nearTen(19) → true
+ */
+public boolean nearTen(int num) {
+    if(num % 10 <= 2 || num % 10 >= 8){
+        return true;
+    }else{
+        return false;
+    }
+}
 }
