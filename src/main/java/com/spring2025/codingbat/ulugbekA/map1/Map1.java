d0d4432ba237b62922fce8676c9dee8c3d7385e5
Ulugbek Attokurov <attokurovulugbek@gmail.com>
2025-07-09 12:26:07 +0600

    mapBully

---
+++ src/main/java/com/spring2025/codingbat/ulugbekA/map1/Map1.java
@@ -0,0 +1,23 @@
+package com.spring2025.codingbat.ulugbekA.map1;
+
+import java.util.Map;
+
+public class Map1 {
+    /*
+Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value,
+and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
+ */
+
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if(map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
+
+}
+
+
+
+
