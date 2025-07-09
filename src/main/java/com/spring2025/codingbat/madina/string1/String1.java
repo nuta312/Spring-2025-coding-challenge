2ea09fd1af0ce8e39f716cf3a6362537e4bb12ce
Madina <1madilyao@gmail.com>
2025-07-09 20:34:49 +0600

    mincat

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -274,15 +274,18 @@ public class String1 {
        }
        return false;
    }
-
-
-
-
-
-
-
-
-
+   /* Given two strings, append them together (known as "concatenation") and return the result. However, if the strings are different lengths, omit chars from the longer string so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
+    minCat("Hello", "Hi") → "loHi"
+    minCat("Hello", "java") → "ellojava"
+    minCat("java", "Hello") → "javaello"*/
+   public String minCat(String a, String b) {
+       if (a.length()>b.length()){
+           a = a.substring(a.length()-b.length());
+       } else if(a.length()<b.length()){
+           b=b.substring(b.length()-a.length());
+       }
+       return a+b;
+   }
 
 
 }
