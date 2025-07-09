12c41b2ee4e12d06afa25cbd7ff78accec19bbc9
Madina <1madilyao@gmail.com>
2025-07-09 23:41:18 +0600

    start

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -177,4 +177,23 @@ public class Array1 {
         }
         return nums;
     }
+   /* Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+
+
+    start1([1, 2, 3], [1, 3]) → 2
+    start1([7, 2, 3], [1]) → 1
+    start1([1, 2], []) → 1
+
+    */
+   public int start1(int[] a, int[] b) {
+       int str =0;
+       if(a.length > 0 && a[0] ==1 && b.length >0 && b[0]==1){
+           return str =2;
+
+       }else if(a.length > 0 && a[0] ==1 || b.length >0 && b[0]==1){
+           return str=1;
+       }else{
+           return str;
+       }
+   }
 }
