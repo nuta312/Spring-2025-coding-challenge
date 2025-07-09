424b1744cf44c7bb8ca1e9d60bfe86173d5cf78f
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:33:09 +0600

    aidai/codingbat/string2-plusOut

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -277,4 +277,25 @@ public class String2 {
         }
         return result;
     }
+    /**
+     * Given a string and a non-empty word string, return a version of the original String where all chars have been replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.
+     *
+     *
+     * plusOut("12xy34", "xy") → "++xy++"
+     * plusOut("12xy34", "1") → "1+++++"
+     * plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"
+     */
+    public String plusOut(String str, String word) {
+        String res = "";
+        for (int i = 0; i < str.length(); ) {
+            if (i <= str.length() -word.length() && str.substring(i,i + word.length()).equals(word)){
+                res += word;
+                i += word.length();
+            }else {
+                res += "+";
+                i++;
+            }
+        }
+        return res;
+    }
 }
