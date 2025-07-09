ab581e05f13eeb698da145d830c9bc9a81828c21
Madina <1madilyao@gmail.com>
2025-07-09 23:24:33 +0600

    makePi

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -28,5 +28,10 @@ public class Array1 {
    public boolean sameFirstLast(int[] nums) {
        return nums.length >= 1 && nums[0] == nums[nums.length - 1];
    }
+    /*Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+    makePi() → [3, 1, 4]*/
+    public int[] makePi() {
+        return  new int [] {3,1,4};
+    }
 
 }
