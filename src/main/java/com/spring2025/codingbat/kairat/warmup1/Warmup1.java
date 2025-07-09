be7b2ad1bb82061f91fc8f96683816c86e951aae
= <=>
2025-07-10 00:08:39 +0600

    notString

--- src/main/java/com/spring2025/codingbat/kairat/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/kairat/warmup1/Warmup1.java
@@ -119,5 +119,21 @@ public class Warmup1 {
         } else return false;
     }
 
+    /*Given a string, return a new string where "not "
+     has been added to the front. However, if the string
+     already begins with "not", return the string unchanged.
+     Note: use .equals() to compare 2 strings.
+
+    notString("candy") → "not candy"
+    notString("x") → "not x"
+    notString("not bad") → "not bad"*/
+
+    public String notString(String str) {
+        if (str.length() < 3) {
+            return "not " + str;
+        }
+        return str.substring(0, 3).equals("not") ? str : "not " + str;
+    }
+
 
 }
