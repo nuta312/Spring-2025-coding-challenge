3f051d5c193311e31cc67c4f6a4e3a4bad6d269c
mahabat <mahabatka@gmail.com>
2025-07-09 07:27:57 +0600

    master/mahabat/codingbat/string/frontAgain/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -270,5 +270,14 @@ public class String1 {
         return  newStr;
     }
 
+    /* Given a string, return true if the first 2 chars in the string also appear
+    at the end of the string, such as with "edited".*/
+
+    public boolean frontAgain(String str) {
+
+        if (str.length() < 2) return false;
+        return str.substring(0, 2).equals(str.substring(str.length() - 2));
+    }
+
 
 }
