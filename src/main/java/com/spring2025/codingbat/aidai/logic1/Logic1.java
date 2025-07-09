b3dd595dda532c5cd6320415b93575cf5f729cbf
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 20:12:48 +0600

    aidai/codingbat/logic1-less20

--- src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
@@ -189,4 +189,19 @@ public boolean old35(int n) {
         return false;
     }
 }
+/**
+ * Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+ *
+ *
+ * less20(18) → true
+ * less20(19) → true
+ * less20(20) → false
+ */
+public boolean less20(int n) {
+    if (n % 20 == 18 || n % 20 == 19){
+        return true;
+    }else{
+        return false;
+    }
+}
 }
