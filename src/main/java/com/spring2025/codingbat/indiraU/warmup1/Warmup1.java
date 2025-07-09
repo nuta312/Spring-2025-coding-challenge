de58ce0f2fb503c92e7dc5c12ab360458cf90af6
indira <you@example.com>
2025-07-09 15:43:30 +0600

    max1020

--- src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
@@ -423,6 +423,28 @@ in3050(40, 50) → true
         }
         return false;
     }
+     /*
+
+Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
+
+
+max1020(11, 19) → 19
+max1020(19, 11) → 19
+max1020(11, 9) → 11
+    */
+
+    public int max1020(int a, int b) {
+
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (a >= 10 && a <= 20) return a;
+        if (b >= 10 && b <= 20) return b;
+        return 0;
+    }
 
 
 
