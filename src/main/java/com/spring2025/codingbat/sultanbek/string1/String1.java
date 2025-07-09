be2b4079aadab8fe6d7d6dd47c037085e5511fab
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-10 01:04:49 +0600

    CoddingBat string1 - helloName + name change into FullLastName

---
+++ src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
@@ -0,0 +1,20 @@
+package com.spring2025.codingbat.sultanbek.string1;
+
+public class String1 {
+    /**
+     * @author Sultanbek
+     */
+
+    /**
+    Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+    helloName("Bob") → "Hello Bob!"
+    helloName("Alice") → "Hello Alice!"
+    helloName("X") → "Hello X!"
+     */
+
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+
+}
