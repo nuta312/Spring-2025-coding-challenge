bb25c56228b66447223662cbea85bbbedbd55c3a
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 08:48:34 +0600

    intMax

--- src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
@@ -358,5 +358,24 @@ public class Warmup1 {
         return s;
     }
 
+    /**
+     * Given three int values, a b c, return the largest.
+     * intMax(1, 2, 3) → 3
+     * intMax(1, 3, 2) → 3
+     * intMax(3, 2, 1) → 3
+     */
+
+    public int intMax(int a, int b, int c) {
+
+        if(a > b && a > c){
+            return a;
+        }
+        if(b > c && b > a){
+            return b;
+        }
+        return c;
+
+    }
+
 
 }
