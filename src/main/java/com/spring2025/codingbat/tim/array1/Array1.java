414dc72d5bd8699257ef760b9213362b35f54456
= <dzhetybaev19emir@gmail.com>
2025-07-09 21:08:53 +0600

    no23 fixed

--- src/main/java/com/spring2025/codingbat/tim/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/tim/array1/Array1.java
@@ -78,5 +78,12 @@ public class Array1 {
         }
         return false;
     }
-
+    public boolean no23(int[] nums) {
+        for(int i =0;i<nums.length;i++){
+            if(nums[i] ==2 || nums[i] ==3){
+                return false;
+            }
+        }
+        return true;
+    }
 }
