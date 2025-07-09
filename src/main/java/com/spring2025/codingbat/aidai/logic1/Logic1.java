7f472f905f4859b701b3d79e1cfee1e51ed7e031
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 20:15:35 +0600

    aidai/codingbat/logic1-answerCell

--- src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
@@ -234,4 +234,21 @@ public int teenSum(int a, int b) {
         return a + b;
     }
 }
+/**
+ * Your cell phone rings. Return true if you should answer it. Normally you answer, except in the morning you only answer if it is your mom calling. In all cases, if you are asleep, you do not answer.
+ *
+ *
+ * answerCell(false, false, false) → true
+ * answerCell(false, false, true) → false
+ * answerCell(true, false, false) → false
+ */
+public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+    if (isAsleep) {
+        return false;
+    }
+    if (isMorning && !isMom) {
+        return false;
+    }
+    return true;
+}
 }
