9f935dc5d000af5656c26bb7e47fa2a619b704ba
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 17:07:43 +0300

    or35

--- src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
@@ -208,9 +208,18 @@ public String backAround(String str) {
         result = str;
     }
     return result + str + result;
-
 }
+/*
+Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+ Use the % "mod" operator
 
+or35(3) → true
+or35(10) → true
+or35(8) → false
+ */
+public boolean or35(int n) {
+    return (n % 3 == 0) || (n % 5 == 0);
+}
 }
 
 
