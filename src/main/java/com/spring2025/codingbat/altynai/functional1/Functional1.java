1544272c75d0383b79214cff992196941c10aac6
Altynai R <madakeevaaltynai@gmail.com>
2025-07-08 21:06:01 -0700

    altynai/codingbat/functional1-lower

--- src/main/java/com/spring2025/codingbat/altynai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/altynai/functional1/Functional1.java
@@ -88,4 +88,18 @@ public class Functional1 {
        }
        return result;
    }
+   //Given a list of strings, return a list where each string is converted to lower case (Note: String toLowerCase() method).
+    //
+    //
+    //lower(["Hello", "Hi"]) → ["hello", "hi"]
+    //lower(["AAA", "BBB", "ccc"]) → ["aaa", "bbb", "ccc"]
+    //lower(["KitteN", "ChocolaTE"]) → ["kitten", "chocolate"]
+
+   public static List<String> lower (List<String> strings){
+       List<String> result=new ArrayList<>();
+       for(String s:strings){
+           result.add(s.toLowerCase());
+       }
+       return result;
+   }
 }
