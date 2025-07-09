b75d86a9c7cf66a9ae3488dc008deaa4c807f054
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 20:22:29 +0600

    aidai/codingbat/logic1-lessBy10

--- src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
@@ -363,4 +363,21 @@ public boolean lastDigit(int a, int b, int c) {
         return false;
     }
 }
+    /**
+     * Given three ints, a b c, return true if one of them is 10 or more less than one of the others.
+     *
+     *
+     * lessBy10(1, 7, 11) → true
+     * lessBy10(1, 7, 10) → false
+     * lessBy10(11, 1, 7) → true
+     */
+    public boolean lessBy10(int a, int b, int c) {
+        if (Math.abs(a - b) >= 10 ||
+                Math.abs(a - c) >= 10 ||
+                Math.abs(b - c) >= 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
