ce368e98d1116f539d614a78b1c915119933c8a6
Madina <1madilyao@gmail.com>
2025-07-09 20:35:48 +0600

    extrafront

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -287,6 +287,19 @@ public class String1 {
        return a+b;
    }
 
+   /* Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+    extraFront("Hello") → "HeHeHe"
+    extraFront("ab") → "ababab"
+    extraFront("H") → "HHH"*/
+   public String extraFront(String str) {
+       if (str.length()>2){
+           str= str.substring(0,2);
+           return str+str+str;
+       }
+       return str+str+str;
+   }
+
+
 
 }
 
