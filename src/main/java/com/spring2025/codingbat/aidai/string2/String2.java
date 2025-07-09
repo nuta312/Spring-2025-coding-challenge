fe8b3570ebabcfebcc1554ef96bebd2ebad8c331
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:32:02 +0600

    aidai/codingbat/string2-starOut

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -258,4 +258,23 @@ public class String2 {
         }
         return res;
     }
+    /**
+     * Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+     *
+     *
+     * starOut("ab*cd") → "ad"
+     * starOut("ab**cd") → "ad"
+     * starOut("sm*eilly") → "silly"
+     */
+    public String starOut(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '*'
+                    && (i == 0 || str.charAt(i - 1) != '*')
+                    && (i == str.length() - 1 || str.charAt(i + 1) != '*')) {
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
 }
