b347be3c762243e57a455afeb35b9553227508ba
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 20:17:16 +0600

    aidai/codingbat/logic1-fizzString

--- src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
@@ -268,4 +268,23 @@ public int teaParty(int tea, int candy) {
         return 1;
     }
 }
+/**
+ * Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged. (See also: FizzBuzz Code)
+ *
+ *
+ * fizzString("fig") → "Fizz"
+ * fizzString("dib") → "Buzz"
+ * fizzString("fib") → "FizzBuzz"
+ */
+public String fizzString(String str) {
+    if (str.startsWith("f") && str.endsWith("b")) {
+        return "FizzBuzz";
+    } else if (str.startsWith("f")) {
+        return "Fizz";
+    } else if (str.endsWith("b")) {
+        return "Buzz";
+    } else {
+        return str;
+    }
+}
 }
