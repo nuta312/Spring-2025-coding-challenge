abc46c7309bd123e1ddaea229d7fd1e6ac9ea39e
aallymbekova <alymbekovaaliya@icloud.com>
2025-07-09 10:34:14 +0600

    fizzString

--- src/main/java/com/spring2025/codingbat/aliia/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aliia/logic1/Logic1.java
@@ -214,4 +214,20 @@ public class Logic1 {
             return 0;
         }
     }
+
+    /*Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz".
+    If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged. (See also: FizzBuzz Code)
+     */
+
+    public String fizzString(String str) {
+        if(str.charAt(0) == 'f' && str.charAt(str.length() - 1) == 'b') {
+            return "FizzBuzz";
+        } else if(str.charAt(0) == 'f') {
+            return "Fizz";
+        } else if(str.charAt(str.length() - 1) == 'b'){
+            return "Buzz";
+        } else {
+            return str;
+        }
+    }
 }
