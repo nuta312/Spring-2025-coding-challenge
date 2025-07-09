07cc7ccdca62c68d6e0176ca59bead0995890677
= <=>
2025-07-09 15:35:29 +0600

    amina/codingbat/warmup2/countXX

--- src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
@@ -215,5 +215,25 @@ public class Warmup2 {
         return result;
     }
 
+    /**
+     Task 12 – countXX
+
+     Count the number of "xx" in the given string. Overlapping is allowed,
+     so "xxx" contains 2 "xx".
+
+     countXX("abcxx") → 1
+     countXX("xxx") → 2
+     countXX("xxxx") → 3
+     */
+    public int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("xx")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 }
