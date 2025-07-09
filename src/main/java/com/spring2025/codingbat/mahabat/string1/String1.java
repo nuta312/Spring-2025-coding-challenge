12eb07290ff1bbab462b00d52145bc73c30e254e
mahabat <mahabatka@gmail.com>
2025-07-09 07:17:54 +0600

    master/mahabat/codingbat/string/atFirst/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -204,6 +204,21 @@ public class String1 {
                 str.length() >= 4 && str.substring(1,4).equals("bad"));
     }
 
+    /* Given a string, return a string length 2 made of its first 2 chars.
+    If the string length is less than 2, use '@' for the missing chars.*/
+
+    public String atFirst(String str) {
+
+        if (str.length() >= 2 ) {
+            return str.substring(0,2);
+        }else if (str.length() == 1 ) {
+            return str + "@";
+        }else{
+            return "@@";
+        }
+    }
+
+
 
 
 
