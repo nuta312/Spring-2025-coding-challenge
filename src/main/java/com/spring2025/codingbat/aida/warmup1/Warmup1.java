a45a4c2098e5ddf3c884e99c527be54cf01416df
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 15:00:29 +0600

    front3

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -124,4 +124,11 @@ public class Warmup1 {
 
         return str.charAt(str.length()-1) + mid + str.charAt(0);
     }
+
+    /* Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front. */
+
+    public String front3(String str) {
+        String front = str.length() < 3 ? str : str.substring(0, 3);
+        return front + front + front;
+    }
 }
