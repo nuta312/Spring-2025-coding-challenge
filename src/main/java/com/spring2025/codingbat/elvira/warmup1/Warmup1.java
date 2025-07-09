3ce400ce7e887fc6ae57a1c2ae7ad12ffd81329e
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 08:45:18 +0600

    delDel

--- src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
@@ -305,5 +305,21 @@ public class Warmup1 {
 
     }
 
+    /**
+     * Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+     *
+     *
+     * delDel("adelbc") → "abc"
+     * delDel("adelHello") → "aHello"
+     * delDel("adedbc") → "adedbc"
+     */
+
+    public String delDel(String str) {
+        if (str.length()>= 4 && str.substring(1, 4).equals("del")){
+            return str.charAt(0) + str.substring(4);
+        }
+        return str;
+    }
+
 
 }
