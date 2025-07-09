70eab4d893851ce841cb09d2542cd210c1856a25
mahabat <mahabatka@gmail.com>
2025-07-09 07:06:14 +0600

    master/mahabat/codingbat/string/left2/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -104,5 +104,15 @@ public class String1 {
         return a.substring(1) + b.substring(1);
     }
 
+    /* Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
+    The string length will be at least 2.*/
+
+    public String left2(String str) {
+
+        if (str.length() < 3) return str;
+        String two = str.substring(0,2);
+        return str.substring(2).concat(two);
+    }
+
 
 }
