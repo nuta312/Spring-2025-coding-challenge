073dac91a4561c8563da62a7b395ed21244ca717
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:39:55 +0600

    aidai/codingbat/array1-frontPiece

--- src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
@@ -340,4 +340,18 @@ public class Array1 {
         int max = Math.max(First, Math.max(middle, Last));
         return max;
     }
+    /**
+     * Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+     *
+     *
+     * frontPiece([1, 2, 3]) → [1, 2]
+     * frontPiece([1, 2]) → [1, 2]
+     * frontPiece([1]) → [1]
+     */
+    public int[] frontPiece(int[] nums) {
+        if (nums.length > 1){
+            return new int[]{nums[0],nums[1]};
+        }
+        return nums;
+    }
 }
