47a6981ada02b7fec35a5301a83763be6c0aba75
mahabat <mahabatka@gmail.com>
2025-07-09 07:05:09 +0600

    master/mahabat/codingbat/string/nonStart/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -97,5 +97,12 @@ public class String1 {
         }
     }
 
+    // Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+    public String nonStart(String a, String b) {
+
+        return a.substring(1) + b.substring(1);
+    }
+
 
 }
