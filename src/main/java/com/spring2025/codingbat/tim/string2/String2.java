b980d7d91a797db5aa9de385c5d7fd493fb18726
= <dzhetybaev19emir@gmail.com>
2025-07-09 20:46:10 +0600

    catDog fixed

--- src/main/java/com/spring2025/codingbat/tim/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/tim/string2/String2.java
@@ -24,4 +24,13 @@ public class String2 {
         }
         return count;
     }
+    public boolean catDog(String str) {
+        int catCount = 0;
+        int dogCount = 0;
+        for(int i =0; i<str.length()-2;i++){
+            if(str.substring(i,i+3).equals("cat")) catCount++;
+            if(str.substring(i,i+3).equals("dog")) dogCount++;
+        }
+        return (catCount == dogCount);
+    }
 }
