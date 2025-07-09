483c31222dbc35f7102816825e667679392bec6a
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 14:52:20 +0600

    frontBack

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -114,4 +114,14 @@ public class Warmup1 {
         return front + back;
     }
 
+    /* Given a string, return a new string where the first and last chars have been exchanged. */
+
+    public String frontBack(String str) {
+        if (str.length() <= 1) return str;
+
+        String mid = str.substring(1, str.length()-1);
+
+
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }
 }
