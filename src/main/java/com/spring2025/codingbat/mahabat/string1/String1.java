b51f8732d6ad05dd983bf872b872d032e06a09b1
mahabat <mahabatka@gmail.com>
2025-07-09 07:16:33 +0600

    master/mahabat/codingbat/string/hasBad/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -194,6 +194,16 @@ public class String1 {
         return result;
     }
 
+    /* Given a string, return true if "bad" appears starting at index 0 or 1 in the string,
+    such as with "badxxx" or "xbadxx" but not "xxbadxx".
+    The string may be any length, including 0. Note: use .equals() to compare 2 strings.*/
+
+    public boolean hasBad(String str) {
+
+        return (str.length()>= 3 && str.startsWith("bad")||
+                str.length() >= 4 && str.substring(1,4).equals("bad"));
+    }
+
 
 
 
