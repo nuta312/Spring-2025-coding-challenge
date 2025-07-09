aed0b749be48a787e02ce5dcf8581699c9609658
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:15:54 +0600

    aidai/codingbat/array1-sum3

--- src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
@@ -65,4 +65,19 @@ public class Array1 {
         }
         return false;
     }
+    /**
+     * Given an array of ints length 3, return the sum of all the elements.
+     *
+     *
+     * sum3([1, 2, 3]) → 6
+     * sum3([5, 11, 2]) → 18
+     * sum3([7, 0, 0]) → 7
+     */
+    public int sum3(int[] nums) {
+        int sum = 0;
+        for (int num : nums){
+            sum += num;
+        }
+        return sum;
+    }
 }
