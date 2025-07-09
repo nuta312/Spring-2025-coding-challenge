e745409f7442b900e49b72d85d09c81420306cc7
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 20:06:03 +0300

    doubleX

--- src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
@@ -58,4 +58,19 @@ public class Warmup2 {
         }
         return count;
     }
+
+    /*
+
+    Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+    doubleX("axxbb") → true
+    doubleX("axaxax") → false
+    doubleX("xxxxx") → true
+     */
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+        if (i == -1) return false;
+        if (i + 1 >= str.length()) return false;
+        return str.substring(i + 1, i + 2).equals("x");
+    }
 }
