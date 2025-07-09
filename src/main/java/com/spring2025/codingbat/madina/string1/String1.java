6a7657d369722e5d502a3f13f7ca0fe2a8ea5a14
Madina <1madilyao@gmail.com>
2025-07-09 19:45:12 +0600

    Helloname

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -2,6 +2,16 @@ package com.spring2025.codingbat.madina.string1;
 
 public class String1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
     }
+
+  /*  Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+    helloName("Bob") → "Hello Bob!"
+    helloName("Alice") → "Hello Alice!"
+    helloName("X") → "Hello X!"
+   */
+  public String helloName(String name) {
+      String hello = "Hello " + name + "!";
+      return hello;
+  }
+
 }
