3cb82303e9040e8e7c0d9149c7875668c6d7eb54
Завира Чыныбаева <zavirachynybaeva@MacBook-Air-Zavira.local>
2025-07-09 17:40:23 +0600

    first task warmup1

--- src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
@@ -1,4 +1,25 @@
 package com.spring2025.codingbat.zavira.warmup1;
 
 public class WarmUp1 {
+    public static void main(String[] args) {
+
+    }
+
+    /* The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+*/
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (weekday == false && vacation == false) {
+            return true;
+        } else if (weekday == true && vacation == false) {
+            return false;
+        } else if (weekday == false && vacation == true) {
+            return true;
+        }
+        return true;
+    }
 }
