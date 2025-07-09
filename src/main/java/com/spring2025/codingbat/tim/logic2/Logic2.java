9d716e4d478a0f1e754db2e0ca7fd6918da9db51
= <dzhetybaev19emir@gmail.com>
2025-07-09 21:45:56 +0600

    roundSum fixed

--- src/main/java/com/spring2025/codingbat/tim/logic2/Logic2.java
+++ src/main/java/com/spring2025/codingbat/tim/logic2/Logic2.java
@@ -49,5 +49,16 @@ public class Logic2 {
             return n;
         }
     }
+    public int roundSum(int a, int b, int c) {
+        return round10(a)+round10(b)+round10(c);
+    }
+    public int round10(int num){
 
+        if(num%10>=5){
+            num = (num/10 +1)*10;
+        }else{
+            num = (num/10)*10;
+        }
+        return num;
+    }
 }
