faa27c935cb505440c8dc12a0900fc013983b545
Madina <1madilyao@gmail.com>
2025-07-09 23:03:55 +0600

    firstlast

---
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -0,0 +1,23 @@
+package com.spring2025.codingbat.madina.array1;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+    }
+    /*Given an array of ints,
+    return true if 6 appears as either the first
+    or last element in the array. The array will be length 1 or more.
+    firstLast6([1, 2, 6]) → true
+    firstLast6([6, 1, 2, 3]) → true
+    firstLast6([13, 6, 1, 2, 3]) → false
+      */
+    public boolean firstLast6(int[] nums) {
+        if (nums[0]==6){
+            return true;
+        }
+        if (nums[nums.length-1]==6){
+            return true;
+        }
+        return false;
+    }
+}
