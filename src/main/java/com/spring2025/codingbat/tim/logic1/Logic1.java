40b6d7152bf576f24dd8297568b26bb2480312ff
= <dzhetybaev19emir@gmail.com>
2025-07-09 21:33:46 +0600

    squirrelPlay fixed

--- src/main/java/com/spring2025/codingbat/tim/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/tim/logic1/Logic1.java
@@ -27,5 +27,18 @@ public class Logic1 {
 
         return result = 1;
     }
-
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if(isSummer){
+            if(temp>=60 && temp<=100){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        if(temp>=60 && temp<=90){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
