2d81487104d3b77ef42bdd33c15e0db19d37f240
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-10 02:12:15 +0600

    CoddingBat string1 - left2

--- src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
@@ -133,5 +133,18 @@ public class String1 {
         return word;
     }
 
+    /**
+     Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+
+     left2("Hello") → "lloHe"
+     left2("java") → "vaja"
+     left2("Hi") → "Hi"
+     */
+
+    public String left2(String str) {
+        String word = "";
+        word = str.substring(2) + str.substring(0,2);
+        return word;
+    }
 
 }
