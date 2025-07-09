4105ef1d9c16a4d25333808028d71ccbfde1558a
Madina <1madilyao@gmail.com>
2025-07-09 17:25:16 +0600

    mixStart

--- src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
@@ -235,4 +235,15 @@ public boolean startHi(String str) {
        }
        return str;
    }
+  /*  Return true if the given string begins with
+   "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+    mixStart("mix snacks") → true
+    mixStart("pix snacks") → true
+    mixStart("piz snacks") → false*/
+  public boolean mixStart(String str) {
+      if (str.contains("x")){
+          return true;
+      }
+      return false;
+  }
 }
