2ae8a650dff099570bad4759a46e7f9d6da646d5
Madina <1madilyao@gmail.com>
2025-07-09 20:04:25 +0600

    left

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -99,4 +99,17 @@ public class String1 {
       }
       return a.substring(1,a.length())+b.substring(1,b.length());
   }
+   /* Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+
+
+    left2("Hello") → "lloHe"
+    left2("java") → "vaja"
+    left2("Hi") → "Hi"
+    */
+   public String left2(String str) {
+       if (str.length() <= 2) {
+           return str;
+       }
+       return str.substring(2) + str.substring(0, 2);
+   }
 }
