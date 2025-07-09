95352dba7e405867dc9c000f4f323030ce75b5f9
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 20:12:08 +0600

    aidai/codingbat/logic1-old35

--- src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
@@ -172,4 +172,21 @@ public boolean more20(int n) {
         return false;
     }
 }
+/**
+ * Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator -- see Introduction to Mod
+ *
+ *
+ * old35(3) → true
+ * old35(10) → true
+ * old35(15) → false
+ */
+public boolean old35(int n) {
+    if (n % 3 == 0 && n % 5 != 0){
+        return true;
+    }else if(n % 3 != 0 && n % 5 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
 }
