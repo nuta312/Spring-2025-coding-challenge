fae782350ff324db26b21e3c30a564c243f2b701
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 18:45:59 +0600

    extraFront

--- src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -423,6 +423,25 @@ public class String1 {
         return aSub + bSub;
     }
 
+    /**
+     * Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+     *
+     *
+     * extraFront("Hello") → "HeHeHe"
+     * extraFront("ab") → "ababab"
+     * extraFront("H") → "HHH"
+     */
+
+    public String extraFront(String str) {
+        String front;
+        if (str.length() >= 2) {
+            front = str.substring(0, 2);
+        } else {
+            front = str;
+        }
+        return front + front + front;
+    }
+
 
 
 
