a5d552a08ed02f32062950761556a339087fb3b3
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 17:44:10 +0300

    max1020

--- src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
@@ -398,6 +398,25 @@ startOz("oxx") → "o"
         return a >= 30 && a <= 40 && b >= 30 && b <= 40 || a >= 40 && a <= 50 && b >= 40 && b <= 50;
     }
 
+    /*
+    Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
+     or return 0 if neither is in that range.
+
+    max1020(11, 19) → 19
+    max1020(19, 11) → 19
+    max1020(11, 9) → 11
+     */
+    public int max1020(int a, int b) {
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (a >= 10 && a <= 20) return a;
+        if (b >= 10 && b <= 20) return b;
+        return 0;
+    }
 }
 
 
