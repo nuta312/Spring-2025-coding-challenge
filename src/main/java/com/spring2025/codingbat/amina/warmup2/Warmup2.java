ff360250b50ad18f16fd008eddad97e20a2c7b0a
= <=>
2025-07-09 15:34:18 +0600

    amina/codingbat/warmup2/arrayCount9

--- src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/amina/warmup2/Warmup2.java
@@ -151,6 +151,23 @@ public class Warmup2 {
         return result;
     }
 
+    /**
+     Task 9 – arrayCount9
+
+     Given an array of ints, return the number of 9's in the array.
+
+     arrayCount9([1, 2, 9]) → 1
+     arrayCount9([1, 9, 9]) → 2
+     arrayCount9([1, 9, 9, 3, 9]) → 3
+     */
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == 9) count++;
+        }
+        return count;
+    }
+
 
 
 }
