335a7b1f2d275e3d00e855954a5745acdeab3cc9
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 08:26:09 +0600

    monkeyTrouble

--- src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
@@ -21,4 +21,26 @@ public class Warmup1 {
             return false;
         }
     }
+
+    /**
+     * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+     *
+     *
+     * monkeyTrouble(true, true) → true
+     * monkeyTrouble(false, false) → true
+     * monkeyTrouble(true, false) → false
+     */
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile){
+            return true;
+        }else if (aSmile){
+            return false;
+        }else if (bSmile){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
 }
