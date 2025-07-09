566c2260e41904bafeda31006861f907d0ef5110
Ulugbek Attokurov <attokurovulugbek@gmail.com>
2025-07-09 12:36:02 +0600

    mapShare

--- src/main/java/com/spring2025/codingbat/ulugbekA/map1/Map1.java
+++ src/main/java/com/spring2025/codingbat/ulugbekA/map1/Map1.java
@@ -16,6 +16,20 @@ and set the key "a" to have the value "". Basically "b" is a bully, taking the v
         return map;
     }
 
+    /*
+Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value.
+In all cases remove the key "c", leaving the rest of the map unchanged.
+
+*/
+    public Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        if (map.containsKey("c")) {
+            map.remove("c");
+        }
+        return map;
+    }
 }
 
 
