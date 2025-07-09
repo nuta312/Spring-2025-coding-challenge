8acad903cf851af5cfcb13b4e0a3b6fd0b2fcab3
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 08:43:07 +0600

    in1020

--- src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
@@ -259,5 +259,18 @@ public class Warmup1 {
         return (temp1 < 0 && temp2 > 100 || temp2 < 0 && temp1 > 100) ? true : false;
     }
 
+    /**
+     * Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+     *
+     *
+     * in1020(12, 99) → true
+     * in1020(21, 12) → true
+     * in1020(8, 99) → false
+     */
+
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20 || b >= 10 && b <= 20);
+    }
+
 
 }
