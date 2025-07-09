2b81afaab6c9646a62a0a9f7d04d064703fdf68a
= <=>
2025-07-10 00:30:28 +0600

    makes10

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -41,4 +41,11 @@ public class WarmUp1 {
 
         }
     }
+    public boolean makes10(int a, int b) {
+        if (a ==10 || b == 10 || (a + b) == 10){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
