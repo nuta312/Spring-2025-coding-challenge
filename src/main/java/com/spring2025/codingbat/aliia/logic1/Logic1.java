55ba049b46e0b0ec29dab3e50d32243f5916442d
aallymbekova <alymbekovaaliya@icloud.com>
2025-07-09 10:32:17 +0600

    answerCell

--- src/main/java/com/spring2025/codingbat/aliia/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aliia/logic1/Logic1.java
@@ -186,6 +186,16 @@ public class Logic1 {
         }
     }
 
+    /*
+    Your cell phone rings. Return true if you should answer it.
+     Normally you answer, except in the morning you only answer if it is your mom calling.
+     In all cases, if you are asleep, you do not answer.
+     */
 
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if((!isMorning && !isAsleep) || (isMom && isMorning && !isAsleep)) return true;
+
+        return false;
+    }
 
 }
