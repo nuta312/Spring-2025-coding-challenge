1df9a874b13997836e56c6342cbe730142f55aa4
= <=>
2025-07-10 00:42:15 +0600

    stringE

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -206,4 +206,13 @@ public class WarmUp1 {
             return 0;
         }
     }
+    public boolean stringE(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i ++ ) {
+            if (str.charAt(i) == 'e') {
+                count ++;
+            }
+        }
+        return count >= 1 && count <= 3;
+    }
 }
