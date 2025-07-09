be58311d24395070de04c5022d1096aa8a3e06b6
Madina <1madilyao@gmail.com>
2025-07-09 20:31:46 +0600

    Str

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -112,4 +112,152 @@ public class String1 {
        }
        return str.substring(2) + str.substring(0, 2);
    }
+  /*  Given a string, return a "rotated right 2"
+   version where the last 2 chars are moved to the start. The string length will be at least 2.
+    right2("Hello") → "loHel"
+    right2("java") → "vaja"
+    right2("Hi") → "Hi"
+     */
+  public String right2(String str) {
+      if (str.length() <= 2) {
+          return str;
+      }
+      return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
 }
+/*Given a string, return a string length 1 from
+    its front, unless front is false, in which case
+            return a string length 1 from its back. The string will be non-empty.
+            theEnd("Hello", true) → "H"
+    theEnd("Hello", false) → "o"
+    theEnd("oh", true) → "o"
+            */
+    public String theEnd(String str, boolean front) {
+        if (front) {
+            return str.substring(0, 1); // Вывод первой буквы
+        } else {
+            return str.substring(str.length() - 1); // вывод последней буквы
+        }
+    }
+  /*  Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+    middleTwo("string") → "ri"
+    middleTwo("code") → "od"
+    middleTwo("Practice") → "ct"
+   */
+  public String middleTwo(String str) {
+      int m = str.length()/2;
+      return str.substring(m-1,m+1);
+  }
+  /*  Given a string, return true if it ends in "ly".
+    endsLy("oddly") → true
+    endsLy("y") → false
+    endsLy("oddy") → false
+     */
+  public boolean endsLy(String str) {
+      if (str.endsWith("ly")){
+          return true;
+      }
+      return false;
+  }
+   /* Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+    nTwice("Hello", 2) → "Helo"
+    nTwice("Chocolate", 3) → "Choate"
+    nTwice("Chocolate", 1) → "Ce"
+    */
+   public String nTwice(String str, int n) {
+       String l=str.substring(0,n);
+       String s =str.substring(str.length()-n);
+       return l+s;
+   }
+  /*  Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+    middleThree("Candy") → "and"
+    middleThree("and") → "and"
+    middleThree("solving") → "lvi"
+   */
+  public String middleThree(String str) {
+      if (str.length()>3){
+          int m = str.length()/2;
+          return str.substring(m-1,m+2);
+      }
+      return str;
+  }
+   /* Given a string, return true if "bad"
+    appears starting at index 0 or 1 in the string,
+    such as with "badxxx" or "xbadxx" but not "xxbadxx".
+     The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+    hasBad("badxx") → true
+    hasBad("xbadxx") → true
+    hasBad("xxbadxx") → false
+     */
+   public boolean hasBad(String str) {
+       if(str.length()>=3&& str.substring(0,3).equals("bad")){
+           return true;
+       }
+       if(str.length()>=4&& str.substring(1,4).equals("bad")){
+           return true;
+       }
+       return false;
+   }
+   /* Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
+    atFirst("hello") → "he"
+    atFirst("hi") → "hi"
+    atFirst("h") → "h@"
+     */
+   public String atFirst(String str) {
+       if (str.length()>=2){
+           return str.substring(0,2);
+       } else if (str.length()==1){
+           return str+"@";
+       }else {
+           return "@@";
+       }
+   }
+   /* Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+
+    lastChars("last", "chars") → "ls"
+    lastChars("yo", "java") → "ya"
+    lastChars("hi", "") → "h@"
+
+    */
+   public String lastChars(String a, String b) {
+       char first;
+       if (a.length() > 0) {
+           first = a.charAt(0);
+       } else {
+           first = '@';
+       }
+       char last;
+       if (b.length() > 0) {
+           last = b.charAt(b.length() - 1);
+       } else {
+           last = '@';
+       }
+       return "" + first + last;
+   }
+  /*  Given a string of any length, return
+  a new string where the last 2 chars,
+   if present, are swapped, so "coding" yields "codign".
+    lastTwo("coding") → "codign"
+    lastTwo("cat") → "cta"
+    lastTwo("ab") → "ba"*/
+  public String lastTwo(String str) {
+      if (str.length() <= 2) {
+          return str;
+      }
+      String start = str.substring(0, str.length() - 2);
+      char secondLast = str.charAt(str.length() - 2);
+      char last = str.charAt(str.length() - 1);
+      return start + last +""+ secondLast;
+  }
+
+
+
+
+
+
+
+
+
+}
+
+
