e91e5a43b511291003f054b5ef243edabae2db9f
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 14:06:06 +0600

    monkeyTrouble

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -13,4 +13,17 @@ public class Warmup1 {
         }
     }
 
+    /* We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+    We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+    */
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (!aSmile && !bSmile) {
+            return true;
+        } else if ( aSmile && bSmile) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
