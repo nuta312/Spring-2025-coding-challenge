157e7a4f47e1317d36a94cd251bb069284e6fb9a
Madina <1madilyao@gmail.com>
2025-07-09 20:03:30 +0600

    nonStrart

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -87,4 +87,16 @@ public class String1 {
             return a+b+a;
         }
     }
+  /*  Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+    nonStart("Hello", "There") → "ellohere"
+    nonStart("java", "code") → "avaode"
+    nonStart("shotl", "java") → "hotlava"
+     */
+  public String nonStart(String a, String b){
+      if(a.length()>2){
+      }
+      if (b.length()>2){
+      }
+      return a.substring(1,a.length())+b.substring(1,b.length());
+  }
 }
