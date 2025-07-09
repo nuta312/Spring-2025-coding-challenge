8a4e80fd427a6c2ffd1d2bb546da3e28481076f5
Madina <1madilyao@gmail.com>
2025-07-09 16:51:29 +0600

    frontBack

--- src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
@@ -75,4 +75,9 @@ public class Warmup1 {
         StringBuilder sb = new StringBuilder(str);
         return sb.deleteCharAt(n).toString();
     }
+    public String frontBack(String str) {
+        if(str.length() < 2) return str;
+        return str.charAt(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0);
+    }
+
 }
