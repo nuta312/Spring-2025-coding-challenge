54ec168510fee1f588d9720640ec2c1bda867a4e
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 20:46:23 +0600

    noTeen

--- src/main/java/com/spring2025/codingbat/elvira/functional2/Fuctional2.java
+++ src/main/java/com/spring2025/codingbat/elvira/functional2/Fuctional2.java
@@ -12,4 +12,12 @@ public class Fuctional2 {
     public List<Integer> noNeg(List<Integer> nums) {
         return nums.stream().filter( n -> n >= 0).collect(Collectors.toList());
     }
+
+    public List<Integer> no9(List<Integer> nums) {
+        return nums.stream().filter(n -> n % 10 != 9).collect(Collectors.toList());
+    }
+
+    public List<Integer> noTeen(List<Integer> nums) {
+        return nums.stream().filter(n -> n < 13 || n > 19).collect(Collectors.toList());
+    }
 }
