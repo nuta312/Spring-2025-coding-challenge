5d6e5ed479cc5b801fea2d27d9caf9aaa3c8d710
Ulugbek Attokurov <attokurovulugbek@gmail.com>
2025-07-09 19:02:53 +0600

    mapAB

--- src/main/java/com/spring2025/codingbat/ulugbekA/map1/Map1.java
+++ src/main/java/com/spring2025/codingbat/ulugbekA/map1/Map1.java
@@ -30,6 +30,17 @@ In all cases remove the key "c", leaving the rest of the map unchanged.
         }
         return map;
     }
+
+    /*
+Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys. If both keys are present,
+append their 2 string values together and store the result under the key "ab".*/
+
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
 }
 
 
