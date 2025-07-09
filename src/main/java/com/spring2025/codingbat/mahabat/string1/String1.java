0cc0ccafc470877c92907403abac11b056539da6
mahabat <mahabatka@gmail.com>
2025-07-09 07:12:59 +0600

    master/mahabat/codingbat/string/nTwice/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -161,6 +161,17 @@ public class String1 {
         return str.endsWith("ly") ? true : false;
     }
 
+    /* Given a string and an int n, return a string made of the first and last n
+    chars from the string. The string length will be at least n.*/
+
+    public String nTwice(String str, int n) {
+
+        String a = str.substring(0,n);
+        String b = str.substring(str.length()-n);
+        return a + b;
+    }
+
+
 
 
 }
