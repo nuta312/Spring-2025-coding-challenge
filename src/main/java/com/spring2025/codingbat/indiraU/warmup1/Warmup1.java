22cb6e61ef0f6784acb1be4e935ab54c05d8a186
indira <you@example.com>
2025-07-09 15:34:01 +0600

    startHi

--- src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
@@ -238,6 +238,24 @@ front22("abc") → "ababcab"
         return front + str + front;
     }
 
+    /*
+
+Given a string, return true if the string starts with "hi" and false otherwise.
+
+
+startHi("hi there") → true
+startHi("hi") → true
+startHi("hello hi") → false
+    */
+    public boolean startHi(String str) {
+
+        if (str.length() >= 2 && str.substring(0, 2).equalsIgnoreCase("hi")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 
 
