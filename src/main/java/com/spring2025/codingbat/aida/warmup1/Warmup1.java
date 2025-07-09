6bd121450527cf82e884aced4517df36d503c56b
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 14:07:50 +0600

    sumDouble

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -26,4 +26,14 @@ public class Warmup1 {
             return false;
         }
     }
+
+    /* Given two int values, return their sum. Unless the two values are the same, then return double their sum. */
+
+    public int sumDouble(int a, int b) {
+        if (a == b) {
+            return (a + b) * 2;
+        } else {
+            return a + b;
+        }
+    }
 }
