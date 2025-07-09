8a5bdf7e487b8183123023f8b0f203a2a0fd2a7c
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:14:03 +0600

    aidai/codingbat/array1-makePi

--- src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
@@ -38,4 +38,14 @@ public class Array1 {
         }
         return false;
     }
+    /**
+     * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+     *
+     *
+     * makePi() → [3, 1, 4]
+     */
+    public int[] makePi() {
+        int nums [] ={3,1,4};
+        return nums;
+    }
 }
