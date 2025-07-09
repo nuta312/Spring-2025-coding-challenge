4b6246ce11923f0b736501922245bfba8a0301d5
mahabat <mahabatka@gmail.com>
2025-07-09 07:14:40 +0600

    master/mahabat/codingbat/string/twoChar/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -171,6 +171,16 @@ public class String1 {
         return a + b;
     }
 
+    /* Given a string and an index, return a string length 2 starting at the given index.
+     If the index is too big or too small to define a string length 2, use the first 2 chars.
+      The string length will be at least 2.*/
+
+    public String twoChar(String str, int index) {
+
+        if(index < 0 || index +1 >= str.length()) return str.substring(0,2);
+        return str.substring(index,index+2);
+    }
+
 
 
 
