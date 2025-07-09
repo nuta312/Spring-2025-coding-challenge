d5c14424be59f92736ce6c2eb9660b20d985cdb3
mahabat <mahabatka@gmail.com>
2025-07-09 07:11:03 +0600

    master/mahabat/codingbat/string/middleTwo/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -145,6 +145,14 @@ public class String1 {
         return str.substring(1,str.length()-1);
     }
 
+    /* Given a string of even length, return a string made of the middle two chars,
+    so the string "string" yields "ri". The string length will be at least 2.*/
+
+    public String middleTwo(String str) {
+
+        int mid = str.length() / 2;
+        return str.substring(mid -1,mid + 1);
+    }
 
 
 }
