6ec2a021dffb203419ea224269f9d2b785754705
indira <you@example.com>
2025-07-09 15:45:34 +0600

    endUp

--- src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
@@ -478,6 +478,24 @@ max1020(11, 9) → 11
     public boolean lastDigit(int a, int b) {
         return a % 10 == b % 10;
     }
+    /*
+
+   Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.
+
+
+   endUp("Hello") → "HeLLO"
+   endUp("hi there") → "hi thERE"
+   endUp("hi") → "HI"
+    */
+    public String endUp(String str) {
+        if (str.length() > 3) {
+            String newSTR = str.substring(0, str.length() - 3);
+            String newSTR2 = str.substring(str.length() - 3);
+            return newSTR + newSTR2.toUpperCase();
+        } else {
+            return str.toUpperCase();
+        }
+    }
 
 
 
