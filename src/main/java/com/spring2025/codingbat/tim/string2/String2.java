99c0c3c0c8f87c1a3e497090acd3dcef72f54702
= <dzhetybaev19emir@gmail.com>
2025-07-09 20:49:13 +0600

    repeatEnd fixed

--- src/main/java/com/spring2025/codingbat/tim/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/tim/string2/String2.java
@@ -99,4 +99,12 @@ public class String2 {
 
         return result;
     }
+    public String repeatEnd(String str, int n) {
+        String result = "";
+        String end = str.substring(str.length()-n);
+        for(int i=0;i<n;i++){
+            result +=end;
+        }
+        return result;
+    }
 }
