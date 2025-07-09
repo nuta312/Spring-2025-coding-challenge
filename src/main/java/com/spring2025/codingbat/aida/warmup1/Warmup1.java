3ee5a6bcd0c8ee11e15ba8f1ecaf6d77f6dce247
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 14:09:53 +0600

    diff21

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -36,4 +36,16 @@ public class Warmup1 {
             return a + b;
         }
     }
+
+    /* Given an int n, return the absolute difference between n and 21,
+    except return double the absolute difference if n is over 21.
+     */
+
+    public int diff21(int n) {
+        if (n <= 21) {
+            return 21 - n;
+        } else {
+            return (n - 21) * 2;
+        }
+    }
 }
