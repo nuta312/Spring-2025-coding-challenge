1a9b7fbf9912dd6233d38218899599bfe314e75f
Madina <1madilyao@gmail.com>
2025-07-09 23:28:10 +0600

    rotateleft

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -53,6 +53,15 @@ public class Array1 {
        }
        return nums[0]+nums[1]+nums[2];
    }
+   /* Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+
+    rotateLeft3([1, 2, 3]) → [2, 3, 1]
+    rotateLeft3([5, 11, 9]) → [11, 9, 5]
+    rotateLeft3([7, 0, 0]) → [0, 0, 7]*/
+   public int[] rotateLeft3(int[] nums) {
+       return new int [] {nums[1],nums[2],nums[0]};
+   }
 
 
 
