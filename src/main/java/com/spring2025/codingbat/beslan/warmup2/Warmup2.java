a530deb5a035cc79e290da424874604690d4b99d
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 20:07:01 +0300

    stringBits

--- src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
@@ -73,4 +73,19 @@ public class Warmup2 {
         if (i + 1 >= str.length()) return false;
         return str.substring(i + 1, i + 2).equals("x");
     }
+
+    /*
+    Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+    stringBits("Hello") → "Hlo"
+    stringBits("Hi") → "H"
+    stringBits("Heeololeo") → "Hello"
+     */
+    public String stringBits(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i += 2) {
+            result = result + str.substring(i, i + 1);
+        }
+        return result;
+    }
 }
