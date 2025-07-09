652a527a7afc54187e396b82f89159d6a23f1215
= <=>
2025-07-10 00:29:50 +0600

    parrotTrouble

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -31,4 +31,14 @@ public class WarmUp1 {
         }
         return num;
     }
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && hour < 7){
+            return true;
+        } else if(talking && hour > 20){
+            return true;
+        } else{
+            return false;
+
+        }
+    }
 }
