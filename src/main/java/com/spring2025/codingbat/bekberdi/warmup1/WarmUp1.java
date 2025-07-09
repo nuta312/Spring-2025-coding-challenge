c2cda55172fab9d502154c6f293f92282e45f448
= <=>
2025-07-10 00:37:17 +0600

    in1020

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -123,4 +123,11 @@ public class WarmUp1 {
         }
         return false;
     }
+    public boolean in1020(int a, int b) {
+        if (a >= 10 && a <= 20 || b >= 10 && b <= 20){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
