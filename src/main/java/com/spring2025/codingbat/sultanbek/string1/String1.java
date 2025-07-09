1aa207fc8a03434b70c111f4ccbf92906dff00c3
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-10 02:21:35 +0600

    CoddingBat string1 - middleThree

--- src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
@@ -235,4 +235,17 @@ public class String1 {
         }return str.substring(0,2);
     }
 
+    /**
+     Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and".
+     The string length will be at least 3.
+     middleThree("Candy") → "and"
+     middleThree("and") → "and"
+     middleThree("solving") → "lvi"
+     */
+
+    public String middleThree(String str) {
+        int len = str.length() / 2;
+        return str.substring(len-1, len+2);
+    }
+
 }
