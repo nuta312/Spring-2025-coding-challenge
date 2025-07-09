4ef3369c175fac5354aae1aa398bcb0aa06015d5
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 17:37:21 +0300

    loneTeen

--- src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
@@ -292,6 +292,21 @@ posNeg(-4, -5, true) → true
     public boolean hasTeen(int a, int b, int c) {
         return a >= 13 && a <= 19 || b >= 13 && b <= 19 || c >= 13 && c <= 19;
     }
+    /*
+    We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+    Given 2 int values, return true if one or the other is teen, but not both.
+
+    loneTeen(13, 99) → true
+    loneTeen(21, 19) → true
+    loneTeen(13, 13) → false
+     */
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
 }
 
 
