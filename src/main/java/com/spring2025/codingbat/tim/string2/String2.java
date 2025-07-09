2c61fc419e90795c97e4691a6eb75b47bd150391
= <dzhetybaev19emir@gmail.com>
2025-07-09 20:46:41 +0600

    countCode fixed

--- src/main/java/com/spring2025/codingbat/tim/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/tim/string2/String2.java
@@ -33,4 +33,12 @@ public class String2 {
         }
         return (catCount == dogCount);
     }
+    public int countCode(String str) {
+        int cc = 0;
+        for(int i =0;i<str.length()-3;i++){
+            if(str.substring(i,i+4).startsWith("co") && str.substring(i,i+4).endsWith("e"))cc++;
+
+        }
+        return cc;
+    }
 }
