2043e42954f9a4a46b9eb745bb13649276638773
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:23:57 +0600

    aidai/codingbat/string2-prefixAgain

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -135,4 +135,22 @@ public class String2 {
         }
         return res;
     }
+    /**
+     * Given a string, consider the prefix string made of the first N chars of the string. Does that prefix string appear somewhere else in the string? Assume that the string is not empty and that N is in the range 1..str.length().
+     *
+     *
+     * prefixAgain("abXYabc", 1) → true
+     * prefixAgain("abXYabc", 2) → true
+     * prefixAgain("abXYabc", 3) → false
+     */
+    public boolean prefixAgain(String str, int n) {
+        String prefix = str.substring(0, n);
+        for (int i = 1; i <= str.length() - n; i++) {
+            String sub = str.substring(i, i + n);
+            if (sub.equals(prefix)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
