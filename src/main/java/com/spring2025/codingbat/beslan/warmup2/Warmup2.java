e363754ebde0331ea8b5f1a85dc5d4891a6d52ec
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 20:08:21 +0300

    stringSplosion

--- src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup2/Warmup2.java
@@ -88,4 +88,20 @@ public class Warmup2 {
         }
         return result;
     }
+
+    /*
+    Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+    stringSplosion("Code") → "CCoCodCode"
+    stringSplosion("abc") → "aababc"
+    stringSplosion("ab") → "aab"
+     */
+    public String stringSplosion(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result = result + str.substring(0, i + 1);
+        }
+        return result;
+    }
+
 }
