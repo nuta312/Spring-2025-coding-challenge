9fce11b1fd805698fee3d04cddadeb8ff7085dac
= <=>
2025-07-10 00:26:07 +0600

    sleepIn

---
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -0,0 +1,11 @@
+package com.spring2025.codingbat.bekberdi.warmup1;
+
+public class WarmUp1 {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
