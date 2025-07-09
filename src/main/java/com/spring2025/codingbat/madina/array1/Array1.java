32f00422ffca0b927f34a9da3c9c65ce5314f7aa
Madina <1madilyao@gmail.com>
2025-07-09 23:56:38 +0600

    frontpiece

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -278,4 +278,19 @@ public class Array1 {
         int last =nums[nums.length -1];
         return Math.max(first,Math.max(middle,last));
     }
+   /* Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+
+
+            frontPiece([1, 2, 3]) → [1, 2]
+    frontPiece([1, 2]) → [1, 2]
+    frontPiece([1]) → [1]
+
+    */
+   public int[] frontPiece(int[] nums) {
+       if(nums.length <=2){
+           return nums ;
+       }else{
+           return new int[] {nums[0],nums[1]} ;
+       }
+   }
 }
