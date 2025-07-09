386c823c228e2611ad8e9a2f3e13c6328b0c047e
Madina <1madilyao@gmail.com>
2025-07-09 17:32:29 +0600

    StringA

--- src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/madina/warmup1/Warmup1.java
@@ -306,5 +306,18 @@ public boolean startHi(String str) {
            return 0;
        }
    }
-
+  /*  Return true if the given string contains between 1 and 3 'e' chars.
+    stringE("Hello") → true
+    stringE("Heelle") → true
+    stringE("Heelele") → false
+ */
+  public boolean stringE(String str) {
+      int count = 0;
+      for (int i = 0; i < str.length() ; i ++ ) {
+          if (str.charAt(i) == 'e') {
+              count += 1;
+          }
+      }
+      return count >= 1 && count <= 3;
+  }
 }
