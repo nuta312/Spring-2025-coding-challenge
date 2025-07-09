cd60d957b517962f05655d0ba737f0c85a138951
Madina <1madilyao@gmail.com>
2025-07-09 23:34:50 +0600

    makeEnds

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -102,4 +102,15 @@ public class Array1 {
           return nums[0]+nums[1];
       }
   }
+   /* Given an array of ints, return a
+    new array length 2 containing the first and
+     last elements from the original array. The original array will be length 1 or more.
+   makeEnds([1, 2, 3]) → [1, 3]
+    makeEnds([1, 2, 3, 4]) → [1, 4]
+    makeEnds([7, 4, 6, 2]) → [7, 2]*/
+   public int[] makeEnds(int[] nums) {
+       if (nums.length>=1){
+       }
+       return new int[] {nums[0],nums[nums.length-1]};
+   }
 }
