aca53b2c86b9b2de24af3071af32393a89468442
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 15:36:13 +0600

    helloName

---
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -0,0 +1,20 @@
+package com.spring2025.codingbat.elvira.string1;
+
+/*
+@author Elvira Ashyralieva
+ */
+public class String1 {
+
+    /**
+     * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+     *
+     *
+     * helloName("Bob") → "Hello Bob!"
+     * helloName("Alice") → "Hello Alice!"
+     * helloName("X") → "Hello X!"
+     */
+
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
