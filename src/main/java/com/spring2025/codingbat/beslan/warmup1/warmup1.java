fe8e49b9abf77d8368957438d3dcbf78caa8ddae
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 17:54:22 +0300

    endUp

--- src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
@@ -466,6 +466,23 @@ Given two non-negative int values, return true if they have the same last digit,
         }
         return result;
     }
+
+    /*
+
+    Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string.
+    So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+
+    everyNth("Miracle", 2) → "Mrce"
+    everyNth("abcdefg", 2) → "aceg"
+    everyNth("abcdefg", 3) → "adg"
+     */
+    public String everyNth(String str, int n) {
+        String result = "";
+        for (int i = 0; i < str.length(); i = i + n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
 }
 
 
