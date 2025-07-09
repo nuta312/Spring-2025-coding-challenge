b46136e12f978e1d98ef7deed0c7ff431467d6c2
mahabat <mahabatka@gmail.com>
2025-07-09 07:02:22 +0600

    master/mahabat/codingbat/string/withoutEnd/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -72,4 +72,16 @@ public class String1 {
         return str.substring(0,str.length() / 2);
     }
 
+    /* Given a string, return a version without the first and last char,
+    so "Hello" yields "ell". The string length will be at least 2.*/
+
+    public String withoutEnd(String str) {
+
+        String result = "";
+        if (str.length() >= 2 ){
+            result = str.substring(1,str.length()-1);
+        }
+        return result;
+    }
+
 }
