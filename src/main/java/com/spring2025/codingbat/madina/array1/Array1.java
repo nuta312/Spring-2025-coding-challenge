8fafa1ec85e542c1ca0dc894aad8c7630cd2fff4
Madina <1madilyao@gmail.com>
2025-07-09 23:32:17 +0600

    maxEnd

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -71,6 +71,21 @@ public class Array1 {
   public int[] reverse3(int[] nums) {
       return new int[] {nums[2],nums[1],nums[0]};
   }
-
-
+  /*Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+    maxEnd3([1, 2, 3]) → [3, 3, 3]
+    maxEnd3([11, 5, 9]) → [11, 11, 11]
+    maxEnd3([2, 11, 3]) → [3, 3, 3]
+    */
+  public int[] maxEnd3(int[] nums) {
+      int first = nums[0];
+      int last = nums[nums.length - 1];
+      int max = first;
+      if (last > first) {
+          max = last;
+      }
+      for (int i = 0; i < nums.length; i++) {
+          nums[i] = max;
+      }
+      return nums;
+  }
 }
