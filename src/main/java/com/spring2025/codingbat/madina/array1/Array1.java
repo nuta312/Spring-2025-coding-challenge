c19fe22deeb6bab9fe8e2c3b63add13a983541c7
Madina <1madilyao@gmail.com>
2025-07-09 23:38:07 +0600

    makelast

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -133,4 +133,16 @@ public class Array1 {
   public boolean no23(int[] nums) {
       return nums[0]!=2 && nums[1]!=2 && nums[0]!=3 && nums[1] !=3 ;
   }
+    /*Given an int array, return a new array with
+    double the length where its last element is the
+    same as the original array, and all the other elements are 0.
+     The original array will be length 1 or more. Note: by default, a new int array contains all 0's.
+    makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
+    makeLast([1, 2]) → [0, 0, 0, 2]
+    makeLast([3]) → [0, 3]*/
+    public int[] makeLast(int[] nums) {
+        int [] new_mas = new int[nums.length *2] ;
+        new_mas[new_mas.length-1] =nums[nums.length-1];
+        return new_mas;
+    }
 }
