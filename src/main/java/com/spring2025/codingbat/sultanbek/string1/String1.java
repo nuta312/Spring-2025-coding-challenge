50a0394b6434cec47c7214ec28241c06159abc44
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-10 01:11:48 +0600

    CoddingBat string1 - withoutEnd

--- src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
@@ -90,4 +90,18 @@ public class String1 {
         return str.substring(0, str.length()/2);
     }
 
+
+    /**
+     Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+     withoutEnd("Hello") → "ell"
+     withoutEnd("java") → "av"
+     withoutEnd("coding") → "odin"
+     */
+    public String withoutEnd(String str) {
+        if (str.length()>=2){
+            return str.substring(1,str.length()-1);
+        }
+        return str;
+    }
+
 }
