152d6cac3305936dfc8cb2de943c930275973ee3
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 17:14:27 +0300

    startHi

--- src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
@@ -238,6 +238,21 @@ public String front22(String str) {
     }
     return result + str + result;
 }
+/*
+
+Given a string, return true if the string starts with "hi" and false otherwise.
+
+startHi("hi there") → true
+startHi("hi") → true
+startHi("hello hi") → false
+ */
+public boolean startHi(String str) {
+    boolean result = false;
+    if (str.length() > 1 && str.substring(0,2).equals("hi")){
+        result = true;
+    }
+    return result;
+}
 }
 
 
