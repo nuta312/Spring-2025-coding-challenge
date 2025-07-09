e7a408d5f7456dc9d6556ba91c36534785bca657
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 20:20:58 +0600

    aidai/codingbat/logic1-inOrderEqual

--- src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aidai/logic1/Logic1.java
@@ -333,4 +333,19 @@ public boolean inOrder(int a, int b, int c, boolean bOk) {
         return a < b && b < c;
     }
 }
+/**
+ * Given three ints, a b c, return true if they are in strict increasing order, such as 2 5 11, or 5 6 7, but not 6 5 7 or 5 5 7. However, with the exception that if "equalOk" is true, equality is allowed, such as 5 5 7 or 5 5 5.
+ *
+ *
+ * inOrderEqual(2, 5, 11, false) → true
+ * inOrderEqual(5, 7, 6, false) → false
+ * inOrderEqual(5, 5, 7, true) → true
+ */
+public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+    if (equalOk) {
+        return a <= b && b <= c;
+    } else {
+        return a < b && b < c;
+    }
+}
 }
