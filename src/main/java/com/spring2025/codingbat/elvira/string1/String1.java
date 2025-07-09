610976f7357b984b69b3f54f3cb434997367dc6b
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 18:39:03 +0600

    lastTwo

--- src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -350,6 +350,25 @@ public class String1 {
 
     }
 
+    /**
+     * Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+     *
+     *
+     * lastTwo("coding") → "codign"
+     * lastTwo("cat") → "cta"
+     * lastTwo("ab") → "ba"
+     */
+
+    public String lastTwo(String str) {
+        int len = str.length();
+        if(len >= 2){
+            String part = str.substring(0, len -2);
+            return part + str.charAt(len - 1) + str.charAt(len - 2);
+        } else {
+            return str;
+        }
+    }
+
 
 
 
