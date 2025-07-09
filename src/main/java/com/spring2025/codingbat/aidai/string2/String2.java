b017d949bfd1d92cb93165c1aefc6343d12ef05a
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:08:19 +0600

    aidai/codingbat/string2-mixString

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -64,4 +64,25 @@ public class String2 {
     public boolean xyBalance(String str) {
         return str.lastIndexOf('y') >= str.lastIndexOf('x');
     }
+    /**
+     * Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+     *
+     *
+     * mixString("abc", "xyz") → "axbycz"
+     * mixString("Hi", "There") → "HTihere"
+     * mixString("xxxx", "There") → "xTxhxexre"
+     */
+    public String mixString(String a, String b) {
+        String res = "";
+        int max = Math.max(a.length(),b.length());
+        for (int i = 0; i < max; i++) {
+            if (i< a.length()){
+                res += a.charAt(i);
+            }
+            if (i < b.length()){
+                res += b.charAt(i);
+            }
+        }
+        return res;
+    }
 }
