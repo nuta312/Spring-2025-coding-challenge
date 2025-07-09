47feb884fce487f4a8b4df8170bf57fcc019dbbf
= <=>
2025-07-10 00:31:54 +0600

    notString

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -64,4 +64,11 @@ public class WarmUp1 {
             return false;
         }
     }
+    public String notString(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        }
+
+        return "not " + str;
+    }
 }
