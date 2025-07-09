2781b2e26f9d5eb3f6ff72864c04a66693368ae3
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 15:20:30 +0600

    backAround

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -131,4 +131,11 @@ public class Warmup1 {
         String front = str.length() < 3 ? str : str.substring(0, 3);
         return front + front + front;
     }
+
+    /* Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more. */
+
+    public String backAround(String str) {
+        String lastChar = str.substring(str.length() - 1);
+        return lastChar + str + lastChar;
+    }
 }
