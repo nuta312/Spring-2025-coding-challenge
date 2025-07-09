a6c43eb04a2d588caaff06117ace7a630959bc16
= <dzhetybaev19emir@gmail.com>
2025-07-09 21:34:57 +0600

    alarmClock fixed

--- src/main/java/com/spring2025/codingbat/tim/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/tim/logic1/Logic1.java
@@ -67,4 +67,18 @@ public class Logic1 {
             return sum;
         }
     }
+    public String alarmClock(int day, boolean vacation) {
+        if(vacation){
+            if(day == 6 || day == 0){
+                return "off";
+            }else{
+                return "10:00";
+            }}
+        if(day == 6 || day == 0){
+            return "10:00";
+        }else{
+            return "7:00";
+        }
+    }
+
 }
