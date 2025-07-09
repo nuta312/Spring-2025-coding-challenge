1258e72b2d5f3bf1b8d104958e6bf2a4100aac0c
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 15:54:58 +0600

    loneTeen

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -181,4 +181,10 @@ public class Warmup1 {
                 (b >= 13 && b <= 19) ||
                 (c >= 13 && c <= 19);
     }
+
+    // We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+
+    public boolean loneTeen(int a, int b) {
+        return (a >= 13 && a <= 19) && (b >= 13 && b <= 19) ? false : (a >= 13 && a <= 19) || (b >= 13 && b <= 19);
+    }
 }
