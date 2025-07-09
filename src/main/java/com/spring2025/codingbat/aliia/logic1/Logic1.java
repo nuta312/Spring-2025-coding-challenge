95b7e91732a5c45e2c1fa00dd312e787be84861b
aallymbekova <alymbekovaaliya@icloud.com>
2025-07-09 10:30:19 +0600

    near10

--- src/main/java/com/spring2025/codingbat/aliia/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/aliia/logic1/Logic1.java
@@ -166,4 +166,12 @@ public class Logic1 {
         return (n + 1) % 20 == 0 || (n + 2) % 20 == 0;
     }
 
+    /*Given a non-negative number "num", return true if num is within 2 of a multiple of 10.
+     Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+     */
+
+    public boolean nearTen(int num) {
+        return num % 10 <= 2 || num % 10 >= 8;
+    }
+
 }
