5055c3b665b226f85bbdf8a09ed0b9e3adef9a4a
indira <you@example.com>
2025-07-09 15:44:12 +0600

    stringE

--- src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
@@ -445,6 +445,26 @@ max1020(11, 9) → 11
         if (b >= 10 && b <= 20) return b;
         return 0;
     }
+    /*
+    Return true if the given string contains between 1 and 3 'e' chars.
+
+
+    stringE("Hello") → true
+    stringE("Heelle") → true
+    stringE("Heelele") → false
+     */
+    public boolean stringE(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == 'e') {
+                count++;
+
+            }
+        }
+
+        return (count >= 1 && count <= 3);
+    }
 
 
 
