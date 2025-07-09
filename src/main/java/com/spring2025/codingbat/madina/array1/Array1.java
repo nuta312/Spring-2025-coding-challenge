568864dc3377f39ec09cf504770afed90b40105b
Madina <1madilyao@gmail.com>
2025-07-09 23:54:05 +0600

    maxTriple

--- src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/madina/array1/Array1.java
@@ -264,4 +264,18 @@ public class Array1 {
        int half= nums.length /2 ;
        return new int[] { nums[half - 1], nums[half], nums[half + 1] };
    }
+    /*Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+
+
+    maxTriple([1, 2, 3]) → 3
+    maxTriple([1, 5, 3]) → 5
+    maxTriple([5, 2, 3]) → 5
+
+     */
+    public int maxTriple(int[] nums) {
+        int first =nums[0];
+        int middle =nums[nums.length/2];
+        int last =nums[nums.length -1];
+        return Math.max(first,Math.max(middle,last));
+    }
 }
