3da8fe971d5a0b32cb7412dbafa674fc9780e220
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:32:50 +0600

    aidai/codingbat/array1-makeMiddle

--- src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/aidai/array1/Array1.java
@@ -268,4 +268,17 @@ public class Array1 {
             return b;
         }
     }
+    /**
+     * Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
+     *
+     *
+     * makeMiddle([1, 2, 3, 4]) → [2, 3]
+     * makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
+     * makeMiddle([1, 2]) → [1, 2]
+     */
+    public int[] makeMiddle(int[] nums) {
+        int firstMiddle = nums.length / 2 - 1;
+        int middle = nums.length / 2;
+        return new int[]{nums[firstMiddle],nums[middle]};
+    }
 }
