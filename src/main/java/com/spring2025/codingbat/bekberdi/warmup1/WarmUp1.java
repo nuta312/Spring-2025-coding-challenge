45810e2c51d31bf2ffe1b304922aaca5aa314228
= <=>
2025-07-10 00:39:22 +0600

    mixStart

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -148,4 +148,11 @@ public class WarmUp1 {
             return str;
         }
     }
+    public boolean mixStart(String str) {
+        if(str.startsWith("ix", 1)){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
