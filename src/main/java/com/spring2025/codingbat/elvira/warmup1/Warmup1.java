ea7e2d21c46e868426ade6b8a1120db484fcda66
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 08:56:59 +0600

    lastDigit

--- src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/elvira/warmup1/Warmup1.java
@@ -7,15 +7,15 @@ public class Warmup1 {
 
     /**
      * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-     *
-     *
+     * <p>
+     * <p>
      * sleepIn(false, false) → true
      * sleepIn(true, false) → false
      * sleepIn(false, true) → true
      */
 
     public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation){
+        if (!weekday || vacation) {
             return true;
         } else {
             return false;
@@ -24,21 +24,21 @@ public class Warmup1 {
 
     /**
      * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
-     *
-     *
+     * <p>
+     * <p>
      * monkeyTrouble(true, true) → true
      * monkeyTrouble(false, false) → true
      * monkeyTrouble(true, false) → false
      */
 
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        if (aSmile && bSmile){
+        if (aSmile && bSmile) {
             return true;
-        }else if (aSmile){
+        } else if (aSmile) {
             return false;
-        }else if (bSmile){
+        } else if (bSmile) {
             return false;
-        }else{
+        } else {
             return true;
         }
 
@@ -46,16 +46,16 @@ public class Warmup1 {
 
     /**
      * Given two int values, return their sum. Unless the two values are the same, then return double their sum.
-     *
-     *
+     * <p>
+     * <p>
      * sumDouble(1, 2) → 3
      * sumDouble(3, 2) → 5
      * sumDouble(2, 2) → 8
      */
 
     public int sumDouble(int a, int b) {
-        if (a == b){
-            return (a+b) * 2;
+        if (a == b) {
+            return (a + b) * 2;
         } else {
             return a + b;
         }
@@ -64,16 +64,16 @@ public class Warmup1 {
 
     /**
      * Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
-     *
-     *
+     * <p>
+     * <p>
      * diff21(19) → 2
      * diff21(10) → 11
      * diff21(21) → 0
      */
 
     public int diff21(int n) {
-        if (n > 21 ){
-            return ( n - 21) *2;
+        if (n > 21) {
+            return (n - 21) * 2;
         } else {
             return 21 - n;
         }
@@ -81,45 +81,45 @@ public class Warmup1 {
 
     /**
      * We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
-     *
-     *
+     * <p>
+     * <p>
      * parrotTrouble(true, 6) → true
      * parrotTrouble(true, 7) → false
      * parrotTrouble(false, 6) → false
      */
 
     public boolean parrotTrouble(boolean talking, int hour) {
-        if (talking && ( hour < 7 || hour > 20)){
+        if (talking && (hour < 7 || hour > 20)) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
     /**
      * Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
-     *
-     *
+     * <p>
+     * <p>
      * makes10(9, 10) → true
      * makes10(9, 9) → false
      * makes10(1, 9) → true
      */
 
-    public  boolean makes10(int a, int b) {
-        return (  a +  b == 10 ||  a == 10 ||  b == 10);
+    public boolean makes10(int a, int b) {
+        return (a + b == 10 || a == 10 || b == 10);
     }
 
     /**
      * Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
-     *
-     *
+     * <p>
+     * <p>
      * nearHundred(93) → true
      * nearHundred(90) → true
      * nearHundred(89) → false
      */
 
     public boolean nearHundred(int n) {
-        if ((Math.abs(100 - n) <=10) || Math.abs(200 - n) <= 10){
+        if ((Math.abs(100 - n) <= 10) || Math.abs(200 - n) <= 10) {
             return true;
         } else {
             return false;
@@ -128,21 +128,21 @@ public class Warmup1 {
 
     /**
      * Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
-     *
-     *
+     * <p>
+     * <p>
      * posNeg(1, -1, false) → true
      * posNeg(-1, 1, false) → true
      * posNeg(-4, -5, true) → true
      */
 
     public boolean posNeg(int a, int b, boolean negative) {
-        return ( a > 0 && b < 0 && !negative ||  b > 0 && a < 0 && !negative || a < 0 && b < 0 && negative);
+        return (a > 0 && b < 0 && !negative || b > 0 && a < 0 && !negative || a < 0 && b < 0 && negative);
     }
 
     /**
      * Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
-     *
-     *
+     * <p>
+     * <p>
      * notString("candy") → "not candy"
      * notString("x") → "not x"
      * notString("not bad") → "not bad"
@@ -154,8 +154,8 @@ public class Warmup1 {
 
     /**
      * Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
-     *
-     *
+     * <p>
+     * <p>
      * missingChar("kitten", 1) → "ktten"
      * missingChar("kitten", 0) → "itten"
      * missingChar("kitten", 4) → "kittn"
@@ -168,15 +168,15 @@ public class Warmup1 {
 
     /**
      * Given a string, return a new string where the first and last chars have been exchanged.
-     *
-     *
+     * <p>
+     * <p>
      * frontBack("code") → "eodc"
      * frontBack("a") → "a"
      * frontBack("ab") → "ba"
      */
 
     public String frontBack(String str) {
-        if(str.length() < 2) return str;
+        if (str.length() < 2) return str;
         return str.charAt(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0);
     }
 
@@ -184,8 +184,8 @@ public class Warmup1 {
      * Given a string, we'll say that the front is the first 3 chars of the string.
      * If the string length is less than 3, the front is whatever is there.
      * Return a new string which is 3 copies of the front.
-     *
-     *
+     * <p>
+     * <p>
      * front3("Java") → "JavJavJav"
      * front3("Chocolate") → "ChoChoCho"
      * front3("abc") → "abcabcabc"
@@ -197,61 +197,61 @@ public class Warmup1 {
 
     /**
      * Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
-     *
-     *
+     * <p>
+     * <p>
      * backAround("cat") → "tcatt"
      * backAround("Hello") → "oHelloo"
      * backAround("a") → "aaa"
      */
 
     public String backAround(String str) {
-        String r = str.substring(str.length() -1);
-        return (str.length() >= 2) ? r+str+r : str+str+str;
+        String r = str.substring(str.length() - 1);
+        return (str.length() >= 2) ? r + str + r : str + str + str;
     }
 
     /**
      * Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
-     *
-     *
+     * <p>
+     * <p>
      * or35(3) → true
      * or35(10) → true
      * or35(8) → false
      */
 
     public boolean or35(int n) {
-        return (n % 3 == 0 || n % 5 ==0) ? true : false;
+        return (n % 3 == 0 || n % 5 == 0) ? true : false;
     }
 
     /**
      * Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
-     *
-     *
+     * <p>
+     * <p>
      * front22("kitten") → "kikittenki"
      * front22("Ha") → "HaHaHa"
      * front22("abc") → "ababcab"
      */
 
     public String front22(String str) {
-        return (str.length() >= 2)? str.substring(0,2) + str + str.substring(0,2) : str+str+str;
+        return (str.length() >= 2) ? str.substring(0, 2) + str + str.substring(0, 2) : str + str + str;
     }
 
     /**
      * Given a string, return true if the string starts with "hi" and false otherwise.
-     *
-     *
+     * <p>
+     * <p>
      * startHi("hi there") → true
      * startHi("hi") → true
      * startHi("hello hi") → false
      */
 
     public boolean startHi(String str) {
-        return (str.startsWith("hi"))? true : false;
+        return (str.startsWith("hi")) ? true : false;
     }
 
     /**
      * Given two temperatures, return true if one is less than 0 and the other is greater than 100.
-     *
-     *
+     * <p>
+     * <p>
      * icyHot(120, -1) → true
      * icyHot(-1, 120) → true
      * icyHot(2, 120) → false
@@ -263,8 +263,8 @@ public class Warmup1 {
 
     /**
      * Given 2 int values, return true if either of them is in the range 10..20 inclusive.
-     *
-     *
+     * <p>
+     * <p>
      * in1020(12, 99) → true
      * in1020(21, 12) → true
      * in1020(8, 99) → false
@@ -277,50 +277,50 @@ public class Warmup1 {
     /**
      * We'll say that a number is "teen" if it is in the range 13..19 inclusive.
      * Given 3 int values, return true if 1 or more of them are teen.
-     *
-     *
+     * <p>
+     * <p>
      * hasTeen(13, 20, 10) → true
      * hasTeen(20, 19, 10) → true
      * hasTeen(20, 10, 13) → true
      */
 
     public boolean hasTeen(int a, int b, int c) {
-        boolean a1 =( a >= 13 && a <= 19);
+        boolean a1 = (a >= 13 && a <= 19);
         boolean b2 = (b >= 13 && b <= 19);
-        boolean c3 = ( c >= 13 && c <= 19);
-        return (a1 || b2 || c3 )? true : false;
+        boolean c3 = (c >= 13 && c <= 19);
+        return (a1 || b2 || c3) ? true : false;
     }
 
     /**
      * We'll say that a number is "teen" if it is in the range 13..19 inclusive.
      * Given 2 int values, return true if one or the other is teen, but not both.
-     *
-     *
+     * <p>
+     * <p>
      * loneTeen(13, 99) → true
      * loneTeen(21, 19) → true
      * loneTeen(13, 13) → false
      */
 
-    public boolean loneTeen(int a, int b){
+    public boolean loneTeen(int a, int b) {
         boolean teen1 = (a >= 13 && a <= 19);
         boolean teen2 = (b >= 13 && b <= 19);
 
-        return ( teen1 ^ teen2);
+        return (teen1 ^ teen2);
 
     }
 
     /**
      * Given a string, if the string "del" appears starting at index 1,
      * return a string where that "del" has been deleted. Otherwise, return the string unchanged.
-     *
-     *
+     * <p>
+     * <p>
      * delDel("adelbc") → "abc"
      * delDel("adelHello") → "aHello"
      * delDel("adedbc") → "adedbc"
      */
 
     public String delDel(String str) {
-        if (str.length()>= 4 && str.substring(1, 4).equals("del")){
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
             return str.charAt(0) + str.substring(4);
         }
         return str;
@@ -329,15 +329,15 @@ public class Warmup1 {
     /**
      * Return true if the given string begins with "mix",
      * except the 'm' can be anything, so "pix", "9ix" .. all count.
-     *
-     *
+     * <p>
+     * <p>
      * mixStart("mix snacks") → true
      * mixStart("pix snacks") → true
      * mixStart("piz snacks") → false
      */
 
     public boolean mixStart(String str) {
-        if (str.length() >=3 && str.substring(1,3).equals("ix")){
+        if (str.length() >= 3 && str.substring(1, 3).equals("ix")) {
             return true;
         } else {
             return false;
@@ -347,8 +347,8 @@ public class Warmup1 {
     /**
      * Given a string, return a string made of the first 2 chars (if present),
      * however include first char only if it is 'o' and include the second only if it is 'z',
-     *
-     *
+     * <p>
+     * <p>
      * so "ozymandias" yields "oz".
      * startOz("ozymandias") → "oz"
      * startOz("bzoo") → "z"
@@ -357,10 +357,10 @@ public class Warmup1 {
 
     public String startOz(String str) {
         String s = "";
-        if(str.length() >= 1 && str.charAt(0) == 'o'){
+        if (str.length() >= 1 && str.charAt(0) == 'o') {
             s += str.charAt(0);
         }
-        if(str.length() >= 2 && str.charAt(1) == 'z'){
+        if (str.length() >= 2 && str.charAt(1) == 'z') {
             s += str.charAt(1);
         }
         return s;
@@ -368,8 +368,8 @@ public class Warmup1 {
 
     /**
      * Given three int values, a b c, return the largest.
-     *
-     *
+     * <p>
+     * <p>
      * intMax(1, 2, 3) → 3
      * intMax(1, 3, 2) → 3
      * intMax(3, 2, 1) → 3
@@ -377,10 +377,10 @@ public class Warmup1 {
 
     public int intMax(int a, int b, int c) {
 
-        if(a > b && a > c){
+        if (a > b && a > c) {
             return a;
         }
-        if(b > c && b > a){
+        if (b > c && b > a) {
             return b;
         }
         return c;
@@ -391,8 +391,8 @@ public class Warmup1 {
      * Given 2 int values, return whichever value is nearest to the value 10,
      * or return 0 in the event of a tie.
      * Note that Math.abs(n) returns the absolute value of a number.
-     *
-     *
+     * <p>
+     * <p>
      * close10(8, 13) → 8
      * close10(13, 8) → 8
      * close10(13, 7) → 0
@@ -402,10 +402,10 @@ public class Warmup1 {
         int nums = Math.abs(10 - a);
         int nums1 = Math.abs(10 - b);
 
-        if(nums > nums1){
+        if (nums > nums1) {
             return b;
         }
-        if(nums == nums1){
+        if (nums == nums1) {
             return 0;
         }
         return a;
@@ -413,21 +413,21 @@ public class Warmup1 {
 
     /**
      * Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
-     *
-     *
+     * <p>
+     * <p>
      * in3050(30, 31) → true
      * in3050(30, 41) → false
      * in3050(40, 50) → true
      */
 
     public boolean in3050(int a, int b) {
-        return ( a >= 30 && a <=40 ) && (b >= 30 && b <=40)|| ( a >= 40 && a <=50 ) && (b >= 40 && b <=50);
+        return (a >= 30 && a <= 40) && (b >= 30 && b <= 40) || (a >= 40 && a <= 50) && (b >= 40 && b <= 50);
     }
 
     /**
      * Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
-     *
-     *
+     * <p>
+     * <p>
      * max1020(11, 19) → 19
      * max1020(19, 11) → 19
      * max1020(11, 9) → 11
@@ -436,21 +436,21 @@ public class Warmup1 {
     public int max1020(int a, int b) {
         boolean numsA = (a >= 10 && a <= 20);
         boolean numsB = (b >= 10 && b <= 20);
-        if(numsA && numsB){
-            return Math.max(a,b);
-        }else if (numsA){
+        if (numsA && numsB) {
+            return Math.max(a, b);
+        } else if (numsA) {
             return a;
-        }else if (numsB){
+        } else if (numsB) {
             return b;
-        }else{
+        } else {
             return 0;
         }
     }
 
     /**
      * Return true if the given string contains between 1 and 3 'e' chars.
-     *
-     *
+     * <p>
+     * <p>
      * stringE("Hello") → true
      * stringE("Heelle") → true
      * stringE("Heelele") → false
@@ -459,7 +459,7 @@ public class Warmup1 {
     public boolean stringE(String str) {
         int count = 0;
 
-        for (int i = 0; i <= str.length() -1 ; i ++ ) {
+        for (int i = 0; i <= str.length() - 1; i++) {
             if (str.charAt(i) == 'e') {
                 count += 1;
             }
@@ -467,7 +467,18 @@ public class Warmup1 {
         return count >= 1 && count <= 3;
     }
 
+    /**
+     * Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+     *
+     *
+     * lastDigit(7, 17) → true
+     * lastDigit(6, 17) → false
+     * lastDigit(3, 113) → true
+     */
 
+    public boolean lastDigit(int a, int b) {
+        return a % 10 == b % 10;
+    }
 
 
 }
