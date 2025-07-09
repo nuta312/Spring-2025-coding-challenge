fd220984cf490b0bd00936d4cb1c5fc76bbfee4b
Beslan Nagoyev <bec.lan3@mail.ru>
2025-07-09 16:27:12 +0300

    sumDouble

--- src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/beslan/warmup1/warmup1.java
@@ -35,4 +35,22 @@ sleepIn(false, true) → true
         }
         return false;
     }
+
+    /*
+    3. sumDouble
+    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+    sumDouble(1, 2) → 3
+    sumDouble(3, 2) → 5
+    sumDouble(2, 2) → 8
+     */
+
+    public int sumDouble(int a, int b) {
+        int sum = a + b;
+        if(a == b){
+            sum = sum * 2;
+        }
+        return sum;
+    }
+
 }
