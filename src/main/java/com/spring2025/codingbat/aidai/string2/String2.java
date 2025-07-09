2ece5eca17f5baa94142de6d07b2948e7c5b71e9
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:20:18 +0600

    aidai/codingbat/string2-repeatFront

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -102,4 +102,19 @@ public class String2 {
         }
         return result;
     }
+    /**
+     * Given a string and an int n, return a string made of the first n characters of the string, followed by the first n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string, inclusive (i.e. n >= 0 and n <= str.length()).
+     *
+     *
+     * repeatFront("Chocolate", 4) → "ChocChoChC"
+     * repeatFront("Chocolate", 3) → "ChoChC"
+     * repeatFront("Ice Cream", 2) → "IcI"
+     */
+    public String repeatFront(String str, int n) {
+        String res = "";
+        for(int i =n;i >=1; i --){
+            res += str.substring(0,i);
+        }
+        return res;
+    }
 }
