ed95471b933cffb9203a0642be6b40d1e8c0ad16
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 13:59:37 +0600

    sleepIn

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -1,4 +1,16 @@
 package com.spring2025.codingbat.aida.warmup1;
 
 public class Warmup1 {
+
+    /* The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+    We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in. */
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if ( !weekday || vacation) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
 }
