4e89f741144f82293521a4237a779171d375f696
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 15:50:42 +0600

    firstHalf

--- src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -87,4 +87,20 @@ public class String1 {
             return str;
         }
     }
+
+    /**
+     * Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+     *
+     *
+     * firstHalf("WooHoo") → "Woo"
+     * firstHalf("HelloThere") → "Hello"
+     * firstHalf("abcdef") → "abc"
+     */
+
+    public String firstHalf(String str) {
+        if(str.length() % 2 == 0){
+            return str.substring(0,str.length() / 2);
+        }
+        return str;
+    }
 }
