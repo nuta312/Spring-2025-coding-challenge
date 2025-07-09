f9a40bb41f77c179ed010a0ba9097a4073ab5c4f
mahabat <mahabatka@gmail.com>
2025-07-09 07:01:20 +0600

    master/mahabat/codingbat/string/firstHalf/

--- src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/mahabat/string1/String1.java
@@ -66,4 +66,10 @@ public class String1 {
         return word;
     }
 
+    // Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+    public String firstHalf(String str) {
+        return str.substring(0,str.length() / 2);
+    }
+
 }
