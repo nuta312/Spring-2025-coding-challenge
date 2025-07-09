efe686bc7d588917a776858d3a67a3995f5496c8
= <=>
2025-07-10 00:41:51 +0600

    max1020

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -191,4 +191,19 @@ public class WarmUp1 {
             return false;
         }
     }
+    public int max1020(int a, int b) {
+        if (a >= 10 && a <=20 && b >= 10 && b <= 20){
+            if(a > b){
+                return a;
+            }else {
+                return b;
+            }
+        }else if(a >= 10 && a <=20){
+            return a;
+        }else if(b >= 10 && b <= 20){
+            return b;
+        }else {
+            return 0;
+        }
+    }
 }
