046267631d494629f7c46281d5c308dacf186323
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:53:39 +0600

    aidai/codingbat/map1-mapAB

--- src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
+++ src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
@@ -38,4 +38,22 @@ public class Map1 {
         map.remove("c");
         return map;
     }
+    /**
+     * Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys. If both keys are present, append their 2 string values together and store the result under the key "ab".
+     *
+     *
+     * mapAB({"a": "Hi", "b": "There"}) → {"a": "Hi", "ab": "HiThere", "b": "There"}
+     * mapAB({"a": "Hi"}) → {"a": "Hi"}
+     * mapAB({"b": "There"}) → {"b": "There"}
+     */
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a" )){
+            if (map.containsKey("b")){
+                String value1 = map.get("a");
+                String value2 = map.get("b");
+                map.put("ab",value1 + value2);
+            }
+        }
+        return map;
+    }
 }
