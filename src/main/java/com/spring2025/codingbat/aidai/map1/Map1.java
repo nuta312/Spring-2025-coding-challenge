83594db66f3eacfa33bfdb6842db6a9456185054
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 23:56:27 +0600

    aidai/codingbat/map1-topping3

--- src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
+++ src/main/java/com/spring2025/codingbat/aidai/map1/Map1.java
@@ -89,4 +89,23 @@ public class Map1 {
         }
         return map;
     }
+    /**
+     * Given a map of food keys and topping values, modify and return the map as follows: if the key "potato" has a value, set that as the value for the key "fries". If the key "salad" has a value, set that as the value for the key "spinach".
+     *
+     *
+     * topping3({"potato": "ketchup"}) → {"potato": "ketchup", "fries": "ketchup"}
+     * topping3({"potato": "butter"}) → {"potato": "butter", "fries": "butter"}
+     * topping3({"salad": "oil", "potato": "ketchup"}) → {"spinach": "oil", "salad": "oil", "potato": "ketchup", "fries": "ketchup"}
+     */
+    public Map<String, String> topping3(Map<String, String> map) {
+        if (map.containsKey("potato")){
+            String value = map.get("potato");
+            map.put("fries",value);
+        }
+        if (map.containsKey("salad")){
+            String value = map.get("salad");
+            map.put("spinach",value);
+        }
+        return map;
+    }
 }
