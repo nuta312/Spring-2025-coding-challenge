24e996511efa6d081fdaa123b8463930d357b949
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 18:23:28 +0600

    theEnd

--- src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -173,6 +173,22 @@ public class String1 {
         return str.substring(str.length() - 2) + str.substring(0,str.length() - 2);
     }
 
+    /**
+     * Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+     *
+     *
+     * theEnd("Hello", true) → "H"
+     * theEnd("Hello", false) → "o"
+     * theEnd("oh", true) → "o"
+     */
+
+    public String theEnd(String str, boolean front) {
+        if(front){
+            return str.substring(0,1);
+        }
+        return str.substring(str.length() -1);
+    }
+
 
 
 
