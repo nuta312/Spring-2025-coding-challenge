321688712c3e57633c8a19f13d6fb93c014debc8
= <=>
2025-07-10 00:28:05 +0600

    sumDouble

--- src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/bekberdi/warmup1/WarmUp1.java
@@ -17,4 +17,11 @@ public class WarmUp1 {
             return true;
         }
     }
+    public int sumDouble(int a, int b) {
+        if(a == b){
+            return (a + b) * 2;
+        }else {
+            return a + b;
+        }
+    }
 }
