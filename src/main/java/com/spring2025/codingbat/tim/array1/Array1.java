e4f024f0bc3c0eb231dca4d4ad4297678fd01aa7
= <dzhetybaev19emir@gmail.com>
2025-07-09 21:02:29 +0600

    sameFirstLast fixed

--- src/main/java/com/spring2025/codingbat/tim/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/tim/array1/Array1.java
@@ -14,5 +14,11 @@ public class Array1 {
         }
         return false;
     }
+    public boolean sameFirstLast(int[] nums) {
+        if(nums.length>=1){
+            return nums[0] == nums[nums.length-1] ;
+        }
+        return false;
+    }
 
 }
