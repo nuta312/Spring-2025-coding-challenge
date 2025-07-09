299b968170feca67c2fec431fe82f8384f8b64b9
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-09 14:41:07 +0600

    notString

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -89,4 +89,16 @@ public class Warmup1 {
             return (a < 0 && b > 0) || (a > 0 && b < 0);
         }
     }
+
+    /* Given a string, return a new string where "not " has been added to the front. However,
+    if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+     */
+
+    public String notString(String str) {
+        if(str.length() >= 3 && str.substring(0,3).equals("not")){
+            return str;
+        } else {
+            return "not " + str;
+        }
+    }
 }
