7c0a97fa182725ce9f7a8b831416dc4a07a474a1
elviraashyralieva <elviraashyralieva7@gmail.com>
2025-07-09 18:36:57 +0600

    atFirst

--- src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/elvira/string1/String1.java
@@ -296,6 +296,26 @@ public class String1 {
         }
     }
 
+    /**
+     * Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
+     *
+     *
+     * atFirst("hello") → "he"
+     * atFirst("hi") → "hi"
+     * atFirst("h") → "h@"
+     */
+
+    public String atFirst(String str) {
+        if (str.length() >= 2){
+            return str.substring(0,2);
+        } else if (str.length() == 1 ){
+            return str + ("@");
+
+        } else{
+            return "@@";
+        }
+    }
+
 
 
 
