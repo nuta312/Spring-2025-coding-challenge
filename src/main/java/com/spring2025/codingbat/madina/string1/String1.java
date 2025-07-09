d7bce09c3368732cb1fd28d1c6d2d9743ee03fea
Madina <1madilyao@gmail.com>
2025-07-09 19:49:25 +0600

    makeRags

--- src/main/java/com/spring2025/codingbat/madina/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/madina/string1/String1.java
@@ -4,7 +4,7 @@ public class String1 {
     public static void main(String[] args) {
     }
 
-  /*  Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+  /**  Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
     helloName("Bob") → "Hello Bob!"
     helloName("Alice") → "Hello Alice!"
     helloName("X") → "Hello X!"
@@ -13,7 +13,7 @@ public class String1 {
       String hello = "Hello " + name + "!";
       return hello;
   }
-    /*Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+    /** Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
     makeAbba("Hi", "Bye") → "HiByeByeHi"
     makeAbba("Yo", "Alice") → "YoAliceAliceYo"
     makeAbba("What", "Up") → "WhatUpUpWhat"
@@ -23,6 +23,16 @@ public class String1 {
         String s2=b+a;
         return s1+s2;
     }
+    /**The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+    makeTags("i", "Yay") → "<i>Yay</i>"
+    makeTags("i", "Hello") → "<i>Hello</i>"
+    makeTags("cite", "Yay") → "<cite>Yay</cite>"
+     */
+    public String makeTags(String tag, String word) {
+        String t="<"+tag+ ">"+word+"</"+tag+">";
+        return t;
+    }
+
 
 
 }
