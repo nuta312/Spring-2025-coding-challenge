13f7fed77c6ddb0a5986efddb4c1b598c510d57e
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-09 19:27:37 +0600

    aidai/codingbat/string2-sameStarChar

--- src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/aidai/string2/String2.java
@@ -191,4 +191,24 @@ public class String2 {
         }
         return str.substring(firstIndex + bread.length(), lastIndex);
     }
+    /**
+     * Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
+     *
+     *
+     * sameStarChar("xy*yzz") → true
+     * sameStarChar("xy*zzz") → false
+     * sameStarChar("*xa*az") → true
+     */
+    public boolean sameStarChar(String str) {
+        for (int i = 1; i < str.length()-1; i++) {
+            if(str.charAt(i) == '*'){
+                char before = str.charAt(i - 1);
+                char after = str.charAt(i + 1);
+                if (before != after) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
