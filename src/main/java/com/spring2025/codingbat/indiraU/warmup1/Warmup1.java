f8cbb281380066888dadb27b21efcfb49b6945e5
indira <you@example.com>
2025-07-09 15:40:14 +0600

    intMax

--- src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indiraU/warmup1/Warmup1.java
@@ -361,6 +361,30 @@ startOz("oxx") → "o"
 
         return result;
     }
+    /*
+
+ Given three int values, a b c, return the largest.
+
+
+ intMax(1, 2, 3) → 3
+ intMax(1, 3, 2) → 3
+ intMax(3, 2, 1) → 3
+     */
+    public int intMax(int a, int b, int c) {
+        int max = 0;
+        if (a > b) {
+            max = a;
+        } else {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+
+        }
+
+        return max;
+    }
+
 
 
 
