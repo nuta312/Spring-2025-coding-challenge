fa56a47ecee3e2d521c5c2387bcc4a3c4043cfbf
Sultanbek <sultanbek.asakeev@gmail.com>
2025-07-10 02:19:04 +0600

    CoddingBat string1 - endsLy

--- src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/sultanbek/string1/String1.java
@@ -197,4 +197,15 @@ public class String1 {
         return str.substring(mid-1, mid+1);
     }
 
+    /**
+     Given a string, return true if it ends in "ly".
+     endsLy("oddly") → true
+     endsLy("y") → false
+     endsLy("oddy") → false
+     */
+
+    public boolean endsLy(String str) {
+        return str.endsWith("ly");
+    }
+
 }
