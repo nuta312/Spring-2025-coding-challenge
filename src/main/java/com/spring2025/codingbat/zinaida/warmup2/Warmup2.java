3135ccaa06f0d118abdce7edd610a14dbe1efdb2
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 08:07:31 +0600

    zinaida/codingbat/warmup2-doubleX

--- src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
@@ -66,4 +66,26 @@ countXX("xxxx") → 3
         return count_xx;
     }
 
+  /*
+Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+doubleX("axxbb") → true
+doubleX("axaxax") → false
+doubleX("xxxxx") → true
+  */
+
+    boolean doubleX(String str) {
+
+        boolean result = false;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'x') {
+                if (str.charAt(i + 1) == 'x') {
+                    result = true;
+                }
+                break;
+            }
+        }
+        return result;
+
+    }
 }
