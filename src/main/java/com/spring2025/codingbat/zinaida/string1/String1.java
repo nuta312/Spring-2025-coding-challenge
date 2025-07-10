979654939fb5cf16356193ce14b52b35e4a4ee40
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 09:09:09 +0600

    zinaida/codingbat/string1-helloName

---
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -0,0 +1,22 @@
+package com.spring2025.codingbat.zinaida.string1;
+
+/*
+@author Zinaida
+ */
+public class String1 {
+
+ /*
+ Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+  */
+
+    public String helloName(String name) {
+
+        return "Hello " + name + "!";
+
+    }
+
+}
