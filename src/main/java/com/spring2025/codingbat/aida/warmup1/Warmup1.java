cb506338c01177ad40dd99c2bb71e94ca9b837b2
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 11:51:17 +0600

    endUp

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -299,6 +299,20 @@ public class Warmup1 {
         return (a % 10) == (b % 10);
     }
 
+    // Given a string, return a new string where the last 3 chars are now in upper case.
+    // If the string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.
+
+    public String endUp(String str) {
+        if (str.length() < 3) {
+            return str.toUpperCase();
+        } else {
+            int cut = str.length() - 3;
+            String front = str.substring(0, cut);
+            String end = str.substring(cut).toUpperCase();
+            return front + end;
+        }
+    }
+
 
 
 }
