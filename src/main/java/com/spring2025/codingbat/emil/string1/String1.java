83a2cd3d38e6773aaff4caad7e5c7f00f2342bed
Emil <emkerimov@mail.ru>
2025-07-11 02:16:04 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -96,4 +96,16 @@ makeOutWord("[[]]", "word") → "[[word]]"
         int middle = str.length() / 2;
         return str.substring(0, middle);
     }
+
+    /*
+    Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+    withoutEnd("Hello") → "ell"
+    withoutEnd("java") → "av"
+    withoutEnd("coding") → "odin"
+     */
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
 }
