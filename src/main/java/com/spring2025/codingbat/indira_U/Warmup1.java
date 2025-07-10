cd3a6f070d52a93b275a940d9e0e5401bb3498e6
indira <you@example.com>
2025-07-10 13:33:07 +0600

    posNeg

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
@@ -110,4 +110,16 @@ parrotTrouble(false, 6) → false
             return false;
         }
     }
+      /*
+
+ Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+
+ posNeg(1, -1, false) → true
+ posNeg(-1, 1, false) → true
+ posNeg(-4, -5, true) → true
+     */
+    public boolean posNeg(int a, int b, boolean negative) {
+        return ((a > 0 && b < 0 && !negative) || (b > 0 && a < 0 && !negative) || (a < 0 && b < 0 && negative)) ? true : false;
+    }
 }
