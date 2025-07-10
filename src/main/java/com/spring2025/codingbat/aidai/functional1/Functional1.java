61aa33a5fb731a47ca0d14a0fb451160d13fd2be
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 16:54:10 +0600

    aidai/codingbat/functional1-moreY

--- src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
@@ -51,4 +51,15 @@ public class Functional1 {
     public List<String> copies3(List<String> strings) {
         return strings.stream().map(s -> s+ s+s).collect(Collectors.toList());
     }
+    /**
+     * Given a list of strings, return a list where each string has "y" added at its start and end.
+     *
+     *
+     * moreY(["a", "b", "c"]) → ["yay", "yby", "ycy"]
+     * moreY(["hello", "there"]) → ["yhelloy", "ytherey"]
+     * moreY(["yay"]) → ["yyayy"]
+     */
+    public List<String> moreY(List<String> strings) {
+        return strings.stream().map(s -> "y" + s + "y").collect(Collectors.toList());
+    }
 }
