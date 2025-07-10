9e03b2e8059cc2e50978e152a6735185ca9fe7d0
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 08:13:32 +0600

    zinaida/codingbat/warmup2-last2

--- src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
@@ -122,4 +122,33 @@ stringSplosion("ab") → "aab"
         }
         return result;
     }
+
+ /*
+Given a string, return the count of the number of times that a substring length 2 appears in the string and also as
+the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+last2("hixxhi") → 1
+last2("xaxxaxaxx") → 1
+last2("axxxaaxx") → 2
+  */
+
+    public int last2(String str) {
+
+        if (str.length() < 2) {
+            return 0;
+        }
+
+        int count = 0;
+        String last = str.substring((str.length() - 2));
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            String sub = str.substring(i, i + 2);
+
+            if (sub.contains(last)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
