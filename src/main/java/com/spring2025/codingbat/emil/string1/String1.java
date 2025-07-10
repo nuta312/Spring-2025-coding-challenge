6cf68bd5d69c794a278318a461fab5dd69442597
Emil <emkerimov@mail.ru>
2025-07-11 00:38:53 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -4,8 +4,21 @@ package com.spring2025.codingbat.emil.string1;
     */
 
 public class String1 {
-    public static void main(String[] args) {
 
+        /*
+        Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+    helloName("Bob") → "Hello Bob!"
+    helloName("Alice") → "Hello Alice!"
+    helloName("X") → "Hello X!"
+         */
+
+        public String helloName(String name){
+            String hello = "Hello ";
+            char symbol = '!';
+            String result = hello.concat(name) + symbol;
+            return result;
 
     }
+
 }
