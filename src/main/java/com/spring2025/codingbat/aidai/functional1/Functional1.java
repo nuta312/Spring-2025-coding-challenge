6b93173afe11bb997737258a3a5c3334d1cddc3d
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 16:57:15 +0600

    aidai/codingbat/functional1-rightDigit

--- src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
@@ -73,4 +73,15 @@ public class Functional1 {
     public List<Integer> math1(List<Integer> nums) {
         return nums.stream().map(n -> (n+1) * 10).collect(Collectors.toList());
     }
+    /**
+     * Given a list of non-negative integers, return an integer list of the rightmost digits. (Note: use %)
+     *
+     *
+     * rightDigit([1, 22, 93]) → [1, 2, 3]
+     * rightDigit([16, 8, 886, 8, 1]) → [6, 8, 6, 8, 1]
+     * rightDigit([10, 0]) → [0, 0]
+     */
+    public List<Integer> rightDigit(List<Integer> nums) {
+        return nums.stream().map(n -> n % 10).collect(Collectors.toList());
+    }
 }
