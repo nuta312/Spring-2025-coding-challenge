4270e89c95e29a915f93d0f2352d0b5908b76d88
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 17:12:02 +0600

    aidai/codingbat/functional2-noLong

--- src/main/java/com/spring2025/codingbat/aidai/functional2/Functional2.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional2/Functional2.java
@@ -51,4 +51,15 @@ public class Functional2 {
     public List<String> noZ(List<String> strings) {
         return strings.stream().filter(s -> !s.contains("z")).collect(Collectors.toList());
     }
+    /**
+     * Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+     *
+     *
+     * noLong(["this", "not", "too", "long"]) → ["not", "too"]
+     * noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+     * noLong(["cccc", "cccc", "cccc"]) → []
+     */
+    public List<String> noLong(List<String> strings) {
+        return strings.stream().filter(n -> n.length() <= 3).collect(Collectors.toList());
+    }
 }
