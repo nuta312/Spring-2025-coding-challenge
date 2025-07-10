aec6e6df8cebf9cbc3c82a0aeef2e1c7f32a2ae0
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:10:54 +0600

    withoutEnd

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -66,4 +66,10 @@ public class String1 {
         String result = str.substring(0, str.length() / 2);
         return result;
     }
+
+    // Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
 }
