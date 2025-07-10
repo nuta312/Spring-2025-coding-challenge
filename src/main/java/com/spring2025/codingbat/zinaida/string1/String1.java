99db5a228bbbabeca755c369c796da50ffe3ebc3
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 10:15:41 +0600

    zinaida/codingbat/string1-firstHalf

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -99,4 +99,19 @@ firstTwo("ab") → "ab"
         }
 
     }
+
+ /*
+
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+firstHalf("WooHoo") → "Woo"
+firstHalf("HelloThere") → "Hello"
+firstHalf("abcdef") → "abc"
+  */
+
+    public String firstHalf(String str) {
+
+        return str.substring(0, str.length() / 2);
+
+    }
 }
