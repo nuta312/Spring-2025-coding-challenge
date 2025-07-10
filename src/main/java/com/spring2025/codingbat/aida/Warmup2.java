e01cb813dbface8616578ee42c5e795322ddf5f1
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 14:13:12 +0600

    stringMatch

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -127,5 +127,24 @@ public class Warmup2 {
         return false;
     }
 
+    /* Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
+    So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+     */
+
+    public int stringMatch(String a, String b) {
+        int len = Math.min(a.length(), b.length());
+        int count = 0;
+
+        for (int i = 0; i < len - 1; i++) {
+            String aSub = a.substring(i, i + 2);
+            String bSub = b.substring(i, i + 2);
+            if (aSub.equals(bSub)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 
 }
