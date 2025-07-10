6633a13b4378bd6bb3cd151ebbc32f019666d951
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:16:03 +0600

    withoutEnd2

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -113,4 +113,13 @@ public class String1 {
             return str.substring(str.length() - 1);
         }
     }
+
+    // Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
+
+    public String withouEnd2(String str) {
+        if (str.length() <= 2) {
+            return "";
+        }
+        return str.substring(1, str.length() - 1);
+    }
 }
