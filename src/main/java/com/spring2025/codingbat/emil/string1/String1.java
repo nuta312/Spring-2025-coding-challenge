fb441a11b741a24767e547af376517ee1a262cb0
Emil <emkerimov@mail.ru>
2025-07-11 02:21:08 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -175,4 +175,21 @@ makeOutWord("[[]]", "word") → "[[word]]"
         }
         return str;
     }
+    /*
+    Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+    theEnd("Hello", true) → "H"
+    theEnd("Hello", false) → "o"
+    theEnd("oh", true) → "o"
+     */
+    public String theEnd(String str, boolean front) {
+        if (str.length() != 0) {
+            if (front){
+                return String.valueOf(str.charAt(0));
+            } else{
+                return String.valueOf(str.charAt(str.length() - 1));
+            }
+        }
+        return str;
+    }
 }
