cf0654df11326b5df7ad809be73e8f75ca3a6b76
indira <you@example.com>
2025-07-10 16:08:19 +0600

    stringSplosion

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
@@ -85,5 +85,21 @@ stringBits("Heeololeo") → "Hello"
         }
         return str2;
     }
+/*
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+
+stringSplosion("Code") → "CCoCodCode"
+stringSplosion("abc") → "aababc"
+stringSplosion("ab") → "aab"
+ */
+public String stringSplosion(String str) {
+    String result = "";
+    for (int i = 1; i <= str.length(); i++) {
+        result += str.substring(0,i);
+
+    }
+    return result;
+}
 
 }
