fcf4a2d9ed6ca95d489cd19a78fbebd41a41c325
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 11:23:32 +0600

    intMax

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -225,4 +225,21 @@ public class Warmup1 {
 
         return result;
     }
+
+    // Given three int values, a b c, return the largest.
+
+    public int intMax(int a, int b, int c) {
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
+
+
 }
