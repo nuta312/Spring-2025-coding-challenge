00f424d3dcdbe04f3b1313125434d296d1e37f53
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 13:43:22 +0600

    frontTimes

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -16,5 +16,15 @@ public class Warmup2 {
         return result.toString();
     }
 
+    // Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+    public String frontTimes(String str, int n) {
+        String front = str.length() < 3 ? str : str.substring(0, 3);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(front);
+        }
+        return result.toString();
+    }
 
 }
