742ac5bf875f0599005b0310d40747154228082f
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 14:01:09 +0600

    arrayCount9

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -92,6 +92,18 @@ public class Warmup2 {
         return count;
     }
 
+    // Given an array of ints, return the number of 9's in the array.
+
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == 9) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 
 }
