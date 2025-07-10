8a22f94d3ff62e290dea25aa72ec28c704292bc9
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 16:53:24 +0600

    aidai/codingbat/functional1-copies3

--- src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
@@ -40,4 +40,15 @@ public class Functional1 {
     public List<String> addStar(List<String> strings) {
         return strings.stream().map(s -> s.concat("*")).collect(Collectors.toList());
     }
+    /**
+     * Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+     *
+     *
+     * copies3(["a", "bb", "ccc"]) → ["aaa", "bbbbbb", "ccccccccc"]
+     * copies3(["24", "a", ""]) → ["242424", "aaa", ""]
+     * copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
+     */
+    public List<String> copies3(List<String> strings) {
+        return strings.stream().map(s -> s+ s+s).collect(Collectors.toList());
+    }
 }
