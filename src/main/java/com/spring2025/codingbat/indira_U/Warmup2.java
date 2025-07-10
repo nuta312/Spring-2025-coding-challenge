dd50caebebc9de4f2d9b5235ed66140c7010d509
indira <you@example.com>
2025-07-10 16:03:44 +0600

    stringTimes

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
@@ -3,5 +3,19 @@ package com.spring2025.codingbat.indira_U;
 public class Warmup2 {
     /**
      * @author Indira
+     *
+    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+    stringTimes("Hi", 2) → "HiHi"
+    stringTimes("Hi", 3) → "HiHiHi"
+    stringTimes("Hi", 1) → "Hi"
      */
+    public String stringTimes(String str, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n ; i++) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
 }
