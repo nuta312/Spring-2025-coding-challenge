797e3677124b2e9900df5c63485e80a054d3b36a
Emil <emkerimov@mail.ru>
2025-07-11 02:15:06 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -85,4 +85,15 @@ makeOutWord("[[]]", "word") → "[[word]]"
         return str;
     }
 
+    /*
+    Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+    firstHalf("WooHoo") → "Woo"
+    firstHalf("HelloThere") → "Hello"
+    firstHalf("abcdef") → "abc"
+     */
+    public String firstHalf(String str) {
+        int middle = str.length() / 2;
+        return str.substring(0, middle);
+    }
 }
