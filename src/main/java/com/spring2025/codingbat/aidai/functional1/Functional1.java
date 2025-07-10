65de86073c363a8c5b10de3aa624513d733cb1e8
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 16:55:45 +0600

    aidai/codingbat/functional1-math1

--- src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
@@ -62,4 +62,15 @@ public class Functional1 {
     public List<String> moreY(List<String> strings) {
         return strings.stream().map(s -> "y" + s + "y").collect(Collectors.toList());
     }
+    /**
+     * Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+     *
+     *
+     * math1([1, 2, 3]) → [20, 30, 40]
+     * math1([6, 8, 6, 8, 1]) → [70, 90, 70, 90, 20]
+     * math1([10]) → [110]
+     */
+    public List<Integer> math1(List<Integer> nums) {
+        return nums.stream().map(n -> (n+1) * 10).collect(Collectors.toList());
+    }
 }
