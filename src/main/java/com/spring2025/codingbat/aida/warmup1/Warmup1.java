ca6da9d681ca500cdfc649497173c385fd4a58fa
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 11:29:35 +0600

    close10

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -241,5 +241,21 @@ public class Warmup1 {
         return max;
     }
 
+    // Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie.
+    // Note that Math.abs(n) returns the absolute value of a number.
+
+    public int close10(int a, int b) {
+        int diffA = Math.abs(10 - a);
+        int diffB = Math.abs(10 - b);
+
+        if (diffA < diffB) {
+            return a;
+        } else if (diffB < diffA) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
+
 
 }
