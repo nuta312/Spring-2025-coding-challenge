1b1d56b081434ec53f02000c5fbee4c0c20fc9ee
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:36:07 +0600

    withoutX

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -325,5 +325,21 @@ public class String1 {
         return "";
     }
 
+    // Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return the string unchanged.
+
+    public String withoutX(String str) {
+        if (str.length() == 0) {
+            return str;
+        }
+        if (str.startsWith("x")) {
+            str = str.substring(1);
+        }
+        if (str.endsWith("x") && str.length() > 0) {
+            str = str.substring(0, str.length() - 1);
+        }
+
+        return str;
+    }
+
 
 }
