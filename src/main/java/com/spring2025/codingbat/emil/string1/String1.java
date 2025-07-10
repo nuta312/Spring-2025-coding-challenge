4f9cd469b6743fa90cb95ef80004fc0a49c7cd30
Emil <emkerimov@mail.ru>
2025-07-11 02:24:28 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -207,4 +207,29 @@ makeOutWord("[[]]", "word") → "[[word]]"
         }
         return str;
     }
+    /*
+    Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+
+    middleTwo("string") → "ri"
+    middleTwo("code") → "od"
+    middleTwo("Practice") → "ct"
+     */
+    public String middleTwo(String str) {
+        int mid = str.length() / 2;
+        return str.substring(mid - 1, mid + 1);
+    }
+    /*
+    Given a string, return true if it ends in "ly".
+
+    endsLy("oddly") → true
+    endsLy("y") → false
+    endsLy("oddy") → false
+     */
+    public boolean endsLy(String str) {
+        if (str.endsWith("ly")){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
