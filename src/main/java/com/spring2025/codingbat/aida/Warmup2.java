4abb25435062a653eaedd2bed7a227064a77710d
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 13:51:37 +0600

    stringBits

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -50,5 +50,15 @@ public class Warmup2 {
         return str.charAt(index + 1) == 'x';
     }
 
+    // Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+    public String stringBits(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i += 2) {
+            result.append(str.charAt(i));
+        }
+        return result.toString();
+    }
+
 
 }
