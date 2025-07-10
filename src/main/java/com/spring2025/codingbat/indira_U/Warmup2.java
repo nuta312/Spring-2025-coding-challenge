2ccb54822eb8a68fcd9198d590eb1a05e9bdd00e
indira <you@example.com>
2025-07-10 16:07:32 +0600

    stringBits

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
@@ -68,4 +68,22 @@ doubleX("xxxxx") → true
         }
         return false;
     }
+    /*
+
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+
+stringBits("Hello") → "Hlo"
+stringBits("Hi") → "H"
+stringBits("Heeololeo") → "Hello"
+     */
+    public String stringBits(String str) {
+        String str2 = "";
+        for (int i = 0; i < str.length() ; i+=2) {
+            str2 += str.charAt(i);
+
+        }
+        return str2;
+    }
+
 }
