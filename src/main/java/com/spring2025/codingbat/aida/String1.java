bb8cf6b11540f42c8cf93fe443588fed68b05a3a
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:24:42 +0600

    atFirst

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -181,5 +181,17 @@ public class String1 {
         return false;
     }
 
+    // Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
+
+    public String atFirst(String str) {
+        if (str.length() >= 2){
+            return str.substring(0,2);
+        } else if (str.length() == 1) {
+            return str + "@";
+        } else {
+            return "@@";
+        }
+    }
+
 
 }
