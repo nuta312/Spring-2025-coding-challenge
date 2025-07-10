bf1e82ece279c040ebfdae630c4ccf67f38f78d3
indira <you@example.com>
2025-07-10 15:06:18 +0600

    stringX

--- src/main/java/com/spring2025/codingbat/indira_U/warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/warmup2.java
@@ -164,4 +164,24 @@ array123([1, 1, 2, 1, 2, 3]) → true
        }
        return count;
    }
+
+    /*Given a string, return a version where all the "x" have been removed. Except an "x" at the very start or end should not be removed.
+
+    stringX("xxHxix") → "xHix"
+    stringX("abxxxcd") → "abcd"
+    stringX("xabxxxcdx") → "xabcdx"*/
+    public String stringX(String str) {
+
+        if (str.length() <= 2) {
+            return str;
+        }
+        String result = str.substring(0, 1);
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) != 'x') {
+                result += str.charAt(i);
+            }
+        }
+        result += str.charAt(str.length() - 1);
+        return result;
+    }
 }
