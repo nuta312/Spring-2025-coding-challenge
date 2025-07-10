571a9f0f835dc7729c9330ce0c120aed9c659013
Emil <emkerimov@mail.ru>
2025-07-11 02:20:28 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -157,4 +157,22 @@ makeOutWord("[[]]", "word") → "[[word]]"
         }
         return str;
     }
+    /*
+    Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
+
+    right2("Hello") → "loHel"
+    right2("java") → "vaja"
+    right2("Hi") → "Hi"
+     */
+    public String right2(String str) {
+        int length = str.length();
+        String lastTwoChars = str.substring(length - 2);
+        String fromTheStart = str.substring(0, length -2);
+        String result = "";
+        if (str.length() >= 2){
+            result = lastTwoChars.concat(fromTheStart);
+            return result;
+        }
+        return str;
+    }
 }
