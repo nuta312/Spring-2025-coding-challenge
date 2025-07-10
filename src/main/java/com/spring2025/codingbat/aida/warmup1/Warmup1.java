49bcbcefcbc1083639ea0d902ee5519f6881ecc1
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 12:01:50 +0600

    everyNth

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -313,6 +313,20 @@ public class Warmup1 {
         }
     }
 
+    /* Given a non-empty string and an int N, return the string made starting with char 0, and then every
+    Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+     */
+
+    public String everyNth(String str, int n) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i += n) {
+            result.append(str.charAt(i));
+        }
+
+        return result.toString();
+    }
+
 
 
 }
