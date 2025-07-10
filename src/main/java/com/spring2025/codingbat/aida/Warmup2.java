ac4981300ff2ac79251d543fe9f4a2951f75f36a
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 14:34:01 +0600

    stringYak

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -177,4 +177,20 @@ public class Warmup2 {
         return result.toString();
     }
 
+    // Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+    public String stringYak(String str) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < str.length()) {
+            if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+                i += 3;
+            } else {
+                result.append(str.charAt(i));
+                i++;
+            }
+        }
+
+        return result.toString();
+    }
 }
