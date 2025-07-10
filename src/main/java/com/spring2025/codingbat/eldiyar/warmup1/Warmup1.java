7d642c38389e098891bf2ebf3ee3319b55d75d13
= <=>
2025-07-10 22:44:39 +0600

    eldiyar/warmup1

--- src/main/java/com/spring2025/codingbat/eldiyar/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/eldiyar/warmup1/Warmup1.java
@@ -2,41 +2,41 @@ package com.spring2025.codingbat.eldiyar.warmup1;
 
 public class Warmup1 {
     public static void main(String[] args) {
-        /**
+        /*
          * В этом классе все решения задач из CodingBat: Warmup-1
          * @author Eldiyar
          */
     }
 
-    /**
-     * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
-     * We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-     * sleepIn(false, false) → true
-     * sleepIn(true, false) → false
-     * sleepIn(false, true) → true
+    /*
+     The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+     We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+      sleepIn(false, false) → true
+      sleepIn(true, false) → false
+      sleepIn(false, true) → true
      */
 
     public boolean sleepIn(boolean weekday, boolean vacation) {
         return !weekday || vacation;
     }
 
-    /**
-     * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
-     * We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
-     * monkeyTrouble(true, true) → true
-     * monkeyTrouble(false, false) → true
-     * monkeyTrouble(true, false) → false
+    /*
+     We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+     We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+      monkeyTrouble(true, true) → true
+      monkeyTrouble(false, false) → true
+      monkeyTrouble(true, false) → false
      */
 
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
         return aSmile == bSmile;
     }
 
-    /**
-     * Given two int values, return their sum. Unless the two values are the same, then return double their sum.
-     * sumDouble(1, 2) → 3
-     * sumDouble(3, 2) → 5
-     * sumDouble(2, 2) → 8
+    /*
+     Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+      sumDouble(1, 2) → 3
+      sumDouble(3, 2) → 5
+      sumDouble(2, 2) → 8
      */
 
     public int sumDouble(int a, int b) {
@@ -47,11 +47,12 @@ public class Warmup1 {
         return sum;
     }
 
-    /**
-     * Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
-     * diff21(19) → 2
-     * diff21(10) → 11
-     * diff21(21) → 0
+    /*
+    Given an int n, return the absolute difference between n and 21, except return
+     double the absolute difference if n is over 21.
+     diff21(19) → 2
+     diff21(10) → 11
+     diff21(21) → 0
      */
     public int diff21(int n) {
         if (n <= 21) {
@@ -61,13 +62,13 @@ public class Warmup1 {
         }
     }
 
-    /**
-     * We have a loud talking parrot. The "hour" parameter is the current hour time in the
-     * range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20.
-     * Return true if we are in trouble.
-     * parrotTrouble(true, 6) → true
-     * parrotTrouble(true, 7) → false
-     * parrotTrouble(false, 6) → false
+    /*
+    We have a loud talking parrot. The "hour" parameter is the current hour time in the
+    range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+    Return true if we are in trouble.
+     parrotTrouble(true, 6) → true
+     parrotTrouble(true, 7) → false
+     parrotTrouble(false, 6) → false
      */
 
     public boolean parrotTrouble(boolean talking, int hour) {
