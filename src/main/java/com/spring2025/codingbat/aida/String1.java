6a5c1b260808435d5d5c1cdd3540d32f65fcf7f9
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:25:31 +0600

    lastChars

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -193,5 +193,13 @@ public class String1 {
         }
     }
 
+    // Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+    public String lastChars(String a, String b) {
+        char firstChar = a.length() > 0 ? a.charAt(0) : '@';
+        char lastChar = b.length() > 0 ? b.charAt(b.length() - 1) : '@';
+        return "" + firstChar + lastChar;
+    }
+
 
 }
