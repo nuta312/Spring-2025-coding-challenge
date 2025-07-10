7e154c8a5861670801f710b47721dbcd75f9c11d
Emil <emkerimov@mail.ru>
2025-07-11 02:14:20 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -69,4 +69,20 @@ makeOutWord("[[]]", "word") → "[[word]]"
         String last2 = str.substring(str.length() - 2);
         return last2 + last2 + last2;
     }
+
+    /*
+    Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
+
+    firstTwo("Hello") → "He"
+    firstTwo("abcdefg") → "ab"
+    firstTwo("ab") → "ab"
+     */
+    public String firstTwo(String str) {
+        if (str.length() > 2) {
+            String str1 = str.substring(0, 2);
+            return str1;
+        }
+        return str;
+    }
+
 }
