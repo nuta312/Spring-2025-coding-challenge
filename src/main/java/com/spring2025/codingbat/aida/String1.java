4b56e2b92a8f929ad56b9de3b74ceabda49a835f
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:35:30 +0600

    startWorld

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -307,5 +307,23 @@ public class String1 {
         return result;
     }
 
+    /* Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string, except its first char does not need to match exactly.
+    On a match, return the front of the string, or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
+     */
+
+    public String startWord(String str, String word) {
+        if (str.length() < word.length()) {
+            return "";
+        }
+        String strSub = str.substring(1, word.length());
+        String wordSub = word.substring(1);
+
+        if (strSub.equals(wordSub)) {
+            return str.substring(0, word.length());
+        }
+
+        return "";
+    }
+
 
 }
