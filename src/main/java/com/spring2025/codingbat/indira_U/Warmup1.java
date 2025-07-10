1192235a4dc0f027b946f3358f46fe11f6d70f0a
indira <you@example.com>
2025-07-10 13:37:44 +0600

    backAround

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
@@ -192,4 +192,18 @@ front3("abc") → "abcabcabc"
         return front + front + front;
 
     }
+
+    /*
+
+ Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+
+
+ backAround("cat") → "tcatt"
+ backAround("Hello") → "oHelloo"
+ backAround("a") → "aaa"
+     */
+    public String backAround(String str) {
+        String str1 = str.substring(str.length() - 1);
+        return str1 + str + str1;
+    }
 }
