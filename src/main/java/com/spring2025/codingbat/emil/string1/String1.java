0ea7fa060bcb8a991cf2a168ba0e3fdfc241f949
Emil <emkerimov@mail.ru>
2025-07-11 00:41:26 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -46,4 +46,14 @@ public class String1 {
         String symbol3 = "</";
         return symbol1.concat(tag).concat(symbol2).concat(word).concat(symbol3).concat(tag).concat(symbol2);
     }
+    /*
+    Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+
+makeOutWord("<<>>", "Yay") → "<<Yay>>"
+makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+makeOutWord("[[]]", "word") → "[[word]]"
+     */
+    public String makeOutWord(String out, String word) {
+        return out.substring(0, 2).concat(word).concat(out.substring(2,4));
+    }
 }
