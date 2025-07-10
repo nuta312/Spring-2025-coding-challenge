73203c88385d8942ccb9623039d0f9ea2fef9e42
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:06:15 +0600

    makeTags

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -21,4 +21,13 @@ public class String1 {
     public String makeAbba(String a, String b) {
         return a + b + b + a;
     }
+
+    /*The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i"
+    tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+     */
+
+    public String makeTags(String tag, String word) {
+
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
 }
