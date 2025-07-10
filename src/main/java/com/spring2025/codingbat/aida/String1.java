5716add443286266152ce976dcea478172ef776b
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:33:08 +0600

    extraFront

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -258,5 +258,17 @@ public class String1 {
         return aTrimmed + bTrimmed;
     }
 
+    // Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+
+    public String extraFront(String str) {
+        String front;
+        if (str.length() >= 2) {
+            front = str.substring(0, 2);
+        } else {
+            front = str;
+        }
+        return front + front + front;
+    }
+
 
 }
