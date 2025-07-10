23fe4960f7f3b926877e14711ea892b3c5882a77
Emil <emkerimov@mail.ru>
2025-07-11 02:29:59 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -309,4 +309,26 @@ hasBad("xxbadxx") → false
             return "@@";
         }
     }
+    /*
+    Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+    lastChars("last", "chars") → "ls"
+    lastChars("yo", "java") → "ya"
+    lastChars("hi", "") → "h@"
+     */
+    public String lastChars(String a, String b) {
+        if (a.length() >=1 && b.length() >= 2) {
+            return a.substring(0,1).concat(b.substring(b.length()-1));
+        }
+        else if (a.length() >=2 && b.length() == 0){
+            return a.substring(0,1) + "@";
+        }
+        else if (a.length() ==0 && b.length() >= 2){
+            return "@" + b.substring(b.length()-1);
+        }
+        else {
+            return "@@";
+        }
+    }
+
 }
