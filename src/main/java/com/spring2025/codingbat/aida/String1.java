ba89605ffbe3df06f235e114a01063c7d5dfb1b2
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:18:16 +0600

    nTwice

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -139,4 +139,12 @@ public class String1 {
             return false;
         }
     }
+
+    // Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+
+    public String nTwice(String str, int n) {
+        String firstPart = str.substring(0, n);
+        String lastPart = str.substring(str.length() - n);
+        return firstPart + lastPart;
+    }
 }
