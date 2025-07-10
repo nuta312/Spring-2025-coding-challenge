80d6599ed0c39da78f09ab704a737d8aa274dde3
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 16:58:04 +0600

    aidai/codingbat/functional1-lower

--- src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
@@ -84,4 +84,15 @@ public class Functional1 {
     public List<Integer> rightDigit(List<Integer> nums) {
         return nums.stream().map(n -> n % 10).collect(Collectors.toList());
     }
+    /**
+     * Given a list of strings, return a list where each string is converted to lower case (Note: String toLowerCase() method).
+     *
+     *
+     * lower(["Hello", "Hi"]) → ["hello", "hi"]
+     * lower(["AAA", "BBB", "ccc"]) → ["aaa", "bbb", "ccc"]
+     * lower(["KitteN", "ChocolaTE"]) → ["kitten", "chocolate"]
+     */
+    public List<String> lower(List<String> strings) {
+        return strings.stream().map(s -> s.toLowerCase()).collect(Collectors.toList());
+    }
 }
