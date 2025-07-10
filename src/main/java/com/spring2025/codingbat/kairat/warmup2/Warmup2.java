0a4d5b395604833ba6ac7bbf36346234ac2d23c1
qairatman <qairatman@gmail.com>
2025-07-10 18:22:40 +0600

    frontTimes

--- src/main/java/com/spring2025/codingbat/kairat/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/kairat/warmup2/Warmup2.java
@@ -21,5 +21,32 @@ public class Warmup2 {
         return str2;
     }
 
+//    Given a string and a non-negative int n,
+//    we'll say that the front of the string is the
+//    first 3 chars, or whatever is there if the string
+//    is less than length 3. Return n copies of the front;
+//
+//
+//    frontTimes("Chocolate", 2) → "ChoCho"
+//    frontTimes("Chocolate", 3) → "ChoChoCho"
+//    frontTimes("Abc", 3) → "AbcAbcAbc"
+
+    public String frontTimes(String str, int n) {
+        if(str.length()<=3){
+            String str2 = "";
+            for(int i = 0;i<n;i++){
+                str2+=str;
+            }
+            return str2;
+        }else {
+            String str1 = str.substring(0,3);
+            String str2 = "";
+            for(int i = 0;i<n;i++){
+                str2+=str1;
+            }
+            return str2;
+        }
+    }
+
 
 }
