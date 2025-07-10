06bd407db3fd1da821b250dd301dac8eb30fbaad
Завира Чыныбаева <zavirachynybaeva@MacBook-Air-Zavira.local>
2025-07-10 23:19:23 +0600

    missingchar

--- src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
@@ -162,4 +162,20 @@ notString("not bad") → "not bad"
         return "not " + str;
     }
 
+    /* Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+    The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+missingChar("kitten", 1) → "ktten"
+missingChar("kitten", 0) → "itten"
+missingChar("kitten", 4) → "kittn"
+
+     */
+
+    public String missingChar(String str, int n) {
+
+        String first = str.substring(0, n);
+        String last = str.substring(n + 1, str.length());
+
+        return first + last;
+    }
 }
