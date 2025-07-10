a57ce917f66ea6662fbcf1136da3e1ae97a77c4c
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 08:24:07 +0600

    zinaida/codingbat/warmup2-stringYak

--- src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
@@ -279,4 +279,28 @@ altPairs("CodingHorror") → "Congrr"
         return result;
     }
 
+ /*
+ Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed,
+ but the "a" can be any char. The "yak" strings will not overlap.
+
+stringYak("yakpak") → "pak"
+stringYak("pakyak") → "pak"
+stringYak("yak123ya") → "123ya"
+  */
+
+    public String stringYak(String str) {
+
+        String result = "";
+        int i = 0;
+        while (i < str.length()) {
+
+            if (i <= str.length() - 3 && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+                i += 3;
+            } else {
+                result += str.charAt(i);
+                i++;
+            }
+        }
+        return result;
+    }
 }
