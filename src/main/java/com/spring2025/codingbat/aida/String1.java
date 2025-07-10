135b15359afa58014f5dbda27b9fd2eb88b0c5a5
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:15:10 +0600

    theEnd

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -103,4 +103,14 @@ public class String1 {
         int len = str.length();
         return str.substring(len - 2) + str.substring(0, len - 2);
     }
+
+    // Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+    public String theEnd(String str, boolean front) {
+        if (front) {
+            return str.substring(0, 1);
+        } else {
+            return str.substring(str.length() - 1);
+        }
+    }
 }
