b105a54f6d7e9157e1bf626ecfc884ffbff67ec9
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 14:31:38 +0600

    altPairs

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -160,4 +160,21 @@ public class Warmup2 {
         return first + middle + last;
     }
 
+    // Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+
+    public String altPairs(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i += 4) {
+            // Append char at i
+            result.append(str.charAt(i));
+            // Append char at i+1 if it exists
+            if (i + 1 < str.length()) {
+                result.append(str.charAt(i + 1));
+            }
+        }
+
+        return result.toString();
+    }
+
 }
