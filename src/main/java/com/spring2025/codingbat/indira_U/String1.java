515e75dde2121f3b262e406cfcd9c61588e2c24a
indira <you@example.com>
2025-07-10 16:35:17 +0600

    firstTwo

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -67,5 +67,20 @@ public String makeOutWord(String out, String word) {
                 +str.substring(str.length()-2)
                 +str.substring(str.length()-2);
     }
+/*
+
+Given a string, return the string made of its first two chars, so the String "Hello"
+ yields "He". If the string is shorter than length 2, return whatever there is, so "X"
+ yields "X", and the empty string "" yields the empty string "". Note that str.length()
+ returns the length of a string.
+ */
+public String firstTwo(String str) {
+    if (str.length() > 2) {
+        return str.substring(0,2);
+    }
+    return str;
+
+}
+
 
 }
