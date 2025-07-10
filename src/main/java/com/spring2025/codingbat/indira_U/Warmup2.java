fe4160e36ee200e276fc036b423638704573d859
indira <you@example.com>
2025-07-10 16:06:40 +0600

    doubleX

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
@@ -51,4 +51,21 @@ frontTimes("Abc", 3) → "AbcAbcAbc"
         }
         return coun;
     }
+    /*
+
+Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+
+doubleX("axxbb") → true
+doubleX("axaxax") → false
+doubleX("xxxxx") → true
+     */
+    boolean doubleX(String str) {
+        for (int i = 0; i < str.length() -1 ; i++) {
+            if (str.charAt(i) == 'x'){
+                return str.charAt(i + 1) == 'x';
+            }
+        }
+        return false;
+    }
 }
