f94bdfb5e3e243657d46cd8d8e8faff87c05f589
Завира Чыныбаева <zavirachynybaeva@MacBook-Air-Zavira.local>
2025-07-10 23:24:05 +0600

    starthi

--- src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
@@ -272,4 +272,20 @@ front22("abc") → "ababcab"
         return front + str + front;
     }
 
+    /* Given a string, return true if the string starts with "hi" and false otherwise.
+
+startHi("hi there") → true
+startHi("hi") → true
+startHi("hello hi") → false
+
+     */
+
+    public boolean startHi(String str) {
+
+        if (str.startsWith("hi")) {
+            return true;
+        }
+        return false;
+    }
+
 }
