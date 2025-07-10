b222ccc0b30c366367216d86e4d7a1192caf518b
Emil <emkerimov@mail.ru>
2025-07-11 02:25:48 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -245,4 +245,19 @@ makeOutWord("[[]]", "word") → "[[word]]"
 
         return str1.concat(str2);
     }
+    /*
+    Given a string and an index, return a string length 2 starting at the given index. If the index is too big or too small to define a string length 2, use the first 2 chars. The string length will be at least 2.
+
+    twoChar("java", 0) → "ja"
+    twoChar("java", 2) → "va"
+    twoChar("java", 3) → "ja"
+
+     */
+    public String twoChar(String str, int index) {
+        if (index >= 0 && index + 1 < str.length()) {
+            return str.substring(index, index + 2);
+        } else {
+            return str.substring(0, 2);
+        }
+    }
 }
