951057ee8622ceff3c63494442be1dc241e17632
Emil <emkerimov@mail.ru>
2025-07-11 02:25:08 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -232,4 +232,17 @@ makeOutWord("[[]]", "word") → "[[word]]"
             return false;
         }
     }
+    /*
+        Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+
+    nTwice("Hello", 2) → "Helo"
+    nTwice("Chocolate", 3) → "Choate"
+    nTwice("Chocolate", 1) → "Ce"
+     */
+    public String nTwice(String str, int n) {
+        String str1 =str.substring(0, n);
+        String str2 =str.substring(str.length() - n);
+
+        return str1.concat(str2);
+    }
 }
