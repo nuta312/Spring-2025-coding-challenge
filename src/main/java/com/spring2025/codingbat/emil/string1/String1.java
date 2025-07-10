7854c0a887ad6dcd5c56dd8c84e436e6126e8316
Emil <emkerimov@mail.ru>
2025-07-11 02:17:59 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -107,5 +107,20 @@ makeOutWord("[[]]", "word") → "[[word]]"
     public String withoutEnd(String str) {
         return str.substring(1, str.length() - 1);
     }
+    /*
+    Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
 
+    comboString("Hello", "hi") → "hiHellohi"
+    comboString("hi", "Hello") → "hiHellohi"
+    comboString("aaa", "b") → "baaab"
+     */
+    public String comboString(String a, String b) {
+        String combo = "false";
+        if (a.length() > b.length()){
+            return b+a+b;
+        } else if (a.length() < b.length()) {
+            return a+b+a;
+        }
+        return combo;
+    }
 }
