9b43f43ce4037de4e24e552f1c714bcab6e3dd91
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 13:35:18 +0600

    stringTimes

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -5,4 +5,16 @@ public class Warmup2 {
     /**
      * @Bershka
      */
+
+    // Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+    public String stringTimes(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(str);
+        }
+        return result.toString();
+    }
+
+
 }
