0a0dac98bd0510789345c3f4b9b79c879888b3ca
indira <you@example.com>
2025-07-10 15:00:54 +0600

    last2

--- src/main/java/com/spring2025/codingbat/indira_U/warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/warmup2.java
@@ -80,4 +80,26 @@ public String stringBits(String str) {
        }
        return result;
    }
+
+    /*Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+    last2("hixxhi") → 1
+    last2("xaxxaxaxx") → 1
+    last2("axxxaaxx") → 2*/
+    public int last2(String str) {
+        int count = 0;
+
+        if (str.length() > 2)  {
+
+            String last = str.substring(str.length() - 2);
+            for (int i = 0; i < str.length() - 2; i++) {
+                String sub = str.substring(i, i + 2);
+                if (sub.equals(last)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
