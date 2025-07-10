2d3ac9f026ac28f6ef415ec09999b7174557bd4e
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:34:40 +0600

    deFront

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -289,5 +289,23 @@ public class String1 {
         return str;
     }
 
+    // Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+
+    public String deFront(String str) {
+        String result = "";
+        if (str.length() >= 1 && str.charAt(0) == 'a') {
+            result += "a";
+        }
+        if (str.length() >= 2 && str.charAt(1) == 'b') {
+            result += "b";
+        }
+
+        if (str.length() > 2) {
+            result += str.substring(2);
+        }
+
+        return result;
+    }
+
 
 }
