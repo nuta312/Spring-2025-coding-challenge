4f61380c2e725bcbb0f3cfc6473afecee3e769b6
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 13:59:29 +0600

    last2

--- src/main/java/com/spring2025/codingbat/aida/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aida/Warmup2.java
@@ -70,5 +70,28 @@ public class Warmup2 {
         return result.toString();
     }
 
+    /* Given a string, return the count of the number of times that a substring length
+    2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+     */
+
+    public int last2(String str) {
+        if (str.length() < 2) {
+            return 0;
+        }
+
+        String last2 = str.substring(str.length() - 2);
+        int count = 0;
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            String sub = str.substring(i, i + 2);
+            if (sub.equals(last2)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
 
 }
