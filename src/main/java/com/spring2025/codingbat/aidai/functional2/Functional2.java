c4b66d95eb3d770c6fcafa7d8db0a198ce16e763
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 17:08:47 +0600

    aidai/codingbat/functional2-noTeen

--- src/main/java/com/spring2025/codingbat/aidai/functional2/Functional2.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional2/Functional2.java
@@ -29,4 +29,15 @@ public class Functional2 {
     public List<Integer> no9(List<Integer> nums) {
         return nums.stream().filter(n-> n % 10 != 9).collect(Collectors.toList());
     }
+    /**
+     * Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive.
+     *
+     *
+     * noTeen([12, 13, 19, 20]) → [12, 20]
+     * noTeen([1, 14, 1]) → [1, 1]
+     * noTeen([15]) → []
+     */
+    public List<Integer> noTeen(List<Integer> nums) {
+        return nums.stream().filter(n -> n < 13 || n > 19).collect(Collectors.toList());
+    }
 }
