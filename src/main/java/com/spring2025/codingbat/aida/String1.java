efa757b17777b6a2ae6b170f0c81a52e261fd51a
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:08:09 +0600

    extraEnd

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -38,4 +38,12 @@ public class String1 {
     public String makeOutWord(String out, String word) {
         return out.substring(0,2).concat(word).concat(out.substring(2,4));
     }
+
+    // Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+
+    public String extraEnd(String str) {
+
+        String last2 = str.substring(str.length() - 2);
+        return last2 + last2 + last2;
+    }
 }
