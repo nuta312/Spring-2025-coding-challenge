e7487a7ce54213cfad5de60cacc3857ae9fedcb2
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:23:43 +0600

    hasBad

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -166,4 +166,20 @@ public class String1 {
         int mid = str.length() / 2;
         return str.substring(mid - 1, mid + 2);
     }
+
+    /* Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with "badxxx" or "xbadxx" but not "xxbadxx".
+    The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+     */
+
+    public boolean hasBad(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("bad")) {
+            return true;
+        }
+        if (str.length() >= 4 && str.substring(1, 4).equals("bad")) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
