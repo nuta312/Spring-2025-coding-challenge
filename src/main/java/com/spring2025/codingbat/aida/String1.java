6ef9eef7fae7d1894c41e845d83847a7820fd7f1
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:12:37 +0600

    nonStart

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -84,4 +84,10 @@ public class String1 {
             return a + b + a;
         }
     }
+
+    // Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+    public String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
 }
