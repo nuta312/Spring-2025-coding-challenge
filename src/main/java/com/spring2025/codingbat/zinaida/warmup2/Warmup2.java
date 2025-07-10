c8679b0206a8ba0bb6f0c03ed9dcd76098914ddd
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 07:58:42 +0600

    zinaida/codingbat/warmup2-stringTimes

---
+++ src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
@@ -0,0 +1,26 @@
+package com.spring2025.codingbat.zinaida.warmup2;
+
+/*
+@author Zinaida
+ */
+public class Warmup2 {
+
+/*
+Given a string and a non-negative int n, return a larger string that is n
+copies of the original string.
+
+stringTimes("Hi", 2) → "HiHi"
+stringTimes("Hi", 3) → "HiHiHi"
+stringTimes("Hi", 1) → "Hi"
+ */
+
+    public String stringTimes(String str, int n) {
+
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += str;
+        }
+        return result;
+
+    }
+}
