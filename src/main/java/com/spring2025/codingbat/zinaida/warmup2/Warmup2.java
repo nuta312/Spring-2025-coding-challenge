673f5fc06b690c899fba87f6b54c4ebb5f6f2776
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 08:18:30 +0600

    zinaida/codingbat/warmup2-stringMatch

--- src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
@@ -209,4 +209,28 @@ array123([1, 1, 2, 1, 2, 3]) → true
         return false;
     }
 
+ /*
+ Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
+ So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in
+ both strings.
+
+stringMatch("xxcaazz", "xxbaaz") → 3
+stringMatch("abc", "abc") → 2
+stringMatch("abc", "axc") → 0
+  */
+
+    public int stringMatch(String a, String b) {
+
+        int count = 0;
+
+        for (int i = 0; i < Math.min(a.length(), b.length()) - 1; i++) {
+            String subA = a.substring(i, i + 2);
+            String subB = b.substring(i, i + 2);
+            if (subA.equals(subB)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
