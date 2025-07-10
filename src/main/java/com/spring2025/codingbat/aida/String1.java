fe5ae0da5858639766a0f46ead53c930d53c0244
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:04:31 +0600

    helloName

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -5,4 +5,14 @@ public class String1 {
     /**
      * @Bershka
      */
+
+    // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+    public String helloName(String name) {
+
+        String hello = "Hello ";
+        char symbol = '!';
+        String result = hello.concat(name) + symbol;
+        return result;
+    }
 }
