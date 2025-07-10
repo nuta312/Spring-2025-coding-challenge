b5e05698709f10e583452231bb9abbc49e5e3e38
Завира Чыныбаева <zavirachynybaeva@MacBook-Air-Zavira.local>
2025-07-10 23:22:35 +0600

    or35

--- src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
@@ -234,4 +234,20 @@ backAround("a") → "aaa"
         return last + str + last;
     }
 
+    /* Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+    Use the % "mod" operator -- see Introduction to Mod
+
+or35(3) → true
+or35(10) → true
+or35(8) → false
+
+     */
+
+    public boolean or35(int n) {
+
+        if (n % 3 == 0 || n % 5 == 0) {
+            return true;
+        }
+        return false;
+    }
 }
