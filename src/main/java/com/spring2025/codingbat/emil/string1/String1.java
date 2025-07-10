c06399ab2fd822d8f32489e4c40a57205a7e7e6a
Emil <emkerimov@mail.ru>
2025-07-11 02:27:02 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -260,4 +260,16 @@ makeOutWord("[[]]", "word") → "[[word]]"
             return str.substring(0, 2);
         }
     }
+    /*
+    Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+
+    middleThree("Candy") → "and"
+    middleThree("and") → "and"
+    middleThree("solving") → "lvi"
+     */
+    public String middleThree(String str) {
+        int mid = str.length() / 2;
+        return str.substring(mid - 1, mid + 2);
+
+    }
 }
