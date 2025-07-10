cbb2482ecb025de254845d7394245b81cf49083b
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:31:43 +0600

    frontAgain

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -236,5 +236,16 @@ public class String1 {
         }
     }
 
+    // Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+
+    public boolean frontAgain(String str) {
+        if (str.length() < 2) {
+            return false;
+        }
+        String front = str.substring(0, 2);
+        String end = str.substring(str.length() - 2);
+        return front.equals(end);
+    }
+
 
 }
