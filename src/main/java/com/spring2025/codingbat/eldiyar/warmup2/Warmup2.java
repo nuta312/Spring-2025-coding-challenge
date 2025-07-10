ce31ac6e7f664389a413fbe4eb4b2f0fb9d80957
= <=>
2025-07-10 22:47:59 +0600

    eldiyar/warmup1

--- src/main/java/com/spring2025/codingbat/eldiyar/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/eldiyar/warmup2/Warmup2.java
@@ -2,17 +2,17 @@ package com.spring2025.codingbat.eldiyar.warmup2;
 
 public class Warmup2 {
     public static void main(String[] args) {
-        /**
-         * В этом классе все решения задач из CodingBat: Warmup-2
-         * @author Eldiyar
+        /*
+          В этом классе все решения задач из CodingBat: Warmup-2
+          @author Eldiyar
          */
     }
 
-    /**
-     * Given a string and a non-negative int n, return a larger string that is n copies of the original string.
-     * stringTimes("Hi", 2) → "HiHi"
-     * stringTimes("Hi", 3) → "HiHiHi"
-     * stringTimes("Hi", 1) → "Hi"
+    /*
+      Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+      stringTimes("Hi", 2) → "HiHi"
+      stringTimes("Hi", 3) → "HiHiHi"
+      stringTimes("Hi", 1) → "Hi"
      */
 
     public String stringTimes(String str, int n) {
@@ -23,13 +23,13 @@ public class Warmup2 {
         return result.toString();
     }
 
-    /**
-     * Given a string and a non-negative int n, we'll say that the front of the string
-     * is the first 3 chars, or whatever is there if the string is less than length 3.
-     * Return n copies of the front;
-     * frontTimes("Chocolate", 2) → "ChoCho"
-     * frontTimes("Chocolate", 3) → "ChoChoCho"
-     * frontTimes("Abc", 3) → "AbcAbcAbc"
+    /*
+      Given a string and a non-negative int n, we'll say that the front of the string
+      is the first 3 chars, or whatever is there if the string is less than length 3.
+      Return n copies of the front;
+      frontTimes("Chocolate", 2) → "ChoCho"
+      frontTimes("Chocolate", 3) → "ChoChoCho"
+      frontTimes("Abc", 3) → "AbcAbcAbc"
      */
 
     public String frontTimes(String str, int n) {
@@ -45,11 +45,11 @@ public class Warmup2 {
         return result.toString();
     }
 
-    /**
-     * Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
-     * countXX("abcxx") → 1
-     * countXX("xxx") → 2
-     * countXX("xxxx") → 3
+    /*
+      Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+      countXX("abcxx") → 1
+      countXX("xxx") → 2
+      countXX("xxxx") → 3
      */
 
     int countXX(String str) {
@@ -62,11 +62,11 @@ public class Warmup2 {
         return count;
     }
 
-    /**
-     * Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
-     * doubleX("axxbb") → true
-     * doubleX("axaxax") → false
-     * doubleX("xxxxx") → true
+    /*
+      Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+      doubleX("axxbb") → true
+      doubleX("axaxax") → false
+      doubleX("xxxxx") → true
      */
 
     boolean doubleX(String str) {
@@ -76,11 +76,11 @@ public class Warmup2 {
         return x.startsWith("xx");
     }
 
-    /**
-     * Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
-     * stringBits("Hello") → "Hlo"
-     * stringBits("Hi") → "H"
-     * stringBits("Heeololeo") → "Hello"
+    /*
+      Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+      stringBits("Hello") → "Hlo"
+      stringBits("Hi") → "H"
+      stringBits("Heeololeo") → "Hello"
      */
 
     public String stringBits(String str) {
@@ -92,11 +92,11 @@ public class Warmup2 {
         return sb.toString();
     }
 
-    /**
-     * Given a non-empty string like "Code" return a string like "CCoCodCode".
-     * stringSplosion("Code") → "CCoCodCode"
-     * stringSplosion("abc") → "aababc"
-     * stringSplosion("ab") → "aab"
+    /*
+      Given a non-empty string like "Code" return a string like "CCoCodCode".
+      stringSplosion("Code") → "CCoCodCode"
+      stringSplosion("abc") → "aababc"
+      stringSplosion("ab") → "aab"
      */
 
     public String stringSplosion(String str) {
@@ -108,13 +108,13 @@ public class Warmup2 {
         return sb1.toString();
     }
 
-    /**
-     * Given a string, return the count of the number of times that a substring length 2 appears
-     * in the string and also as the last 2 chars of the string, so "hixxxhi"
-     * yields 1 (we won't count the end substring).
-     * last2("hixxhi") → 1
-     * last2("xaxxaxaxx") → 1
-     * last2("axxxaaxx") → 2
+    /*
+      Given a string, return the count of the number of times that a substring length 2 appears
+      in the string and also as the last 2 chars of the string, so "hixxxhi"
+      yields 1 (we won't count the end substring).
+      last2("hixxhi") → 1
+      last2("xaxxaxaxx") → 1
+      last2("axxxaaxx") → 2
      */
 
     public int last2(String str) {
@@ -131,11 +131,11 @@ public class Warmup2 {
         return count;
     }
 
-    /**
-     * Given an array of ints, return the number of 9's in the array.
-     * arrayCount9([1, 2, 9]) → 1
-     * arrayCount9([1, 9, 9]) → 2
-     * arrayCount9([1, 9, 9, 3, 9]) → 3
+    /*
+      Given an array of ints, return the number of 9's in the array.
+      arrayCount9([1, 2, 9]) → 1
+      arrayCount9([1, 9, 9]) → 2
+      arrayCount9([1, 9, 9, 3, 9]) → 3
      */
 
     public int arrayCount9(int[] nums) {
@@ -149,12 +149,12 @@ public class Warmup2 {
         return count;
     }
 
-    /**
-     * Given an array of ints, return true if one of the first 4 elements in the array is a 9.
-     * The array length may be less than 4.
-     * arrayFront9([1, 2, 9, 3, 4]) → true
-     * arrayFront9([1, 2, 3, 4, 9]) → false
-     * arrayFront9([1, 2, 3, 4, 5]) → false
+    /*
+      Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+      The array length may be less than 4.
+      arrayFront9([1, 2, 9, 3, 4]) → true
+      arrayFront9([1, 2, 3, 4, 9]) → false
+      arrayFront9([1, 2, 3, 4, 5]) → false
      */
 
     public boolean arrayFront9(int[] nums) {
@@ -169,11 +169,11 @@ public class Warmup2 {
         return false;
     }
 
-    /**
-     * Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
-     * array123([1, 1, 2, 3, 1]) → true
-     * array123([1, 1, 2, 4, 1]) → false
-     * array123([1, 1, 2, 1, 2, 3]) → true
+    /*
+      Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+      array123([1, 1, 2, 3, 1]) → true
+      array123([1, 1, 2, 4, 1]) → false
+      array123([1, 1, 2, 1, 2, 3]) → true
      */
 
     public boolean array123(int[] nums) {
@@ -183,13 +183,13 @@ public class Warmup2 {
         return false;
     }
 
-    /**
-     * Given 2 strings, a and b, return the number of the positions where they
-     * contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx",
-     * "aa", and "az" substrings appear in the same place in both strings.
-     * stringMatch("xxcaazz", "xxbaaz") → 3
-     * stringMatch("abc", "abc") → 2
-     * stringMatch("abc", "axc") → 0
+    /*
+      Given 2 strings, a and b, return the number of the positions where they
+      contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx",
+      "aa", and "az" substrings appear in the same place in both strings.
+      stringMatch("xxcaazz", "xxbaaz") → 3
+      stringMatch("abc", "abc") → 2
+      stringMatch("abc", "axc") → 0
      */
 
     public int stringMatch(String a, String b) {
@@ -204,12 +204,12 @@ public class Warmup2 {
         return count;
     }
 
-    /**
-     * Given a string, return a version where all the "x" have been removed. Except an "x"
-     * at the very start or end should not be removed.
-     * stringX("xxHxix") → "xHix"
-     * stringX("abxxxcd") → "abcd"
-     * stringX("xabxxxcdx") → "xabcdx"
+    /*
+      Given a string, return a version where all the "x" have been removed. Except an "x"
+      at the very start or end should not be removed.
+      stringX("xxHxix") → "xHix"
+      stringX("abxxxcd") → "abcd"
+      stringX("xabxxxcdx") → "xabcdx"
      */
 
     public String stringX(String str) {
@@ -223,11 +223,11 @@ public class Warmup2 {
         return start + middle + end;
     }
 
-    /**
-     * Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
-     * altPairs("kitten") → "kien"
-     * altPairs("Chocolate") → "Chole"
-     * altPairs("CodingHorror") → "Congrr"
+    /*
+      Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+      altPairs("kitten") → "kien"
+      altPairs("Chocolate") → "Chole"
+      altPairs("CodingHorror") → "Congrr"
      */
 
     public String altPairs(String str) {
@@ -241,12 +241,12 @@ public class Warmup2 {
         return result.toString();
     }
 
-    /**
-     * Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak"
-     * are removed, but the "a" can be any char. The "yak" strings will not overlap.
-     * stringYak("yakpak") → "pak"
-     * stringYak("pakyak") → "pak"
-     * stringYak("yak123ya") → "123ya"
+    /*
+      Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak"
+      are removed, but the "a" can be any char. The "yak" strings will not overlap.
+      stringYak("yakpak") → "pak"
+      stringYak("pakyak") → "pak"
+      stringYak("yak123ya") → "123ya"
      */
 
     public String stringYak(String str) {
@@ -262,12 +262,12 @@ public class Warmup2 {
         return result.toString();
     }
 
-    /**
-     * Given an array of ints, return the number of times that two 6's are next to each other
-     * in the array. Also count instances where the second "6" is actually a 7.
-     * array667([6, 6, 2]) → 1
-     * array667([6, 6, 2, 6]) → 1
-     * array667([6, 7, 2, 6]) → 1
+    /*
+      Given an array of ints, return the number of times that two 6's are next to each other
+      in the array. Also count instances where the second "6" is actually a 7.
+      array667([6, 6, 2]) → 1
+      array667([6, 6, 2, 6]) → 1
+      array667([6, 7, 2, 6]) → 1
      */
 
     public int array667(int[] nums) {
@@ -280,12 +280,12 @@ public class Warmup2 {
         return count;
     }
 
-    /**
-     * Given an array of ints, we'll say that a triple is a value appearing 3 times in a
-     * row in the array. Return true if the array does not contain any triples.
-     * noTriples([1, 1, 2, 2, 1]) → true
-     * noTriples([1, 1, 2, 2, 2, 1]) → false
-     * noTriples([1, 1, 1, 2, 2, 2, 1]) → false
+    /*
+      Given an array of ints, we'll say that a triple is a value appearing 3 times in a
+      row in the array. Return true if the array does not contain any triples.
+      noTriples([1, 1, 2, 2, 1]) → true
+      noTriples([1, 1, 2, 2, 2, 1]) → false
+      noTriples([1, 1, 1, 2, 2, 2, 1]) → false
      */
 
     public boolean noTriples(int[] nums) {
@@ -297,13 +297,13 @@ public class Warmup2 {
         return true;
     }
 
-    /**
-     * Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value,
-     * followed by the value plus 5, followed by the value minus 1. Additionally the 271 counts
-     * even if the "1" differs by 2 or less from the correct value.
-     * has271([1, 2, 7, 1]) → true
-     * has271([1, 2, 8, 1]) → false
-     * has271([2, 7, 1]) → true
+    /*
+      Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value,
+      followed by the value plus 5, followed by the value minus 1. Additionally the 271 counts
+      even if the "1" differs by 2 or less from the correct value.
+      has271([1, 2, 7, 1]) → true
+      has271([1, 2, 8, 1]) → false
+      has271([2, 7, 1]) → true
      */
 
     public boolean has271(int[] nums) {
