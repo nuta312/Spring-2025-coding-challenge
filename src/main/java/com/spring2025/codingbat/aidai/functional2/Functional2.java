2acfbd9666bf14612f5eb204ccdf720dcd5c3bc3
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 17:15:28 +0600

    aidai/codingbat/functional2-two2

--- src/main/java/com/spring2025/codingbat/aidai/functional2/Functional2.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional2/Functional2.java
@@ -84,5 +84,16 @@ public class Functional2 {
     public List<String> noYY(List<String> strings) {
         return strings.stream().map(n -> n.concat("y")).filter(n -> !n.contains("yy")).collect(Collectors.toList());
     }
+    /**
+     * Given a list of non-negative integers, return a list of those numbers multiplied by 2, omitting any of the resulting numbers that end in 2.
+     *
+     *
+     * two2([1, 2, 3]) → [4, 6]
+     * two2([2, 6, 11]) → [4]
+     * two2([0]) → [0]
+     */
+    public List<Integer> two2(List<Integer> nums) {
+        return nums.stream().map(n -> n * 2).filter(n -> n % 10 !=2).collect(Collectors.toList());
+    }
 }
 
