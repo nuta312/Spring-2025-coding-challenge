7a96e71d36fc8f6ee0093b64617e66809fde575f
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 10:13:50 +0600

    zinaida/codingbat/string1-extraEnd

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -65,4 +65,19 @@ makeOutWord("[[]]", "word") → "[[word]]"
         return out.substring(0, 2).concat(word).concat(out.substring(2, 4));
     }
 
+ /*
+ Given a string, return a new string made of 3 copies of the last 2 chars of the
+ original string. The string length will be at least 2.
+
+extraEnd("Hello") → "lololo"
+extraEnd("ab") → "ababab"
+extraEnd("Hi") → "HiHiHi"
+  */
+
+    public String extraEnd(String str) {
+
+        String last2 = str.substring(str.length() - 2);
+        return last2 + last2 + last2;
+
+    }
 }
