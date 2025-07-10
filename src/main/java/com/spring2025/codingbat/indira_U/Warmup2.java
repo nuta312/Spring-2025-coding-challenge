18d330886113c85c0944360634adffc7378a7162
indira <you@example.com>
2025-07-10 16:10:03 +0600

    arrayCount9

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup2.java
@@ -126,4 +126,23 @@ public int last2(String str) {
 
     return count;
 }
+
+    /*Given an array of ints, return the number of 9's in the array.
+
+
+    arrayCount9([1, 2, 9]) → 1
+    arrayCount9([1, 9, 9]) → 2
+    arrayCount9([1, 9, 9, 3, 9]) → 3*/
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        int num9 = 9;
+        for (int num : nums) {
+            if (num == num9) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
