f0eeaae0b275a27e7b24c33699152fb40b024766
indira <you@example.com>
2025-07-10 14:58:00 +0600

    countXX

--- src/main/java/com/spring2025/codingbat/indira_U/warmup2.java
+++ src/main/java/com/spring2025/codingbat/indira_U/warmup2.java
@@ -34,5 +34,21 @@ frontTimes("Abc", 3) → "AbcAbcAbc"
         }
         return sb.toString();
     }
+/*
 
+Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+countXX("abcxx") → 1
+countXX("xxx") → 2
+countXX("xxxx") → 3
+ */
+int countXX(String str) {
+    int coun = 0;
+    for (int i = 0; i < str.length()-1; i++) {
+        if (str.substring(i, i + 2).equals("xx")) {
+            coun++;
+        }
+    }
+    return coun;
+}
 }
