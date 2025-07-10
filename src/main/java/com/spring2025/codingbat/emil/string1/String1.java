dbbb8ba0ff83a4a225086e42c99dd5df598b28f3
Emil <emkerimov@mail.ru>
2025-07-11 00:40:50 +0600

    String1

--- src/main/java/com/spring2025/codingbat/emil/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/emil/string1/String1.java
@@ -33,4 +33,17 @@ public class String1 {
         return a + b + b + a;
     }
 
+    /*
+    The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+
+    makeTags("i", "Yay") → "<i>Yay</i>"
+    makeTags("i", "Hello") → "<i>Hello</i>"
+    makeTags("cite", "Yay") → "<cite>Yay</cite>"
+     */
+    public String makeTags(String tag, String word) {
+        String symbol1 = "<";
+        String symbol2 = ">";
+        String symbol3 = "</";
+        return symbol1.concat(tag).concat(symbol2).concat(word).concat(symbol3).concat(tag).concat(symbol2);
+    }
 }
