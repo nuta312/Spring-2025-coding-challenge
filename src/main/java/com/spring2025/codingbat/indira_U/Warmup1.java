8d7a4229c8f02a03a9a7414f70188825d254540a
indira <you@example.com>
2025-07-10 13:44:19 +0600

    mixStart

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
@@ -324,4 +324,19 @@ loneTeen(13, 13) → false
 
         return str;
     }
+     /*
+
+Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+
+mixStart("mix snacks") → true
+mixStart("pix snacks") → true
+mixStart("piz snacks") → false
+    */
+    public boolean mixStart(String str) {
+        if (str.contains("ix")) {
+            return true;
+        }
+        return false;
+    }
 }
