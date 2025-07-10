051156c996b9d85af56712d20088c7572898230c
indira <you@example.com>
2025-07-10 13:36:36 +0600

    frontBack

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
@@ -154,4 +154,22 @@ missingChar("kitten", 4) → "kittn"
         String end = str.substring(n + 1);
         return nachalo + end;
     }
+     /*
+
+ Given a string, return a new string where the first and last chars have been exchanged.
+
+
+ frontBack("code") → "eodc"
+ frontBack("a") → "a"
+ frontBack("ab") → "ba"
+     */
+    public String frontBack(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+        String a = str.substring(0, 1);
+        String midle = str.substring(1, str.length() - 1);
+        String b = str.substring(str.length() - 1);
+        return b + midle + a;
+    }
 }
