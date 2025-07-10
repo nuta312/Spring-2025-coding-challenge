627e6f8b4297c5acc801412dfa462c5938c2577f
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 10:22:27 +0600

    zinaida/codingbat/string1-theEnd

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -209,4 +209,26 @@ right2("Hi") → "Hi"
         }
         return result;
     }
+
+ /*
+ Given a string, return a string length 1 from its front, unless front is false,
+  in which case return a string length 1 from its back. The string will be non-empty.
+
+
+theEnd("Hello", true) → "H"
+theEnd("Hello", false) → "o"
+theEnd("oh", true) → "o"
+  */
+
+    public String theEnd(String str, boolean front) {
+        String result;
+        if (front) {
+            result = str.substring(0, 1);
+            return result;
+        } else {
+            result = str.substring(str.length() - 1);
+            return result;
+        }
+    }
+
 }
