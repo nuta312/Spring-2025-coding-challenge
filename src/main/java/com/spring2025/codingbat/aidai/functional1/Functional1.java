bff9b7b1b4d4c95b2c3137fe0a3c87af0c097c1b
Aidai Talantbekova <aidai0296@gmail.com>
2025-07-10 16:58:45 +0600

    aidai/codingbat/functional1-noX

--- src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/aidai/functional1/Functional1.java
@@ -95,4 +95,15 @@ public class Functional1 {
     public List<String> lower(List<String> strings) {
         return strings.stream().map(s -> s.toLowerCase()).collect(Collectors.toList());
     }
+    /**
+     * Given a list of strings, return a list where each string has all its "x" removed.
+     *
+     *
+     * noX(["ax", "bb", "cx"]) → ["a", "bb", "c"]
+     * noX(["xxax", "xbxbx", "xxcx"]) → ["a", "bb", "c"]
+     * noX(["x"]) → [""]
+     */
+    public List<String> noX(List<String> strings) {
+        return strings.stream().map(s -> s.replace("x","")).collect(Collectors.toList());
+    }
 }
