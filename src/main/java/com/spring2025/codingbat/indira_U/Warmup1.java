9d7faedd586f5883cc0436b1ab26e4312e7945d0
indira <you@example.com>
2025-07-10 13:37:09 +0600

    front3

--- src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Warmup1.java
@@ -172,4 +172,24 @@ missingChar("kitten", 4) → "kittn"
         String b = str.substring(str.length() - 1);
         return b + midle + a;
     }
+    /*
+
+Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+front3("Java") → "JavJavJav"
+front3("Chocolate") → "ChoChoCho"
+front3("abc") → "abcabcabc"
+    */
+    public String front3(String str) {
+        String front;
+        if (str.length() >= 3) {
+            front = str.substring(0, 3);
+        } else {
+            front = str;
+        }
+
+        return front + front + front;
+
+    }
 }
