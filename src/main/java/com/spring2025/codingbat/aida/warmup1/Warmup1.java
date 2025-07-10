1b32ffee251b6a5b493cbb658e88358678c9fde2
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 12:29:51 +0600

    author tag

--- src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aida/warmup1/Warmup1.java
@@ -2,6 +2,10 @@ package com.spring2025.codingbat.aida.warmup1;
 
 public class Warmup1 {
 
+    /**
+     * @Bershka
+     */
+
     /* The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
     We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in. */
 
