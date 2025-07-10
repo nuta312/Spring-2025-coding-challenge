0adab311e88a2e13a700edc81810aa13888bf0e7
Bershka Bershka <bershkabershka@Aida-Anarbek.local>
2025-07-10 15:20:28 +0600

    middleThree

--- src/main/java/com/spring2025/codingbat/aida/String1.java
+++ src/main/java/com/spring2025/codingbat/aida/String1.java
@@ -159,4 +159,11 @@ public class String1 {
             return str.substring(0, 2);
         }
     }
+
+    // Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+
+    public String middleThree(String str) {
+        int mid = str.length() / 2;
+        return str.substring(mid - 1, mid + 2);
+    }
 }
