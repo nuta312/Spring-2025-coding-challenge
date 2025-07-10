febde09abdbb0bc831a57043805c6fa1132efb65
Завира Чыныбаева <zavirachynybaeva@MacBook-Air-Zavira.local>
2025-07-10 23:20:08 +0600

    frontback

--- src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
@@ -178,4 +178,23 @@ missingChar("kitten", 4) → "kittn"
 
         return first + last;
     }
+
+    /* Given a string, return a new string where the first and last chars have been exchanged.
+
+frontBack("code") → "eodc"
+frontBack("a") → "a"
+frontBack("ab") → "ba"
+
+     */
+
+    public String frontBack(String str) {
+
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        String mid = str.substring (1, str.length() -1);
+
+        return str.charAt(str.length() -1) + mid + str.charAt(0);
+    }
 }
