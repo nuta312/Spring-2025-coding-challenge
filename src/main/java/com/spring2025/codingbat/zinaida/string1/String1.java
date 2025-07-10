a7b6331db92f039223628d17f01a096ade66b711
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 10:10:14 +0600

    zinaida/codingbat/string1-makeAbba

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -19,4 +19,18 @@ helloName("X") → "Hello X!"
 
     }
 
+ /*
+Given two strings, a and b, return the result of putting them together in the order
+abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+makeAbba("Hi", "Bye") → "HiByeByeHi"
+makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+makeAbba("What", "Up") → "WhatUpUpWhat"
+  */
+
+    public String makeAbba(String a, String b) {
+
+        return a + b + b + a;
+
+    }
 }
