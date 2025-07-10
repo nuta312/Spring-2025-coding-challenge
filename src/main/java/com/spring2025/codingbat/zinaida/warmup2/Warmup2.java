f5e4c8e52af905a1cebff1828ebcbfa8d4961773
Zinaida <begmetovazinaida@rambler.ru>
2025-07-10 08:03:07 +0600

    zinaida/codingbat/warmup2-countXX

--- src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/warmup2/Warmup2.java
@@ -47,4 +47,23 @@ frontTimes("Abc", 3) → "AbcAbcAbc"
         return result;
     }
 
+ /*
+ Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+countXX("abcxx") → 1
+countXX("xxx") → 2
+countXX("xxxx") → 3
+  */
+
+    int countXX(String str) {
+
+        int count_xx = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("xx")) {
+                count_xx += 1;
+            }
+        }
+        return count_xx;
+    }
+
 }
