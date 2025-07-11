8de0c40ac42ec9553c36e91920dca3ef348cd38c
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 12:06:01 +0600

    zinaida/codingbat/logic2-roundSum

--- src/main/java/com/spring2025/codingbat/zinaida/logic2/Logic2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/logic2/Logic2.java
@@ -75,9 +75,35 @@ noTeenSum(2, 1, 14) → 3
     }
 
     public int fixTeen(int n) {
-        if ((n >= 13 && n <= 19) && n != 15 && n != 16){
+        if ((n >= 13 && n <= 19) && n != 15 && n != 16) {
             return 0;
         } else return n;
 
     }
+
+    /*
+For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more,
+so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less
+than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code
+repetition, write a separate helper "public int round10(int num) {" and call it 3 times. Write the helper
+entirely below and at the same indent level as roundSum().
+
+roundSum(16, 17, 18) → 60
+roundSum(12, 13, 14) → 30
+roundSum(6, 4, 4) → 10
+     */
+    public int roundSum(int a, int b, int c) {
+
+        return round10(a) + round10(b) + round10(c);
+
+    }
+
+    public int round10(int num) {
+        if (num % 10 >= 1 && num % 10 <= 4) {
+            return num - (num % 10);
+        }
+        if (num % 10 >= 5) return num + (10 - num % 10);
+        else return num;
+    }
+
 }
