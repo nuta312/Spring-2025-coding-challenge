d5c9ea2f2a53e4ca726dff6661a0f320bb695740
Madina <1madilyao@gmail.com>
2025-07-11 18:41:06 +0600

    no34

--- src/main/java/com/spring2025/codingbat/Madina/Functional2/Functional2.java
+++ src/main/java/com/spring2025/codingbat/Madina/Functional2/Functional2.java
@@ -56,5 +56,17 @@ public class Functional2 {
     public List<String> noLong(List<String> strings) {
         return strings.stream().filter(s->s.length()<=3).collect(Collectors.toList());
     }
+   /* Given a list of strings, return a list of the strings, omitting any string length 3 or 4.
+
+
+    no34(["a", "bb", "ccc"]) → ["a", "bb"]
+    no34(["a", "bb", "ccc", "dddd"]) → ["a", "bb"]
+    no34(["ccc", "dddd", "apple"]) → ["apple"]
+
+    */
+   public List<String> no34(List<String> strings) {
+       return strings.stream().filter(s->s.length()!=3 && s.length() !=4)
+               .collect(Collectors.toList());
+   }
 
 }
