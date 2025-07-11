67e1551b5afb10c0233ac7a6c05519bb8976d001
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 11:05:24 +0600

    zinaida/codingbat/logic1-cigarParty

---
+++ src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
@@ -0,0 +1,25 @@
+package com.spring2025.codingbat.zinaida.logic1;
+
+/*
+@author Zinaida
+ */
+public class Logic1 {
+
+    /*
+When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the
+number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper
+bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
+
+cigarParty(30, false) → false
+cigarParty(50, false) → true
+cigarParty(70, true) → true
+     */
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+
+        if (cigars >= 40 && cigars <= 60 && !isWeekend) {
+            return true;
+        }
+        return cigars >= 40 && isWeekend;
+
+    }
+}
