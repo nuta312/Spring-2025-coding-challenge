91c3990f05d656a1a5f5f9c859b35800702bad52
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 10:43:36 +0600

    zinaida/codingbat/array1-double23

--- src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
@@ -206,4 +206,24 @@ makeLast([3]) → [0, 3]
         } else return new int[]{};
 
     }
+
+    /*
+   Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
+
+  double23([2, 2]) → true
+  double23([3, 3]) → true
+  double23([2, 3]) → false
+     */
+    public boolean double23(int[] nums) {
+
+        if (nums.length == 0 || nums.length == 1) return false;
+
+        if (nums.length == 2) {
+            if (nums[0] == nums[1] && (nums[0] == 2 || nums[0] == 3)) {
+                return true;
+            } else return false;
+        } else return false;
+
+    }
+
 }
