8edce4dc5c347b9f1b4cce83f11278820094a936
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 10:54:03 +0600

    zinaida/codingbat/array1-maxTriple

--- src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
@@ -351,4 +351,26 @@ midThree([1, 2, 3]) → [1, 2, 3]
 
     }
 
+    /*
+Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+
+maxTriple([1, 2, 3]) → 3
+maxTriple([1, 5, 3]) → 5
+maxTriple([5, 2, 3]) → 5
+     */
+    public int maxTriple(int[] nums) {
+
+        int max = 0;
+        if (nums[0] > nums[nums.length - 1] && nums[0] > nums[nums.length / 2]) {
+            max = nums[0];
+        }
+        if (nums[nums.length - 1] > nums[0] && nums[nums.length - 1] > nums[nums.length / 2]) {
+            max = nums[nums.length - 1];
+        }
+        if (nums[nums.length / 2] > nums[nums.length - 1] && nums[0] < nums[nums.length / 2]) {
+            max = nums[nums.length / 2];
+        }
+        return max;
+
+    }
 }
