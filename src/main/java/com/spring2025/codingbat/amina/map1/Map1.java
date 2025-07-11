e8e0ca219c37a57e0b83b220fbd99c9e8207d4ea
= <=>
2025-07-12 01:09:07 +0600

    amina/codingbat/map1/mapAB

--- src/main/java/com/spring2025/codingbat/amina/map1/Map1.java
+++ src/main/java/com/spring2025/codingbat/amina/map1/Map1.java
@@ -124,4 +124,22 @@ public class Map1 {
         }
         return map;
     }
+
+    /**
+     Task 7 – mapAB
+
+     Modify and return the given map as follows:
+     for this problem the map may or may not contain the "a" and "b" keys.
+     If both keys are present, append their 2 string values together and store the result under the key "ab".
+
+     mapAB({"a": "Hi", "b": "There"}) → {"a": "Hi", "ab": "HiThere", "b": "There"}
+     mapAB({"a": "Hi"}) → {"a": "Hi"}
+     mapAB({"b": "There"}) → {"b": "There"}
+     */
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
 }
