a0d47bf49f99d31a7ea5b290a1ea28b471258f43
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 11:23:50 +0600

    zinaida/codingbat/logic1-answerCell

--- src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
@@ -276,4 +276,18 @@ nearTen(19) → true
 
     }
 
+    /*
+ Your cell phone rings. Return true if you should answer it. Normally you answer, except in the morning you
+ only answer if it is your mom calling. In all cases, if you are asleep, you do not answer.
+
+answerCell(false, false, false) → true
+answerCell(false, false, true) → false
+answerCell(true, false, false) → false
+     */
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+
+        return !(isAsleep || (isMorning && !isMom));
+
+    }
+
 }
