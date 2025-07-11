7089c16b275856759fed11b2dbd3a9734d6d3600
Madina <1madilyao@gmail.com>
2025-07-11 18:41:48 +0600

    noyy

--- src/main/java/com/spring2025/codingbat/Madina/Functional2/Functional2.java
+++ src/main/java/com/spring2025/codingbat/Madina/Functional2/Functional2.java
@@ -68,5 +68,16 @@ public class Functional2 {
        return strings.stream().filter(s->s.length()!=3 && s.length() !=4)
                .collect(Collectors.toList());
    }
+    /*Given a list of strings, return a list where each string has "y" added at its end, omitting any resulting strings that contain "yy" as a substring anywhere.
+
+
+    noYY(["a", "b", "c"]) → ["ay", "by", "cy"]
+    noYY(["a", "b", "cy"]) → ["ay", "by"]
+    noYY(["xx", "ya", "zz"]) → ["xxy", "yay", "zzy"]
+
+     */
+    public List<String> noYY(List<String> strings) {
+        return strings.stream().map(s -> s.concat("y")).filter(s->!s.contains("yy")).collect(Collectors.toList());
+    }
 
 }
