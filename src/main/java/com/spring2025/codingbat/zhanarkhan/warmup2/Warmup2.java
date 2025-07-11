e21309dcbbd8178034f677df9d2d55087fc8fb41
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-11 09:16:48 +0600

    stringTimes

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
@@ -4,5 +4,19 @@ public class Warmup2 {
     public static void main(String[] args) {
     }
 
+//    Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+//
+//
+//            stringTimes("Hi", 2) → "HiHi"
+//    stringTimes("Hi", 3) → "HiHiHi"
+//    stringTimes("Hi", 1) → "Hi"
 
+    public String stringTimes(String str, int n) {
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            result += str;
+        }
+        return result;
+    }
 }
