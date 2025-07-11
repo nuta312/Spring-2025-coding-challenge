974f78000ab37ebe29d909ca71a34d05a7017099
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 08:56:50 +0600

    zinaida/codingbat/string1-lastTwo

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -417,4 +417,21 @@ conCat("abc", "") → "abc"
         }
 
     }
+
+ /*
+Given a string of any length, return a new string where the last 2 chars, if present,
+are swapped, so "coding" yields "codign".
+
+lastTwo("coding") → "codign"
+lastTwo("cat") → "cta"
+lastTwo("ab") → "ba"
+  */
+
+    public String lastTwo(String str) {
+
+        return (str.length() >= 2) ? str.substring(0, str.length()-2) +
+                (str.substring(str.length()-1) + (str.substring(str.length()-2, str.length()-1))) : str;
+
+    }
+
 }
