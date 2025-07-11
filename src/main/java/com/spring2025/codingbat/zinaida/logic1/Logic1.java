dbe55fbee5662ca18be816f4c6fe06f338bbaa55
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 11:10:46 +0600

    zinaida/codingbat/logic1-alarmClock

--- src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
@@ -110,9 +110,38 @@ sortaSum(10, 11) → 21
      */
     public int sortaSum(int a, int b) {
 
-        int sum = a+b;
-        if (sum >=10 && sum<= 19) return 20;
+        int sum = a + b;
+        if (sum >= 10 && sum <= 19) return 20;
         else return sum;
 
     }
+
+    /*
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on
+vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the
+alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation --
+then on weekdays it should be "10:00" and weekends it should be "off".
+
+alarmClock(1, false) → "7:00"
+alarmClock(5, false) → "7:00"
+alarmClock(0, false) → "10:00"
+     */
+    public String alarmClock(int day, boolean vacation) {
+
+        String result = "";
+        if (!vacation) {
+            if (day > 0 && day < 6) {
+                result = "7:00";
+            }
+            if (day == 0 || day == 6) result = "10:00";
+        } else {
+            if (day > 0 && day < 6) {
+                result = "10:00";
+            }
+            if (day == 0 || day == 6) result = "off";
+        }
+        return result;
+
+    }
+
 }
