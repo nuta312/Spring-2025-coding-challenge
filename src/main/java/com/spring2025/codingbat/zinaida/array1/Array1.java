ec5487c56d5dd1750a5f797eb5c6edd763134daa
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 10:28:59 +0600

    zinaida/codingbat/array1-sum3

--- src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
@@ -57,8 +57,21 @@ commonEnd([1, 2, 3], [1, 3]) → true
      */
     public boolean commonEnd(int[] a, int[] b) {
 
-        if(a[0] == b[0] || a[a.length-1] == b[b.length-1]){
+        if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) {
             return true;
-        }else return false;
+        } else return false;
+    }
+
+    /*
+Given an array of ints length 3, return the sum of all the elements.
+
+sum3([1, 2, 3]) → 6
+sum3([5, 11, 2]) → 18
+sum3([7, 0, 0]) → 7
+     */
+    public int sum3(int[] nums) {
+
+        return nums[0] + nums[1] + nums[2];
+
     }
 }
