0082093b583b9231d76a91db7dce1604c0389872
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-11 10:59:50 +0600

    stringSplosion

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
@@ -104,4 +104,24 @@ public class Warmup2 {
         }
         return result;
     }
+
+
+//    Given a non-empty string like "Code" return a string like "CCoCodCode".
+//
+//
+//    stringSplosion("Code") → "CCoCodCode"
+//    stringSplosion("abc") → "aababc"
+//    stringSplosion("ab") → "aab"
+
+
+    public String stringSplosion(String str) {
+        String result = "";
+
+        for(int i = 0; i < str.length()+1; i++) {
+            result += str.substring(0, i);
+        }
+        return result;
+    }
+
+
 }
