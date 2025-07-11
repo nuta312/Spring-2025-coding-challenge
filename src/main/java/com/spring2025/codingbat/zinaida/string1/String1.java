e1cb35a621e47e143808de0b5e7c589b08c6a226
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 09:17:47 +0600

    zinaida/codingbat/string1-frontAgain

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -454,4 +454,21 @@ seeColor("blueTimes") → "blue"
         }
     }
 
+ /*
+ Given a string, return true if the first 2 chars in the string also appear at the end
+ of the string, such as with "edited".
+
+frontAgain("edited") → true
+frontAgain("edit") → false
+frontAgain("ed") → true
+  */
+
+    public boolean frontAgain(String str) {
+
+        if (str.length() >= 2 && str.substring(0, 2).equalsIgnoreCase(str.substring(str.length() - 2))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
