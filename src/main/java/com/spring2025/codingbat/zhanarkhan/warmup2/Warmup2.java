37fb1e987f38546c04f449d3d39cdc4b8109f164
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-11 10:55:33 +0600

    doubleX

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
@@ -70,4 +70,19 @@ public class Warmup2 {
         return count;
     }
 
+//    Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+//
+//
+//    doubleX("axxbb") → true
+//    doubleX("axaxax") → false
+//    doubleX("xxxxx") → true
+
+    public boolean doubleX(String str) {
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'x') {
+                return str.charAt(i + 1) == 'x'; // сразу проверяем следующую и возвращаем результат
+            }
+        }
+        return false; // если не нашли ни одной 'x'
+    }
 }
