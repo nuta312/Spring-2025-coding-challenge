75b97f9ad2ec42cb7fe61452a4b3370971094059
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 08:53:04 +0600

    zinaida/codingbat/string1-atFirst

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -355,4 +355,24 @@ hasBad("xxbadxx") → false
         return false;
     }
 
+ /*
+Given a string, return a string length 2 made of its first 2 chars. If the string length
+is less than 2, use '@' for the missing chars.
+
+atFirst("hello") → "he"
+atFirst("hi") → "hi"
+atFirst("h") → "h@"
+  */
+
+    public String atFirst(String str) {
+        String result = "";
+        if (str.length() == 0) {
+            return result + "@" + "@";
+        } else if (str.length() < 2) {
+            result = str + "@";
+        } else {
+            result = str.substring(0, 2);
+        }
+        return result;
+    }
 }
