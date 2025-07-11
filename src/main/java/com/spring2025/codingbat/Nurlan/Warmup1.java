419d4d836846273ddc7be642718b8af8786c73c3
MACBOOK PRO <macbookpro@MacBook-Pro-MACBOOK.local>
2025-07-11 11:52:53 +0600

    Warmup1

---
+++ src/main/java/com/spring2025/codingbat/Nurlan/Warmup1.java
@@ -0,0 +1,20 @@
+package com.spring2025.codingbat.Nurlan;
+
+public class Warmup1 {
+   // Given an int n, return the absolute difference between n and 21,
+    // except return double the absolute difference if n is over 21.
+
+
+//    diff21(19) → 2
+//    diff21(10) → 11
+//    diff21(21) → 0
+
+
+public int diff21(int n) {
+    if(n <= 21){
+        return Math.abs(21 - n);
+    }else{
+        return Math.abs(n-21)*2;
+    }
+}
+}
