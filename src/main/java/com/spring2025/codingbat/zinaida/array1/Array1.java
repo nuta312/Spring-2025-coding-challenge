dc572a6265dd49d939f0b8e3cf72a724f8b4745b
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 10:25:17 +0600

    zinaida/codingbat/array1-makePi

--- src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
@@ -35,4 +35,16 @@ public class Array1 {
         } else return false;
     }
 
+    /*
+ Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+ makePi() → [3, 1, 4]
+     */
+
+    public int[] makePi() {
+
+        return new int[]{3, 1, 4};
+
+    }
+
 }
