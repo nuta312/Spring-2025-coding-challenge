d409f31cecc6243db52048bf093bb9dbee1444f0
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 08:46:20 +0600

    zinaida/codingbat/string1-endsLy

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -260,8 +260,26 @@ middleTwo("Practice") → "ct"
 
     public String middleTwo(String str) {
 
-        return str.substring(str.length()/2-1, str.length()/2 + 1);
+        return str.substring(str.length() / 2 - 1, str.length() / 2 + 1);
     }
 
+/*
+Given a string, return true if it ends in "ly".
 
+endsLy("oddly") → true
+endsLy("y") → false
+endsLy("oddy") → false
+ */
+
+    public boolean endsLy(String str) {
+
+        if (str.length() < 2) {
+            return false;
+        }
+        if (str.substring(str.length() - 2).equalsIgnoreCase("ly")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
