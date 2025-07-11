20d65318c39a85a5db74685a4e9d18c5275bebfe
Madina <1madilyao@gmail.com>
2025-07-11 17:13:38 +0600

    copies

--- src/main/java/com/spring2025/codingbat/Madina/Functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/Madina/Functional1/Functional1.java
@@ -27,5 +27,13 @@ public class Functional1 {
       strings.replaceAll(n->n+"*");
       return strings;
   }
-
+  /*  Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together
+    copies3(["a", "bb", "ccc"]) → ["aaa", "bbbbbb", "ccccccccc"]
+    copies3(["24", "a", ""]) → ["242424", "aaa", ""]
+    copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
+   */
+  public List<String> copies3(List<String> strings) {
+      strings.replaceAll(n->n+n+n);
+      return strings;
+  }
 }
