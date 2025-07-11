8f2f655205da19ccc402381b9ca1fcec410481c6
= <=>
2025-07-12 02:51:49 +0600

    amina/codingbat/logic1/greenTicket

--- src/main/java/com/spring2025/codingbat/amina/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/amina/logic1/Logic1.java
@@ -496,5 +496,23 @@ public class Logic1 {
         return die1 + die2;
     }
 
+    /**
+     Task 29 - greenTicket
+
+     If all different → 0
+     If two same → 10
+     If all same → 20
+
+     greenTicket(1, 2, 3) → 0
+     greenTicket(2, 2, 2) → 20
+     greenTicket(1, 1, 2) → 10
+     */
+
+    public int greenTicket(int a, int b, int c) {
+        if (a == b && b == c) return 20;
+        if (a == b || b == c || a == c) return 10;
+        return 0;
+    }
+
 
 }
