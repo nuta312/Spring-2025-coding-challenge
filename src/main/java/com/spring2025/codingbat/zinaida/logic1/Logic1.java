b117c418cdecdc52fb8ebac16c4c17d2735df9d4
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 11:08:40 +0600

    zinaida/codingbat/logic1-caughtSpeeding

--- src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
@@ -66,4 +66,37 @@ squirrelPlay(95, true) → true
 
     }
 
+    /*
+You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded
+as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0.
+If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2.
+Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+
+caughtSpeeding(60, false) → 0
+caughtSpeeding(65, false) → 1
+caughtSpeeding(65, true) → 0
+     */
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+
+        int res = 0;
+        if (!isBirthday) {
+            if (speed <= 60) {
+                res = 0;
+            }
+            if (speed > 61 && speed <= 80) {
+                res = 1;
+            }
+            if (speed > 80) res = 2;
+        } else {
+            if (speed <= 65) {
+                res = 0;
+            }
+            if (speed > 66 && speed <= 85) {
+                res = 1;
+            }
+            if (speed > 85) res = 2;
+        }
+        return res;
+
+    }
 }
