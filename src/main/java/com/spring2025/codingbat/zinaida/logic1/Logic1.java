e533c5b1e7344689f0fddc12595c2f237e3edb3c
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 11:31:23 +0600

    zinaida/codingbat/logic1-inOrder

--- src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
@@ -366,4 +366,28 @@ twoAsOne(3, 2, 2) → false
 
     }
 
+    /*
+Given three ints, a b c, return true if b is greater than a, and c is greater than b. However, with the
+exception that if "bOk" is true, b does not need to be greater than a.
+
+inOrder(1, 2, 4, false) → true
+inOrder(1, 2, 1, false) → false
+inOrder(1, 1, 2, true) → true
+     */
+    public boolean inOrder(int a, int b, int c, boolean bOk) {
+
+        if (!bOk) {
+            if (b > a && b < c) {
+                return true;
+            }
+            return false;
+        }
+        if (b < c) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
