3383ee5a41afc4b462e094823133a105e450b9b7
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 08:50:27 +0600

    zinaida/codingbat/string1-middleThree

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -318,4 +318,20 @@ twoChar("java", 3) → "ja"
         return result;
     }
 
+ /*
+Given a string of odd length, return the string length 3 from its middle, so "Candy"
+yields "and". The string length will be at least 3.
+
+middleThree("Candy") → "and"
+middleThree("and") → "and"
+middleThree("solving") → "lvi"
+  */
+
+    public String middleThree(String str) {
+
+        int index = str.length() / 2;
+        return str.substring(index - 1, index + 2);
+
+    }
+
 }
