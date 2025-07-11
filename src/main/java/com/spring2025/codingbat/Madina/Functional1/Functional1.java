2fc38bb89b01d690d3281a94e9ae99e32d3c20fe
Madina <1madilyao@gmail.com>
2025-07-11 17:17:57 +0600

    lower

--- src/main/java/com/spring2025/codingbat/Madina/Functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/Madina/Functional1/Functional1.java
@@ -64,4 +64,17 @@ public class Functional1 {
       nums.replaceAll(n -> n % 10);
       return nums;
   }
+ /*   Given a list of strings, return a list where each string is converted to lower case (Note: String toLowerCase() method).
+
+
+    lower(["Hello", "Hi"]) → ["hello", "hi"]
+    lower(["AAA", "BBB", "ccc"]) → ["aaa", "bbb", "ccc"]
+    lower(["KitteN", "ChocolaTE"]) → ["kitten", "chocolate"]
+
+  */
+ public List<String> lower(List<String> strings) {
+     strings.replaceAll(n -> n.toLowerCase());
+     return strings;
+ }
+
 }
