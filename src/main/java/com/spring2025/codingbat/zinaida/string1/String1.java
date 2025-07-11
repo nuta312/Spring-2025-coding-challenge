1578286498362d9233b16c9b05ff17a6db5aaf84
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 09:25:18 +0600

    zinaida/codingbat/string1-withoutX

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -581,4 +581,29 @@ startWord("hippo", "i") → "h"
         }
     }
 
+ /*
+ Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return
+ the string unchanged.
+
+withoutX("xHix") → "Hi"
+withoutX("xHi") → "Hi"
+withoutX("Hxix") → "Hxi"
+  */
+
+    public String withoutX(String str) {
+
+        if (str.equals("x")) return "";
+        if (str.startsWith("x") && str.endsWith("x")) {
+            return str.substring(1, str.length() - 1);
+        }
+        if (str.startsWith("x")) {
+            return str.substring(1);
+        }
+        if (str.endsWith("x")) {
+            return str.substring(0, str.length() - 1);
+        } else {
+            return str;
+        }
+    }
+
 }
