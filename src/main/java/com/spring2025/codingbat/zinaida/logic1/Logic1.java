1da74d843227b0a0d7d7b927abf475d5886cdae6
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 11:26:27 +0600

    zinaida/codingbat/logic1-fizzSrting

--- src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/logic1/Logic1.java
@@ -312,4 +312,24 @@ teaParty(20, 6) → 2
             return 1;
         } else return 0;
     }
+
+    /*
+ Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz".
+ If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string
+ unchanged. (See also: FizzBuzz Code)
+
+fizzString("fig") → "Fizz"
+fizzString("dib") → "Buzz"
+fizzString("fib") → "FizzBuzz"
+     */
+    public String fizzString(String str) {
+
+        if ((str.startsWith("f") || str.startsWith("F")) && (str.endsWith("b") || str.endsWith("B"))) {
+            return "FizzBuzz";
+        }
+        if (str.startsWith("f") || str.startsWith("F")) return "Fizz";
+        if (str.endsWith("b") || str.endsWith("B")) return "Buzz";
+        else return str;
+
+    }
 }
