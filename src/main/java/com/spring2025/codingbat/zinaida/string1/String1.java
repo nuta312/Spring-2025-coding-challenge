2ddb5bc88b992f5a02ecfbfdcb5c4d59834c523f
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 08:44:57 +0600

    zinaida/codingbat/string1-withouEnd2

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -231,4 +231,22 @@ theEnd("oh", true) → "o"
         }
     }
 
+/*
+Given a string, return a version without both the first and last char of the string.
+The string may be any length, including 0.
+
+withouEnd2("Hello") → "ell"
+withouEnd2("abc") → "b"
+withouEnd2("ab") → ""
+ */
+
+    public String withouEnd2(String str) {
+
+        if (str.length() <= 1) {
+            return "";
+        } else {
+            return str.substring(1, str.length() - 1);
+        }
+    }
+
 }
