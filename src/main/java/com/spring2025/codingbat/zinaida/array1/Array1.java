1a6ef84fd1573b5bf1e2c2aa8359166aa1124d59
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 10:39:53 +0600

    zinaida/codingbat/array1-makeLast

--- src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
@@ -188,4 +188,22 @@ no23([3, 5]) → false
         return !(nums[0] == 2 || nums[1] == 2 || nums[0] == 3 || nums[1] == 3);
 
     }
+
+    /*
+Given an int array, return a new array with double the length where its last element is the same as
+the original array, and all the other elements are 0. The original array will be length 1 or more. Note: by default, a new int array contains all 0's.
+
+makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
+makeLast([1, 2]) → [0, 0, 0, 2]
+makeLast([3]) → [0, 3]
+     */
+    public int[] makeLast(int[] nums) {
+
+        if (nums.length >= 1) {
+            int[] arr = new int[nums.length * 2];
+            arr[arr.length - 1] = nums[nums.length - 1];
+            return arr;
+        } else return new int[]{};
+
+    }
 }
