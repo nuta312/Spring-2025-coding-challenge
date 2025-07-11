4a3202addfb76701388184b311e6e57892a7737d
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 10:55:11 +0600

    zinaida/codingbat/array1-frontPiece

--- src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/array1/Array1.java
@@ -373,4 +373,21 @@ maxTriple([5, 2, 3]) → 5
         return max;
 
     }
+
+    /*
+Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+
+frontPiece([1, 2, 3]) → [1, 2]
+frontPiece([1, 2]) → [1, 2]
+frontPiece([1]) → [1]
+     */
+    public int[] frontPiece(int[] nums) {
+
+        if (nums.length < 2) {
+            return nums;
+        } else {
+            return new int[]{nums[0], nums[1]};
+        }
+
+    }
 }
