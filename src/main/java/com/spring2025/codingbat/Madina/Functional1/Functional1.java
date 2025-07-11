040836f7041b1de6a2b44420d513e64448678d07
Madina <1madilyao@gmail.com>
2025-07-11 17:15:44 +0600

    math1

--- src/main/java/com/spring2025/codingbat/Madina/Functional1/Functional1.java
+++ src/main/java/com/spring2025/codingbat/Madina/Functional1/Functional1.java
@@ -45,5 +45,16 @@ public class Functional1 {
        strings.replaceAll(n->"y"+n+"y");
        return strings;
    }
+   /* Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+    math1([1, 2, 3]) → [20, 30, 40]
+    math1([6, 8, 6, 8, 1]) → [70, 90, 70, 90, 20]
+    math1([10]) → [110]
+
+    */
+   public List<Integer> math1(List<Integer> nums) {
+       nums.replaceAll(n-> n*10+10);
+       return nums;
+   }
+
 
 }
