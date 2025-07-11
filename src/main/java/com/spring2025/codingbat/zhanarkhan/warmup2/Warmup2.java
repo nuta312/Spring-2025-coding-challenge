bff4286a5ee9adb2a0abf8f6334101233da17bd0
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-11 11:09:55 +0600

    noTriples

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
@@ -336,9 +336,23 @@ public class Warmup2 {
     }
 
 
+//    Given an array of ints, we'll say that a triple is a value appearing 3 times in a row in the array. Return true if the array does not contain any triples.
+//
+//
+//    noTriples([1, 1, 2, 2, 1]) → true
+//    noTriples([1, 1, 2, 2, 2, 1]) → false
+//    noTriples([1, 1, 1, 2, 2, 2, 1]) → false
 
 
-
+    public boolean noTriples(int[] nums) {
+        boolean result = true;
+        for (int i = 0; i < nums.length-2; i ++) {
+            if (nums[i+1] == nums[i] && nums[i+2] == nums[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
 
 
 
