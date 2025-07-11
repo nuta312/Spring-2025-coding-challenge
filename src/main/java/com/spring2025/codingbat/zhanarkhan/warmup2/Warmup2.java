35b0590fec456e05151241de643da2f757ea4801
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-11 11:03:19 +0600

    arrayCount9

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
@@ -149,4 +149,21 @@ public class Warmup2 {
         return count;
 
     }
+
+//    Given an array of ints, return the number of 9's in the array.
+//
+//
+//    arrayCount9([1, 2, 9]) → 1
+//    arrayCount9([1, 9, 9]) → 2
+//    arrayCount9([1, 9, 9, 3, 9]) → 3
+
+    public int arrayCount9(int[] nums) {
+        int result = 0;
+        for (int count : nums){
+            if(count == 9) {
+                result ++;
+            }
+        }
+        return result;
+    }
 }
