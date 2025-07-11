46152da6996719a2ca1b6915260da5333b02b6fa
= <=>
2025-07-12 02:44:45 +0600

    amina/codingbat/logic1/inOrder

--- src/main/java/com/spring2025/codingbat/amina/logic1/Logic1.java
+++ src/main/java/com/spring2025/codingbat/amina/logic1/Logic1.java
@@ -290,4 +290,23 @@ public class Logic1 {
 
         return str;
     }
+
+    /**
+     Task 17 - inOrder
+
+     Given three ints, a b c, return true if b is greater than a, and c is greater than b.
+     However, with the exception that if "bOk" is true, b does not need to be greater than a.
+
+     inOrder(1, 2, 4, false) → true
+     inOrder(1, 2, 1, false) → false
+     inOrder(1, 1, 2, true) → true
+     */
+
+    public boolean inOrder(int a, int b, int c, boolean bOk) {
+        if (bOk) {
+            return c > b;
+        }
+        return b > a && c > b;
+    }
+
 }
