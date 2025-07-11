c891dc7599539ece6e355ab4d3a8193cefb3ff01
= <=>
2025-07-12 01:40:53 +0600

    amina/codingbat/map2/mordLen

--- src/main/java/com/spring2025/codingbat/amina/map2/Map2.java
+++ src/main/java/com/spring2025/codingbat/amina/map2/Map2.java
@@ -64,4 +64,22 @@ public class Map2 {
         return map;
     }
 
+    /**
+     Task 4 – wordLen
+
+     Given an array of strings, return a Map<String, Integer>
+     containing a key for every different string in the array,
+     and the value is that string's length.
+
+     wordLen(["a", "bb", "a", "bb"]) → {"bb": 2, "a": 1}
+     wordLen(["this", "and", "that", "and"]) → {"that": 4, "and": 3, "this": 4}
+     wordLen(["code", "code", "code", "bug"]) → {"code": 4, "bug": 3}
+     */
+    public Map<String, Integer> wordLen(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : strings) {
+            map.put(str, str.length());
+        }
+        return map;
+    }
 }
