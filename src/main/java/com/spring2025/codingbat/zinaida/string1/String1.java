498ca60fd91c14f0b6ceda7c4925e746f9e741c6
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 09:26:25 +0600

    zinaida/codingbat/string1-withoutX2

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -606,4 +606,29 @@ withoutX("Hxix") → "Hxi"
         }
     }
 
+ /*
+ Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise
+ return the string unchanged. This is a little harder than it looks.
+
+withoutX2("xHi") → "Hi"
+withoutX2("Hxi") → "Hi"
+withoutX2("Hi") → "Hi"
+  */
+
+    public String withoutX2(String str) {
+
+        if (str.length() <= 1) return "";
+        if (str.startsWith("x") && str.charAt(1) == 'x') {
+            return str.substring(2);
+        }
+        if (str.startsWith("x")) {
+            return str.substring(1);
+        }
+        if (str.charAt(1) == 'x') {
+            return str.charAt(0) + str.substring(2, str.length());
+        } else {
+            return str;
+        }
+    }
+
 }
