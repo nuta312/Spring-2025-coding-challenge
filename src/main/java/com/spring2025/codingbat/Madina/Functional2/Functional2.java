5d15fefa7488e6c1ee666c38376db00550fa5112
Madina <1madilyao@gmail.com>
2025-07-11 17:44:33 +0600

    noNeg

---
+++ src/main/java/com/spring2025/codingbat/Madina/Functional2/Functional2.java
@@ -0,0 +1,20 @@
+package com.spring2025.codingbat.Madina.Functional2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Functional2 {
+    public static void main(String[] args) {
+        /*
+        @author Madina
+         */
+    }
+   /* Given a list of integers, return a list of the integers, omitting any that are less than 0.
+    noNeg([1, -2]) → [1]
+    noNeg([-3, -3, 3, 3]) → [3, 3]
+    noNeg([-1, -1, -1]) → []
+    */
+   public List<Integer> noNeg(List<Integer> nums) {
+       return nums.stream().filter(n->n>=0).collect(Collectors.toList());
+   }
+}
