271452a25b9cfa6274496441c77acfa90dc64fdd
Madina <1madilyao@gmail.com>
2025-07-11 18:40:29 +0600

    nolong

--- src/main/java/com/spring2025/codingbat/Madina/Functional2/Functional2.java
+++ src/main/java/com/spring2025/codingbat/Madina/Functional2/Functional2.java
@@ -44,4 +44,17 @@ public class Functional2 {
    public List<String> noZ(List<String> strings) {
        return strings.stream().filter(n->!n.contains("z")).collect(Collectors.toList());
    }
+    /*Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+
+
+
+    noLong(["this", "not", "too", "long"]) → ["not", "too"]
+    noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+    noLong(["cccc", "cccc", "cccc"]) → []
+
+     */
+    public List<String> noLong(List<String> strings) {
+        return strings.stream().filter(s->s.length()<=3).collect(Collectors.toList());
+    }
+
 }
