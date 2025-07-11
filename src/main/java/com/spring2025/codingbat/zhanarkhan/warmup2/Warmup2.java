4fc9bbf82b86aa7eba76a9ec1ce98b8fd4af10dc
Zhanarkhan <zhanarkhan.utemisov@gmail.com>
2025-07-11 11:04:04 +0600

    arrayFront9

--- src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/zhanarkhan/warmup2/Warmup2.java
@@ -166,4 +166,26 @@ public class Warmup2 {
         }
         return result;
     }
+
+//    Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+//
+//
+//    arrayFront9([1, 2, 9, 3, 4]) → true
+//    arrayFront9([1, 2, 3, 4, 9]) → false
+//    arrayFront9([1, 2, 3, 4, 5]) → false
+
+    public boolean arrayFront9(int[] nums) {
+        boolean result = false;
+        int count = 0;
+
+        for(int num : nums) {
+            if (count == 4) break;
+
+            if(num == 9) {
+                result = true;
+            }
+            count ++;
+        }
+        return result;
+    }
 }
