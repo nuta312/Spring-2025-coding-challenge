bd544434f4dd38bbc2c867a174cb5a2715a809a1
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 09:20:48 +0600

    zinaida/codingbat/string1-extraFront

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -494,5 +494,19 @@ minCat("java", "Hello") → "javaello"
 
     }
 
+ /*
+Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may
+be any length. If there are fewer than 2 chars, use whatever is there.
+
+extraFront("Hello") → "HeHeHe"
+extraFront("ab") → "ababab"
+extraFront("H") → "HHH"
+  */
+
+    public String extraFront(String str) {
+
+        return (str.length() >= 2) ? str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2) : str + str + str;
+
+    }
 
 }
