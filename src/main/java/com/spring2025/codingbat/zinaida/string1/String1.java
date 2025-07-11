1227cf901f2064843188b5d83edff9c7403bf21a
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 08:55:17 +0600

    zinaida/codingbat/string1-conCat

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -392,4 +392,29 @@ lastChars("hi", "") → "h@"
         String lastChar = (b.length() >= 1) ? b.substring(b.length() - 1) : "@";
         return firstChar + lastChar;
     }
+
+/*
+Given two strings, append them together (known as "concatenation") and return the result.
+However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+
+conCat("abc", "cat") → "abcat"
+conCat("dog", "cat") → "dogcat"
+conCat("abc", "") → "abc"
+ */
+
+    public String conCat(String a, String b) {
+
+        if (a.length() == 0 || b.length() == 0) {
+            return a + b;
+        } else {
+            String lastCahr = a.substring(a.length() - 1);
+            String firstCahr = b.substring(0, 1);
+            if (lastCahr.equals(firstCahr)) {
+                return a + b.substring(1, b.length());
+            } else {
+                return a + b;
+            }
+        }
+
+    }
 }
