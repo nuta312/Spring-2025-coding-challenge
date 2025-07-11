79e3f12230ace96018310d9613200c59d1134577
Zinaida <begmetovazinaida@rambler.ru>
2025-07-11 09:21:45 +0600

    zinaida/codingbat/string1-without2

--- src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string1/String1.java
@@ -508,5 +508,26 @@ extraFront("H") → "HHH"
         return (str.length() >= 2) ? str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2) : str + str + str;
 
     }
+ /*
+Given a string, if a length 2 substring appears at both its beginning and end, return a string without the substring
+at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself, so "Hi" yields "". Otherwise,
+return the original string unchanged.
 
+without2("HelloHe") → "lloHe"
+without2("HelloHi") → "HelloHi"
+without2("Hi") → ""
+  */
+
+    public String without2(String str) {
+
+        String result = "";
+        if (str.length() == 2) {
+            return "";
+        }
+        if (str.length() > 2 && (str.substring(0, 2).equalsIgnoreCase(str.substring(str.length() - 2)))) {
+            return str.substring(2);
+        }
+        return str;
+
+    }
 }
