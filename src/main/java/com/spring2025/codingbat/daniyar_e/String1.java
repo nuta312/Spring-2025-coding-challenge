55beed37c4c8e6c5b2d8d6a97ced76b969d96b8a
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:39:48 +0600

    middleTwo

--- src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
+++ src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
@@ -217,5 +217,19 @@ public class String1 {
         }return str;
     }
 
+    /*
+    Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+
+
+    middleTwo("string") → "ri"
+    middleTwo("code") → "od"
+    middleTwo("Practice") → "ct"
+     */
+
+    public String middleTwo(String str) {
+        int half = str.length()/2;
+        return str.substring(half-1,half+1);
+    }
+
 
 }
