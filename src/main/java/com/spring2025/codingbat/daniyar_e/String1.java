669f4b9b759db5c794637945b3685c0a90f37e24
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:50:43 +0600

    seeColor

--- src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
+++ src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
@@ -356,4 +356,23 @@ public class String1 {
         char lastB = b.charAt(b.length() - 1);
         return "" + lastA + lastB;
     }
+
+    /*
+    Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+
+
+    seeColor("redxx") → "red"
+    seeColor("xxred") → ""
+    seeColor("blueTimes") → "blue"
+     */
+
+    public String seeColor(String str) {
+        if (str.length()<3){
+            return "";
+        }else if (str.substring(0,3).equals("red")){
+            return "red";
+        }else if (str.length()>=4 && str.substring(0,4).equals("blue")){
+            return "blue";
+        }else return "";
+    }
 }
