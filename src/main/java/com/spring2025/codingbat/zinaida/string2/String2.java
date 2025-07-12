6d5f422d376c9eb8a990beb539b98b55b5eb39bc
Zinaida <begmetovazinaida@rambler.ru>
2025-07-12 23:26:11 +0600

    zinaida/codingbat/string2-endOther

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -80,4 +80,27 @@ countCode("cozexxcope") → 2
         return count;
     }
 
+    /*
+Given two strings, return true if either of the strings appears at the very end of the other string,
+ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+Note: str.toLowerCase() returns the lowercase version of a string.
+
+endOther("Hiabc", "abc") → true
+endOther("AbC", "HiaBc") → true
+endOther("abc", "abXabc") → true
+     */
+    public boolean endOther(String a, String b) {
+        String a1 = a.toLowerCase();
+        String b1 = b.toLowerCase();
+
+        if (a1.endsWith(b1)) {
+            return true;
+        }
+
+        if (b1.endsWith(a1)) {
+            return true;
+        } else return false;
+    }
+
+
 }
