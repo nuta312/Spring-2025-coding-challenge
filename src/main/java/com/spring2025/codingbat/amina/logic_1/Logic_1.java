f8cfda2fcea82658665e7ce5a59d43a6a6071b71
= <=>
2025-07-13 01:25:19 +0600

    maxMod5

--- src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
+++ src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
@@ -144,5 +144,22 @@ public class Logic_1 {
         return a1 == b1 || a1 == c1 || b1 == c1;
     }
 
+    /**
+     Task 9 — maxMod5
+
+     Return the larger value. However if the two values have the same remainder when divided by 5,
+     then return the smaller value. However, in all cases, if the two values are the same, return 0.
+
+     Examples:
+     maxMod5(2, 3) → 3
+     maxMod5(6, 2) → 6
+     maxMod5(3, 2) → 3
+     */
+    public int maxMod5(int a, int b) {
+        if (a == b) return 0;
+        if (a % 5 == b % 5) return Math.min(a, b);
+        return Math.max(a, b);
+    }
+
 
 }
