1bb194648f3392200186078ab1b821a95b01323c
Zinaida <begmetovazinaida@rambler.ru>
2025-07-12 23:30:53 +0600

    zinaida/codingbat/string2-xyBalance

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -145,4 +145,23 @@ bobThere("bac") → false
         return false;
     }
 
+    /*
+We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere
+later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+
+xyBalance("aaxbby") → true
+xyBalance("aaxbb") → false
+xyBalance("yaaxbb") → false
+     */
+    public boolean xyBalance(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'x'){
+                if (!str.substring(i + 1).contains("y"))
+                    return false;
+            }
+        }
+        return true;
+    }
+
 }
