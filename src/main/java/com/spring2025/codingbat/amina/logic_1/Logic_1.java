d56e610ed174210aa357daa05284e48a5a6c0eff
= <=>
2025-07-13 01:24:35 +0600

    twoAsOne

--- src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
+++ src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
@@ -114,5 +114,21 @@ public class Logic_1 {
         return 1;
     }
 
+    /**
+     Task 7 — twoAsOne
+
+     Given three ints, a b c, return true if it is possible to add two of the ints
+     to get the third.
+
+     Examples:
+     twoAsOne(1, 2, 3) → true
+     twoAsOne(3, 1, 2) → true
+     twoAsOne(3, 2, 2) → false
+     */
+    public boolean twoAsOne(int a, int b, int c) {
+        return a + b == c || a + c == b || b + c == a;
+    }
+
+
 
 }
