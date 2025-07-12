c3d408020811c7d61cbf8d38448a1fcbf3545852
= <=>
2025-07-13 01:22:11 +0600

    love6

--- src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
+++ src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
@@ -48,4 +48,21 @@ public class Logic_1 {
         return 2;
     }
 
+    /**
+     Task 3 — love6
+
+     The number 6 is a truly great number.
+     Given two int values, a and b, return true if either one is 6.
+     Or if their sum or difference is 6.
+
+     Examples:
+     love6(6, 4) → true
+     love6(4, 5) → false
+     love6(1, 5) → true
+     */
+    public boolean love6(int a, int b) {
+        return (a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6);
+    }
+
+
 }
