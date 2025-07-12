495028ffaefaffdb229c8161b131f5c0f1b5cc20
Zinaida <begmetovazinaida@rambler.ru>
2025-07-12 23:32:09 +0600

    zinaida/codingbat/string2-mixString

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -164,4 +164,28 @@ xyBalance("yaaxbb") → false
         return true;
     }
 
+    /*
+Given two strings, a and b, create a bigger string made of the first char of a, the first char of b,
+the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+
+mixString("abc", "xyz") → "axbycz"
+mixString("Hi", "There") → "HTihere"
+mixString("xxxx", "There") → "xTxhxexre"
+     */
+    public String mixString(String a, String b) {
+
+        String result = "";
+        int maxLength = Math.max(a.length(), b.length());
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < a.length()) {
+                result += a.charAt(i);
+            }
+            if (i < b.length()) {
+                result += b.charAt(i);
+            }
+        }
+        return result;
+    }
+
 }
