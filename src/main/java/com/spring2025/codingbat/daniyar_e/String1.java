8417229ef77d8b01ab3a113004cb6bb52e0bf6bb
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:27:50 +0600

    helloName

---
+++ src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
@@ -0,0 +1,22 @@
+package com.spring2025.codingbat.daniyar_e;
+
+/*
+@author Daniyar Elebesov
+ */
+
+public class String1 {
+
+    /*
+    Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+    helloName("Bob") → "Hello Bob!"
+    helloName("Alice") → "Hello Alice!"
+    helloName("X") → "Hello X!"
+     */
+
+    public String helloName(String name) {
+        return ("Hello " + name + "!");
+    }
+
+}
