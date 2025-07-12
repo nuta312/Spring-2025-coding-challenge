a7da956729aa809165febfd520463b932ebbc982
= <=>
2025-07-13 01:26:54 +0600

    in1To10

--- src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
+++ src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
@@ -224,6 +224,22 @@ public class Logic_1 {
         return sum;
     }
 
+    /**
+     Task 13 — in1To10
+
+     Given a number n, return true if n is in the range 1..10, inclusive.
+     Unless "outsideMode" is true, in which case return true if the number is <= 1 or >= 10.
+
+     Examples:
+     in1To10(5, false) → true
+     in1To10(11, false) → false
+     in1To10(11, true) → true
+     */
+    public boolean in1To10(int n, boolean outsideMode) {
+        if (outsideMode) return n <= 1 || n >= 10;
+        return n >= 1 && n <= 10;
+    }
+
 
 
 }
