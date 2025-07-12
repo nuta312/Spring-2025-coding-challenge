84f167b3c4e0e91fc36abd25b8e83da2ae871ff8
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:05:06 +0600

    startHi

--- src/main/java/com/spring2025/codingbat/daniyar_e/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/daniyar_e/warmup1/Warmup1.java
@@ -261,4 +261,20 @@ public class Warmup1 {
         }return s+str+s;
     }
 
+    /*
+    Given a string, return true if the string starts with "hi" and false otherwise.
+
+
+    startHi("hi there") → true
+    startHi("hi") → true
+    startHi("hello hi") → false
+     */
+
+    public boolean startHi(String str) {
+        if(str.startsWith("hi")){
+            return true;
+        }
+        return false;
+    }
+
 }
