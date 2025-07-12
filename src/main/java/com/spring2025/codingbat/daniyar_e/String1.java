6a3be2757e93a61112228b63d86e282c1bc86f7d
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:38:30 +0600

    theEnd

--- src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
+++ src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
@@ -182,4 +182,21 @@ public class String1 {
             return right2 + str.substring(0,str.length()-2);
         }return str;
     }
+
+    /*
+    Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+
+    theEnd("Hello", true) → "H"
+    theEnd("Hello", false) → "o"
+    theEnd("oh", true) → "o"
+     */
+
+    public String theEnd(String str, boolean front) {
+        if(front){
+            return str.substring(0,1);
+        }else {
+            return str.substring(str.length()-1);
+        }
+    }
 }
