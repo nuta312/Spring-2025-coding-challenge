524dd4b713ea79bde079d5a8cbf04b999acd8ce1
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:52:22 +0600

    extraFront

--- src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
+++ src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
@@ -396,5 +396,22 @@ public class String1 {
         return false;
     }
 
+    /*
+    Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+
+
+    extraFront("Hello") → "HeHeHe"
+    extraFront("ab") → "ababab"
+    extraFront("H") → "HHH"
+     */
+
+    public String extraFront(String str) {
+        if(str.length()<=1){
+            return str+str+str;
+        }
+        String f = str.substring(0,2);
+        return f+f+f;
+    }
+
 
 }
