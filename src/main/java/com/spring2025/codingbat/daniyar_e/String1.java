ea5e730e263afff66b07c5ea7a17fde998bc962a
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:28:51 +0600

    makeAbba

--- src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
+++ src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
@@ -19,4 +19,18 @@ public class String1 {
         return ("Hello " + name + "!");
     }
 
+    /*
+    Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+
+    makeAbba("Hi", "Bye") → "HiByeByeHi"
+    makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+    makeAbba("What", "Up") → "WhatUpUpWhat"
+     */
+
+    public String makeAbba(String a, String b) {
+        return (a+b+b+a);
+    }
+
+
 }
