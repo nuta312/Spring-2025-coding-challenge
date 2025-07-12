b083d78d6b8e34e50888e3d803b29eede4b704ad
= <=>
2025-07-13 01:21:47 +0600

    caughtSpeeding

--- src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
+++ src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
@@ -24,4 +24,28 @@ public class Logic_1 {
             return cigars >= 40 && cigars <= 60;
         }
     }
+
+    /**
+     Task 2 — caughtSpeeding
+
+     You are driving a little too fast, and a police officer stops you.
+     Write code to compute the result: 0=no ticket, 1=small ticket, 2=big ticket.
+     If speed is 60 or less, the result is 0.
+     If speed is between 61 and 80 inclusive, the result is 1.
+     If speed is 81 or more, the result is 2.
+     Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+
+     Examples:
+     caughtSpeeding(60, false) → 0
+     caughtSpeeding(65, false) → 1
+     caughtSpeeding(65, true) → 0
+     */
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        int allowance = isBirthday ? 5 : 0;
+
+        if (speed <= 60 + allowance) return 0;
+        if (speed <= 80 + allowance) return 1;
+        return 2;
+    }
+
 }
