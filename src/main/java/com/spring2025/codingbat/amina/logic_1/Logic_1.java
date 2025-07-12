1af3c67e0ba8769f60271e5a927eca54665ec6c9
= <=>
2025-07-13 01:21:17 +0600

    cigarParty

---
+++ src/main/java/com/spring2025/codingbat/amina/logic_1/Logic_1.java
@@ -0,0 +1,27 @@
+package com.spring2025.codingbat.amina.logic_1;
+import java.util.*;
+/**
+ * @author Amina
+ */
+public class Logic_1 {
+
+
+    /**
+     Task 1 – cigarParty
+
+     When squirrels get together for a party, they like to have cigars.
+     A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+     Unless it is the weekend — then there is no upper bound on the number of cigars.
+
+     cigarParty(30, false) → false
+     cigarParty(50, false) → true
+     cigarParty(70, true) → true
+     */
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend) {
+            return cigars >= 40;
+        } else {
+            return cigars >= 40 && cigars <= 60;
+        }
+    }
+}
