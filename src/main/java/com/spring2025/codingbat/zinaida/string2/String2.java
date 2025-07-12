3cf28c50e4c35209bad643b45ac32a0b5357d9ce
Zinaida <begmetovazinaida@rambler.ru>
2025-07-12 23:21:21 +0600

    zinaida/codingbat/string2-catDog

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -38,4 +38,28 @@ countHi("hihi") →
 
         return count;
     }
+
+    /*
+Return true if the string "cat" and "dog" appear the same number of times in the given string.
+
+catDog("catdog") → true
+catDog("catcat") → false
+catDog("1cat1cadodog") → true
+     */
+    public boolean catDog(String str) {
+        int countCat = 0;
+        int countDog = 0;
+
+        for (int i = 0; i <= str.length()-3; i++){
+            if (str.substring(i, i+3).equals("cat")){
+                countCat += 1;
+            }
+            if(str.substring(i, i+3).equals("dog")){
+                countDog += 1;
+            }
+        }
+        if (countCat == countDog) return true;
+        else return false;
+    }
+
 }
