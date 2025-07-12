49d936bac9d48f3b6ca076c115962ab145e83565
Daniyar <daniyar.elebesov@gmail.com>
2025-07-12 14:37:56 +0600

    right2

--- src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
+++ src/main/java/com/spring2025/codingbat/daniyar_e/String1.java
@@ -166,4 +166,20 @@ public class String1 {
 
         }return str;
     }
+
+    /*
+    Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
+
+
+    right2("Hello") → "loHel"
+    right2("java") → "vaja"
+    right2("Hi") → "Hi"
+     */
+
+    public String right2(String str) {
+        String right2 = str.substring(str.length()-2);
+        if(str.length()>2){
+            return right2 + str.substring(0,str.length()-2);
+        }return str;
+    }
 }
