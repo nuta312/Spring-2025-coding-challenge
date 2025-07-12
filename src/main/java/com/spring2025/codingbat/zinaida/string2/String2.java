04655ca50af06c2711c6b748cf8b96c1963a6557
Zinaida <begmetovazinaida@rambler.ru>
2025-07-12 23:23:33 +0600

    zinaida/codingbat/string2-countCode

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -62,4 +62,22 @@ catDog("1cat1cadodog") → true
         else return false;
     }
 
+    /*
+Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+countCode("aaacodebbb") → 1
+countCode("codexxcode") → 2
+countCode("cozexxcope") → 2
+     */
+    public int countCode(String str) {
+        int count = 0;
+
+        for (int i = 0; i <= str.length()-4; i++){
+            if(str.substring(i, i+4).startsWith("co") && str.substring(i, i+4).endsWith("e")){
+                count += 1;
+            }
+        }
+        return count;
+    }
+
 }
