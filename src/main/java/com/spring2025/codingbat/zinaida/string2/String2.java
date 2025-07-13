5003e89519f8c7ab5ac2e488b2fca15c1ce733b3
Zinaida <begmetovazinaida@rambler.ru>
2025-07-13 13:00:35 +0600

    zinaida/codingbat/string2-oneTwo

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -324,4 +324,22 @@ sameStarChar("*xa*az") → true
         return true;
     }
 
+    /*
+Given a string, compute a new string by moving the first char to come after the next two chars, so "abc"
+yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd".
+Ignore any group of fewer than 3 chars at the end.
+
+oneTwo("abc") → "bca"
+oneTwo("tca") → "cat"
+oneTwo("tcagdo") → "catdog"
+     */
+    public String oneTwo(String str) {
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length() - 2; i += 3) {
+            result.append(str.charAt(i + 1)).append(str.charAt(i + 2)).append(str.charAt(i));
+        }
+        return result.toString();
+    }
+
 }
