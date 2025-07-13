72a1dface6d2743a5b637db08dace7fec5eb958a
dinara <diaitbayeva@gmail.com>
2025-07-13 23:42:21 +0500

    dinara/codingbat/warmup1-or35

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -190,5 +190,16 @@ public class Warmup1 {
         char last = str.charAt(str.length() - 1);
         return last + str + last;
     }
+    /*
+     Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+     Use the % "mod" operator -- see Introduction to Mod
+
+     or35(3) → true
+     or35(10) → true
+     or35(8) → false */
+
+    public boolean or35(int n) {
+        return(n % 5 == 0 || n % 3 == 0);
+    }
 
 }
