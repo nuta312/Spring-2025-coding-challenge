ba596557d0abd8b5d248317f25407a10483d7e96
Zinaida <begmetovazinaida@rambler.ru>
2025-07-13 12:53:14 +0600

    zinaida/codingbat/string2-repeatEnd

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -188,4 +188,22 @@ mixString("xxxx", "There") → "xTxhxexre"
         return result;
     }
 
+    /*
+Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
+You may assume that n is between 0 and the length of the string, inclusive.
+
+repeatEnd("Hello", 3) → "llollollo"
+repeatEnd("Hello", 2) → "lolo"
+repeatEnd("Hello", 1) → "o"
+     */
+    public String repeatEnd(String str, int n) {
+
+        String result = "";
+        String end = str.substring(str.length()- n);
+
+        for (int i = 0; i < n; i++) {
+            result += end;
+        }
+        return result;
+    }
 }
