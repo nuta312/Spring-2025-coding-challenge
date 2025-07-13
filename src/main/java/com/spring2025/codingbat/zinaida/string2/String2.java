90d6feaab7625db7bcea9f49df76ab9c3235bd55
Zinaida <begmetovazinaida@rambler.ru>
2025-07-13 12:55:16 +0600

    zinaida/codingbat/string2-repeatSeparator

--- src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string2/String2.java
@@ -225,4 +225,24 @@ repeatFront("Ice Cream", 2) → "IcI"
         return result;
     }
 
+    /*
+Given two strings, word and a separator sep, return a big string made of count occurrences of the word,
+separated by the separator string.
+
+repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+repeatSeparator("This", "And", 2) → "ThisAndThis"
+repeatSeparator("This", "And", 1) → "This"
+     */
+    public String repeatSeparator(String word, String sep, int count) {
+
+        String result = "";
+
+        for (int i = 0; i < count; i++) {
+            result += word;
+            if (i < count - 1) {
+                result += sep;
+            }
+        }
+        return result;
+    }
 }
