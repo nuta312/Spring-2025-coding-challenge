1c574f16af526a74ac8219f5b6a19489ca1b65ce
Zinaida <begmetovazinaida@rambler.ru>
2025-07-13 16:05:57 +0600

    zinaida/codingbat/string3-gHappy

--- src/main/java/com/spring2025/codingbat/zinaida/string3/String3.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string3/String3.java
@@ -82,4 +82,29 @@ equalIsNot("noisxxnotyynotxisi") → true
         return countIs == counNot;
     }
 
+    /*
+We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its
+left or right. Return true if all the g's in the given string are happy.
+
+gHappy("xxggxx") → true
+gHappy("xxgxx") → false
+gHappy("xxggyygxx") → false
+     */
+
+    public boolean gHappy(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g') {
+                boolean isHappy = false;
+                if (i > 0 && str.charAt(i - 1) == 'g') isHappy = true;
+                if (i < str.length() - 1 && str.charAt(i + 1) == 'g') isHappy = true;
+
+                if (!isHappy) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
