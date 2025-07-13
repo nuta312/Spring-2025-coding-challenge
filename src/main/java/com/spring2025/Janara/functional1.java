db3abe49837b3d30aafda848ebf4f76c9df5ff26
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 17:41:32 +0300

    rightDigit

--- src/main/java/com/spring2025/Janara/functional1.java
+++ src/main/java/com/spring2025/Janara/functional1.java
@@ -89,4 +89,18 @@ public class functional1 {
                 .map(n->(n+1)*10)
                 .collect(Collectors.toList());
     }
+    /**
+     * Given a list of non-negative integers, return an integer list of the rightmost digits. (Note: use %)
+     *
+     *
+     * rightDigit([1, 22, 93]) → [1, 2, 3]
+     * rightDigit([16, 8, 886, 8, 1]) → [6, 8, 6, 8, 1]
+     * rightDigit([10, 0]) → [0, 0]
+     */
+    public List<Integer> rightDigit(List<Integer> nums) {
+        return nums.stream()
+                .map(n->n%10)
+                .collect(Collectors.toList());
+    }
+
 }
