8b9f528448bd1affb22b416907324de707e6a021
dinara <diaitbayeva@gmail.com>
2025-07-13 23:46:26 +0500

    dinara/codingbat/warmup1-icyHot

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -228,5 +228,15 @@ public class Warmup1 {
     public boolean startHi(String str) {
         return(str.startsWith("hi"));
     }
+    /*
+     Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+     icyHot(120, -1) → true
+     icyHot(-1, 120) → true
+     icyHot(2, 120) → false */
+
+    public boolean icyHot(int temp1, int temp2) {
+        return(temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2< 0);
+    }
 
 }
