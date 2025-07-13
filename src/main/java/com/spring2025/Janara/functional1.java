3cd551ded311f88f537dc6cd3c72be55ee5be592
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 17:42:33 +0300

    lower

--- src/main/java/com/spring2025/Janara/functional1.java
+++ src/main/java/com/spring2025/Janara/functional1.java
@@ -102,5 +102,18 @@ public class functional1 {
                 .map(n->n%10)
                 .collect(Collectors.toList());
     }
+    /**
+     *
+     Given a list of strings, return a list where each string is converted to lower case (Note: String toLowerCase() method).
 
+
+     lower(["Hello", "Hi"]) → ["hello", "hi"]
+     lower(["AAA", "BBB", "ccc"]) → ["aaa", "bbb", "ccc"]
+     lower(["KitteN", "ChocolaTE"]) → ["kitten", "chocolate"]
+     */
+    public List<String> lower(List<String> strings) {
+        return strings.stream()
+                .map(s->s.toLowerCase())
+                .collect(Collectors.toList());
+    }
 }
