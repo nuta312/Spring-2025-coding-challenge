eedab23c3ebc97bf4f2b1d2dcb2e05bcf0e32832
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 14:09:50 +0300

    frontBack

--- src/main/java/com/spring2025/codingbat/janara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/janara/warmup1/Warmup1.java
@@ -120,5 +120,16 @@ public String missingChar(String str, int n) {
     StringBuilder sb1 = new StringBuilder(str);
     return (str.length() > n && n >= 0)? sb1.deleteCharAt(n).toString() : str;
 }
+/**
+ * Given a string, return a new string where the first and last chars have been exchanged.
+ *
+ *
+ * frontBack("code") → "eodc"
+ * frontBack("a") → "a"
+ * frontBack("ab") → "ba"
+ */
+public String frontBack(String str) {
+    return(str.length()>1)? str.substring(str.length()-1)+str.substring(1,str.length()-1)+str.substring(0,1):str;
+}
 
 }
