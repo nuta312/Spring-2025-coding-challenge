3198cef19ade1404410d13565c81b8f9e991767b
dinara <diaitbayeva@gmail.com>
2025-07-14 00:04:04 +0500

    dinara/codingbat/warmup1-everyNth

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -430,5 +430,22 @@ public class Warmup1 {
             return str.substring(0, str.length() - 3) + str.substring(str.length() - 3).toUpperCase();
         }
     }
+    /*
+     Given a non-empty string and an int N, return the string made starting with char 0,
+     and then every Nth char of the string.
+     So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+
+     everyNth("Miracle", 2) → "Mrce"
+     everyNth("abcdefg", 2) → "aceg"
+     everyNth("abcdefg", 3) → "adg" */
+
+    public String everyNth(String str, int n) {
+        String result = "";
+        for (int i = 0; i < str.length(); i += n) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
 
 }
