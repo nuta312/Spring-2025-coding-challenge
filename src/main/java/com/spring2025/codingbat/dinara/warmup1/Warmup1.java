906c57bec870492d3c1d1db03a2945427ad624fa
dinara <diaitbayeva@gmail.com>
2025-07-13 23:36:52 +0500

    dinara/codingbat/warmup1-missingChar

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -136,4 +136,18 @@ public class Warmup1 {
     public String notString(String str) {
         return (str.startsWith("not") ? str : "not ".concat(str));
     }
+    /*
+     Given a non-empty string and an int n, return a new string where the char at index n has been removed.
+     The value of n will be a valid index of a char in the original string
+     (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+     missingChar("kitten", 1) → "ktten"
+     missingChar("kitten", 0) → "itten"
+     missingChar("kitten", 4) → "kittn" */
+
+    public String missingChar(String str, int n) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.deleteCharAt(n).toString();
+    }
+
 }
