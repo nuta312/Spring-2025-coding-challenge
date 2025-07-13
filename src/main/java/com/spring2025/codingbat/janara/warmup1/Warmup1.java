9875ab054693477a8dc06c27c780ffde3f469ffa
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 14:20:28 +0300

    front3

--- src/main/java/com/spring2025/codingbat/janara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/janara/warmup1/Warmup1.java
@@ -131,5 +131,17 @@ public String missingChar(String str, int n) {
 public String frontBack(String str) {
     return(str.length()>1)? str.substring(str.length()-1)+str.substring(1,str.length()-1)+str.substring(0,1):str;
 }
+/**
+ * Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+ *
+ *
+ * front3("Java") → "JavJavJav"
+ * front3("Chocolate") → "ChoChoCho"
+ * front3("abc") → "abcabcabc"
+ */
+public String front3(String str) {
+    String front = str.length() < 3 ? str : str.substring(0, 3);
+    return front + front + front;
+}
 
 }
