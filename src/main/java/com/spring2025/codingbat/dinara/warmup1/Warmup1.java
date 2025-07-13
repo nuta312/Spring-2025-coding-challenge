581f1a5d27650cefe1d626610e551b85fe38c9c8
dinara <diaitbayeva@gmail.com>
2025-07-13 23:52:03 +0500

    dinara/codingbat/warmup1-delDel

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -272,5 +272,20 @@ public class Warmup1 {
         return ((a >= 13 && a <= 19) && !(b >= 13 && b <= 19))
                 || (!(a >= 13 && a <= 19) && (b >= 13 && b <= 19));
     }
+    /*
+     Given a string, if the string "del" appears starting at index 1,
+     return a string where that "del" has been deleted.
+     Otherwise, return the string unchanged.
 
+     delDel("adelbc") → "abc"
+     delDel("adelHello") → "aHello"
+     delDel("adedbc") → "adedbc" */
+
+    public String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+            return str.charAt(0) + str.substring(4);
+        } else {
+            return str;
+        }
+    }
 }
