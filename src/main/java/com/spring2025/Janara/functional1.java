1001591dd39e847d45b4caa01ba5a414b313f4e5
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 17:43:48 +0300

    noX

--- src/main/java/com/spring2025/Janara/functional1.java
+++ src/main/java/com/spring2025/Janara/functional1.java
@@ -116,4 +116,19 @@ public class functional1 {
                 .map(s->s.toLowerCase())
                 .collect(Collectors.toList());
     }
+    /**
+     *
+     Given a list of strings, return a list where each string has all its "x" removed.
+
+
+     noX(["ax", "bb", "cx"]) → ["a", "bb", "c"]
+     noX(["xxax", "xbxbx", "xxcx"]) → ["a", "bb", "c"]
+     noX(["x"]) → [""]
+     */
+    public List<String> noX(List<String> strings) {
+        return strings.stream()
+                .map(s->s.replace("x",""))
+                .collect(Collectors.toList());
+    }
+
 }
