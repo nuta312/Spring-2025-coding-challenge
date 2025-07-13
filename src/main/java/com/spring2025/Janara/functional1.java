372c15c3f5c4b7bc8174275fdf292de5e31016e6
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 17:37:12 +0300

    copies3

--- src/main/java/com/spring2025/Janara/functional1.java
+++ src/main/java/com/spring2025/Janara/functional1.java
@@ -49,5 +49,19 @@ public class functional1 {
                 .map(s->s.concat( "*"))
                 .collect(Collectors.toList());
     }
+    /**
+     * Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+     *
+     *
+     * copies3(["a", "bb", "ccc"]) → ["aaa", "bbbbbb", "ccccccccc"]
+     * copies3(["24", "a", ""]) → ["242424", "aaa", ""]
+     * copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
+     */
+    public List<String> copies3(List<String> strings) {
+        return strings.stream()
+                .map(s->s+s+s)
+                .collect(Collectors.toList());
+    }
+
 
 }
