1ed0f925da3269461ddd62237b80ce03ccfd9eac
dinara <diaitbayeva@gmail.com>
2025-07-13 23:38:00 +0500

    dinara/codingbat/warmup1-frontBack

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -149,5 +149,16 @@ public class Warmup1 {
         StringBuilder sb = new StringBuilder(str);
         return sb.deleteCharAt(n).toString();
     }
+    /*
+     Given a string, return a new string where the first and last chars have been exchanged.
+
+     frontBack("code") → "eodc"
+     frontBack("a") → "a"
+     frontBack("ab") → "ba" */
+
+    public String frontBack(String str) {
+        if(str.length() < 2) return str;
+        return str.charAt(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0);
+    }
 
 }
