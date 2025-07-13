c7e1b3b3957db8d7122cd222acecb86043ef9117
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 17:35:35 +0300

    addStar

--- src/main/java/com/spring2025/Janara/functional1.java
+++ src/main/java/com/spring2025/Janara/functional1.java
@@ -36,5 +36,18 @@ public class functional1 {
 
 
     }
+    /**
+     * Given a list of strings, return a list where each string has "*" added at its end.
+     *
+     *
+     * addStar(["a", "bb", "ccc"]) → ["a*", "bb*", "ccc*"]
+     * addStar(["hello", "there"]) → ["hello*", "there*"]
+     * addStar(["*"]) → ["**"]
+     */
+    public List<String> addStar(List<String> strings) {
+        return strings.stream()
+                .map(s->s.concat( "*"))
+                .collect(Collectors.toList());
+    }
 
 }
