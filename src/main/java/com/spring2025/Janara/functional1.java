809908bb7d4ba280a0ca6e42cb7e04a953afc451
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 17:38:59 +0300

    moreY

--- src/main/java/com/spring2025/Janara/functional1.java
+++ src/main/java/com/spring2025/Janara/functional1.java
@@ -62,6 +62,19 @@ public class functional1 {
                 .map(s->s+s+s)
                 .collect(Collectors.toList());
     }
+    /**
+     *
+     Given a list of strings, return a list where each string has "y" added at its start and end.
 
+
+     moreY(["a", "b", "c"]) → ["yay", "yby", "ycy"]
+     moreY(["hello", "there"]) → ["yhelloy", "ytherey"]
+     moreY(["yay"]) → ["yyayy"]
+     */
+    public List<String> moreY(List<String> strings) {
+        return strings.stream()
+                .map(s->("y")+s.concat("y"))
+                .collect(Collectors.toList());
+    }
 
 }
