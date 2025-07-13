882f0b8d524555caef584cb9820a041bcbff1845
Zinaida <begmetovazinaida@rambler.ru>
2025-07-13 16:07:54 +0600

    zinaida/codingbat/string3-sumDigits

--- src/main/java/com/spring2025/codingbat/zinaida/string3/String3.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string3/String3.java
@@ -126,4 +126,26 @@ countTriple("a") → 0
         return countTriple;
     }
 
+    /*
+Given a string, return the sum of the digits 0-9 that appear in the string, ignoring all other
+characters. Return 0 if there are no digits in the string. (Note: Character.isDigit(char) tests
+if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+
+sumDigits("aa1bc2d3") → 6
+sumDigits("aa11b33") → 8
+sumDigits("Chocolate") → 0
+     */
+    public int sumDigits(String str) {
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))){
+                String char1 = str.substring(i,i+1);
+                count += Integer.parseInt(char1);
+            }
+        }
+        return count;
+    }
+
 }
