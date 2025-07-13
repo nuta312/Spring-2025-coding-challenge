e9b7ab5eb22cc1f4ef56d521080cf5c5b779018d
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 14:01:39 +0300

    missingChar

--- src/main/java/com/spring2025/codingbat/janara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/janara/warmup1/Warmup1.java
@@ -108,5 +108,17 @@ public String notString(String str) {
         return "not "+str;
     }
 }
+/**
+ * Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+ *
+ *
+ * missingChar("kitten", 1) → "ktten"
+ * missingChar("kitten", 0) → "itten"
+ * missingChar("kitten", 4) → "kittn"
+ */
+public String missingChar(String str, int n) {
+    StringBuilder sb1 = new StringBuilder(str);
+    return (str.length() > n && n >= 0)? sb1.deleteCharAt(n).toString() : str;
+}
 
 }
