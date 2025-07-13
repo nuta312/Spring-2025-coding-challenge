f7ec09eb0d7d53e594d7e6c1406ffeddcf66cba9
dinara <diaitbayeva@gmail.com>
2025-07-13 23:50:51 +0500

    dinara/codingbat/warmup1-loneTeen

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -260,7 +260,17 @@ public class Warmup1 {
     public boolean hasTeen(int a, int b, int c) {
         return(a >= 13 && a <=19) || (b >= 13 && b <=19) || (c >= 13 && c <= 19);
     }
+    /*
+     We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+     Given 2 int values, return true if one or the other is teen, but not both.
 
+     loneTeen(13, 99) → true
+     loneTeen(21, 19) → true
+     loneTeen(13, 13) → false */
 
+    public boolean loneTeen(int a, int b) {
+        return ((a >= 13 && a <= 19) && !(b >= 13 && b <= 19))
+                || (!(a >= 13 && a <= 19) && (b >= 13 && b <= 19));
+    }
 
 }
