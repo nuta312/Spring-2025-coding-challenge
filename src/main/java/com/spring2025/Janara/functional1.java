3ba9260f61487aa156bb447b2f4a739da89eb73f
JanarALAM <mairambekovajanar15@gmail.com>
2025-07-13 17:26:03 +0300

    Janara/codingbat/functional-doubling

--- src/main/java/com/spring2025/Janara/functional1.java
+++ src/main/java/com/spring2025/Janara/functional1.java
@@ -1,7 +1,25 @@
 package com.spring2025.Janara;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class functional1 {
     /**
-     *
+     * author janara
      */
+    /**
+     *
+     Given a list of integers, return a list where each integer is multiplied by 2.
+
+
+     doubling([1, 2, 3]) → [2, 4, 6]
+     doubling([6, 8, 6, 8, -1]) → [12, 16, 12, 16, -2]
+     doubling([]) → []
+     */
+    public List<Integer> doubling(List<Integer> nums) {
+        return nums.stream()
+                .map(n->n*2)
+                .collect(Collectors.toList());
+    }
+
 }
