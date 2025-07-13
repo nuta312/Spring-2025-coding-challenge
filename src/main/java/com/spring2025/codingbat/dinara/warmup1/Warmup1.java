e05578ebc920deabfa1001091493a29d0e9fd36c
dinara <diaitbayeva@gmail.com>
2025-07-13 23:39:30 +0500

    dinara/codingbat/warmup1-front3

--- src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/dinara/warmup1/Warmup1.java
@@ -160,5 +160,20 @@ public class Warmup1 {
         if(str.length() < 2) return str;
         return str.charAt(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0);
     }
+    /*
+     Given a string, we'll say that the front is the first 3 chars of the string.
+     If the string length is less than 3, the front is whatever is there.
+     Return a new string which is 3 copies of the front.
 
+     front3("Java") → "JavJavJav"
+     front3("Chocolate") → "ChoChoCho"
+     front3("abc") → "abcabcabc" */
+
+    public String front3(String str) {
+        if (str.length()<3){
+            return str+str+str;
+        } else {
+            return (str.substring(0,3)) + (str.substring(0,3)) +(str.substring(0,3));
+        }
+    }
 }
