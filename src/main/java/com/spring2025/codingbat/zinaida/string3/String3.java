85eb07c2d5f67a41ba8d681c05908890d55d1779
Zinaida <begmetovazinaida@rambler.ru>
2025-07-13 16:10:52 +0600

    zinaida/codingbat/string3-maxBlock

--- src/main/java/com/spring2025/codingbat/zinaida/string3/String3.java
+++ src/main/java/com/spring2025/codingbat/zinaida/string3/String3.java
@@ -187,4 +187,28 @@ mirrorEnds("aba") → "aba"
         return string.substring(0, i);
     }
 
+    /*
+Given a string, return the length of the largest "block" in the string. A block is a run of adjacent chars that are the same.
+
+maxBlock("hoopla") → 2
+maxBlock("abbCCCddBBBxx") → 3
+maxBlock("") → 0
+     */
+    public int maxBlock(String str) {
+
+        int count = 1;
+        int maxCount = 0;
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)){
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count > maxCount){
+                maxCount = count;
+            }
+        }
+        return maxCount;
+    }
+
 }
