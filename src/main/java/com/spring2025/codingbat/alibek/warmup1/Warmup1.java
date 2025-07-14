e0a0e6d445799ed3ae5da3321ee4df701a664421
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:48:42 +0600

    missingChar

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -69,6 +69,11 @@ public class Warmup1 {
         }return "not " + str;
     }
 
+    public String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+        String back = str.substring(n+1, str.length());
+        return front + back;
+    }
 
 
 }
