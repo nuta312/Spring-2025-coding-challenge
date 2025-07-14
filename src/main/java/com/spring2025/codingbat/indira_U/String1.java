58be0f98ea05099bf182f7d53cab4ef7c73ab988
indira <you@example.com>
2025-07-14 09:46:14 +0600

    withoutEnd

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -92,7 +92,18 @@ public String firstTwo(String str) {
         return  str.substring(0, str.length() / 2);
 
     }
+    /*
 
+Given a string, return a version without the first and last char, so "Hello" yields "ell".
+ The string length will be at least 2.
+
+withoutEnd("Hello") → "ell"
+withoutEnd("java") → "av"
+withoutEnd("coding") → "odin"
+     */
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length()-1);
+    }
 
 
 }
