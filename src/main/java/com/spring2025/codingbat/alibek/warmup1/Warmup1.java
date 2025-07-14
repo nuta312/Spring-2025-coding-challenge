48d87903638fd2563d4b9952b57a5e0022b95afb
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:59:52 +0600

    close10

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -176,5 +176,17 @@ public class Warmup1 {
         }return max;
     }
 
+    public int close10(int a, int b) {
+        int A = Math.abs(a - 10);
+        int B = Math.abs(b - 10);
+        if (A < B) {
+            return a;
+        } else if (B < A) {
+            return b;
+        } else {
+            return 0;
+        }
+    }
+
 
 }
