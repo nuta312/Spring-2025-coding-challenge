d2ef2b6dc03b699975073344322d52fdbbcceb87
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:50:29 +0600

    front3public String front3(String str) {

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -88,5 +88,15 @@ public class Warmup1 {
         return result;
     }
 
+    public String front3(String str) {
+        String front;
+        if (str.length() >= 3) {
+            front = str.substring(0, 3);
+        } else {
+            front = str;
+        }return front + front + front;
+    }
+
+
 
 }
