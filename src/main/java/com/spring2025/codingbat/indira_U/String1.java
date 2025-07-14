d7013a60e14c3be3f746a827a981afa5dae869a2
indira <you@example.com>
2025-07-14 09:56:21 +0600

    middleTwo

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -191,4 +191,18 @@ withouEnd2("ab") → ""
             return str.substring(1, str.length() - 1);
         }
     }
+
+    /*
+
+Given a string of even length, return a string made of the middle two chars,
+so the string "string" yields "ri". The string length will be at least 2.
+
+middleTwo("string") → "ri"
+middleTwo("code") → "od"
+middleTwo("Practice") → "ct"
+     */
+    public String middleTwo(String str) {
+        int mid = str.length() / 2;
+        return str.substring(mid - 1, mid + 1);
+    }
 }
