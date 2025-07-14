d713b385f549a7d701ebd16ae679146219eaf8f7
indira <you@example.com>
2025-07-14 16:22:33 +0600

    twoChar

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -238,4 +238,18 @@ nTwice("Chocolate", 1) → "Ce"
         return star + end;
     }
 
+    /*Given a string and an index, return a string length 2
+    starting at the given index. If the index is too big or too
+     small to define a string length 2, use the first 2 chars. The string length will be at least 2.
+
+    twoChar("java", 0) → "ja"
+    twoChar("java", 2) → "va"
+    twoChar("java", 3) → "ja"*/
+    public String twoChar(String str, int index) {
+        if (index < 0 || index > str.length() - 2) {
+            return str.substring(0, 2);
+        } else {
+            return str.substring(index, index + 2);
+        }
+    }
 }
