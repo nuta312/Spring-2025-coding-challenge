3eca2daa4bdb7d90ed91970f44416db78bc86fee
Begimai <tentieva869@gamil.com>
2025-07-14 17:17:07 +0600

    Warmup2

--- src/main/java/com/spring2025/codingbat/bepus/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/bepus/warmup1/Warmup1.java
@@ -1,4 +1,5 @@
 package com.spring2025.codingbat.bepus.warmup1;
+// @Begimai
 
 public class Warmup1 {
     public static void main(String[] args) {
@@ -12,5 +13,19 @@ public class Warmup1 {
         }else{
             return true;
         }
+
+        // We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+        public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+            if (aSmile == true && bSmile == false){
+                return false;
+            }else if (aSmile == false && bSmile == true){
+                return false;
+            }else{
+                return true;
+            }
+        }
+
     }
+
 }
