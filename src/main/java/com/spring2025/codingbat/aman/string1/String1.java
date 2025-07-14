57f506ad47106a7d1f0ef7b934cb7c21d7d7fd5a
Aman <me@gmail.com>
2025-07-14 18:31:14 +0600

    aman/string1/helloName

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -1,4 +1,14 @@
-package com.spring2025.codingbat.aman;
+package com.spring2025.codingbat.aman.string1;
 
 public class String1 {
+
+
+    /* 1.
+    Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+     */
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+
 }
