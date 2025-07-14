6cf7acf271aded2c3dded2a491c182c19d86c9b0
indira <you@example.com>
2025-07-14 16:42:25 +0600

    seeColor

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -361,4 +361,21 @@ lastTwo("ab") → "ba"
             return begin + last + second;
         }
     }
+
+    /*Given a string, if the string begins with "red" or "blue"
+     return that color string, otherwise return the empty string.
+
+   seeColor("redxx") → "red"
+    seeColor("xxred") → ""
+    seeColor("blueTimes") → "blue"*/
+    public String seeColor(String str) {
+        if (str.startsWith("blue")) {
+            return "blue";
+        } else if (str.startsWith("red")) {
+            return "red";
+        } else {
+            return "";
+        }
+    }
+
 }
