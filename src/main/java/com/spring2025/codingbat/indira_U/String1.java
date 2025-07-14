f386f6c5b34a9025580c8948fc84b2bbcf6b9ab8
indira <you@example.com>
2025-07-14 16:48:00 +0600

    startWord

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -482,5 +482,29 @@ deFront("away") → "aay"
         }
         return result;
     }
+    /*
+
+Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string, except its first char does not need to match exactly. On a match, return the front of the string, or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
+
+
+startWord("hippo", "hi") → "hi"
+startWord("hippo", "xip") → "hip"
+startWord("hippo", "i") → "h"
+     */
+    public String startWord(String str, String word) {
+        if (str.length() < word.length()){
+            return "";
+        }
+
+        String substr = str.substring(1, word.length());
+        String sumword = word.substring(1);
+
+        if (substr.equals(sumword)){
+            return str.substring(0, word.length());
+        }
+
+
+        return "";
+    }
 
 }
