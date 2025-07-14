f5ffe2b3b12cb50276328862eaecc33f18658caf
indira <you@example.com>
2025-07-14 16:46:40 +0600

    without2

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -437,4 +437,26 @@ extraFront("H") → "HHH"
             return start + start + start;
         }
     }
+
+    /*
+
+Given a string, if a length 2 substring appears at both its
+beginning and end, return a string without the substring at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+
+without2("HelloHe") → "lloHe"
+without2("HelloHi") → "HelloHi"
+without2("Hi") → ""
+     */
+    public String without2(String str) {
+        if (str.length() >= 2) {
+
+
+            String s1 = str.substring(0, 2);
+            String s2 = str.substring(str.length() - 2);
+            if (s1.equals(s2)) {
+                return str.substring(2);
+            }
+        }
+        return str;
+    }
 }
