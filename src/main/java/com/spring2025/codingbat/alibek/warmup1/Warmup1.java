f84095af0ec91a585d7611fd306651827a228ced
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:56:50 +0600

    delDel

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -139,5 +139,11 @@ public class Warmup1 {
         return (!a1 && b1) || (a1 && !b1) ? true : false;
     }
 
+    public String delDel(String str) {
+        if (str.length()>=4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4);
+        }return str;
+    }
+
 
 }
