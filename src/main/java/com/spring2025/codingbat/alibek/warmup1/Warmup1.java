4835cebf337c3e7475f7925f3fd5d6c4f45fad29
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:46:05 +0600

    nearHundred

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -51,5 +51,13 @@ public class Warmup1 {
         }
     }
 
+    public boolean nearHundred(int n) {
+        if((Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 
 }
