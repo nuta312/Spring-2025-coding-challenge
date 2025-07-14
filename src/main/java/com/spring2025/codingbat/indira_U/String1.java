c4dbf6260d6f333cef249cc056eca645c398736a
indira <you@example.com>
2025-07-14 16:20:06 +0600

    endsLy

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -205,4 +205,20 @@ middleTwo("Practice") → "ct"
         int mid = str.length() / 2;
         return str.substring(mid - 1, mid + 1);
     }
+
+    /*
+
+Given a string, return true if it ends in "ly".
+
+endsLy("oddly") → true
+endsLy("y") → false
+endsLy("oddy") → false
+     */
+    public boolean endsLy(String str) {
+        if (str.endsWith("ly")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
