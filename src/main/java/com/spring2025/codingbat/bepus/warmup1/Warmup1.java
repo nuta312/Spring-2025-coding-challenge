c29c5c212ab25f46249eff70f40eb2a089013b19
Begimai <tentieva869@gamil.com>
2025-07-14 17:07:16 +0600

    Warmup1

---
+++ src/main/java/com/spring2025/codingbat/bepus/warmup1/Warmup1.java
@@ -0,0 +1,16 @@
+package com.spring2025.codingbat.bepus.warmup1;
+
+public class Warmup1 {
+    public static void main(String[] args) {
+    }
+
+   // The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if(weekday == true && vacation == false){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
