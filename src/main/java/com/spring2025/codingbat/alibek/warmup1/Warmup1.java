c4368708ae940c7987b117fce62a43f4fa5fe9fa
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:55:49 +0600

    hasTeen

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -129,5 +129,9 @@ public class Warmup1 {
         return (a >= 10 && a <= 20 || b >= 10 && b <= 20);
     }
 
+    public boolean hasTeen(int a, int b, int c) {
+        return (a>=13 && a<=19) || (b>=13 && b<=19) || (c>=13 && c<=19) ? true : false;
+    }
+
 
 }
