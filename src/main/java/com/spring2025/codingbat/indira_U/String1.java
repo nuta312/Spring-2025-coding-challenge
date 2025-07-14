cc3f217b18c2e1d39413012f9ab78a369d668b3e
indira <you@example.com>
2025-07-14 16:39:50 +0600

    conCat

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -322,4 +322,21 @@ hasBad("xxbadxx") → false
         return first + end;
     }
 
+    /*
+
+    Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+
+    conCat("abc", "cat") → "abcat"
+    conCat("dog", "cat") → "dogcat"
+    conCat("abc", "") → "abc"
+     */
+    public String conCat(String a, String b) {
+        if (a.length() == 0) return b;
+        if (b.length() == 0) return a;
+
+        if (a.charAt(a.length() - 1) == b.charAt(0)) {
+            return a + b.substring(1);
+        }
+        return a + b;
+    }
 }
