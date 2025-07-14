19c5d15e91f308394f2773622be20faa690d3ba9
alibek <sadyrkulovvv@gmail.com>
2025-07-14 19:02:03 +0600

    stringE

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -212,6 +212,12 @@ public class Warmup1 {
         }
     }
 
+    public boolean stringE(String str) {
+        int count = 0;
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == 'e') count++;
+        }return (count >= 1 && count <= 3);
+    }
 
 
 }
