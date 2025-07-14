aa4bf30b914ec2ff6b1c2dd9beacfcef00404d92
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:58:14 +0600

    startOz

--- src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -155,5 +155,15 @@ public class Warmup1 {
         }
     }
 
+    public String startOz(String str) {
+        String result = "";
+        if (str.length() >= 1 && str.charAt(0)=='o') {
+            result = result + str.charAt(0);
+        } if (str.length() >= 2 && str.charAt(1)=='z') {
+            result = result + str.charAt(1);
+        }
+        return result;
+    }
+
 
 }
