4c4da0ce8846a332170976cf68d0f2a76ae42691
indira <you@example.com>
2025-07-14 16:33:45 +0600

    atFirst

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -288,4 +288,24 @@ hasBad("xxbadxx") → false
         return false;
     }
 
+    /*
+
+    Given a string, return a string length 2 made of its first 2
+    chars. If the string length is less than 2, use '@' for the missing chars.
+
+    atFirst("hello") → "he"
+    atFirst("hi") → "hi"
+    atFirst("h") → "h@"
+     */
+    public String atFirst(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
+        }
+        if (str.length() == 1) {
+            return str + "@";
+        }
+        return "@@";
+    }
+
+
 }
