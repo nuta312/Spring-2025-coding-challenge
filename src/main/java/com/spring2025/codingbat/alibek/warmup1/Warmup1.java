6c184e0d52feb22882edd43a27b8645d90842d35
alibek <sadyrkulovvv@gmail.com>
2025-07-14 18:33:44 +0600

    sleepIn

---
+++ src/main/java/com/spring2025/codingbat/alibek/warmup1/Warmup1.java
@@ -0,0 +1,13 @@
+package com.spring2025.codingbat.alibek.warmup1;
+/*
+@author Alibek
+ */
+public class Warmup1 {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
