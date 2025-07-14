add68324b8c53d0bc8fb1435ede4b9ef81ade106
indira <you@example.com>
2025-07-14 16:43:05 +0600

    frontAgain

--- src/main/java/com/spring2025/codingbat/indira_U/String1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/String1.java
@@ -378,4 +378,25 @@ lastTwo("ab") → "ba"
         }
     }
 
+    /*
+
+    Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+
+
+    frontAgain("edited") → true
+    frontAgain("edit") → false
+    frontAgain("ed") → true
+     */
+    public boolean frontAgain(String str) {
+        if (str.length() <= 1) {
+            return false;
+        }
+        String start1 = str.substring(0, 2);
+        String end = str.substring(str.length() - 2);
+        if (start1.equals(end)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
