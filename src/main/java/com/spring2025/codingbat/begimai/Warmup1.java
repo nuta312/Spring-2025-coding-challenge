65b68bfa5a0e7de9fe61a27de6426f6cf838fb6f
Begimai <tentieva869@gamil.com>
2025-07-15 13:20:15 +0600

    notString

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -132,6 +132,21 @@ posNeg(-4, -5, true) → true
             return ((a < 0 && b > 0) || (a> 0 && b <0));
         }
     }
+/*
 
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+
+
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
+ */
+public String notString(String str) {
+    if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+        return str;
+    }
+
+    return "not " + str;
+}
 }
 
