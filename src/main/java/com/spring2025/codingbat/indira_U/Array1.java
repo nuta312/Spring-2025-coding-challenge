08e7daf0763ad55c21deabf979257b1e9e593f72
indira <you@example.com>
2025-07-15 12:55:55 +0600

    maxTriple

--- src/main/java/com/spring2025/codingbat/indira_U/Array1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Array1.java
@@ -332,6 +332,29 @@ public int[] midThree(int[] nums) {
 
     return new int[] {nums[s -1], nums [s] , nums [s +1]};
 }
+/*
+
+Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+
+
+maxTriple([1, 2, 3]) → 3
+maxTriple([1, 5, 3]) → 5
+maxTriple([5, 2, 3]) → 5
+ */
+public int maxTriple(int[] nums) {
+    int ferst = nums [0];
+    int midle = nums[nums.length / 2];
+    int end =  nums [nums.length -1];
+    int max = ferst;
+    if (midle > ferst){
+        max = midle;
+    }
+    if (end > max){
+        max = end;
+
+    }
+    return max;
+}
 
 
 }
