0982c6f0eb0d67e59c47dd14e201740c5c060c83
indira <you@example.com>
2025-07-15 12:49:11 +0600

    has23

--- src/main/java/com/spring2025/codingbat/indira_U/Array1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Array1.java
@@ -147,4 +147,24 @@ public int[] makeEnds(int[] nums) {
     }
     return new int[] {nums [0], nums [0]};
 }
+/*
+Given an int array length 2, return true if it contains a 2 or a 3.
+
+
+has23([2, 5]) → true
+has23([4, 3]) → true
+has23([4, 5]) → false
+ */
+public boolean has23(int[] nums) {
+
+    if (nums [0] == 2 || nums [1] == 3){
+        return true;
+    } else if (nums [0] == 3 || nums [1] == 2){
+        return true;
+
+    } else {
+        return false;
+    }
+}
+
 }
