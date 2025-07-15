6c716f507b34fa01984668d148c6a598059b5d91
indira <you@example.com>
2025-07-15 12:53:35 +0600

    makeMiddle

--- src/main/java/com/spring2025/codingbat/indira_U/Array1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Array1.java
@@ -272,5 +272,18 @@ public int[] biggerTwo(int[] a, int[] b) {
     }
     return a;
 }
+/*
+
+Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
+
+
+makeMiddle([1, 2, 3, 4]) → [2, 3]
+makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
+makeMiddle([1, 2]) → [1, 2]
+ */
+public int[] makeMiddle(int[] nums) {
+    int midle = nums.length /2;
+    return new int[] {nums[midle-1], nums[midle ]};
+}
 
 }
