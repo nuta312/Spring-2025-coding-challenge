b27be56288eddda87b1ccc27f06546ad3fd782cf
Begimai <tentieva869@gamil.com>
2025-07-15 13:34:23 +0600

    icyHot

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -268,6 +268,19 @@ public boolean icyHot(int temp1, int temp2) {
         return false;
     }
 }
+/*
+
+Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+
+
+in1020(12, 99) → true
+in1020(21, 12) → true
+in1020(8, 99) → false
+ */
+public boolean in1020(int a, int b) {
+    return ((a >= 10 && a <= 20) || (b >= 10 && b <= 20));
+}
+
 
 }
 
