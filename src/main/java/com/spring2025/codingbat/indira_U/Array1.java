4b350337765386705cfbae69552588d7a2353730
indira <you@example.com>
2025-07-15 12:42:04 +0600

    sameFirstLast

--- src/main/java/com/spring2025/codingbat/indira_U/Array1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Array1.java
@@ -13,4 +13,18 @@ firstLast6([13, 6, 1, 2, 3]) → false
     public boolean firstLast6(int[] nums) {
         return nums[0] == 6 || nums[nums.length - 1] == 6;
     }
+    /*
+    Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+
+sameFirstLast([1, 2, 3]) → false
+sameFirstLast([1, 2, 3, 1]) → true
+sameFirstLast([1, 2, 1]) → true
+     */
+    public boolean sameFirstLast(int[] nums) {
+        if (nums.length < 1){
+            return false;
+        }
+        return nums [0] == nums [nums.length -1];
+    }
+
 }
