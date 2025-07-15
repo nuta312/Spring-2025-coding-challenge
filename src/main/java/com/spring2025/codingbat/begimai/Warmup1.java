4936a0c5e397a6658ed67948c74ca35e3db1c7ac
Begimai <tentieva869@gamil.com>
2025-07-15 13:51:54 +0600

    stringE

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -435,6 +435,23 @@ public int max1020(int a, int b) {
     if (b >= 10 && b <= 20) return b;
     return 0;
 }
+/*
+
+Return true if the given string contains between 1 and 3 'e' chars.
+
+
+stringE("Hello") → true
+stringE("Heelle") → true
+stringE("Heelele") → false
+ */
+public boolean stringE(String str) {
+    int count = 0;
+
+    for (int i=0; i<str.length(); i++) {
+        if (str.charAt(i) == 'e') count++;
+    }
+    return (count >= 1 && count <= 3);
+}
 
 }
 
