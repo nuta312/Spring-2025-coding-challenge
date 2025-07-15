679bbdb2a9774f1921c3c7447c6c929bc6a7063a
indira <you@example.com>
2025-07-15 12:43:14 +0600

    makePi

--- src/main/java/com/spring2025/codingbat/indira_U/Array1.java
+++ src/main/java/com/spring2025/codingbat/indira_U/Array1.java
@@ -26,5 +26,16 @@ sameFirstLast([1, 2, 1]) → true
         }
         return nums [0] == nums [nums.length -1];
     }
+    /*
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+makePi() → [3, 1, 4]
+     */
+    public int[] makePi() {
+
+        return new int[] {3, 1, 4};
+    }
 
 }
