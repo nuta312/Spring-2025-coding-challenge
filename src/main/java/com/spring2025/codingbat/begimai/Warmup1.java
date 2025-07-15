7562a26f83e8719e43bcea0042a387f335f72813
Begimai <tentieva869@gamil.com>
2025-07-15 13:17:56 +0600

    nearHundred

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -103,6 +103,17 @@ public boolean makes10(int a, int b) {
     return (a ==10 || b == 10 || a+b ==10);
 
 }
+/*
+Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
 
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false
+ */
+public boolean nearHundred(int n) {
+    return ((Math.abs(100 - n ) <= 10) || (Math.abs(200-n)<= 10));
+
+}
 }
 
