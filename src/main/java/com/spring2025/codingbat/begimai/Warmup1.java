eb1d4561bd3e031f327d4c33014af88951d6f054
Begimai <tentieva869@gamil.com>
2025-07-15 13:24:20 +0600

    front3

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -176,6 +176,24 @@ public String frontBack(String str) {
     String mid = str.substring(1, str.length()-1);
     return str.charAt(str.length()-1) + mid + str.charAt(0);
 }
+/*
+
+Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+front3("Java") → "JavJavJav"
+front3("Chocolate") → "ChoChoCho"
+front3("abc") → "abcabcabc"
+ */
+public String front3(String str) {
+    String front;
+    if (str.length() >= 3) {
+        front = str.substring(0, 3);
+    }else {
+        front = str;
+    }
+    return front + front + front;
+}
 
 }
 
