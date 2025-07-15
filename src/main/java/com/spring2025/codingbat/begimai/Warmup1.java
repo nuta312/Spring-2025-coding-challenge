f0e88896a1e580977a397210a51353471cf4b852
Begimai <tentieva869@gamil.com>
2025-07-15 13:27:49 +0600

    front22

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -218,6 +218,22 @@ or35(8) → false
 public boolean or35(int n) {
     return(n % 3== 0)||(n % 5 == 0);
 }
+/*
+
+Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+front22("kitten") → "kikittenki"
+front22("Ha") → "HaHaHa"
+front22("abc") → "ababcab"
+ */
+public String front22(String str) {
+    int take = 2;
+    if (take > str.length()) {
+        take = str.length();
+    }
+    String front = str.substring(0, take);
+    return front + str + front;
+}
 
 }
 
