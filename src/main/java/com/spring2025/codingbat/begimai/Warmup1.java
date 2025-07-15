e9cb0af188edcebfb5bb311a14c23b5fd184653a
Begimai <tentieva869@gamil.com>
2025-07-15 13:14:57 +0600

    parrotTrouble

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -72,5 +72,24 @@ diff21(21) → 0
         }
 
     }
+    /*
+
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+
+parrotTrouble(true, 6) → true
+parrotTrouble(true, 7) → false
+parrotTrouble(false, 6) → false
+     */
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (!talking){
+            return false;
+        }else if (hour > 20 ||  hour < 7){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
 
