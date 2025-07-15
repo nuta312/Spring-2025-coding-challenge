8268f5c78b6d69496ce182c6bae569d3f416ba8d
Begimai <tentieva869@gamil.com>
2025-07-15 13:30:15 +0600

    startHi

--- src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/begimai/Warmup1.java
@@ -234,6 +234,24 @@ public String front22(String str) {
     String front = str.substring(0, take);
     return front + str + front;
 }
+/*
+
+Given a string, return true if the string starts with "hi" and false otherwise.
+
+
+startHi("hi there") → true
+startHi("hi") → true
+startHi("hello hi") → false
+ */
+public boolean startHi(String str) {
+    if (str.length() < 2) return false;
+    String firstTwo = str.substring(0, 2);
+    if (firstTwo.equals("hi")) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 }
 
