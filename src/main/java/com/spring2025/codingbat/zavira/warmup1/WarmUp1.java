9e766342eed4bc927ad4bf4eccb08d48aca793fe
Завира Чыныбаева <zavirachynybaeva@MacBook-Air-Zavira.local>
2025-07-16 18:44:39 +0600

    endUp

--- src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/zavira/warmup1/WarmUp1.java
@@ -537,4 +537,25 @@ lastDigit(3, 113) → true
         return(a % 10 == b % 10);
 
     }
+
+    /* Given a string, return a new string where the last 3 chars are now in upper case.
+    If the string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.
+
+endUp("Hello") → "HeLLO"
+endUp("hi there") → "hi thERE"
+endUp("hi") → "HI"
+
+     */
+
+    public String endUp(String str) {
+
+        if(str.length() <= 3) {
+            return str.toUpperCase();
+        }
+
+        String first = str.substring(0, str.length()-3);
+        String last = str.substring(str.length()-3).toUpperCase();
+        return first + last;
+    }
+
 }
