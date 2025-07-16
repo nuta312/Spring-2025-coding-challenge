818a669bb8151ecdce96eec0fbdf392ea2e90a87
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 07:13:24 -0700

    altynai/codingbat/array1-maxTriple

--- src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
+++ src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
@@ -293,6 +293,18 @@ public static boolean sameFirstLast(int[]nums){
         return new int[]{nums[mids-1], nums[mids], nums[mids+1]};
     }
 
+    //Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+    //maxTriple([1, 2, 3]) → 3
+    //maxTriple([1, 5, 3]) → 5
+   // maxTriple([5, 2, 3]) → 5
+
+    public int maxTriple(int[] nums) {
+        int first=nums[0];
+        int mid=nums[nums.length /2];
+        int last=nums[nums.length-1];
+        return Math.max(first, Math.max(mid, last));
+    }
+
 
 
 
