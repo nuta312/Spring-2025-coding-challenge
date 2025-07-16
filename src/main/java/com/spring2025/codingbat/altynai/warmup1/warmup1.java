9a02340d13aa5157e4eec8ab7edb6f7d022ecc01
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 09:23:45 -0700

    altynai/codingbat/warmup1-stringE

--- src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
@@ -371,4 +371,22 @@ public class warmup1 {
         }
         return Math.max(a,b)  ;
     }
+
+    //Return true if the given string contains between 1 and 3 'e' chars.
+    //
+    //
+    //stringE("Hello") → true
+    //stringE("Heelle") → true
+    //stringE("Heelele") → false
+
+    public boolean stringE(String str) {
+        int count=0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)=='e'){
+                count +=1;
+            }
+        }
+        return count >=1 && count<=3;
+    }
+
 }
