bcd10ebf6318096b38a248e209fd65142dbc48d3
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 09:14:46 -0700

    altynai/codingbat/warmup1-delDel

--- src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
@@ -261,4 +261,19 @@ public class warmup1 {
         boolean bTeen = (b >= 13 && b <= 19);
         return aTeen ^ bTeen; // возвращает true, если только один из них — подросток
     }
+
+    //Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+    //
+    //
+    //delDel("adelbc") → "abc"
+    //delDel("adelHello") → "aHello"
+    //delDel("adedbc") → "adedbc"
+
+    public String delDel(String str) {
+        if (str.length() >= 4 && str.substring(1, 4).equals("del")) {
+            return str.charAt(0) + str.substring(4);
+        } else {
+            return str;
+        }
+    }
 }
