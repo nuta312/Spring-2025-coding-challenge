8b869d8ce25281d2e132bf9857654bc971fac03b
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 09:22:49 -0700

    altynai/codingbat/warmup1-max1020

--- src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
@@ -355,4 +355,20 @@ public class warmup1 {
                 ((a>=40 && a<=50) && (b>=40 && b<=50));
     }
 
+    //Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
+    //
+    //
+    //max1020(11, 19) → 19
+    //max1020(19, 11) → 19
+    //max1020(11, 9) → 11
+
+    public int max1020(int a, int b) {
+        if(a<10 || a>20){
+            a=0;
+        }
+        if(b<10 || b>20){
+            b=0;
+        }
+        return Math.max(a,b)  ;
+    }
 }
