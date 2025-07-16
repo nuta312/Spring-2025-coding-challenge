051954093d8d08aedb8b7d3e3ce3aa8ac790e41e
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 06:09:17 -0700

    altynai/codingbat/array1-has23

--- src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
+++ src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
@@ -130,6 +130,20 @@ public static boolean sameFirstLast(int[]nums){
         return new int[]{nums[0], nums[nums.length-1]};
     }
 
+    //Given an int array length 2, return true if it contains a 2 or a 3.
+    //
+    //
+    //has23([2, 5]) → true
+    //has23([4, 3]) → true
+    //has23([4, 5]) → false
+
+    public boolean has23(int[] nums) {
+        if(nums[0]==2 || nums[1]==2 || nums[0]==3 || nums[1]==3){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 
 }
