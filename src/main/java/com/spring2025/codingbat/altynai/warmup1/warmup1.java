7941b3c3cb9836e09cc209b548042129aa3c99ce
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 09:10:29 -0700

    altynai/codingbat/warmup1-icyHot

--- src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
@@ -216,4 +216,12 @@ public class warmup1 {
         return str.startsWith("hi");
     }
 
+    //Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+    //icyHot(120, -1) → true
+    //icyHot(-1, 120) → true
+    //icyHot(2, 120) → false
+
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1<0 && temp2>100 )|| (temp1>100 && temp2<0);
+    }
 }
