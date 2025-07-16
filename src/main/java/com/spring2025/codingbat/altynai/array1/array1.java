149d38366fda500e06890608f9fe80bb2618fb3c
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 07:11:44 -0700

    altynai/codingbat/array1-meiThree

--- src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
+++ src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
@@ -281,6 +281,18 @@ public static boolean sameFirstLast(int[]nums){
         return nums;// вернуть изменённый массив
     }
 
+    //Given an array of ints of odd length, return a new array length 3 containing the elements from the middle of the array. The array length will be at least 3.
+    //
+    //
+    //midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
+    //midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
+    //midThree([1, 2, 3]) → [1, 2, 3]
+
+    public int[] midThree(int[] nums) {
+        int mids=nums.length/2;
+        return new int[]{nums[mids-1], nums[mids], nums[mids+1]};
+    }
+
 
 
 
