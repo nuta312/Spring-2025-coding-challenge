6a3e7b15ae03bec3e7dd5964c0a7e12378861eee
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 09:08:11 -0700

    altynai/codingbat/warmup1-front22

--- src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
@@ -188,4 +188,20 @@ public class warmup1 {
         return (n % 3 == 0 || n % 5 == 0) ? true : false;
     }
 
+    //Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+    //
+    //
+    //front22("kitten") → "kikittenki"
+    //front22("Ha") → "HaHaHa"
+    //front22("abc") → "ababcab"
+
+    public String front22(String str) {
+        String front;
+        if(str.length()<2){
+            front= str;
+        }else{
+            front=str.substring(0,2);
+        }
+        return front + str +front;
+    }
 }
