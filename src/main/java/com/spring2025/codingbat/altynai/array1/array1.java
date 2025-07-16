78641037f04541dcb794dd1ac578e247055c8829
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 07:08:19 -0700

    altynai/codingbat/array1-makeMiddle

--- src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
+++ src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
@@ -242,6 +242,20 @@ public static boolean sameFirstLast(int[]nums){
         }
     }
 
+    //Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
+    //
+    //
+    //makeMiddle([1, 2, 3, 4]) → [2, 3]
+    //makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
+    //makeMiddle([1, 2]) → [1, 2]
+
+    public int[] makeMiddle(int[] nums) {
+        int mid = nums.length / 2; // середина
+        return new int[] { nums[mid - 1], nums[mid] };
+
+    }
+
+
 
 
 }
