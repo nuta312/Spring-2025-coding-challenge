01a578415c44ba6c91a9b3568a29080575da9272
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 05:56:04 -0700

    altynai/codingbat/array1-commondEnd

--- src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
+++ src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
@@ -39,4 +39,13 @@ public static boolean sameFirstLast(int[]nums){
         int[] myArray={3,1,4};
         return myArray;
     }
+
+    // Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+    //commonEnd([1, 2, 3], [7, 3]) → true
+    // commonEnd([1, 2, 3], [7, 3, 2]) → false
+    //commonEnd([1, 2, 3], [1, 3]) → true
+    public static boolean commonEnd(int[] a, int[] b) {
+        return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+    }
+
 }
