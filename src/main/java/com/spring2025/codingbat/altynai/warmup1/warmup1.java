d6682671fd934dbdddfc6ba3c790a87db5ccf7c7
Altynai R <madakeevaaltynai@gmail.com>
2025-07-16 09:13:50 -0700

    altynai/codingbat/warmup1-loneTeen

--- src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
+++ src/main/java/com/spring2025/codingbat/altynai/warmup1/warmup1.java
@@ -250,4 +250,15 @@ public class warmup1 {
                 (c>=13 && c<=19);
     }
 
+    //We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+    //loneTeen(13, 99) → true
+    //loneTeen(21, 19) → true
+    //loneTeen(13, 13) → false
+
+    public boolean loneTeen(int a, int b) {
+
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+        return aTeen ^ bTeen; // возвращает true, если только один из них — подросток
+    }
 }
