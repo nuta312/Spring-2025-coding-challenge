84a59c8a2887382bafe9db3436f4928690bee155
Aman <me@gmail.com>
2025-07-19 23:38:00 +0600

    aman/warmup-StringE

--- src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
@@ -212,6 +212,17 @@ public class Warmup1 {
         }
         return 0;
     }
+    // 28.
+    public boolean stringE(String str) {
+        int counter = 0;
+        for (int i = 0; i < str.length() ; i++) {
+            if (str.charAt(i) == 'e'){
+                counter++;
+            }
+        }
+        return counter >= 1 && counter <= 3;
+    }
+
 
 
 
