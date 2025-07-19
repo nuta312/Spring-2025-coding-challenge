7f1f0f1befa94664ba454ee670338c0962f86998
Aman <me@gmail.com>
2025-07-20 00:05:52 +0600

    aman/string1-frontAgain

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -167,6 +167,16 @@ public class String1 {
         } else
             return "";
     }
+    // 26.
+    public boolean frontAgain(String str) {
+        if(str.length() < 2){
+            return false;
+        }
+        if (str.substring(0,2).equals(str.substring(str.length() - 2))){
+            return true;
+        }else
+            return false;
+    }
 
 
 
