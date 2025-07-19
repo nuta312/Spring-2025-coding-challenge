c1fe369fbecfe86ecf2d981a084c5e6e2682e4db
Aman <me@gmail.com>
2025-07-20 00:07:16 +0600

    aman/string1-startWord

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -217,6 +217,21 @@ public class String1 {
         }
         return result;
     }
+    // 31.
+    public String startWord(String str, String word) {
+        if (word.length() > str.length()) {
+            return "";
+        }
+
+        String strSub = str.substring(1, word.length());
+        String wordSub = word.substring(1);
+
+        if (strSub.equals(wordSub)) {
+            return str.substring(0, word.length());
+        } else {
+            return "";
+        }
+    }
 
 
 }
