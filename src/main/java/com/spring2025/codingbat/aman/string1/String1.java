5871e60b783d5653da23ee9c2cb8104c140a1546
Aman <me@gmail.com>
2025-07-20 00:04:42 +0600

    aman/string1-atFirst

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -127,5 +127,16 @@ public class String1 {
         }
         return false;
     }
+    // 21.
+    public String atFirst(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
+        } else if (str.length() == 1) {
+            return str + "@";
+        } else {
+            return "@@";
+        }
+    }
+
 
 }
