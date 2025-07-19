f420010fbb06adf8669ea4383a1ef1261911c411
Aman <me@gmail.com>
2025-07-19 23:30:51 +0600

    aman/warmup1-posNeg

--- src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
@@ -49,7 +49,14 @@ public class Warmup1 {
     public boolean nearHundred(int n) {
         return (Math.abs(100-n) <= 10) || (Math.abs(200-n) <= 10);
     }
-
+    // 8.
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        } else {
+            return (a < 0 && b > 0) || (a > 0 && b < 0);
+        }
+    }
 
 
  }
