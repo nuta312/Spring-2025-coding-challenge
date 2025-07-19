4e1abb61cefb8581ebf5575b209722fedf2c1e43
Aman <me@gmail.com>
2025-07-19 23:30:00 +0600

    aman/warmup1-nearHundred

--- src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
@@ -45,6 +45,10 @@ public class Warmup1 {
     public boolean makes10(int a, int b) {
         return (a == 10 || b == 10 || a+b == 10);
     }
+    // 7.
+    public boolean nearHundred(int n) {
+        return (Math.abs(100-n) <= 10) || (Math.abs(200-n) <= 10);
+    }
 
 
 
