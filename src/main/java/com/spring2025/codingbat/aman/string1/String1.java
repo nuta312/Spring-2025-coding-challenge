3824c33b609bcbce2446ba66cb2a7d953ced902c
Aman <me@gmail.com>
2025-07-20 00:06:39 +0600

    aman/string1-without2

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -196,6 +196,13 @@ public class String1 {
         }
         return front + front + front;
     }
+    // 29.
+    public String without2(String str) {
+        if(str.length() >= 2 && str.substring(0,2).equals(str.substring(str.length() - 2))){
+            return str.substring(2);
+        } else
+            return str;
+    }
 
 
 
