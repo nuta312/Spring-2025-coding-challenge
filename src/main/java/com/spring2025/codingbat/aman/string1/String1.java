ec4300e63b9da26998613ad41be31de56506fd97
Aman <me@gmail.com>
2025-07-20 00:02:16 +0600

    aman/string1-theEnd

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -73,6 +73,13 @@ public class String1 {
         }
         return str.substring(str.length()-2) + str.substring(0,str.length()-2);
     }
+    // 13.
+    public String theEnd(String str, boolean front) {
+        if(front){
+            return str.substring(0,1);
+        }
+        return str.substring(str.length()-1);
+    }
 
 
 }
