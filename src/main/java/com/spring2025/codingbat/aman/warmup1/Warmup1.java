81926fb0c75330ce29933657511a915bfd1bd14d
Aman <me@gmail.com>
2025-07-19 23:35:09 +0600

    aman/warmup1-hasTeen

--- src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
@@ -123,6 +123,10 @@ public class Warmup1 {
     public boolean in1020(int a, int b) {
         return ((a >= 10 && a <= 20) || (b >= 10 && b <= 20));
     }
+    // 19.
+    public boolean hasTeen(int a, int b, int c) {
+        return  ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19));
+    }
 
 
 
