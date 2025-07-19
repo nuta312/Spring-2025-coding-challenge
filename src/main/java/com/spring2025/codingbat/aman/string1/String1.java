a202c670dbafaee8766eb14ea295ef4ee771abd1
Aman <me@gmail.com>
2025-07-20 00:06:08 +0600

    aman/string1-minCat

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -177,6 +177,15 @@ public class String1 {
         }else
             return false;
     }
+    // 27.
+    public String minCat(String a, String b) {
+        if(a.length() < b.length()){
+            return a + b.substring(b.length() - a.length());
+        } else if(b.length() < a.length()){
+            return a.substring(a.length() - b.length()) + b;
+        }else
+            return a+b;
+    }
 
 
 
