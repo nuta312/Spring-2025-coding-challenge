7aa73d82df44f73396643a6d121c5cdb164f0d6e
Aman <me@gmail.com>
2025-07-20 00:01:33 +0600

    aman/string1-nonStart

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -53,6 +53,13 @@ public class String1 {
         }
         return shorter + longer + shorter;
     }
+    // 10.
+    public String nonStart(String a, String b) {
+        if(a.length() < 1 && b.length() < 1) {
+            return a;
+        }
+        return a.substring(1) + b.substring(1);
+    }
 
 
 }
