1538924b0425271d20373116acd8ce4f8a8c4aed
Aman <me@gmail.com>
2025-07-19 23:37:05 +0600

    aman/warmup-close10

--- src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
@@ -175,6 +175,18 @@ public class Warmup1 {
         }
         return max;
     }
+    // 25.
+    public int close10(int a, int b) {
+        int a1 = Math.abs(a - 10);
+        int b1 = Math.abs(b - 10);
+        if (a1 < b1){
+            return a;
+        }
+        if (b1 < a1){
+            return b;
+        }
+        return 0;
+    }
 
 
 
