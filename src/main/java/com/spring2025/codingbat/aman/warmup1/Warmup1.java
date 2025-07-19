860f908371358e12e724366491dede669f43a6df
Aman <me@gmail.com>
2025-07-19 23:36:48 +0600

    aman/warmup-intMax

--- src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
@@ -162,6 +162,19 @@ public class Warmup1 {
         }
         return result;
     }
+    // 24.
+    public int intMax(int a, int b, int c) {
+        int max;
+        if (a > b) {
+            max = a;
+        } else {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
 
 
 
