18c36ca2a8e25713ea7c17832c217be38d7d8778
Aman <me@gmail.com>
2025-07-19 23:59:53 +0600

    aman/string1-makeOutWord

--- src/main/java/com/spring2025/codingbat/aman/string1/String1.java
+++ src/main/java/com/spring2025/codingbat/aman/string1/String1.java
@@ -16,5 +16,9 @@ public class String1 {
     public String makeTags(String tag, String word) {
         return "<" + tag + ">" + word + "<" + "/" + tag + ">";
     }
+    // 4.
+    public String makeOutWord(String out, String word) {
+        return out.substring(0,2) + word + out.substring(2,4);
+    }
 
 }
