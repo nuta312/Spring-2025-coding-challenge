ece601736d300dd286bfd37918c83b2eba238750
Aman <me@gmail.com>
2025-07-19 23:24:49 +0600

    aman/warmup1-monkeyTrouble

--- src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aman/warmup1/Warmup1.java
@@ -3,12 +3,18 @@ package com.spring2025.codingbat.aman.warmup1;
 @author Aman
  */
 public class Warmup1 {
-    /** 1.
-    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
-    We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-    */
+    // 1.
     public boolean sleepIn(boolean weekday , boolean vacation) {
         return !weekday || vacation;
+    }
+    // 2.
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if(aSmile && bSmile){
+            return true;
+        } if(!aSmile && !bSmile){
+            return true;
+        } else
+            return false;
     }
 
 
