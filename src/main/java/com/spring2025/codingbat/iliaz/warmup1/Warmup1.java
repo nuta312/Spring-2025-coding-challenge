0ea8685ad0f8dfdf1b70d5cc9ac0655c54a3cb27
Dadabaev Iliaz <ilazdadabaev@gmail.com>
2025-07-19 11:56:08 +0600

    вторая задача

--- src/main/java/com/spring2025/codingbat/iliaz/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/iliaz/warmup1/Warmup1.java
@@ -19,5 +19,20 @@ sleepIn(false, true) → true
         }
         return sleep;
     }
+// Given a string,
+// return true if the first instance of "x" in the string is
+// immediately followed by another "x".
+
+//doubleX("axxbb") → true
+//doubleX("axaxax") → false
+//doubleX("xxxxx") → true
+public boolean doubleX(String str){
+        int i = str.indexOf("x");
+        if (i == -1 || i == str.length() -1){
+            return false;
+        }
+        return str.substring(i + 1, i + 2).equals("x");
+}
+
 
 }
