4a8b8e768293c4edd4640a9f4400a1cd656fe7a6
Dadabaev Iliaz <ilazdadabaev@gmail.com>
2025-07-20 13:13:32 +0600

    posNeg

--- src/main/java/com/spring2025/codingbat/iliaz/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/iliaz/warmup1/Warmup1.java
@@ -23,16 +23,42 @@ sleepIn(false, true) → true
 // return true if the first instance of "x" in the string is
 // immediately followed by another "x".
 
-//doubleX("axxbb") → true
+    //doubleX("axxbb") → true
 //doubleX("axaxax") → false
 //doubleX("xxxxx") → true
-public boolean doubleX(String str){
+    public boolean doubleX(String str) {
         int i = str.indexOf("x");
-        if (i == -1 || i == str.length() -1){
+        if (i == -1 || i == str.length() - 1) {
             return false;
         }
         return str.substring(i + 1, i + 2).equals("x");
-}
+    }
 
+    /* Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false
+
+     */
+    public boolean nearHundred(int n) {
+        if (Math.abs(100 - n) <= 10 || (Math.abs(200 - n) <= 10)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /* Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true
+
+     */
+    public boolean posNeg(int a, int b, boolean negative) {
+        return ((a > 0 && b < 0 && !negative) || (b > 0 && a < 0 && !negative) || (a < 0 && b < 0 && negative));
+    }
 
 }
