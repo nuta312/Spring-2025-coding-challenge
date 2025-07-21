b5b6f8603914305c6cb76d15b0f542c843f018a4
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 11:13:51 -0700

    altynai/codingbat/string2-zipZap

--- src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
+++ src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
@@ -317,4 +317,33 @@ public class string2 {
         }
         return result;
     }
+
+    //Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+    //
+    //
+    //zipZap("zipXzap") → "zpXzp"
+    //zipZap("zopzop") → "zpzp"
+    //zipZap("zzzopzop") → "zzzpzp"
+
+
+    public String zipZap(String str) {
+        String result="";
+        int i=0;
+        while(i<str.length()-2){
+            if(str.charAt(i)=='z'&&str.charAt(i+2)=='p'){
+                result+="zp";
+                i+=3;
+            }else{
+                result+=str.charAt(i);
+                i++;
+            }
+        }
+        while(i<str.length()){
+            result+=str.charAt(i);
+            i++;
+        }
+        return result;
+
+    }
+
 }
