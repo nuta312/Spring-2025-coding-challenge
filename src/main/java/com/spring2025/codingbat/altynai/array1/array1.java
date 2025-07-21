770e406b9efc9dd6cd864f1e54c8970d45252569
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 10:20:37 -0700

    altynai/codingbat/array1-firstLast6

---
+++ src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
@@ -0,0 +1,22 @@
+package com.spring2025.codingbat.altynai.array1;
+
+public class array1 {
+    public static void main(String[] args) {
+
+    }
+    //Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+    //
+    //
+    //firstLast6([1, 2, 6]) → true
+    //firstLast6([6, 1, 2, 3]) → true
+    //firstLast6([13, 6, 1, 2, 3]) → false
+
+    public boolean firstLast6(int[] nums) {
+        if(nums[0] ==6 || nums[nums.length-1]==6){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
