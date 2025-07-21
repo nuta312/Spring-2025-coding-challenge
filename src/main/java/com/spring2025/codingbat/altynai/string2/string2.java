c9b1039a62f8904974dfae136404df4296f80757
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 11:02:18 -0700

    altynai/codingbat/string2-xyBalance

--- src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
+++ src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
@@ -135,4 +135,24 @@ public class string2 {
         return false;
     }
 
+    //We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+    //
+    //
+    //xyBalance("aaxbby") → true
+    //xyBalance("aaxbb") → false
+    //xyBalance("yaaxbb") → false
+
+    public boolean xyBalance(String str) {
+        boolean xUnbalanced=false;
+        for(int i=0; i<str.length();i++){
+            if(str.charAt(i)=='x'){
+                xUnbalanced=true;
+            }
+            if(str.charAt(i)=='y'){
+                xUnbalanced=false;
+            }
+        }
+        return !xUnbalanced;
+    }
+
 }
