e1abdbfa7989236d648fbeb7a06940aa2c1fd702
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 11:00:12 -0700

    altynai/codingbat/string2-xyzThere

--- src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
+++ src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
@@ -82,4 +82,39 @@ public class string2 {
 
     }
 
+    //Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+    //
+    //
+    //endOther("Hiabc", "abc") → true
+    //endOther("AbC", "HiaBc") → true
+    //endOther("abc", "abXabc") → true
+
+    public boolean endOther(String a, String b) {
+        a=a.toLowerCase();
+        b=b.toLowerCase();
+        if(a.endsWith(b) || b.endsWith(a)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+    //
+    //
+    //xyzThere("abcxyz") → true
+    //xyzThere("abc.xyz") → false
+    //xyzThere("xyz.abc") → true
+
+    public boolean xyzThere(String str) {
+        for(int i=0; i<str.length()-2; i++){
+            String s=str.substring(i, i+3);
+            if(s.equals("xyz")){
+                if(i==0 || str.charAt(i-1) != '.'){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
