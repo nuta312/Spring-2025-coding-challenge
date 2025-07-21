1a5de1e77ac7c4255e44317a1561ce5bf9e6e9a4
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 11:08:53 -0700

    altynai/codingbat/string2-xyzMiddle

--- src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
+++ src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
@@ -246,4 +246,25 @@ public class string2 {
         }
         return false;
     }
+
+    //Given a string, does "xyz" appear in the middle of the string? To define middle, we'll say that the number of chars to the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.
+    //
+    //
+    //xyzMiddle("AAxyzBB") → true
+    //xyzMiddle("AxyzBB") → true
+    //xyzMiddle("AxyzBBB") → false
+
+    public boolean xyzMiddle(String str) {
+
+        for(int i=0; i<=str.length()-3;i++){
+            if(str.substring(i,i+3).equals("xyz")){
+                int leftLength=i;
+                int rigthLength=str.length()-(i+3);
+                if(Math.abs(leftLength-rigthLength)<=1){
+                    return true;
+                }
+            }
+        }return false;
+    }
+
 }
