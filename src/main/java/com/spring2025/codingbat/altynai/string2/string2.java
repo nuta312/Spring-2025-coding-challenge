8e51fa969ecc2c214cf05253a1e892bf3596af0d
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 11:03:23 -0700

    altynai/codingbat/string2-mixString

--- src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
+++ src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
@@ -155,4 +155,25 @@ public class string2 {
         return !xUnbalanced;
     }
 
+    //Given two strings, a and b, create a bigger string made of the first char of a, the first char of b, the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+    //
+    //
+    //mixString("abc", "xyz") → "axbycz"
+    //mixString("Hi", "There") → "HTihere"
+    //mixString("xxxx", "There") → "xTxhxexre"
+
+    public String mixString(String a, String b){
+        String result="";
+        int maxLen=Math.max(a.length(), b.length());
+        for(int i=0; i<maxLen;i++){
+            if(i<a.length()){
+                result+=a.charAt(i);
+            }
+            if(i<b.length()){
+                result+=b.charAt(i);
+            }
+        }
+        return result;
+    }
+
 }
