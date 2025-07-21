3c328c68880a2e28d57835043b5b66c1e47b7e92
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 11:12:29 -0700

    altynai/codingbat/string2-oneTwo

--- src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
+++ src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
@@ -299,4 +299,22 @@ public class string2 {
         return true;
     }
 
+    //Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
+    //
+    //
+    //oneTwo("abc") → "bca"
+    //oneTwo("tca") → "cat"
+    //oneTwo("tcagdo") → "catdog"
+
+    public String oneTwo(String str) {
+        String result="";
+        for(int i=0; i<=str.length()-3; i+=3){
+            char a=str.charAt(i) ;
+            char b=str.charAt(i+1);
+            char c=str.charAt(i+2);
+
+            result+=""+b+c+a;
+        }
+        return result;
+    }
 }
