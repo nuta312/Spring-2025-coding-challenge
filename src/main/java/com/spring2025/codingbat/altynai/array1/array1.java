9c0470aa29ce5492fcc88fb1ab5a5695e89b4d8f
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 10:32:39 -0700

    altynai/codingbat/array1-has23

--- src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
+++ src/main/java/com/spring2025/codingbat/altynai/array1/array1.java
@@ -144,5 +144,17 @@ public class array1 {
         return new int[]{nums[0], nums[nums.length-1]};
     }
 
+    //Given an int array length 2, return true if it contains a 2 or a 3.
+    //has23([2, 5]) → true
+    //has23([4, 3]) → true
+    //has23([4, 5]) → false
+
+    public boolean has23(int[] nums) {
+        if(nums[0]==2 || nums[1]==2 || nums[0]==3 || nums[1]==3){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 }
