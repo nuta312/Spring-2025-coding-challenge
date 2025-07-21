cfaa0e080d8b3814526b55e627749c6deb007728
Altynai R <madakeevaaltynai@gmail.com>
2025-07-21 11:06:42 -0700

    altynai/codingbat/string2-repeatSeparator

--- src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
+++ src/main/java/com/spring2025/codingbat/altynai/string2/string2.java
@@ -209,4 +209,23 @@ public class string2 {
 
         return result;
     }
+
+    //iven two strings, word and a separator sep, return a big string made of count occurrences of the word, separated by the separator string.
+    //
+    //
+    //repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+    //repeatSeparator("This", "And", 2) → "ThisAndThis"
+    //repeatSeparator("This", "And", 1) → "This"
+
+    public String repeatSeparator(String word, String sep, int count) {
+        String result="";
+        for(int i=0; i<count;i++){
+            result+=word;
+            if(i<count-1){
+                result+=sep;
+            }
+        }
+        return result;
+    }
+
 }
