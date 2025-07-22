fe0b259885c16ac80e810b40a90364cc2cae7c20
Aziz <azzizz5265@gmail.com>
2025-07-22 15:47:06 +0600

    aziz/wwarmup(first 6 tasks)

--- src/main/java/com/spring2025/codingbat/aziz/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aziz/warmup1/Warmup1.java
@@ -1,7 +1,54 @@
 package com.spring2025.codingbat.aziz.warmup1;
 
 public class Warmup1 {
-    public static void main(String[] args) {
 
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if(weekday == false || vacation == true){
+            return true;
+        }else{
+            return false;
+        }
     }
+
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if(aSmile == bSmile){
+            return true;
+        }
+        return false;
+    }
+
+    public int sumDouble(int a, int b) {
+        if(a != b){
+            return a + b;
+        }else {
+            return (a + b) * 2;
+        }
+    }
+
+
+    public int diff21(int n) {
+        if(n <= 21){
+            return 21 - n;
+        }else{
+            return (n - 21) * 2;
+        }
+    }
+
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return (talking && (hour < 7 || hour > 20));
+    }
+
+
+    public boolean makes10(int a, int b) {
+        if(a == 10 || b == 10 || a+b == 10){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 }
