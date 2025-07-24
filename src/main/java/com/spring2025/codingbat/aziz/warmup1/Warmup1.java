095d60df7b8eadac5879572e9a8736c3d00b7587
Aziz <azzizz5265@gmail.com>
2025-07-24 13:53:59 +0600

    aziz/warmup1(|oneTeen)

--- src/main/java/com/spring2025/codingbat/aziz/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aziz/warmup1/Warmup1.java
@@ -148,5 +148,16 @@ public class Warmup1 {
     }
 
 
+    public boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19 || b >= 13 && b <=19 || c >= 13 && c <= 19);
+    }
+
+
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
 
 }
