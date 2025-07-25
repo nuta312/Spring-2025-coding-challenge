50e042fa9e5216bc50df17501cce77ccaa5342a5
tukeraliev <tukeeraliev@gmail.com>
2025-07-25 09:22:31 -0400

    tugolbai/warmup1

--- src/main/java/com/spring2025/codingbat/tugolbai/warm_up1/WarmUp1.java
+++ src/main/java/com/spring2025/codingbat/tugolbai/warm_up1/WarmUp1.java
@@ -12,4 +12,19 @@ public class WarmUp1 {
         }
         return false;
     }
+
+    /*
+    We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+    We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+     */
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && !bSmile){
+            return true;
+        }
+        return false;
+    }
 }
