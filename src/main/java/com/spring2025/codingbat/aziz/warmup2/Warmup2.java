11b7002d056b38c7010f5a6f7164650808985304
Aziz <azzizz5265@gmail.com>
2025-08-05 17:18:46 +0600

    aziz/warmup2(doubleX)

--- src/main/java/com/spring2025/codingbat/aziz/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aziz/warmup2/Warmup2.java
@@ -27,5 +27,25 @@ public class Warmup2 {
     }
 
 
+    int countXX(String str) {
+        int count = 0;
+        for(int i = 0; i < str.length()-1; i++){
+            if(str.substring(i,i+2).equals("xx")){
+                count++;
+            }
+        }
+        return count;
+    }
 
+
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+        if (i == -1){
+            return false;
+        }
+        if (i + 1 >= str.length()){
+            return false;
+        }
+        return str.substring(i+1, i+2).equals("x");
+    }
 }
