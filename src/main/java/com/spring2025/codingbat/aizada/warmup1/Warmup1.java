8c126341bd11799647bfd532effe1ecf9f1b865f
Ayzada <ayzada19@mail.ru>
2025-08-06 18:59:17 +0600

    or35

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -220,6 +220,20 @@ missingChar("kitten", 4) → "kittn"
         }
         return str;
     }
+/*
+Warmup-1 > or35
+prev  |  next  |  chance
+Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+
+
+or35(3) → true
+or35(10) → true
+or35(8) → false
+ */
+
+    public boolean or35(int n) {
+        return  (n % 3 == 0 || n % 5 == 0);
+    }
 
 
 }
