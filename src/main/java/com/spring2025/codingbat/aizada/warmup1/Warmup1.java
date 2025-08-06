e6bacd7014c3ba606c1822c14cd165ff1dc16e3e
Ayzada <ayzada19@mail.ru>
2025-08-06 18:52:55 +0600

    parrotTrouble

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -72,4 +72,20 @@ sumDouble(2, 2) → 8
         return res;
     }
 
+    /*
+    Warmup-1 > parrotTrouble
+prev  |  next  |  chance
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+
+parrotTrouble(true, 6) → true
+parrotTrouble(true, 7) → false
+parrotTrouble(false, 6) → false
+     */
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return (talking &&  (hour < 7 || hour > 20));
+    }
+
+
 }
