f5a01de2644835293f8888bc1dd9d8bd3425ed4f
Ayzada <ayzada19@mail.ru>
2025-08-06 18:51:15 +0600

    sumDouble

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -35,8 +35,24 @@ monkeyTrouble(true, false) → false
         return (aSmile && bSmile || !aSmile && !bSmile);
     }
 
+/*
+Warmup-1 > sumDouble
+prev  |  next  |  chance
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
 
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+ */
+
+    public int sumDouble(int a, int b) {
+        int sum = a + b;
+        if(a == b){
+            return sum * 2;
+        }
+        return sum;
+    }
 
 
 }
