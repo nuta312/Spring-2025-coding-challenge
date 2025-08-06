9091ef82c66046ac922e895095ffeceddb496fbc
Ayzada <ayzada19@mail.ru>
2025-08-06 19:06:44 +0600

    max1020

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -445,5 +445,25 @@ in3050(40, 50) → true
         return false;
     }
 
+/*
+Warmup-1 > max1020
+prev  |  next  |  chance
+Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
+
+
+max1020(11, 19) → 19
+max1020(19, 11) → 19
+max1020(11, 9) → 11
+ */
+
+    public int max1020(int a, int b) {
+        if ((a >= 10 && a <= 20) && (a >= b || b < 10 || b > 20)) {
+            return a;
+        }
+        if (b >= 10 && b <= 20) {
+            return b;
+        }
+        return 0;
+    }
 
 }
