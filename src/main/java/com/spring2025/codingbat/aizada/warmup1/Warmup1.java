5986ab881397c3eb5c50c6f399076ce1b4f41f1e
Ayzada <ayzada19@mail.ru>
2025-08-06 18:56:40 +0600

    notString

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -149,6 +149,22 @@ notString("not bad") → "not bad"
         return "not " + str;
     }
 
+    /*
+    Warmup-1 > missingChar
+prev  |  next  |  chance
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+
+missingChar("kitten", 1) → "ktten"
+missingChar("kitten", 0) → "itten"
+missingChar("kitten", 4) → "kittn"
+     */
+
+    public String missingChar(String str, int n) {
+        String front = str.substring(0, n);
+        String back = str.substring(n+1,  str.length());
+        return front + back;
+    }
 
 
 }
