d2493ed9f4132501e98e9fd999b397d89662c986
Ayzada <ayzada19@mail.ru>
2025-08-06 14:54:56 +0600

    Warmup1 monkeyTrouble

---
+++ src/main/java/com/spring2025/codingbat/ayzada/warmup1/MonkeyTrouble.java
@@ -0,0 +1,24 @@
+package com.spring2025.codingbat.ayzada.warmup1;
+/*
+ @author Aizada
+ */
+
+public class MonkeyTrouble {
+  /*
+   Warmup-1 > monkeyTrouble
+prev  |  next  |  chance
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+We are in trouble if they are both smiling or if neither of them is smiling.
+Return true if we are in trouble.
+
+
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false
+
+   */
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return (aSmile && bSmile || !aSmile && !bSmile);
+    }
+}
