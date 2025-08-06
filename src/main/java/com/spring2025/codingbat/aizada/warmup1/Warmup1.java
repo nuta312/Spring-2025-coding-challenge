4993b251012c4d4851a2dd32faa5705b8be5888c
Ayzada <ayzada19@mail.ru>
2025-08-06 18:56:06 +0600

    notString

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -131,6 +131,23 @@ public boolean posNeg(int a, int b, boolean negative) {
         return (a < 0 && b > 0 || a > 0 && b < 0);
     }
 }
+/*
+Warmup-1 > notString
+prev  |  next  |  chance
+Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+
+
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
+ */
+
+    public String notString(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        }
+        return "not " + str;
+    }
 
 
 
