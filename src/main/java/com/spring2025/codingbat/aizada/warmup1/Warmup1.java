95ae63f6ed4778b458c17b373e49cb6b8084834d
Ayzada <ayzada19@mail.ru>
2025-08-06 18:54:06 +0600

    makes10

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -84,7 +84,21 @@ parrotTrouble(false, 6) → false
      */
 
     public boolean parrotTrouble(boolean talking, int hour) {
-        return (talking &&  (hour < 7 || hour > 20));
+        return (talking && (hour < 7 || hour > 20));
+    }
+/*
+Warmup-1 > makes10
+prev  |  next  |  chance
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true
+ */
+
+    public boolean makes10(int a, int b) {
+        return (a + b == 10 || a == 10 || b == 10);
     }
 
 
