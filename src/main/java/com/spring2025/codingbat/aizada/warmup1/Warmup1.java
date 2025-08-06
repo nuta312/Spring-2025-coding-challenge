695dd189b72d8c1a04cc396def5d46e12241fdd1
Ayzada <ayzada19@mail.ru>
2025-08-06 19:08:24 +0600

    endUp

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -497,8 +497,27 @@ lastDigit(3, 113) → true
  */
 
     public boolean lastDigit(int a, int b) {
-        return  (a % 10 == b % 10);
+        return (a % 10 == b % 10);
     }
 
+    /*
+    Warmup-1 > endUp
+    prev  |  next  |  chance
+    Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.
+
+
+    endUp("Hello") → "HeLLO"
+    endUp("hi there") → "hi thERE"
+    endUp("hi") → "HI"
+     */
+    public String endUp(String str) {
+        String strUp = "";
+        if (str.length() <= 3) {
+            return str.toUpperCase();
+        } else {
+            strUp = str.substring(0, str.length() - 3) + str.toUpperCase().substring(str.length() - 3);
+        }
+        return strUp;
+    }
 
 }
