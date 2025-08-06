009caf5e1edb6bd458230f43fa91de4162f73d8e
Ayzada <ayzada19@mail.ru>
2025-08-06 19:01:33 +0600

    in1020

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -285,6 +285,19 @@ or35(8) → false
     public boolean icyHot(int temp1, int temp2) {
         return (temp1 < 0 && temp2 > 100 || temp2 < 0 && temp1 > 100);
     }
+/*
+Warmup-1 > in1020
+prev  |  next  |  chance
+Given 2 int values, return true if either of them is in the range 10..20 inclusive.
 
+
+in1020(12, 99) → true
+in1020(21, 12) → true
+in1020(8, 99) → false
+ */
+
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20 || b >= 10 && b <= 20);
+    }
 
 }
