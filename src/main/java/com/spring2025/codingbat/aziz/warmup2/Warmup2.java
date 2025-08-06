72e64a1c7fe823a40c12b4948ee8895ad7a1180a
Aziz <azzizz5265@gmail.com>
2025-08-06 13:16:50 +0600

    aziz/warmup2(has271)

--- src/main/java/com/spring2025/codingbat/aziz/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aziz/warmup2/Warmup2.java
@@ -187,5 +187,14 @@ public class Warmup2 {
     }
 
 
-
+    public boolean has271(int[] nums){
+        for (int i = 0; i < nums.length - 2; i++) {
+            int val = nums[i];
+            if (nums[i + 1] == val + 5 &&
+                    Math.abs(nums[i + 2] - (val - 1)) <= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
