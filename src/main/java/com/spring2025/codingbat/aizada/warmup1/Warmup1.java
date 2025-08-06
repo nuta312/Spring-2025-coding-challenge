633ed1ca3b491a2567535b47bb2ba7af99e0c5af
Ayzada <ayzada19@mail.ru>
2025-08-06 19:04:54 +0600

    intMax

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -387,6 +387,24 @@ startOz("oxx") → "o"
         return st;
     }
 
+    /*
+    Warmup-1 > intMax
+    prev  |  next  |  chance
+    Given three int values, a b c, return the largest.
+
+
+    intMax(1, 2, 3) → 3
+    intMax(1, 3, 2) → 3
+    intMax(3, 2, 1) → 3
+     */
+    public int intMax(int a, int b, int c) {
+        int max = Math.max(a, b);
+        if (max > c) {
+            return max;
+        }
+        return c;
+
+    }
 
 
 }
