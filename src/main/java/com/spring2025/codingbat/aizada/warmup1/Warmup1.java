8104dd7967656f4996dd235daf1a18a7cd242791
Ayzada <ayzada19@mail.ru>
2025-08-06 18:55:26 +0600

    posNeg

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -114,5 +114,24 @@ nearHundred(89) → false
     public boolean nearHundred(int n) {
         return (Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10);
     }
+/*
+Warmup-1 > posNeg
+prev  |  next  |  chance
+Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true
+ */
+public boolean posNeg(int a, int b, boolean negative) {
+    if (negative) {
+        return (a < 0 && b < 0);
+    } else {
+        return (a < 0 && b > 0 || a > 0 && b < 0);
+    }
+}
+
+
 
 }
