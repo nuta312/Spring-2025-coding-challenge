bbd0ce6a69ad399cd8533999251da627f7cd238b
Ayzada <ayzada19@mail.ru>
2025-08-06 18:52:14 +0600

    diff21

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -5,20 +5,20 @@ package com.spring2025.codingbat.aizada.warmup1;
  */
 
 public class Warmup1 {
-   /* Warmup-1 > sleepIn
-    prev  |  next  |  chance
-    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+    /* Warmup-1 > sleepIn
+     prev  |  next  |  chance
+     The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
 
 
-    sleepIn(false, false) → true
-    sleepIn(true, false) → false
-    sleepIn(false, true) → true
+     sleepIn(false, false) → true
+     sleepIn(true, false) → false
+     sleepIn(false, true) → true
 
-    */
-   public boolean sleepIn(boolean weekday, boolean vacation) {
-       return (vacation || !weekday);
+     */
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return (vacation || !weekday);
 
-   }
+    }
 
    /*Warmup-1 > monkeyTrouble
 prev  |  next  |  chance
@@ -48,11 +48,28 @@ sumDouble(2, 2) → 8
 
     public int sumDouble(int a, int b) {
         int sum = a + b;
-        if(a == b){
+        if (a == b) {
             return sum * 2;
         }
         return sum;
     }
 
+    /*
+    Warmup-1 > diff21
+    prev  |  next  |  chance
+    Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+
+    diff21(19) → 2
+    diff21(10) → 11
+    diff21(21) → 0
+     */
+    public int diff21(int n) {
+        int res = Math.abs(n - 21);
+        if (n > 21) {
+            return res * 2;
+        }
+        return res;
+    }
 
 }
