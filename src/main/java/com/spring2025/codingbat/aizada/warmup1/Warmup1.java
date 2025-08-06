ede67582fba127d15d3c05cb0d062d8f526dce96
Ayzada <ayzada19@mail.ru>
2025-08-06 18:58:45 +0600

    backAround

--- src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup1/Warmup1.java
@@ -204,5 +204,22 @@ missingChar("kitten", 4) → "kittn"
         return front + front + front;
     }
 
+    /*
+    Warmup-1 > backAround
+    prev  |  next  |  chance
+    Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+
+
+    backAround("cat") → "tcatt"
+    backAround("Hello") → "oHelloo"
+    backAround("a") → "aaa"
+     */
+    public String backAround(String str) {
+        if (str.length() > 0) {
+            return (str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1));
+        }
+        return str;
+    }
+
 
 }
