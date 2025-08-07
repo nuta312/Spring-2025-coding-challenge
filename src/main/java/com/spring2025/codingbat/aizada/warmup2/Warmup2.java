df9416096a2a523a58d84d85617d4cad524b3693
Ayzada <ayzada19@mail.ru>
2025-08-07 08:47:36 +0600

    countXX

--- src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
@@ -44,5 +44,30 @@ public class Warmup2 {
         }
         return res;
     }
+/*
+Warmup-2 > countXX
+prev  |  next  |  chance
+Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+
+countXX("abcxx") → 1
+countXX("xxx") → 2
+countXX("xxxx") → 3
+ */
+
+    int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equalsIgnoreCase("xx")) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+
+
+
 
 }
