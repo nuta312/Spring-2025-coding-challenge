adee56732f5b633e19d183840192674f4e23e400
Ayzada <ayzada19@mail.ru>
2025-08-07 08:49:58 +0600

    last2

--- src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
@@ -127,5 +127,29 @@ countXX("xxxx") → 3
 
     }
 
+    /*
+    Warmup-2 > last2
+prev  |  next  |  chance
+Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+last2("hixxhi") → 1
+last2("xaxxaxaxx") → 1
+last2("axxxaaxx") → 2
+     */
+    public int last2(String str) {
+        int count = 0;
+        if (str.length() >= 2) {
+            String last2 = str.substring(str.length() - 2);
+            for (int i = 0; i < str.length() - 2; i++) {
+                if (str.substring(i, i + 2).equals(last2)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+        return 0;
+    }
+
 
 }
