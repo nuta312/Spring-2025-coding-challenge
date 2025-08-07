d6cdf521498481247a94e845345dc20ecd1632ec
Ayzada <ayzada19@mail.ru>
2025-08-07 08:52:07 +0600

    stringMatch

--- src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
@@ -212,5 +212,26 @@ array123([1, 1, 2, 1, 2, 3]) → true
         }
         return false;
     }
+/*
+Warmup-2 > stringMatch
+prev  |  next  |  chance
+Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+
+
+stringMatch("xxcaazz", "xxbaaz") → 3
+stringMatch("abc", "abc") → 2
+stringMatch("abc", "axc") → 0
+ */
+
+    public int stringMatch(String a, String b) {
+        int count = 0;
+        int minLength = Math.min(a.length(), b.length());
+        for (int i = 0; i < minLength - 1; i++) {
+            if (a.substring(i, i + 2).equals(b.substring(i, i + 2))) {
+                count++;
+            }
+        }
+        return count;
+    }
 
 }
