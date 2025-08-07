cd2249a6035f0568034e4e55bfbe0c2aa77ff96a
Ayzada <ayzada19@mail.ru>
2025-08-07 08:51:01 +0600

    arrayFront9

--- src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
@@ -170,5 +170,28 @@ last2("axxxaaxx") → 2
         }
         return count;
     }
+/*
+Warmup-2 > arrayFront9
+prev  |  next  |  chance
+Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+
+arrayFront9([1, 2, 9, 3, 4]) → true
+arrayFront9([1, 2, 3, 4, 9]) → false
+arrayFront9([1, 2, 3, 4, 5]) → false
+ */
+
+    public boolean arrayFront9(int[] nums) {
+        int end = nums.length;
+        if (nums.length > 4) {
+            end = 4;
+        }
+        for (int i = 0; i < end; i++) {
+            if (nums[i] == 9) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
