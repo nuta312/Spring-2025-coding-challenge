7617af13bb956ef3599b322a80afa3891857ef8f
Ayzada <ayzada19@mail.ru>
2025-08-07 08:53:15 +0600

    altPairs

--- src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
+++ src/main/java/com/spring2025/codingbat/aizada/warmup2/Warmup2.java
@@ -253,5 +253,25 @@ stringX("xabxxxcdx") → "xabcdx"
         }
         return res;
     }
+/*
+Warmup-2 > altPairs
+prev  |  next  |  chance
+Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+
+
+altPairs("kitten") → "kien"
+altPairs("Chocolate") → "Chole"
+altPairs("CodingHorror") → "Congrr"
+ */
+
+    public String altPairs(String str) {
+        String res = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 4 == 0 || i % 4 == 1) {
+                res += str.charAt(i);
+            }
+        }
+        return res;
+    }
 
 }
